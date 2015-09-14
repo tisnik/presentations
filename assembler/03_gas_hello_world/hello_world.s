bd1569c6ea619de211af9ba9c656cf2dcb7b5f8b
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-14 19:57:55 +0200

    Example #3: Hello world in GNU as

---
+++ assembler/03_gas_hello_world/hello_world.s
@@ -0,0 +1,41 @@
+# asmsyntax=as
+
+# Jednoducha aplikace typu "Hello world!" naprogramovana
+# v assembleru GNU as.
+#
+# Autor: Pavel Tisnovsky
+
+
+
+# Linux kernel system call table
+sys_exit=1
+sys_write=4
+
+
+
+#-----------------------------------------------------------------------------
+.section .data
+
+hello_lbl:
+        .string "Hello World!\n"
+
+#-----------------------------------------------------------------------------
+.section .bss
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start          # tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov   $sys_write, %eax  # cislo syscallu pro funkci "write"
+        mov   $1,%ebx           # standardni vystup
+        mov   $hello_lbl,%ecx   # adresa retezce, ktery se ma vytisknout
+        mov   $13,%edx          # pocet znaku, ktere se maji vytisknout
+        int   $0x80             # volani Linuxoveho kernelu
+
+        movl  $sys_exit,%eax    # cislo sycallu pro funkci "exit"
+        movl  $0,%ebx           # exit code = 0
+        int   $0x80             # volani Linuxoveho kernelu
+
