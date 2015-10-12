bb53691800e101b2fcf10610fadfebd64656e0fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-10-12 19:14:29 +0200

    Slides for second presentation

---
+++ linuxdays2015/arm_opt/slides/float_or_double.c
@@ -0,0 +1,5 @@
+float calcVAT(float price)
+{
+    return 20.0*price;
+}
+
---
+++ linuxdays2015/arm_opt/slides/intrinsic.c
@@ -0,0 +1,7 @@
+#include <arm_neon.h>
+ 
+uint32x4_t double_elements(uint32x4_t input)
+{ 
+    return(vaddq_u32(input, input)); 
+}
+
