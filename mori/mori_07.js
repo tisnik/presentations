ae55a495b43069ad2638806ca9c09a33e913a8f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-11 20:46:52 +0100

    Mori example #7

---
+++ mori/mori_07.js
@@ -0,0 +1,46 @@
+// ------------------------------------------------------------
+// Knihovna Mori: demonstracni priklad cislo 7
+//
+// Otestovani funkce mori.partition()
+// ------------------------------------------------------------
+
+
+
+// ziskani typu sekvence
+function sequenceType(sequence) {
+    return mori.isList(sequence)   ? "list" :
+           mori.isVector(sequence) ? "vector" :
+           mori.isMap(sequence)    ? "map" :
+           mori.isSet(sequence)    ? "set" :
+           mori.isSeq(sequence)    ? "sequence" :
+           "unknown";
+}
+
+
+
+// vypis informaci o vybrane sekvenci
+function printSequenceInfo(declaration) {
+    var sequence = eval(declaration);
+    var type = sequenceType(sequence);
+    console.log("---------------------------------");
+    console.log(declaration);
+    console.log("type:    " + type);
+    console.log("length:  " + mori.count(sequence));
+    console.log("content: " + sequence);
+}
+
+var seq = mori.range(0,12);
+
+printSequenceInfo("mori.partition(1, seq)");
+printSequenceInfo("mori.partition(2, seq)");
+printSequenceInfo("mori.partition(3, seq)");
+printSequenceInfo("mori.partition(4, seq)");
+printSequenceInfo("mori.partition(6, seq)");
+printSequenceInfo("mori.partition(20, seq)");
+
+
+
+// ------------------------------------------------------------
+// Finito
+// ------------------------------------------------------------
+
