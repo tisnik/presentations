68be0c5695ff6d5ee96e5d882f5d5711cbf4c882
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-11 21:15:30 +0100

    Mori example #8

---
+++ mori/mori_08.js
@@ -0,0 +1,65 @@
+// ------------------------------------------------------------
+// Knihovna Mori: demonstracni priklad cislo 8
+//
+// Otestovani funkce mori.partitionBy()
+// ------------------------------------------------------------
+
+
+
+// ziskani typu sekvence
+function sequenceType(sequence) {
+    return mori.isList(sequence)   ? "list" :
+           mori.isVector(sequence) ? "vector" :
+           mori.isMap(sequence)    ? "map" :
+           mori.isSet(sequence)    ? "set" :
+           mori.isSeq(sequence)    ? "sequence" :
+           "unknown";
+}
+
+
+
+// vypis informaci o vybrane sekvenci
+function printSequenceInfo(declaration) {
+    var sequence = eval(declaration);
+    var type = sequenceType(sequence);
+    console.log("---------------------------------");
+    console.log(declaration);
+    console.log("type:    " + type);
+    console.log("length:  " + mori.count(sequence));
+    console.log("content: " + sequence);
+}
+
+function f0(n) {
+    return "konstanta"
+}
+
+function f1(n) {
+    return n < 6;
+}
+
+function f2(n) {
+    return n % 2 == 1;
+}
+
+function f3(n) {
+    return n % 4 == 3;
+}
+
+function f4(n) {
+    return Math.random() < 0.5;
+}
+
+var seq = mori.range(0,12);
+
+printSequenceInfo("mori.partitionBy(f0, seq)");
+printSequenceInfo("mori.partitionBy(f1, seq)");
+printSequenceInfo("mori.partitionBy(f2, seq)");
+printSequenceInfo("mori.partitionBy(f3, seq)");
+printSequenceInfo("mori.partitionBy(f4, seq)");
+
+
+
+// ------------------------------------------------------------
+// Finito
+// ------------------------------------------------------------
+
