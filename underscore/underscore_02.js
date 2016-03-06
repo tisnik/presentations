1e59803d6d5bfc674a89544e458ea9a677346d9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-06 12:15:24 +0100

    Updated 2nd test for Underscore.js library

--- underscore/underscore_02.js
+++ underscore/underscore_02.js
@@ -1,5 +1,6 @@
 // ------------------------------------------------------------
 // Knihovna Underscore.js: demonstracni priklad cislo 2
+//                         Pouziti funkce range().
 // ------------------------------------------------------------
 
 
@@ -39,6 +40,11 @@ printArrayInfo("_.range(0, 10, 100)");
 // lze pouzit i desetinna cisla
 printArrayInfo("_.range(10, 15, 0.5)");
 printArrayInfo("_.range(10,  5,-0.5)");
+
+// desetinna cisla pro urceni zacatku i konce
+printArrayInfo("_.range(1.5, 3.5, 1/2)");
+printArrayInfo("_.range(3.2, 1.8, -1/4)");
+printArrayInfo("_.range(1/2, 3/4, 1/10)");
 
 // stara znama chyba se zde projevuje jen castecne
 printArrayInfo("_.range(0, 10, 0.1)");
