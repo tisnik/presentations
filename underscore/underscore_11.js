4cc410e7ca8e84a3cf58d02047d5ab466c107648
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-11 23:41:49 +0100

    Underscore.js test #11

---
+++ underscore/underscore_11.js
@@ -0,0 +1,29 @@
+// ------------------------------------------------------------
+// Knihovna Underscore.js: demonstracni priklad cislo 11
+//                         Pouziti funkce each
+// ------------------------------------------------------------
+
+
+
+// alternativni zpusob vypoctu faktorialu
+function factorial(n)
+{
+    // vytvori se posloupnost 1..n
+    // postupne se jednotlive prvky poslouposti nasobi
+    // a vrati se vysledek
+    return _.reduce(_.range(1,n+1), function(x,y) {return x*y;});
+}
+
+// tisk nekolika faktorialu pro vstupni hodnoty 1..10
+// (vime jiz, ze funkce range nezahrnuje mezni hodnotu)
+
+// funkce each pracuje jako smycka foreach
+// (neni to tedy cista funkce, protoze ma a musi mit vedlejsi efekt)
+_.each(_.range(1,11), function(n) {console.log(n + "! = " + factorial(n));});
+
+
+
+// ------------------------------------------------------------
+// Finito
+// ------------------------------------------------------------
+
