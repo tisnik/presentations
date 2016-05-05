dfda3788943426c8f366879995b12a6f80661d84
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-05 11:30:48 +0200

    Matplotlib example #7

---
+++ matplotlib/example07.py
@@ -0,0 +1,40 @@
+# Knihovny Numpy a matplotlib
+#
+# Sedmý demonstrační příklad:
+# - vykreslení průběhů funkcí sin a cos
+# - nastavení mřížky
+# - nastavení rozsahů na obou osách
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 2*np.pi, 100)
+
+# hodnoty na y-ové ose: první funkce
+y1 = np.sin(x)
+
+# hodnoty na y-ové ose: druhá funkce
+y2 = np.cos(x)
+
+# vykreslit průběh obou funkcí
+# se změnou stylu vykreslování
+plt.plot(x, y1, "b-", label="sin")
+plt.plot(x, y2, "r-", label="cos")
+
+# přidání legendy
+plt.legend(loc="lower left")
+
+# nastavení rozsahů na obou osách
+plt.axis([-1, 8, -1.5, 1.5])
+
+# povolení zobrazení mřížky
+plt.grid(True)
+
+# popis os
+plt.xlabel("x")
+plt.ylabel("sin(x) a cos(x)")
+
+# zobrazení grafu
+plt.show()
+
