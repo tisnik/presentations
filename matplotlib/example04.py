fc2167a9744baeeb48cb3893c62fc3665aec36f6
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-05 11:29:30 +0200

    Matplotlib example #4

---
+++ matplotlib/example04.py
@@ -0,0 +1,38 @@
+# Knihovny Numpy a matplotlib
+#
+# Čtvrtý demonstrační příklad:
+# - vykreslení průběhů funkcí sin a cos a sinc
+#   do jediného grafu
+# - změna stylu vykreslování průběhů funkcí
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+x = np.linspace(0.01, 2*np.pi, 100)
+
+# hodnoty na y-ové ose: první funkce
+y1 = np.sin(x)
+
+# hodnoty na y-ové ose: druhá funkce
+y2 = np.cos(x)
+
+# hodnoty na y-ové ose: třetí funkce
+y3 = np.sin(x)/x
+
+# vykreslit průběh všech tří funkcí
+# se změnou stylu vykreslování
+plt.plot(x, y1, "b-", label="sin")
+plt.plot(x, y2, "r.", label="cos")
+plt.plot(x, y3, "g--", label="sinc")
+
+# přidání legendy
+plt.legend(loc="lower left")
+
+# popis os
+plt.xlabel("x")
+plt.ylabel("sin(x), cos(x) a sinc(x)")
+
+# zobrazení grafu
+plt.show()
+
