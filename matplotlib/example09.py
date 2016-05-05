772420870fe88fc07fad76acb47083ae34b9d491
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-05 11:31:09 +0200

    Matplotlib example #9

---
+++ matplotlib/example09.py
@@ -0,0 +1,23 @@
+# Knihovny Numpy a matplotlib
+#
+# Devátý demonstrační příklad:
+# - základní polární graf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# úhel v polárním grafu
+theta = np.linspace(0.01, 2*np.pi, 150)
+
+# vzdálenost od středu
+radius = np.log(theta)
+
+ax=plt.subplot(111, projection="polar")
+
+# vykreslit průběh funkce
+# v polárním grafu
+ax.plot(theta, radius)
+
+# zobrazení grafu
+plt.show()
+
