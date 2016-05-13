8e1352b8ce9868ed1a20c041311e411fa998d0b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-13 19:03:59 +0200

    Matplotlib example #25

---
+++ matplotlib/example25.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Knihovny Numpy a matplotlib
+#
+# Dvacátý pátý demonstrační příklad:
+# - zobrazení 3D grafu funkce typu z=f(x,y)
+
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+delta = 0.1
+
+# průběh nezávislé proměnné x
+x = np.arange(-10.0, 10.0, delta)
+
+# průběh nezávislé proměnné y
+y = np.arange(-10.0, 10.0, delta)
+
+# vytvoření dvou polí se souřadnicemi [x,y] 
+X, Y = np.meshgrid(x, y)
+
+# vzdálenost od bodu [0,0]
+R = np.sqrt(X*X+Y*Y)
+
+# výpočet funkce, kterou použijeme při vykreslování grafu
+Z = np.sin(R)/R
+
+# zobrazení 3D grafu formou plochy
+ax.plot_surface(X, Y, Z, rstride=2, cstride=2, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# zobrazení grafu
+plt.show()
+
