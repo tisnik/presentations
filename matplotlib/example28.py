281cf39a165f32a29279b33ceae436021a89cb36
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-05-14 22:46:42 +0200

    Matplotlib example #28

---
+++ matplotlib/example28.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Knihovny Numpy a matplotlib
+#
+# Dvacátý osmý demonstrační příklad:
+# - zobrazení 3D grafu funkce typu [x,y,z]=f(t)
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+# nezávislá proměnná
+t = np.arange(0, 8*np.pi, 0.1)
+
+# vzdálenost od osy spirály
+r = 10.0/(t+4)
+
+# výpočet souřadnic [x,y,z]) pro každé t
+x = r*np.cos(t)
+y = r*np.sin(t)
+z = t
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# vykreslení grafu
+ax.plot(x, y, z)
+
+# zobrazení grafu
+plt.show()
+
