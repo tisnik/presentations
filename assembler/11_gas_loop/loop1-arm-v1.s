529d1a99f97ab50010410b8776e81c13a7b5142d
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-07-16 14:21:00 +0200

    New assembly example: loop1 for ARM CPU (v1)

---
+++ assembler/11_gas_loop/loop1-arm-v1.s
@@ -0,0 +1,59 @@
+# asmsyntax=as
+ 
+# Testovaci program naprogramovany v assembleru GNU as
+# - pocitana programova smycka
+# - uprava pro mikroprocesory s architekturou ARM
+#
+# Autor: Pavel Tisnovsky
+ 
+ 
+ 
+# Linux kernel system call table
+sys_exit   = 1
+sys_write  = 4
+ 
+# Dalsi konstanty pouzite v programu - standardni streamy
+std_input  = 0
+std_output = 1
+
+# pocet opakovani znaku
+rep_count  = 40
+ 
+ 
+ 
+#-----------------------------------------------------------------------------
+.section .data
+ 
+ 
+ 
+#-----------------------------------------------------------------------------
+.section .bss
+        .lcomm buffer, rep_count     @ rezervace bufferu pro vystup
+ 
+ 
+ 
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start               @ tento symbol ma byt dostupny i linkeru
+ 
+_start:
+        ldr   r1, =buffer            @ zapis se bude provadet do tohoto bufferu
+        mov   r2, $rep_count         @ pocet opakovani znaku
+        mov   r3, #'*'               @ zapisovany znak
+loop:
+        strb  r3, [r1]               @ zapis znaku do bufferu
+        add   r1, r1, #1             @ uprava ukazatele do bufferu
+        sub   r2, r2, #1             @ zmenseni pocitadla
+        cmp   r2, #0                 @ otestovani, zda jsme jiz nedosahli nuly
+        bne   loop                   @ pokud jsme se nedostali k nule, skok na zacatek smycky
+ 
+        mov   r7, $sys_write         @ cislo syscallu pro funkci "write"
+        mov   r0, $std_output        @ standardni vystup
+        ldr   r1, =buffer            @ adresa retezce, ktery se ma vytisknout
+        mov   r2, $rep_count         @ pocet znaku, ktere se maji vytisknout
+        svc   0                      @ volani Linuxoveho kernelu
+ 
+        mov   r7, $sys_exit          @ cislo sycallu pro funkci "exit"
+        mov   r0, #0                 @ exit code = 0
+        svc   0                      @ volani Linuxoveho kernelu
+
