b040a422c42fff7741653cc2bbad85b61803a5d5
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-09-28 12:50:09 +0200

    An example with many classes

---
+++ spoon/ManyClasses.java
@@ -0,0 +1,15 @@
+public class ManyClasses {
+    class InnerClass {
+    }
+    class OtherInnerClass {
+        class NestedClass {
+        }
+    }
+}
+
+class OtherClass {
+}
+
+class YetAnotherClass {
+}
+
