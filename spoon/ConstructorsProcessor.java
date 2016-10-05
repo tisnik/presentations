0e25414648bb6fa5eca8b64a3284704bb1fb80a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-10-05 19:06:05 +0200

    ConstructorProcessor, 1st version

---
+++ spoon/ConstructorsProcessor.java
@@ -0,0 +1,15 @@
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.reference.CtVariableReference;
+import spoon.reflect.cu.SourcePosition;
+
+public class ConstructorsProcessor extends AbstractProcessor<CtConstructor> {
+    public void process(CtConstructor element) {
+        SourcePosition sp = element.getPosition();
+        String position = sp == null ? "unknown" : sp.toString();
+        String name = element.getSimpleName();
+        String signature = element.getSignature();
+        System.out.println("constructor '" + name + "' with signature '" + signature + "' declared at:" + sp);
+    }
+}
+
