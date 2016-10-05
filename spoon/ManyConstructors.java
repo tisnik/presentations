3c7c4ca2c235fe688c0e41b92bbef68e13e700b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-10-05 19:05:14 +0200

    Example class with many constructors

---
+++ spoon/ManyConstructors.java
@@ -0,0 +1,13 @@
+public class ManyConstructors {
+    ManyConstructors() {
+    }
+    ManyConstructors(int x) {
+    }
+    ManyConstructors(int x, int y) {
+    }
+    ManyConstructors(String str) {
+    }
+    ManyConstructors(String... strs) {
+    }
+}
+
