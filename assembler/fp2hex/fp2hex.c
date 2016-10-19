f95f1050fcbad4b942f7f8d8be49f8628ed8f8fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-10-19 20:55:34 +0200

    Helper tool: fp2hex

---
+++ assembler/fp2hex/fp2hex.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+union {
+    float flt;
+    int   hex;
+} float_hex;
+
+int main(int argc, char **argv)
+{
+    if (argc == 2) {
+        float_hex.flt = atof(argv[1]);
+        printf("%08x\n", float_hex.hex);
+    }
+    return 0;
+}
+
