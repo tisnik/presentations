41d9a8fe9a6505a0745039d0e8ad33d89254106e
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-03 22:25:41 +0100

    Rust example #8: signed versus unsigned in comparison

---
+++ rust/08_signed_unsigned.rs
@@ -0,0 +1,21 @@
+fn if_expression(value:u32) {
+    let value_type =
+        if value < 0 {
+            "zaporna"
+        }
+        else {
+            if value == 0 {
+                "nulova"
+            }
+            else {
+                "kladna"
+            }
+        };
+    println!("Hodnota {} je {}", value, value_type);
+}
+ 
+fn main() {
+    if_expression(0);
+    if_expression(10);
+}
+
