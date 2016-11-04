b9e0eab62bd0af4465f74891b281c69882a4f29c
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-04 21:16:54 +0100

    Rust example #17: function return values

---
+++ rust/17_return_value.rs
@@ -0,0 +1,18 @@
+fn foo() -> i32 {
+    42
+}
+
+fn bar(argument:i32) -> i32 {
+    return argument * 2;
+}
+
+fn baz(argument:i32) -> i32 {
+    argument * 2
+}
+
+fn main() {
+    println!("{}", foo());
+    println!("{}", bar(21));
+    println!("{}", baz(21));
+}
+
