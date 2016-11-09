4443315ce55ba1fbda01b1aa103a5591bc465bc7
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-09 20:27:40 +0100

    Rust example #25

---
+++ rust/25_match_as_expression.rs
@@ -0,0 +1,16 @@
+fn clasify(x:i32) -> &'static str {
+    match x {
+        0 => "zero",
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        _ => "something else",
+    }
+}
+
+fn main() {
+    for x in 0..10 {
+        println!("{}:{}", x, clasify(x))
+    }
+}
+
