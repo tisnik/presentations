18b3548cf9847c5771e6d01714849894ae7299e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-15 12:21:29 +0100

    Two rust examples: call by value/reference

---
+++ rust/30_func_params.rs
@@ -0,0 +1,20 @@
+fn mutate_variable(mut arg1:i32, arg2:&mut i32) {
+    let x = arg1 * 2;
+    arg1  = x;
+    *arg2 = arg1;
+}
+
+fn print_variables(arg1:i32, arg2:i32) {
+    println!("Variable1: {}", arg1);
+    println!("Variable2: {}", arg2);
+}
+
+fn main() {
+    let variable1 = 1;
+    let mut variable2 = 1;
+
+    print_variables(variable1, variable2);
+    mutate_variable(variable1, &mut variable2);
+    print_variables(variable1, variable2);
+}
+
