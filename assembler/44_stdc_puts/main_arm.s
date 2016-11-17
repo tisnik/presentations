23099367ee72d8c2777e5ed507be6a8757a0fc50
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-17 10:23:52 +0100

    Calling puts from the assembly - ARM variant

---
+++ assembler/44_stdc_puts/main_arm.s
@@ -0,0 +1,28 @@
+# asmsyntax=as
+
+# Program pro otestovani volani funkci ze standardni knihovny jazyka C
+# - volani funkce 'puts'
+# - varianta urcena pro klasickou 32bitovou architekturu ARM
+#
+# Autor: Pavel Tisnovsky
+
+
+
+#-----------------------------------------------------------------------------
+.section .data
+hello_world_message:
+        .asciz "Hello world!\n"         @ zprava, ktera se ma vytisknout na standardni vystup
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global main                    @ tento symbol ma byt dostupny i linkeru
+
+main:
+        stmfd sp!, {lr}                 @ ulozeni zvolenych registru na zasobnik
+        ldr   r0, =hello_world_message  @ adresa zpravy, ktera se ma vytisknout
+        bl    puts                      @ zavolani knihovni funkce puts()
+        mov   r0, #42                   @ navratova hodnota (exit status)
+        ldmfd sp!, {pc}                 @ obnova registru, ukonceni aplikace (rizeni se vrati na adresu ulozenou v LR)
+
