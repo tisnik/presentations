1d364faf864bc61ccac55e6e074b2f2501424d65
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-23 22:49:27 +0100

    Rust example #52

---
+++ rust/52_mutable_struct2_err.rs
@@ -0,0 +1,12 @@
+struct Complex {
+    mut real: f32,
+    mut imag: f32,
+}
+
+fn main() {
+    let c1 = Complex{real:0.0, imag:0.0};
+    c1.real = 10.0;
+    c1.imag = 20.0;
+    println!("complex number: {}+{}i", c1.real, c1.imag);
+}
+
