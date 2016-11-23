1bc14dd2d72311b22568d92f456dcae1582a7f50
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-23 23:05:47 +0100

    Rust example #57

---
+++ rust/57_tuple_types.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let c1:(f32,f32) = (1.0, 2.0);
+    let c2:(f32,f32) = (0.0, 0.0);
+
+    println!("complex number 1: {}+{}i", c1.0, c1.1);
+    println!("complex number 2: {}+{}i", c2.0, c2.1);
+}
+
