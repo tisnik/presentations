731fea5dd0dd4a11fbaeb5eb0483736310c21feb
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-23 22:52:38 +0100

    Rust example #53

---
+++ rust/53_mutable_struct3_ok.rs
@@ -0,0 +1,13 @@
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+fn main() {
+    let mut c1 = Complex{real:0.0, imag:0.0};
+    println!("complex number: {}+{}i", c1.real, c1.imag);
+    c1.real = 10.0;
+    c1.imag = 20.0;
+    println!("complex number: {}+{}i", c1.real, c1.imag);
+}
+
