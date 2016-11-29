39073781af6503f4b8cd6acf5dbfcbe659dba8ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-29 20:24:33 +0100

    Struct ownership #1

---
+++ rust/63_struct_ownership.rs
@@ -0,0 +1,15 @@
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+fn print_complex(c:Complex) {
+    println!("complex number: {}+{}i", c.real, c.imag);
+}
+
+fn main() {
+    let c1 = Complex{real:1.0, imag:2.0};
+    let c2 = c1;
+    print_complex(c2);
+}
+
