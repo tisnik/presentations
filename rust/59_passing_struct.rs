5028bd289241721492dce196fdc3d1849e414e0b
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-11-29 19:31:31 +0100

    Passing struct into function, 1st solution

---
+++ rust/59_passing_struct.rs
@@ -0,0 +1,15 @@
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+fn print_complex(c:Complex) {
+    println!("complex number: {}+{}i", c.real, c.imag);
+}
+
+fn main() {
+    let c1 = Complex{real:1.0, imag:2.0};
+    println!("complex number: {}+{}i", c1.real, c1.imag);
+    print_complex(c1);
+}
+
