1560bb731824d607b1853a6a7c6d7a6947cd37a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-12-07 19:56:51 +0100

    Rust: example #71

---
+++ rust/71_two_methods.rs
@@ -0,0 +1,24 @@
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+
+impl Complex {
+    fn abs(&self) -> f32 {
+        (self.real * self.real + self.imag * self.imag).sqrt()
+    }
+}
+
+impl Complex {
+    fn print(&self) {
+        println!("complex number: {}+{}i", self.real, self.imag);
+    }
+}
+
+fn main() {
+    let c1 = Complex{real:3.0, imag:4.0};
+    c1.print();
+    println!("absolute value: {}", c1.abs());
+}
+
