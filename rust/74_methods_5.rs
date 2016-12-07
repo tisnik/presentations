bbbf1b3aebb429e3f3c7f3e27d30e35b96bedc6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-12-07 19:57:44 +0100

    Rust: example #74

---
+++ rust/74_methods_5.rs
@@ -0,0 +1,40 @@
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+
+impl Complex {
+    fn abs(&self) -> f32 {
+        (self.real * self.real + self.imag * self.imag).sqrt()
+    }
+
+    fn sqr(&self) -> Complex {
+        Complex{real: self.real * self.real - self.imag * self.imag,
+                imag: 2.0*self.real * self.imag}
+    }
+
+    fn add(&self, c:&Complex) -> Complex {
+        Complex{real: self.real + c.real, imag: self.imag + c.imag}
+    }
+
+    fn mul(&self, c:&Complex) -> Complex {
+        Complex{real: self.real * c.real - self.imag * c.imag, imag: self.real * c.imag + self.imag * c.real}
+    }
+
+    fn print(&self) {
+        println!("complex number: {}+{}i", self.real, self.imag);
+    }
+}
+
+fn main() {
+    let c1 = Complex{real:1.0, imag:2.0};
+    let c2 = Complex{real:3.0, imag:4.0};
+    let c3 = c1.add(&c2);
+    let c4 = c1.mul(&c1);
+    c1.print();
+    c2.print();
+    c3.print();
+    c4.print();
+}
+
