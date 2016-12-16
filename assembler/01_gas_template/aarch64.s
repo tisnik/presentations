b11ebfcf034ef1e2972045c2c589c00944ca5f05
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-12-17 00:29:36 +0100

    AArch64 variant

---
+++ assembler/01_gas_template/aarch64.s
@@ -0,0 +1,33 @@
+# asmsyntax=as
+
+# Sablona pro zdrojovy kod Linuxoveho programu naprogramovaneho
+# v assembleru GNU AS pro architekturu AArch64.
+#
+# Autor: Pavel Tisnovsky
+
+
+
+# Linux kernel system call table
+sys_exit=93
+
+
+
+#-----------------------------------------------------------------------------
+.section .data
+
+
+
+#-----------------------------------------------------------------------------
+.section .bss
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start          // tento symbol ma byt dostupny i z linkeru
+
+_start:
+        mov  x8, #sys_exit      // cislo sycallu pro funkci "exit"
+        mov  x0, #0             // exit code = 0
+        svc   0                 // volani Linuxoveho kernelu
+
