96897f8bc8370f26a67d9d02f228844a39f38405
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-09 09:47:39 +0100

    Rust example #101

---
+++ rust/101_rc_no_clone.rs
@@ -0,0 +1,45 @@
+use std::rc::Rc;
+
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+impl Complex {
+
+    fn new(real: f32, imag: f32) -> Complex {
+        println!("Constructing complex number: {:}+{:}i", real, imag);
+        Complex{real:real, imag:imag}
+    }
+
+    fn print(&self) {
+        println!("complex number: {:?}+{:?}i", self.real, self.imag);
+    }
+
+}
+
+impl Drop for Complex {
+    fn drop(&mut self) {
+        println!("Dropping complex number: {:}+{:}i", self.real, self.imag);
+    }
+}
+
+fn main() {
+    println!("main begin");
+    let c = Rc::new(Complex::new(0.0, 0.0));
+    c.print();
+    {
+        println!("inner block begin");
+        let c2 = Rc::new(Complex::new(0.0, 0.0));
+        c2.print();
+        {
+            println!("inmost block begin");
+            let c3 = Rc::new(Complex::new(0.0, 0.0));
+            c3.print();
+            println!("inmost block end");
+        }
+        println!("inner block end");
+    }
+    println!("main end");
+}
+
