795ea6ebba70ef32156ade8352c2ad376b34f779
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-09 09:48:18 +0100

    Rust example #105

---
+++ rust/105_rc_move_semantic_clone.rs
@@ -0,0 +1,42 @@
+use std::rc::Rc;
+
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+impl Complex {
+
+    fn new(real: f32, imag: f32) -> Complex {
+        println!("Constructing complex number: {:}+{:}i", real, imag);
+        Complex{real:real, imag:imag}
+    }
+
+    fn print(&self) {
+        println!("complex number: {:?}+{:?}i", self.real, self.imag);
+    }
+
+}
+
+impl Drop for Complex {
+    fn drop(&mut self) {
+        println!("Dropping complex number: {:}+{:}i", self.real, self.imag);
+    }
+}
+
+fn f1() -> Rc<Complex> {
+    Rc::new(Complex::new(0.0, 0.0))
+}
+
+fn f2(c:Rc<Complex>) {
+    c.print();
+}
+
+fn main() {
+    println!("main begin");
+    let c = f1();
+    c.print();
+    f2(c.clone());
+    f2(c.clone());
+}
+
