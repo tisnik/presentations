165f1ae148a70b68bc26dc9318c7bc28bff51704
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-14 22:25:00 +0100

    Rust example #111

---
+++ rust/111_thread_lambda.rs
@@ -0,0 +1,10 @@
+use std::thread;
+
+fn main() {
+    println!("Starting");
+    for i in 1..10 {
+        thread::spawn(|| {println!("Hello from a thread...");});
+    }
+    println!("Stopping");
+}
+
