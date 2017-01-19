866e9440f6f093a373079549803e174f52cd1f70
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-19 21:16:27 +0100

    fractals03_1.c

---
+++ fractals/root.cz/fractals03_1.c
@@ -0,0 +1,244 @@
+//-----------------------------------------------------------------------------
+// Fraktaly v pocitacove grafice
+// Ukazkovy priklad cislo 1
+// Autor: Pavel Tisnovsky
+//
+// Program po svem spusteni otevre hlavni okno a vykresli do nej jednu
+// pixmapu. Pixely jsou ulozeny ve formatu RGB a kazda barvova slozka
+// obsazuje jeden byte. Pixmapu je mozne ulozit do exterbiho souboru typu
+// TGA pomoci klavesy 'S'
+//-----------------------------------------------------------------------------
+
+#ifdef __BORLANDC__
+#include <windows.h>
+#endif
+
+#include <GL/glut.h>                            // hlavickovy soubor funkci GLUTu a OpenGL
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WINDOW_TITLE    "Fraktaly 1"            // titulek okna
+#define WINDOW_WIDTH    256                     // pocatecni velikost okna
+#define WINDOW_HEIGHT   256
+#define FILE_NAME       "fraktaly1.tga"         // jmeno souboru pro ulozeni pixmapy
+
+#define PIXMAP_WIDTH    256                     // sirka pixmapy
+#define PIXMAP_HEIGHT   256                     // vyska pixmapy
+
+typedef struct {                                // struktura popisujici pixmapu
+    unsigned int width;
+    unsigned int height;
+    unsigned char *pixels;
+} pixmap;
+
+pixmap *pix;
+
+
+
+//-----------------------------------------------------------------------------
+// Funkce pro vytvoreni pixmapy o zadane velikosti
+//-----------------------------------------------------------------------------
+pixmap * createPixmap(const unsigned int width, const unsigned int height)
+{
+    pixmap *p=(pixmap *)malloc(sizeof(pixmap)); // alokace struktury pixmapy
+    if (!p) return NULL;
+    p->width=width;                             // naplneni struktury
+    p->height=height;
+    p->pixels=(unsigned char *)malloc(3*width*height);
+    if (!p->pixels) {                           // alokace pole pro pixely
+        free(p);
+        return NULL;
+    }
+    return p;
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Funkce pro zruseni pixmapy
+//-----------------------------------------------------------------------------
+void destroyPixmap(pixmap *p)
+{
+    if (p->pixels) free(p->pixels);             // uvolnit vlastni pixmapu
+    if (p) free(p);                             // i okolni strukturu
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Funkce pro vykresleni pixmapy do barvoveho bufferu
+//-----------------------------------------------------------------------------
+void drawPixmap(const pixmap *p)
+{
+    if (!p || !p->pixels) return;
+    glDrawPixels(                               // vykresleni pixmapy
+            p->width, p->height,                // sirka a vyska pixmapy
+            GL_RGB,                             // format dat pixelu
+            GL_UNSIGNED_BYTE,                   // datovy typ kazde barevne slozky
+            p->pixels);                         // ukazatel na pamet s barvami pixelu
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Ulozeni pixmapy do externiho souboru
+//-----------------------------------------------------------------------------
+void savePixmap(const pixmap *p, const char *fileName)
+{
+    FILE *fout;
+    unsigned int i, j, k;
+    unsigned char tgaHeader[18]={               // hlavicka formatu typu TGA
+                        0x00,                   // typ hlavicky TGA
+                        0x00,                   // nepouzivame paletu
+                        0x02,                   // typ obrazku je RGB TrueColor
+                        0x00, 0x00,             // delka palety je nulova
+                        0x00, 0x00, 0x00,       // pozice v palete nas nezajima
+                        0x00, 0x00, 0x00, 0x00, // obrazek je umisteny na pozici [0, 0]
+                        0x00, 0x00, 0x00, 0x00, // sirka a vyska obrazku (dva byty na polozku)
+                        0x18,                   // format je 24 bitu na pixel
+                        0x20                    // orientace bitmapy v obrazku
+    };
+    if (!p || !p->pixels) return;
+    memcpy(tgaHeader+12, &(p->width), 2);       // do hlavicky TGA zapsat velikost obrazku
+    memcpy(tgaHeader+14, &(p->height), 2);
+    fout=fopen(fileName, "wb");
+    if (fout) {
+        fwrite(tgaHeader, 18, 1, fout);         // zapsat hlavicku TGA do souboru
+        for (j=p->height; j; j--) {             // radky zapisovat v opacnem poradi
+            unsigned int yoff=(j-1)*3*p->width; // y-ovy offset v poli
+            unsigned int xoff=0;                // x-ovy offset v poli
+            for (i=0; i<p->width; i++) {        // pro kazdy pixel na radku
+                for (k=0; k<3; k++) {           // prohodit barvy RGB na BGR
+                    fputc(p->pixels[xoff+yoff+2-k], fout); // a zapsat do souboru
+                }
+                xoff+=3;
+            }
+        }
+        fclose(fout);
+    }
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Zmena barvy pixelu na zadanych souradnicich
+//-----------------------------------------------------------------------------
+void putpixel(pixmap *p,
+              const unsigned int x,
+              const unsigned int y,
+              const unsigned char r,
+              const unsigned char g,
+              const unsigned char b)
+{
+    int pos;
+    // zde se vyuziva zkraceneho vyhodnoceni vyrazu - pokud plati !p, nic se dale netestuje
+    if (!p || !p->pixels || x>=p->width || y>=p->height) return;
+    pos=3*(x+y*p->width);
+    p->pixels[pos++]=r;
+    p->pixels[pos++]=g;
+    p->pixels[pos]=b;
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Funkce volana pro inicializaci vykreslovani
+//-----------------------------------------------------------------------------
+void onInit(void)
+{
+    int x, y;
+    unsigned char r, g, b;                      // barevne slozky pixelu
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);       // barva pozadi
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);      // mod ulozeni pixelu
+
+    pix=createPixmap(PIXMAP_WIDTH, PIXMAP_HEIGHT);
+
+    for (y=0; y<PIXMAP_HEIGHT; y++) {           // pro vsechny radky pixmapy
+        for (x=0; x<PIXMAP_WIDTH; x++) {        // pro vsechny pixely na radku
+            r=x;
+            g=x-y;
+            b=y;
+            putpixel(pix, x, y, r, g, b);       // zapsat barvu pixelu
+        }
+    }
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Nastaveni souradneho systemu v zavislosti na velikosti okna
+//-----------------------------------------------------------------------------
+void onResize(int w, int h)                     // argumenty w a h reprezentuji novou velikost okna
+{
+    glViewport(0, 0, w, h);                     // viditelna oblast pres cele okno
+    glMatrixMode(GL_PROJECTION);                // zacatek modifikace projekcni matice
+    glLoadIdentity();                           // vymazani projekcni matice (=identita)
+    glOrtho(0, w, 0, h, -1, 1);                 // mapovani abstraktnich souradnic do souradnic okna
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Tato callback funkce je zavolana pri kazdem prekresleni okna
+//-----------------------------------------------------------------------------
+void onDisplay(void)
+{
+    int i;
+    glClear(GL_COLOR_BUFFER_BIT);               // vymazani vsech bitovych rovin barvoveho bufferu
+    glDrawBuffer(GL_FRONT);                     // pixmapa se bude kreslit do predniho barvoveho bufferu
+    glRasterPos2i(0, 0);                        // nastaveni souradnic leveho spodniho rohu pixmapy
+    drawPixmap(pix);                            // vykresleni pixmapy
+    glFlush();                                  // provedeni a vykresleni vsech zmen
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Tato callback funkce je zavolana pri stlaceni ASCII klavesy
+//-----------------------------------------------------------------------------
+#ifdef __BORLANDC__
+#pragma option -w-par
+#endif
+void onKeyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+        case 27: exit(0);
+            break;                              // pokud byla stlacena klavesa ESC, konec programu
+        case 's':
+        case 'S':
+            savePixmap(pix, FILE_NAME);         // ulozeni pixmapy
+            break;
+        default:
+            break;
+    }
+}
+#ifdef __BORLANDC__
+#pragma option -w+par
+#endif
+
+
+
+//-----------------------------------------------------------------------------
+// Hlavni funkce konzolove aplikace
+//-----------------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);                      // inicializace knihovny GLUT
+    glutCreateWindow(WINDOW_TITLE);             // vytvoreni okna pro kresleni
+    glutReshapeWindow(WINDOW_WIDTH, WINDOW_HEIGHT);// zmena velikosti okna
+    glutPositionWindow(100, 100);               // pozice leveho horniho rohu okna
+    glutDisplayFunc(onDisplay);                 // registrace funkce volane pri prekreslovani okna
+    glutReshapeFunc(onResize);                  // registrace funkce volane pri zmene velikosti okna
+    glutKeyboardFunc(onKeyboard);               // registrace funkce volani pri stlaceni klavesy
+    onInit();                                   // inicializace vykreslovani
+    glutMainLoop();                             // nekonecna smycka, kde se volaji zaregistrovane funkce
+    return 0;                                   // navratova hodnota vracena operacnimu systemu
+}
+
+
+
+//-----------------------------------------------------------------------------
+// finito
+//-----------------------------------------------------------------------------
+
