180571be3da10407a863499531e02bcd38dc5837
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-25 11:35:53 +0100

    Rust example #118

---
+++ rust/118_str_pass_to_function.rs
@@ -0,0 +1,11 @@
+fn print_str(message: &str) {
+    println!("{}", message);
+}
+
+fn main() {
+    let message = "Hello world!";
+    print_str(message);
+    print_str(message);
+    print_str(message);
+}
+
