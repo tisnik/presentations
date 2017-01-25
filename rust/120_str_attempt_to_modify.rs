cc1be83f259a0019d50a5684f3bc9bf5a4f19aa1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-01-25 11:36:27 +0100

    Rust example #120

---
+++ rust/120_str_attempt_to_modify.rs
@@ -0,0 +1,12 @@
+fn print_str(message: &str) {
+    println!("{}", message);
+}
+
+fn main() {
+    let message = "Hello world!";
+    print_str(message);
+    message = "Something else";
+    print_str(message);
+    print_str(message);
+}
+
