1297f1a5ca6f1f568585262f1392c85e717d46d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-06 12:53:18 +0100

    Rust example #155

---
+++ rust/155_immutable_vector.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let vector = vec![1; 10];
+
+    println!("vector has {} items", vector.len());
+
+    print!("[");
+    for i in 0..vector.len() {
+        print!("{} ", vector[i]);
+    }
+    println!("]");
+
+    vector[5] = 100;
+
+    print!("[");
+    for i in 0..vector.len() {
+        print!("{} ", vector[i]);
+    }
+    println!("]");
+
+}
+
