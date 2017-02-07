5c513dd8d862a6a778cd77699581cc0739442d7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-07 19:48:31 +0100

    Rust example #166

---
+++ rust/166_slice_correct.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let vector = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    println!("vector has {} items", vector.len());
+
+    let slice = &vector[3..7];
+
+    println!("slice has {} items", slice.len());
+
+    for item in slice {
+        println!("{}", item);
+    }
+}
+
