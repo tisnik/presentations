ccf990d995307a91addd1d54e7273d966007c23e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-08 18:30:17 +0100

    Rust example #180

---
+++ rust/180_full_type_info.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let array:[i32;15]  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    let vector:Vec<i32> = array.iter()
+                             .take(10)
+                             .filter(|&x| x % 3 ==0)
+                             .map(|&x| x*2)
+                             .collect();
+
+    println!("vector has {} items", vector.len());
+
+    for item in &vector {
+        println!("{}", item);
+    }
+}
+
