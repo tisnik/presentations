16552c98917a2d6de092c77ab5f0d05eeae108b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-15 10:03:50 +0100

    Comparison Rust with C (2)

---
+++ rust/192_expansion_in_c_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define sqr(x) (x)*(x)
+
+int main(void)
+{
+    printf("%d\n", sqr(10));
+    printf("%d\n", sqr(5+5));
+    
+    int x = 2;
+    printf("%d\n", sqr(x));
+    printf("%d\n", sqr(x++));
+
+    return 0;
+}
+
