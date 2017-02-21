ade9ca588bbf022ae37b7c9601ad67f998a640c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-21 18:05:52 +0100

    Assembler in C, 1st example

---
+++ assembler/asm_in_c/asm_in_c_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    __asm__ __volatile__(
+        "nop   \n\t"
+        : /* zadne vystupni registry */
+        : /* zadne vstupni operandy */
+        : /* zadne registry pouzivane uvnitr kodu */
+    );
+
+    return 0;
+}
+
