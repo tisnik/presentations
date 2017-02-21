9cf2df13481d3ba5b5d066aafcdcac664d89b7dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-21 18:06:18 +0100

    Assembler in C, 7th example

---
+++ assembler/asm_in_c/asm_in_c_7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+unsigned long long x = 10;
+unsigned long long y = 20;
+unsigned long long result;
+
+int main ()
+{
+    __asm__ __volatile__(
+        "mov    %1, %%rax;   \n\t"
+        "add    %2, %%rax;   \n\t"
+        : "=a" (result)    /* vystupni operand */
+        : "r" (x), "r" (y) /* dva vstupni operandy */
+        :                  /* registry pouzivane uvnitr kodu */
+    );
+
+    printf("%Ld\n", result);
+
+    return 0;
+}
+
