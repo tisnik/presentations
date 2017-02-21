552bcf12d61164b7826b850f2051419c700db2fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-21 18:06:11 +0100

    Assembler in C, 5th example

---
+++ assembler/asm_in_c/asm_in_c_5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+unsigned long long result;
+
+int main ()
+{
+    __asm__ __volatile__(
+        "mov    $42, %%rbx;   \n\t"
+        : "=b" (result)  /* vystupni operand */
+        :                /* zadne vstupni operandy */
+        :                /* registry pouzivane uvnitr kodu */
+    );
+
+    printf("%Ld\n", result);
+
+    return 0;
+}
+
