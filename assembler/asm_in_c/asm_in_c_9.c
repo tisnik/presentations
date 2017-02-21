14ceac692bcd6d4d6132eadfd05833cba9f84284
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-21 18:07:02 +0100

    Assembler in C, 9th example

---
+++ assembler/asm_in_c/asm_in_c_9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned long long x = 10;
+unsigned long long y = 20;
+unsigned long long result;
+
+int main ()
+{
+    __asm__ __volatile__(
+        "add    %%rbx, %%rax;   \n\t"
+        : "=a" (result)    /* vystupni operand */
+        : "a" (x), "b" (y) /* dva vstupni operandy v registrech rbx a rcx */
+        :                  /* registry pouzivane uvnitr kodu */
+    );
+
+    printf("%Ld\n", result);
+
+    return 0;
+}
+
