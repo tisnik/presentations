03c4471a7cfe7c87c6769805e051589a5b6c38fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-23 00:06:46 +0100

    Rust example #209

---
+++ rust/209_option_and_then.rs
@@ -0,0 +1,29 @@
+fn div(x: i32, y: i32) -> Option<i32> {
+    if y != 0 {
+        Some(x/y)
+    }
+    else {
+        None
+    }
+}
+
+fn sqr(x: i32) -> Option<i32> {
+    Some(x*x)
+}
+
+fn div_square(x: i32, y :i32) -> String {
+    let result = div(x, y).and_then(sqr);
+
+    if let Some(val) = result {
+        format!("({} / {}) ^ 2 = {}", x, y, val)
+    }
+    else {
+        String::from("Divide by zero")
+    }
+}
+
+fn main() {
+    println!("{}", div_square(2, 1));
+    println!("{}", div_square(2, 0));
+}
+
