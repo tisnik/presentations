26f3c56587ee8d47003fb1ade65e203bb4211743
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-23 00:06:31 +0100

    Rust example #206

---
+++ rust/206_option_if_let.rs
@@ -0,0 +1,28 @@
+fn div(x: i32, y: i32) -> Option<i32> {
+    if y != 0 {
+        Some(x/y)
+    }
+    else {
+        None
+    }
+}
+
+fn div_and_print(x: i32, y :i32) {
+    let result = div(x, y);
+    println!("{:?}", result);
+
+    if let Some(val) = result {
+        println!("{} / {} = {}", x, y, val);
+    }
+    else {
+        println!("Divide by zero");
+    }
+
+    println!("");
+}
+
+fn main() {
+    div_and_print(2, 1);
+    div_and_print(2, 0);
+}
+
