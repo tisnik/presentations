046e9ec950136f1c69c71f65c8fc332efa5d37a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-07 20:16:45 +0100

    Rust example #227

---
+++ rust/227_format_padding.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let value1 =  42;
+    let value2 = -42;
+
+    println!("|{:-010}|", value1);
+    println!("|{:+010}|", value1);
+    println!("|{:-010}|", value2);
+    println!("|{:+010}|", value2);
+}
+
