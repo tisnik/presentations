c0279f7d74fe7a6a9a824cf632fe047f1a89a5ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-07 20:16:45 +0100

    Rust example #221

---
+++ rust/221_format_basic.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let value = 42;
+
+    println!("{}", value);
+    println!("0{:o}", value);
+    println!("0x{:x}", value);
+    println!("0x{:X}", value);
+    println!("0b{:b}", value);
+}
+
