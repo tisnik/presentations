9ba6e311d7544c3e3fc069d18a2301d94a9b4a09
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-07 20:16:45 +0100

    Rust example #226

---
+++ rust/226_format_sign.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let value1 =  42;
+    let value2 = -42;
+
+    println!("|{}|", value1);
+    println!("|{:-10}|", value1);
+    println!("|{:+10}|", value1);
+    println!("|{}|", value2);
+    println!("|{:-10}|", value2);
+    println!("|{:+10}|", value2);
+}
+
