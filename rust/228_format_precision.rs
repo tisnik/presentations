23a8d6ea424467ca3fed068fe8ef675261e282d4
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-07 20:16:45 +0100

    Rust example #228

---
+++ rust/228_format_precision.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let value = 42.5;
+
+    println!("{}",        value);
+    println!("{:10}",     value);
+    println!("{:10.4}",   value);
+    println!("{:+10}",    value);
+    println!("{:+10.4}",  value);
+    println!("{:^+10}",   value);
+    println!("{:^+10.4}", value);
+}
+
