fdc2073baddb6d481ed81b3479a77d48461ceb30
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-07 20:16:45 +0100

    Rust example #222

---
+++ rust/222_format_alternate_forms.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let value = 42;
+
+    println!("{}", value);
+    println!("{:#o}", value);
+    println!("{:#x}", value);
+    println!("{:#X}", value);
+    println!("{:#b}", value);
+}
+
