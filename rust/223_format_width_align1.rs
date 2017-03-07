e3386eef9e850c493df8ddee0bd6c9ea79b3ba51
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-07 20:16:45 +0100

    Rust example #223

---
+++ rust/223_format_width_align1.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let value = 42;
+
+    println!("|{}|", value);
+    println!("|{:20}|", value);
+    println!("|{:>20}|", value);
+    println!("|{:<20}|", value);
+    println!("|{:^20}|", value);
+}
+
