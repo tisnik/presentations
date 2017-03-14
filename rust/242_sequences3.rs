89c504444a71f0d6d5ea5df2d2d02d1239833ed5
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-14 21:17:39 +0100

    Rust example #242

---
+++ rust/242_sequences3.rs
@@ -0,0 +1,39 @@
+use std::collections::LinkedList;
+
+fn print_list(list: &LinkedList<i32>) {
+
+    if list.is_empty() {
+        println!("list is empty");
+    } else {
+        println!("list has {} items", list.len());
+    }
+
+    for item in list.iter() {
+        println!("{}", item);
+    }
+
+    println!("-------------------------");
+}
+
+fn main() {
+    let mut list: LinkedList<i32> = LinkedList::new();
+
+    println!("new list");
+    print_list(&list);
+
+    for i in 0..10 {
+        list.push_back(i);
+    }
+
+    println!("after 10x push_back");
+    print_list(&list);
+
+    for i in 0..10 {
+        list.push_front(i);
+    }
+
+    println!("after 10x push_front");
+    print_list(&list);
+
+}
+
