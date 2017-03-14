87674862f743ab1dcf8a47cf61d9f1729514c390
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-14 21:17:40 +0100

    Rust example #248

---
+++ rust/248_sequences9.rs
@@ -0,0 +1,38 @@
+use std::collections::VecDeque;
+
+fn print_deque(deque: &VecDeque<i32>) {
+
+    if deque.is_empty() {
+        print!("deque is empty");
+    } else {
+        print!("deque has {} items: ", deque.len());
+    }
+
+    for item in deque.iter() {
+        print!("{:2} ", item);
+    }
+
+    println!("");
+}
+
+fn main() {
+    let mut deque: VecDeque<i32> = VecDeque::new();
+
+    for i in 0..5 {
+        deque.push_front(i);
+        print_deque(&deque);
+    }
+
+    for i in 10..20 {
+        deque.pop_back();
+        deque.push_front(i);
+        print_deque(&deque);
+    }
+
+    for _ in 0..6 {
+        deque.pop_back();
+        print_deque(&deque);
+    }
+
+}
+
