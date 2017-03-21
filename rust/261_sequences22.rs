bf0c5641cf7c2450fcf2c03fc58fa518399263f6
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-21 08:49:11 +0100

    Rust example #261

---
+++ rust/261_sequences22.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+fn print_map(map: &HashMap<&str, &str>) {
+    if map.is_empty() {
+        println!("empty collection");
+    } else {
+        for (name, role) in map.iter() {
+            println!("{:15} \"{}\"", name, role);
+        }
+    }
+    println!("");
+}
+
+fn main() {
+    let mut map = HashMap::new();
+
+    print_map(&map);
+
+    map.insert("Trachta",      "inspektor");
+    map.insert("Hlavacek",     "praktikant");
+    map.insert("Bierhanzel",   "tovarnik");
+    map.insert("Meyer",        "tovarnik");
+    map.insert("Vaclav Kotek", "stevard");
+
+    print_map(&map);
+
+    for (_, role) in map.iter() {
+        *role = "?"
+    }
+
+    print_map(&map);
+}
+
