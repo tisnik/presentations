5e8d4c1194daa48a0f2e4279a09067dd8d3123f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-24 20:36:58 +0100

    Pyglet example #6

---
+++ pyglet/06_opengl.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import pyglet
+import pyglet.gl
+
+window = pyglet.window.Window(width=640,
+                              height=480,
+                              caption="Pyglet+OpenGL")
+
+@window.event
+def on_draw():
+    pyglet.gl.glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
+    pyglet.gl.glLoadIdentity()
+    pyglet.gl.glBegin(pyglet.gl.GL_TRIANGLES)
+    pyglet.gl.glVertex2f(0,0)
+    pyglet.gl.glVertex2f(window.width,0)
+    pyglet.gl.glVertex2f(window.width,window.height)
+    pyglet.gl.glEnd()
+
+pyglet.app.run()
+
