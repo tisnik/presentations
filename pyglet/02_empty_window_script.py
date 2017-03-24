4dcd6157179e1b6187f251d0359a10fc141e70d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-24 20:36:44 +0100

    Pyglet example #2

---
+++ pyglet/02_empty_window_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pyglet
+
+window = pyglet.window.Window(width=640,
+                              height=480,
+                              caption="Pyglet library")
+
+pyglet.app.run()
+
