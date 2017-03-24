57731a3edd19a21201975dbe9f2160051f18d31a
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-03-24 20:36:52 +0100

    Pyglet example #4

---
+++ pyglet/04_label.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import pyglet
+
+window = pyglet.window.Window(width=640,
+                              height=480,
+                              caption="Pyglet library")
+
+label = pyglet.text.Label("Pyglet library",
+                          font_name="Terminus",
+                          font_size=36,
+                          x=window.width//2,
+                          y=window.height//2)
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
+
