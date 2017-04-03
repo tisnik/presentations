c24cae75fe6f024017a8740c5ee059ce01e368c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-04 00:03:18 +0200

    Rust example #277

---
+++ rust/277_sequences38.rs
@@ -0,0 +1,47 @@
+use std::collections::LinkedList;
+
+struct Complex {
+    real: f32,
+    imag: f32,
+}
+
+impl Complex {
+    fn new(real: f32, imag: f32) -> Complex {
+        Complex{real:real, imag:imag}
+    }
+    fn print(&self) {
+        println!("complex number: {:}+{:}i", self.real, self.imag);
+    }
+}
+
+impl Drop for Complex {
+    fn drop(&mut self) {
+        println!("Dropping complex number: {:}+{:}i", self.real, self.imag);
+    }
+}
+
+fn print_list(list: &LinkedList<Complex>) {
+
+    if list.is_empty() {
+        println!("list is empty");
+    } else {
+        println!("list has {} items", list.len());
+    }
+
+    for item in list.iter() {
+        item.print();
+    }
+}
+
+fn main() {
+    let mut list: LinkedList<Complex> = LinkedList::new();
+
+    list.push_back(Complex::new(0.0, 0.0));
+    list.push_back(Complex::new(1.0, 1.0));
+    list.push_back(Complex::new(2.0, 2.0));
+
+    print_list(&list);
+
+    println!("exit from main()");
+}
+
