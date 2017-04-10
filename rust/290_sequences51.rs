0f22a20c7346bf533d1e0d4c9cb024e628e11093
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-10 11:19:22 +0200

    Rust example #290

---
+++ rust/290_sequences51.rs
@@ -0,0 +1,24 @@
+use std::collections::BinaryHeap;
+
+fn main() {
+    let mut heap = BinaryHeap::new();
+
+    heap.push(1);
+    heap.push(2);
+    heap.push(3);
+    heap.push(4);
+    heap.push(100);
+    heap.push(5);
+    heap.push(6);
+    heap.push(7);
+    heap.push(8);
+
+    println!("max value: {:?}", heap.peek());
+
+    let vec = heap.into_sorted_vec();
+
+    for item in &vec {
+        println!("{}", item);
+    }
+}
+
