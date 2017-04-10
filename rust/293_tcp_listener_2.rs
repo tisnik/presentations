f8c9fb87a442cbaf869bcfe2a075a79224bcf4ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-10 11:19:50 +0200

    Rust example #293

---
+++ rust/293_tcp_listener_2.rs
@@ -0,0 +1,24 @@
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handler(mut stream:TcpStream) {
+    println!("Accepted connection");
+    stream.write(b"Server response...\r\n").unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+
+    for tcp_stream in listener.incoming() {
+        match tcp_stream {
+            Ok(tcp_stream) => {
+                handler(tcp_stream);
+            }
+            Err(e) => {
+                println!("connection failed: {}", e);
+            }
+        }
+    }
+}
+
