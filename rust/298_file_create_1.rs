d1019719689db9bc64e914ddf8d966f937e02c31
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-25 08:50:10 +0200

    Rust example #298

---
+++ rust/298_file_create_1.rs
@@ -0,0 +1,9 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() {
+    let mut fout = File::create("test.txt").unwrap();
+    let written = fout.write(b"Hello, world!\n").unwrap();
+    println!("{} bytes written", written);
+}
+
