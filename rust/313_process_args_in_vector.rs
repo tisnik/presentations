59ae3b9b6378a7c739ec2672f6d4728aab8b6758
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-10 11:52:53 +0200

    Rust example #313

---
+++ rust/313_process_args_in_vector.rs
@@ -0,0 +1,24 @@
+use std::process::Command;
+
+fn main() {
+    let process = Command::new("ls").args(&["-1", "/bin", "-c"]).spawn();
+
+    match process {
+        Ok(mut child) => {
+            println!("spawn ok");
+            match child.wait() {
+                Ok(code) => {
+                    println!("process exited with code: {}", code);
+                }
+                Err(err) => {
+                    println!("failed to wait on child: {}", err);
+                }
+            }
+        }
+        Err(err) => {
+            println!("spawn error: {}", err);
+        }
+    }
+
+}
+
