125658d84b8632ad87c6fdf83a811c2cf7e0bf3e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-15 09:37:48 +0200

    Pyglet example #29

---
+++ pyglet/29_sprite.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pyglet
+
+window = pyglet.window.Window(width=640,
+                              height=480,
+                              caption="Pyglet library")
+
+image_stream = open("gnome-globe.png", "rb")
+image = pyglet.image.load('gnome-globe.png', file=image_stream)
+
+sprite = pyglet.sprite.Sprite(image)
+
+@window.event
+def on_draw():
+    window.clear()
+    sprite.draw()
+
+pyglet.app.run()
+
