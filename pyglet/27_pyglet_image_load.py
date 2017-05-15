4dc90e86098ab41d589e29bb896874db53a226b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-15 09:37:25 +0200

    Pyglet example #27

---
+++ pyglet/27_pyglet_image_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pyglet
+
+image_stream = open("gnome-globe.png", "rb")
+image = pyglet.image.load('gnome-globe.png', file=image_stream)
+
+print("Loaded image with size %d x %d pixels" % (image.width, image.height))
+
