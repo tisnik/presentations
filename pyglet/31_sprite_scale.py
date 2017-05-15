341317d1b8a348bebe93745c5fd4ad1669878aa1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-15 09:38:14 +0200

    Pyglet example #31

---
+++ pyglet/31_sprite_scale.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import pyglet
+import math
+
+window = pyglet.window.Window(width=640,
+                              height=480,
+                              caption="Pyglet library")
+
+image_stream = open("gnome-globe.png", "rb")
+image = pyglet.image.load('gnome-globe.png', file=image_stream)
+
+sprite = pyglet.sprite.Sprite(image)
+sprite.x = window.width / 2
+sprite.y = window.height / 2
+sprite.t = 0
+
+@window.event
+def on_draw():
+    window.clear()
+    sprite.draw()
+
+def update(dt):
+    sprite.t += 1
+    sprite.scale = 5.0 * math.sin(sprite.t/10.)
+
+pyglet.clock.schedule_interval(update, 1/60.)
+pyglet.app.run()
+
