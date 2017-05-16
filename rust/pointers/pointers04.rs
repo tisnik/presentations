59173c3932c47c0ade4c40caefceb27b2739d2b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-16 10:53:05 +0200

    Pointers in Rust, example #4

---
+++ rust/pointers/pointers04.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let mut value: i32 = 42;
+
+    let pointer: *mut i32 = &mut value;
+
+    println!("{}", value);
+    unsafe {
+        println!("{}", *pointer);
+    }
+
+    value = 1;
+
+    println!("{}", value);
+    unsafe {
+        println!("{}", *pointer);
+    }
+
+    unsafe {
+        *pointer = 20;
+    }
+    println!("{}", value);
+    unsafe {
+        println!("{}", *pointer);
+    }
+}
+
