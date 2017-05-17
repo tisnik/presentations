5e75ac0dfd9479757b2e239d0d44e5c24da0acde
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-17 10:42:32 +0200

    Updated pointer example #3

--- rust/pointers/pointers03.rs
+++ rust/pointers/pointers03.rs
@@ -1,15 +1,22 @@
 fn main() {
     let mut value: i32 = 42;
 
-    let reference: &i32 = &value;
     let pointer: *const i32 = &value;
 
     println!("{}", value);
-    println!("{}", reference);
     println!("{:?}", pointer);
 
     unsafe {
         println!("{}", *pointer);
     }
+
+    value = 100;
+    println!("{}", value);
+    println!("{:?}", pointer);
+
+    unsafe {
+        println!("{}", *pointer);
+    }
+
 }
 
