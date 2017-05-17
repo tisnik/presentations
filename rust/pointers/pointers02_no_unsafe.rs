f905e74e9110b482d492794f5942578e9095fba8
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-17 10:51:38 +0200

    Unsafe block is missing

---
+++ rust/pointers/pointers02_no_unsafe.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let value: i32 = 42;
+
+    let reference: &i32 = &value;
+    let pointer: *const i32 = &value;
+
+    println!("{}", value);
+    println!("{}", reference);
+    println!("{:?}", pointer);
+
+    println!("{}", *pointer);
+}
+
