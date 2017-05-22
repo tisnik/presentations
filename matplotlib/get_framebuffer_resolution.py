cebf740a6282e0c01d219ccd27ece0a2c9b51cc1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-22 08:50:42 +0200

    Pygame+Matplotlib how to read framebuffer resolution

---
+++ matplotlib/get_framebuffer_resolution.py
@@ -0,0 +1,15 @@
+import subprocess
+import re
+
+def get_framebuffer_resolution(framebuffer_device):
+    fbset_output = subprocess.check_output(["fbset", "-s", "-fb", framebuffer_device])
+
+    for line in fbset_output.split("\n"):
+        line = line.strip()
+        if line.startswith("geometry"):
+            print(line)
+            parsed = re.match(r"geometry (\d+) (\d+)", line)
+            return (parsed.group(1), parsed.group(2))
+
+print(get_framebuffer_resolution("/dev/fb0"))
+
