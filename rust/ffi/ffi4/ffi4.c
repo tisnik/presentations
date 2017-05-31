2e60bf77313fc62376ad3e961863a23e2e32d79f
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-05-31 20:02:41 +0200

    Updated 4th Makefile and example as well

--- rust/ffi/ffi4/ffi4.c
+++ rust/ffi/ffi4/ffi4.c
@@ -1,9 +1,11 @@
+#include <stdint.h>
+
 #include <stdio.h>
 
-int string_length(const char *str)
+int32_t string_length(const char *str)
 {
     printf("C side pointer:    %p\n", str);
-    int len = 0;
+    int32_t len = 0;
     for (; *str; str++, len++)
         ;
     return len;
