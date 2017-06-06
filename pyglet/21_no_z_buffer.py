eb3c3a4f3c4278680e1859c4da170e013aade04b
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-06-06 11:07:16 +0200

    Removed semicolons that are not needed here

--- pyglet/21_no_z_buffer.py
+++ pyglet/21_no_z_buffer.py
@@ -107,7 +107,7 @@ def on_draw():
 
     glMatrixMode(GL_PROJECTION)               # zacatek modifikace projekcni matice
     glLoadIdentity()                          # vymazani projekcni matice (=identita)
-    gluPerspective(fov, 1.0, nearPlane, farPlane);
+    gluPerspective(fov, 1.0, nearPlane, farPlane)
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()                          # nahrat jednotkovou matici
