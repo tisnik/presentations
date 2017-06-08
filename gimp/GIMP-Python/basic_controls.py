1c0ed97e9b5a8058202d2a3a773dca18b616bc59
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-06-08 11:13:27 +0200

    Updated to conform with PEP8

--- gimp/GIMP-Python/basic_controls.py
+++ gimp/GIMP-Python/basic_controls.py
@@ -2,6 +2,7 @@
 
 from gimpfu import *
 
+
 # funkce zavolana po spusteni pluginu uzivatelem
 def basic_controls():
     pass
@@ -18,17 +19,19 @@ register(
     "Open source",
     "2017-02-11",
     "Zobrazeni zakladnich ovladacich prvku",
-    "", # plugin se spusti jen pokud neexistuje obrazek
+    "",  # plugin se spusti jen pokud neexistuje obrazek
     [
         (PF_INT,    "number",   "Cele cislo", 42),
         (PF_FLOAT,  "angle",    "Desetinne cislo", 0.5),
         (PF_STRING, "string",   "String", "bla bla"),
-        (PF_TEXT,   "text",     "Text",   "The quick red fox jumped over the lazy dog"),
+        (PF_TEXT,   "text",     "Text",
+         "The quick red fox jumped over the lazy dog"),
 
         (PF_BOOL,   "prepinac",  "Prepinac",  True),
         (PF_TOGGLE, "prepinac2", "Prepinac2", 1),
         (PF_OPTION, "vyber1",   "List box",  2, ("foo", "bar", "baz")),
-        (PF_RADIO,  "vyber2",   "Prepinace", "jpg", (("png", "png"), ("jpg", "jpg"))),
+        (PF_RADIO,  "vyber2",   "Prepinace",
+         "jpg", (("png", "png"), ("jpg", "jpg"))),
 
         (PF_SPINNER, "spinner", "Numericky vstup", 42, (1, 8000, 1)),
         (PF_SLIDER,  "slider",  "Posuvnik", 100, (0, 100, 1)),
@@ -41,4 +44,3 @@ register(
     menu="<Image>/Filters/Test/")
 
 main()
-
