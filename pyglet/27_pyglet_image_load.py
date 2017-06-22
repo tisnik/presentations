32d66273a42345b437761910df0e546711afa250
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-06-22 15:18:37 +0200

    PEP8 compatibility

--- pyglet/27_pyglet_image_load.py
+++ pyglet/27_pyglet_image_load.py
@@ -6,4 +6,3 @@ image_stream = open("gnome-globe.png", "rb")
 image = pyglet.image.load('gnome-globe.png', file=image_stream)
 
 print("Loaded image with size %d x %d pixels" % (image.width, image.height))
-
