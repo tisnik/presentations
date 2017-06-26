695a8fc85bf7e78b233ef83d58e9fe8abb5d9c29
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-06-26 23:28:01 +0200

    It is not strictly needed to use extern 'C'

--- rust/libs/test5/src/lib.rs
+++ rust/libs/test5/src/lib.rs
@@ -2,7 +2,7 @@ use std::ffi::CStr;
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern "C" fn print_string(what: *const c_char) -> () {
+pub extern fn print_string(what: *const c_char) -> () {
     unsafe {
         let c_string = CStr::from_ptr(what).to_str().unwrap();
         println!("{:?}", c_string);
