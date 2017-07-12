cb912fc0d7e3ee92a2e063a0a582702266c30e05
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-12 10:25:25 +0200

    Make pygame28.py pep8-compatible

--- pygame/pygame28.py
+++ pygame/pygame28.py
@@ -4,10 +4,13 @@
 # Demonstrační příklady využívající knihovnu Pygame
 
 # Příklad číslo 28: použití objektů typu Surface, metoda blit()
-#                   a operace pygame.transform.rotate() + pygame.transform.rotozoom().
+#                   a operace pygame.transform.rotate()
+#                   + pygame.transform.rotozoom().
 
 
-import pygame, sys, os
+import pygame
+import sys
+import os
 
 # Nutno importovat kvůli konstantám QUIT atd.
 from pygame.locals import *
@@ -16,7 +19,7 @@ from pygame.locals import *
 WIDTH = 400
 HEIGHT = 400
 
-# Inicializace knihovny Pygame 
+# Inicializace knihovny Pygame
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -28,7 +31,7 @@ display = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Pygame test #28')
 
 # Konstanty s n-ticemi představujícími základní barvy
-BLACK   = (  0,   0,   0)
+BLACK = (0, 0, 0)
 
 # Vyplnění plochy okna černou barvou
 display.fill(BLACK)
@@ -59,7 +62,6 @@ display.blit(rot_and_scaled_image_1, (220, 120))
 display.blit(rot_and_scaled_image_2, (200, 200))
 
 
-
 # Hlavní herní smyčka
 while True:
     # Načtení a zpracování všech událostí z fronty
@@ -77,4 +79,3 @@ while True:
     clock.tick(20)
 
 # finito
-
