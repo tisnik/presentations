7e3ae4754e54117ad8a6b77a7bc12f022a43f7ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-14 10:56:24 +0200

    Hello world using PySide library, second version

---
+++ Python_GUI/PySide/hello_world2.py
@@ -0,0 +1,22 @@
+import sys
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+qt_application = QApplication(sys.argv)
+
+
+class HelloWorldLabel(QLabel):
+    def __init__(self):
+        QLabel.__init__(self, "Hello world!")
+
+        self.setMinimumSize(QSize(600, 400))
+        self.setAlignment(Qt.AlignCenter)
+        self.setWindowTitle('Hello world!')
+
+    def run(self):
+        self.show()
+        qt_application.exec_()
+
+
+HelloWorldLabel().run()
