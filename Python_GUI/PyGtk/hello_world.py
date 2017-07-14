f1f5223a659f48676a02229b97dfbc7e18f285e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-14 10:54:57 +0200

    Hello world using PyGtk library

---
+++ Python_GUI/PyGtk/hello_world.py
@@ -0,0 +1,25 @@
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+
+def delete_event(widget, event, data=None):
+    print "delete event occurred"
+    return False
+
+
+def destroy(widget, data=None):
+    print "destroy signal occurred"
+    gtk.main_quit()
+
+
+window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+window.connect("delete_event", delete_event)
+window.connect("destroy", destroy)
+
+label = gtk.Label("Hello world!")
+window.add(label)
+label.show()
+
+window.show()
+gtk.main()
