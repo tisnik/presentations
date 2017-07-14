842f1b66064cc5c17e0c7998578617e49cb8eece
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-14 10:55:35 +0200

    Hello world using PyQt library

---
+++ Python_GUI/PyQt/hello_world.py
@@ -0,0 +1,22 @@
+import sys
+
+# zajisteni importu noveho rozhrani
+import sip
+
+sip.setapi('QDate', 2)
+sip.setapi('QDateTime', 2)
+sip.setapi('QString', 2)
+sip.setapi('QTextStream', 2)
+sip.setapi('QTime', 2)
+sip.setapi('QUrl', 2)
+sip.setapi('QVariant', 2)
+
+from PyQt4.Qt import *
+
+qt_application = QApplication(sys.argv)
+
+label = QLabel('Hello world!')
+
+label.show()
+
+qt_application.exec_()
