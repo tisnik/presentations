4990c5aa1a8e1ecaf5a8f7db3c81ecb63208e33b
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-17 11:42:07 +0200

    PyGTK example, now without events

---
+++ Python_GUI/PyGtk/hello_world_no_events.py
@@ -0,0 +1,13 @@
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+
+window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+label = gtk.Label("Hello world!")
+window.add(label)
+label.show()
+
+window.show()
+gtk.main()
