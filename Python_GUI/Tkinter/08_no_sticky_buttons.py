22e1cb16e6b67652800a215d0bcca1a29290e0d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-19 12:13:35 +0200

    New example: no 'sticky' buttons on grid

---
+++ Python_GUI/Tkinter/08_no_sticky_buttons.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from tkinter import *
+from tkinter import ttk
+
+import sys
+
+
+def exit():
+    sys.exit(0)
+
+
+root = Tk()
+
+button1 = ttk.Button(root, text="1st btn", command=lambda: sys.exit(0))
+button2 = ttk.Button(root, text="Second button", command=lambda: sys.exit(0))
+button3 = ttk.Button(root, text="Third button", command=lambda: sys.exit(0))
+button4 = ttk.Button(root, text="This is fourth button, the last one",
+                     command=lambda: sys.exit(0))
+
+button1.grid(column=1, row=1)
+button2.grid(column=2, row=1)
+button3.grid(column=1, row=2)
+button4.grid(column=2, row=2)
+
+root.mainloop()
