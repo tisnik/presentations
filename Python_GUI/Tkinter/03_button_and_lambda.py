126b233a3c6eca5099890aeb6203c863ad0204d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-19 12:16:21 +0200

    Make this source PEP8 compatible

--- Python_GUI/Tkinter/03_button_and_lambda.py
+++ Python_GUI/Tkinter/03_button_and_lambda.py
@@ -8,10 +8,9 @@ import sys
 root = Tk()
 
 label = ttk.Label(root, text="Hello world!")
-button = ttk.Button(root, text="Close window", command=lambda:sys.exit(0))
+button = ttk.Button(root, text="Close window", command=lambda: sys.exit(0))
 
 label.pack()
 button.pack()
 
 root.mainloop()
-
