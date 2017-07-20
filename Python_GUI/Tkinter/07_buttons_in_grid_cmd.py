201bd5187d4cb1ea150c15771e862a0c45be4a2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-20 10:30:25 +0200

    Use lambdas -> no exit() function is needed

--- Python_GUI/Tkinter/07_buttons_in_grid_cmd.py
+++ Python_GUI/Tkinter/07_buttons_in_grid_cmd.py
@@ -5,15 +5,12 @@ from tkinter import ttk
 
 import sys
 
-def exit():
-    sys.exit(0)
-
 root = Tk()
 
-button1 = ttk.Button(root, text="First button", command=lambda:sys.exit(0))
-button2 = ttk.Button(root, text="Second button", command=lambda:sys.exit(0))
-button3 = ttk.Button(root, text="Third button", command=lambda:sys.exit(0))
-button4 = ttk.Button(root, text="Fourth button", command=lambda:sys.exit(0))
+button1 = ttk.Button(root, text="First button", command=lambda: sys.exit(0))
+button2 = ttk.Button(root, text="Second button", command=lambda: sys.exit(0))
+button3 = ttk.Button(root, text="Third button", command=lambda: sys.exit(0))
+button4 = ttk.Button(root, text="Fourth button", command=lambda: sys.exit(0))
 
 button1.grid(column=2, row=4)
 button2.grid(column=3, row=1)
@@ -21,4 +18,3 @@ button3.grid(column=1, row=3)
 button4.grid(column=4, row=2)
 
 root.mainloop()
-
