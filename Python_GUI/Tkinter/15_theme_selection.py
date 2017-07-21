69c70f87b796f61463b841069a25a63f6378112e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-21 11:35:01 +0200

    Tkinter example #15, Ttk theme selection

---
+++ Python_GUI/Tkinter/15_theme_selection.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from tkinter import *
+from tkinter import ttk
+
+import sys
+
+
+def exit():
+    sys.exit(0)
+
+
+root = Tk()
+
+style = ttk.Style()
+
+style.configure('Red.TButton', background='#ff8080')
+
+button1 = ttk.Button(root, text="clam",
+                     command=lambda: style.theme_use("clam"))
+button2 = ttk.Button(root, text="alt",
+                     command=lambda: style.theme_use("alt"))
+button3 = ttk.Button(root, text="default",
+                     command=lambda: style.theme_use("default"))
+button4 = ttk.Button(root, text="classic",
+                     command=lambda: style.theme_use("classic"))
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+button1.grid(column=1, row=1, sticky="we")
+button2.grid(column=2, row=1, sticky="we")
+button3.grid(column=1, row=2, sticky="we")
+button4.grid(column=2, row=2, sticky="we")
+
+quitButton.grid(column=2, row=5, sticky="we")
+
+label = Label(root, text='Hello world')
+entry = Entry(root)
+checkbutton = Checkbutton(text='Do you like Tkinter?')
+
+checkbutton.grid(column=1, row=3, columnspan=2, sticky="w")
+label.grid(column=1, row=4)
+entry.grid(column=2, row=4)
+
+root.mainloop()
