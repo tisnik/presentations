9bcb432258785bad95d033cb0555aff807309d0a
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-26 11:17:20 +0200

    Tkinter example #22: grid padding

---
+++ Python_GUI/Tkinter/22_grid_padding.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from tkinter import *
+from tkinter import ttk
+
+import sys
+
+
+def exit():
+    sys.exit(0)
+
+
+root = Tk()
+
+style = ttk.Style()
+style.configure('Red.TButton', background='#ff8080')
+
+buttonStyles = ("sunken", "solid", "flat", "groove", "raised", "ridge")
+
+buttons = (Button(root, text=buttonStyle, relief=buttonStyle)
+           for buttonStyle in buttonStyles)
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+for i, button in enumerate(buttons):
+    button.grid(column=1, row=i, sticky="we", padx=6, pady=6)
+
+quitButton.grid(column=2, row=6, sticky="we", padx=6, pady=6)
+
+root.mainloop()
