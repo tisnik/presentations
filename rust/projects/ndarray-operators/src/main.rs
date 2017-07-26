72aacdc6f791421bb13808d371056168481e9a18
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-26 11:12:56 +0200

    More examples about broadcasting arrays

--- rust/projects/ndarray-operators/src/main.rs
+++ rust/projects/ndarray-operators/src/main.rs
@@ -84,6 +84,31 @@ fn array_broadcasting() {
     println!("result:\n{}", result);
 
     println!();
+
+    let array3 = Array::from_iter(0..24).into_shape((2,3,4)).unwrap();
+    let array4 = Array::from_vec(vec![2]);
+
+    println!("array3:\n{}\n", array3);
+    println!("array4:\n{}\n", array4);
+
+    let result = &array3 * &array4;
+    println!("array3 * array4:\n{}", result);
+
+    println!();
+}
+
+
+fn array_broadcasting_scalar_value() {
+    let array1 = Array::from_iter(100..124).into_shape((2,3,4)).unwrap();
+    println!("array1:\n{}\n", array1);
+
+    let result = &array1 * 10;
+    println!("array1 * 10:\n{}\n", result);
+
+    let result = &array1 + 1000;
+    println!("array1 + 1000:\n{}\n", result);
+
+    println!();
 }
 
 
@@ -93,4 +118,5 @@ fn main() {
     array_consume_operators();
     array_in_place_operators();
     array_broadcasting();
+    array_broadcasting_scalar_value();
 }
