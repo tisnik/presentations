cbd646792c7e1d3b02232cce8f00be4d43383df6
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-31 10:45:23 +0200

    Pyglet example #27

---
+++ Python_GUI/Tkinter/27_checkbox_variable.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+style = ttk.Style()
+style.theme_use("alt")
+style.configure('Red.TButton', background='#ff8080')
+
+delete_internet = tkinter.IntVar()
+
+checkbutton = ttk.Checkbutton(root, text="Delete Internet?",
+                              variable=delete_internet,
+                              command=lambda: print(delete_internet.get()))
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+checkbutton.grid(column=1, row=1, sticky="we", padx=6, pady=6)
+quitButton.grid(column=1, row=2, sticky="we", padx=6, pady=6)
+
+root.mainloop()
