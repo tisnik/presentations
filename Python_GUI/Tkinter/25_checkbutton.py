21d2349d93bb307d41ac137b4dc6e4d15e85ff75
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-31 10:45:08 +0200

    Pyglet example #25

---
+++ Python_GUI/Tkinter/25_checkbutton.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+style = ttk.Style()
+style.configure('Red.TButton', background='#ff8080')
+
+checkbutton = ttk.Checkbutton(root, text="Delete Internet?",
+                              command=lambda: print("changed"))
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+checkbutton.grid(column=1, row=1, sticky="we", padx=6, pady=6)
+quitButton.grid(column=1, row=2, sticky="we", padx=6, pady=6)
+
+root.mainloop()
