80a52a3fe61bd4d0d9e0e5f30a4bc0ed9e8b2d69
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-02 11:34:55 +0200

    Make PEP8-compatible

--- pyglet/06_opengl.py
+++ pyglet/06_opengl.py
@@ -7,15 +7,16 @@ window = pyglet.window.Window(width=640,
                               height=480,
                               caption="Pyglet+OpenGL")
 
+
 @window.event
 def on_draw():
     pyglet.gl.glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
     pyglet.gl.glLoadIdentity()
     pyglet.gl.glBegin(pyglet.gl.GL_TRIANGLES)
-    pyglet.gl.glVertex2f(0,0)
-    pyglet.gl.glVertex2f(window.width,0)
-    pyglet.gl.glVertex2f(window.width,window.height)
+    pyglet.gl.glVertex2f(0, 0)
+    pyglet.gl.glVertex2f(window.width, 0)
+    pyglet.gl.glVertex2f(window.width, window.height)
     pyglet.gl.glEnd()
 
-pyglet.app.run()
 
+pyglet.app.run()
