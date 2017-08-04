d96a310f011c8f8a16bca597003a232ef7b0c330
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-04 15:08:11 +0200

    Tkinter example #33

---
+++ Python_GUI/Tkinter/33_radio_button_default_value.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import tkinter
+
+import sys
+
+root = tkinter.Tk()
+
+radio_var = tkinter.StringVar()
+
+radio_var.set("C")
+
+radio1 = tkinter.Radiobutton(root, variable=radio_var, value="Assembler",
+                             text="Assembler")
+
+radio2 = tkinter.Radiobutton(root, variable=radio_var, value="Basic",
+                             text="Basic")
+
+radio3 = tkinter.Radiobutton(root, variable=radio_var, value="Brainfuck",
+                             text="Brainfuck")
+
+radio4 = tkinter.Radiobutton(root, variable=radio_var, value="C",
+                             text="C")
+
+radio5 = tkinter.Radiobutton(root, variable=radio_var, value="Python",
+                             text="Python")
+
+showButton = tkinter.Button(root, text="Show var",
+                            command=lambda: print(radio_var.get()))
+
+quitButton = tkinter.Button(root, text="Exit", background='#ff8080',
+                            command=exit)
+
+radio1.grid(column=1, row=1, sticky="w")
+radio2.grid(column=1, row=2, sticky="w")
+radio3.grid(column=1, row=3, sticky="w")
+radio4.grid(column=1, row=4, sticky="w")
+radio5.grid(column=1, row=5, sticky="w")
+
+showButton.grid(column=2, row=6, sticky="we", padx=6, pady=6)
+quitButton.grid(column=2, row=7, sticky="we", padx=6, pady=6)
+
+root.mainloop()
