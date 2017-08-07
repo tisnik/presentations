38bc0d6876e3f49055896258a558aae5c3b0da32
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-07 10:30:44 +0200

    Tkinter example #39

---
+++ Python_GUI/Tkinter/39_listbox_scroll.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+scrollbar = tkinter.Scrollbar(root)
+
+langs = ("Assembler", "Basic", "Brainfuck", "C", "C++", "Java", "Julia",
+         "Perl", "Python")
+
+listbox = tkinter.Listbox(root, height=4)
+
+
+for lang in langs:
+    listbox.insert(tkinter.END, lang)
+
+
+def on_listbox_select(event):
+    index = listbox.curselection()[0]
+    global langs
+    print(langs[index])
+
+
+listbox.bind("<<ListboxSelect>>", on_listbox_select)
+
+quitButton = ttk.Button(root, text="Exit", command=exit)
+
+listbox.grid(column=1, row=1, sticky="nswe")
+scrollbar.grid(column=2, row=1, sticky="ns")
+quitButton.grid(column=1, row=2)
+
+root.mainloop()
