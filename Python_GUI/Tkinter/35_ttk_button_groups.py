a3917968a932b7a1676ff9250ce53ed5fdbff2c8
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-07 10:30:02 +0200

    Tkinter example #35

---
+++ Python_GUI/Tkinter/35_ttk_button_groups.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+style = ttk.Style()
+style.theme_use("alt")
+style.configure('Red.TButton', background='#ff8080')
+
+radio_var = tkinter.StringVar()
+radio_var.set("Python")
+
+langs = ("Assembler", "Basic", "Brainfuck", "C", "Python")
+
+radio_buttons = (ttk.Radiobutton(root, text=lang, value=lang,
+                                 variable=radio_var)
+                 for lang in langs)
+
+showButton = ttk.Button(root, text="Show var",
+                        command=lambda: print(radio_var.get()))
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+for i, radio_button in enumerate(radio_buttons):
+    radio_button.grid(column=1, row=i, sticky="w")
+
+showButton.grid(column=2, row=6, sticky="we", padx=6, pady=6)
+quitButton.grid(column=2, row=7, sticky="we", padx=6, pady=6)
+
+root.mainloop()
