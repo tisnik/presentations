f56190396091ceaf6b1f75d81c4320e37929fe44
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-08 11:11:03 +0200

    Pyglet example #73

---
+++ pyglet/73_on_text.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import pyglet
+
+
+def create_window(width, height):
+    return pyglet.window.Window(width=width,
+                                height=height,
+                                caption="Pyglet library")
+
+
+def create_label():
+    return pyglet.text.Label("Event:",
+                             font_size=18,
+                             x=10,
+                             y=window.height//2,
+                             anchor_x='left',
+                             anchor_y='center')
+
+
+window = create_window(640, 480)
+label = create_label()
+
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+@window.event
+def on_text(text):
+    text = format("Event: following text has been entered: '%s'" % text)
+    label.text = text
+    print(text)
+    on_draw()
+
+
+pyglet.app.run()
