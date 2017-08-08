64082cc1cc3890f1b5f578f7e3e09e8b323ad5d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-08 11:10:32 +0200

    Pyglet example #69

---
+++ pyglet/69_on_keypress.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import pyglet
+
+
+def create_window(width, height):
+    return pyglet.window.Window(width=width,
+                                height=height,
+                                caption="Pyglet library")
+
+
+def create_label():
+    return pyglet.text.Label("Event:",
+                             font_size=18,
+                             x=window.width//4,
+                             y=window.height//2,
+                             anchor_x='left',
+                             anchor_y='center')
+
+
+window = create_window(640, 480)
+label = create_label()
+
+
+def display_key_event(symbol, modifiers, action):
+    text = format("Event: key with code %d %s" % (symbol, action))
+    label.text = text
+    print(symbol)
+    on_draw()
+
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    display_key_event(symbol, modifiers, "pressed")
+
+
+@window.event
+def on_key_release(symbol, modifiers):
+    display_key_event(symbol, modifiers, "released")
+
+
+pyglet.app.run()
