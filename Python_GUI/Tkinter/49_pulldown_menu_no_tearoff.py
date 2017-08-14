610ace83a31ecc01f87fb4ca8cba1ebad0a3ffa7
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-14 10:14:17 +0200

    Tkinter example #49

---
+++ Python_GUI/Tkinter/49_pulldown_menu_no_tearoff.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+
+def test():
+    print("Test!")
+
+
+root = tkinter.Tk()
+
+menubar = tkinter.Menu(root)
+
+root.option_add('*tearOff', False)
+
+filemenu = tkinter.Menu(menubar)
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+editmenu = tkinter.Menu(menubar)
+editmenu.add_command(label="Undo")
+editmenu.add_separator()
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+editmenu.add_command(label="Delete")
+editmenu.add_separator()
+editmenu.add_command(label="Select All")
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+helpmenu = tkinter.Menu(menubar)
+helpmenu.add_command(label="About", command=test)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+
+root.mainloop()
