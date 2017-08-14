1339887347f3ee0fd0c696a58fe0a6f949b65132
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-14 10:14:39 +0200

    Tkinter example #51

---
+++ Python_GUI/Tkinter/51_menu_colors.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+
+def test():
+    print("Test!")
+
+
+root = tkinter.Tk()
+
+menubar = tkinter.Menu(root)
+
+filemenu = tkinter.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+editmenu = tkinter.Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo")
+editmenu.add_separator()
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+editmenu.add_command(label="Delete")
+editmenu.add_separator()
+editmenu.add_command(label="Select All")
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+colors = ("white", "yellow", "orange", "red", "magenta",
+          "blue", "cyan", "green")
+colormenu = tkinter.Menu(menubar, tearoff=0)
+
+for color in colors:
+    colormenu.add_command(label=color, background=color)
+
+menubar.add_cascade(label="Colors", menu=colormenu)
+
+helpmenu = tkinter.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About", command=test)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+
+root.mainloop()
