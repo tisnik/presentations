61347fde35e6c97d169ea19148a9a80b50b7bb4c
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-14 10:16:05 +0200

    Tkinter example #53

---
+++ Python_GUI/Tkinter/53_menu_images.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+
+def test():
+    print("Test!")
+
+
+root = tkinter.Tk()
+
+menubar = tkinter.Menu(root)
+
+open_image = tkinter.PhotoImage(file="icons/document-open.gif")
+save_image = tkinter.PhotoImage(file="icons/document-save.gif")
+exit_image = tkinter.PhotoImage(file="icons/application-exit.gif")
+undo_image = tkinter.PhotoImage(file="icons/edit-undo.gif")
+cut_image = tkinter.PhotoImage(file="icons/edit-cut.gif")
+copy_image = tkinter.PhotoImage(file="icons/edit-copy.gif")
+paste_image = tkinter.PhotoImage(file="icons/edit-paste.gif")
+delete_image = tkinter.PhotoImage(file="icons/edit-delete.gif")
+select_all_image = tkinter.PhotoImage(file="icons/edit-select-all.gif")
+
+filemenu = tkinter.Menu(menubar, tearoff=0)
+
+filemenu.add_command(label="Open", underline=0, image=open_image,
+                     compound="left")
+
+filemenu.add_command(label="Save", underline=0, image=save_image,
+                     compound="left")
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", underline=1, image=exit_image,
+                     compound="left", command=root.quit)
+
+menubar.add_cascade(label="File", menu=filemenu, underline=0)
+
+
+editmenu = tkinter.Menu(menubar, tearoff=0)
+
+editmenu.add_command(label="Undo", underline=0, image=undo_image,
+                     compound="left")
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut", underline=2, image=cut_image,
+                     compound="left")
+
+editmenu.add_command(label="Copy", underline=0, image=copy_image,
+                     compound="left")
+
+editmenu.add_command(label="Paste", underline=0, image=paste_image,
+                     compound="left")
+
+editmenu.add_command(label="Delete", underline=2, image=delete_image,
+                     compound="left")
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Select All", underline=7, image=select_all_image,
+                     compound="left")
+
+menubar.add_cascade(label="Edit", menu=editmenu, underline=0)
+
+
+colors = ("white", "yellow", "orange", "red", "magenta",
+          "blue", "cyan", "green")
+colormenu = tkinter.Menu(menubar, tearoff=0)
+
+for color in colors:
+    colormenu.add_command(label=color, background=color)
+
+menubar.add_cascade(label="Colors", menu=colormenu, underline=0)
+
+helpmenu = tkinter.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About", command=test, underline=0)
+menubar.add_cascade(label="Help", menu=helpmenu, underline=0)
+
+root.config(menu=menubar)
+
+root.mainloop()
