4c1e90d4275c89f02f8e77d7ea1e0124b4fe28e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-15 10:35:24 +0200

    Pyglet example #86

---
+++ pyglet/86_custom_and_standard_cursors.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pyglet
+
+
+def create_window(width, height):
+    return pyglet.window.Window(width=width,
+                                height=height,
+                                caption="Pyglet library")
+
+
+def load_cursor(filename):
+    image = pyglet.image.load(filename)
+    return pyglet.window.ImageMouseCursor(image, image.width/2, image.height/2)
+
+
+window = create_window(640, 480)
+cursor = load_cursor("gnome-globe.png")
+window.set_mouse_cursor(cursor)
+
+
+@window.event
+def on_draw():
+    window.clear()
+
+
+pyglet.app.run()
