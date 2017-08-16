5e8781b98613ccdfca6e8ad0461ca18724ed5d40
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-16 11:02:10 +0200

    Updated to support standard cursors as well

--- pyglet/86_custom_and_standard_cursors.py
+++ pyglet/86_custom_and_standard_cursors.py
@@ -15,13 +15,36 @@ def load_cursor(filename):
 
 
 window = create_window(640, 480)
-cursor = load_cursor("gnome-globe.png")
-window.set_mouse_cursor(cursor)
+custom_cursor = load_cursor("gnome-globe.png")
 
 
 @window.event
 def on_draw():
     window.clear()
+
+
+cursors = {
+    pyglet.window.key.F1: pyglet.window.Window.CURSOR_DEFAULT,
+    pyglet.window.key.F2: pyglet.window.Window.CURSOR_HAND,
+    pyglet.window.key.F3: pyglet.window.Window.CURSOR_HELP,
+    pyglet.window.key.F4: pyglet.window.Window.CURSOR_SIZE,
+    pyglet.window.key.F5: pyglet.window.Window.CURSOR_SIZE_UP,
+    pyglet.window.key.F6: pyglet.window.Window.CURSOR_SIZE_DOWN,
+    pyglet.window.key.F7: pyglet.window.Window.CURSOR_SIZE_LEFT,
+    pyglet.window.key.F8: pyglet.window.Window.CURSOR_SIZE_RIGHT,
+    pyglet.window.key.F9: pyglet.window.Window.CURSOR_WAIT,
+    pyglet.window.key.F10: pyglet.window.Window.CURSOR_NO
+}
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.SPACE:
+        cursor = custom_cursor
+    else:
+        cursor_type = cursors.get(symbol)
+        cursor = window.get_system_mouse_cursor(cursor_type)
+    window.set_mouse_cursor(cursor)
 
 
 pyglet.app.run()
