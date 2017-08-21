b58755df478e0a21f08f930f38f2c8e507f615fa
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-21 18:45:52 +0200

    Tkinter example #66

---
+++ Python_GUI/Tkinter/66_splines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+
+
+root = tkinter.Tk()
+
+canvas = tkinter.Canvas(root, width=300, height=300)
+canvas.pack()
+
+canvas.create_line(0, 150,
+                   80, 20,
+                   220, 280,
+                   300, 150, dash=10)
+
+canvas.create_line(0, 150,
+                   80, 20,
+                   220, 280,
+                   300, 150, smooth=True, width=2, fill="red")
+
+root.mainloop()
