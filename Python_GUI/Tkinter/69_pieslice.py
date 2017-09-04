799a6c064d208e6297c871a7645322f64c6b2067
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-04 11:10:50 +0200

    Tkinter example #69

---
+++ Python_GUI/Tkinter/69_pieslice.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+import sys
+
+
+WIDTH = 400
+HEIGHT = 400
+GRID_SIZE = 100
+
+
+def exit():
+    sys.exit(0)
+
+
+def basic_canvas(root, width, height, grid_size):
+    canvas = tkinter.Canvas(root, width=width, height=height,
+                            background='#ccffcc')
+    canvas.pack()
+
+    draw_grid(canvas, width, height, grid_size)
+    return canvas
+
+
+def draw_grid(canvas, width, height, grid_size):
+    for x in range(0, width, grid_size):
+        canvas.create_line(x, 0, x, height, dash=7, fill="gray")
+    for y in range(0, height, grid_size):
+        canvas.create_line(0, y, width, y, dash=7, fill="gray")
+
+
+root = tkinter.Tk()
+
+canvas = basic_canvas(root, WIDTH, HEIGHT, GRID_SIZE)
+
+canvas.create_arc(0, 0, 100, 100, fill='#ff8080')
+canvas.create_arc(100, 100, 200, 200, fill='#8080ff', start=45)
+canvas.create_arc(200, 0, 300, 100, fill='#80ffff', extent=180)
+canvas.create_arc(300, 100, 400, 200, fill='#ffff80', start=45, extent=270)
+
+canvas.create_arc(0, 200, 100, 300, fill='#ff8080', start=90, extent=270)
+canvas.create_arc(100, 300, 200, 400, fill='#8080ff', start=90+45, extent=270)
+canvas.create_arc(200, 200, 300, 300, fill='#80ffff', start=180, extent=180)
+canvas.create_arc(300, 300, 400, 400, fill='#ffff80', start=-45, extent=90)
+
+root.mainloop()
