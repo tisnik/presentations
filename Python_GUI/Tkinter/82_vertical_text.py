7915f01b4f116890e619a7e5d80a838dd89b021d
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-05 10:59:00 +0200

    Tkinter example #82

---
+++ Python_GUI/Tkinter/82_vertical_text.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+import sys
+
+
+WIDTH = 400
+HEIGHT = 400
+GRID_SIZE = 100
+
+
+def exit():
+    sys.exit(0)
+
+
+def basic_canvas(root, width, height, grid_size):
+    canvas = tkinter.Canvas(root, width=width, height=height,
+                            background='#ccffcc')
+    canvas.pack()
+
+    draw_grid(canvas, width, height, grid_size)
+    return canvas
+
+
+def draw_grid(canvas, width, height, grid_size):
+    for x in range(0, width, grid_size):
+        canvas.create_line(x, 0, x, height, dash=7, fill="gray")
+    for y in range(0, height, grid_size):
+        canvas.create_line(0, y, width, y, dash=7, fill="gray")
+
+
+def draw_text(canvas, x, y, anchor):
+    canvas.create_text(x, y, text="\n".join("Test"),
+                       fill="gray", anchor=anchor, font="Helvetica 12")
+    canvas.create_line(x-5, y, x+5, y, fill="red")
+    canvas.create_line(x, y-5, x, y+5, fill="red")
+
+
+root = tkinter.Tk()
+
+canvas = basic_canvas(root, WIDTH, HEIGHT, GRID_SIZE)
+
+draw_text(canvas, 50, 50, "ne")
+draw_text(canvas, 150, 50, "n")
+draw_text(canvas, 250, 50, "nw")
+
+draw_text(canvas, 50, 200, "e")
+draw_text(canvas, 150, 200, "center")
+draw_text(canvas, 250, 200, "w")
+
+draw_text(canvas, 50, 350, "se")
+draw_text(canvas, 150, 350, "s")
+draw_text(canvas, 250, 350, "sw")
+
+root.mainloop()
