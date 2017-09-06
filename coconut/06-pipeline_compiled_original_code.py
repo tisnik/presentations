b45f6589c66a2f55ff3a6c09b030e76149d2cbf2
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-06 11:03:22 +0200

    Coconut: transpiled source with line numbers + original sources

---
+++ coconut/06-pipeline_compiled_original_code.py
@@ -0,0 +1,15 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+(print)((abs)(-42))  # line 1: -42 |> abs |> print
+
+(print)((hex)((abs)((ord)("B"))))  # line 3: "B" |> ord |> abs |> hex |> print
+
+(print)((sum)(range(11)))  # line 5: range(11) |> sum |> print
+
+(print)((sum)((reversed)(range(11))))  # line 7: range(11) |> reversed |> sum |> print
+
+@_coconut_tco  # line 9: def evens(sequence):
+def evens(sequence):  # line 9: def evens(sequence):
+    return _coconut_tail_call(filter, lambda x: x % 2 == 0, sequence)  # line 10:     return filter(lambda x: x % 2 == 0, sequence)
+
+(print)((sum)((evens)([1, 2, 3, 4, 5, 6, 30])))  # line 12: [1, 2, 3, 4, 5, 6, 30] |> evens |> sum |> print
