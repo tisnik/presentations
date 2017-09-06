dc85e319c8c934bbccbae6dac586e86218a47c34
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-06 11:02:50 +0200

    Coconut: transpiled source

---
+++ coconut/06-pipeline_compiled.py
@@ -0,0 +1,15 @@
+# Compiled Coconut: -----------------------------------------------------------
+
+(print)((abs)(-42))
+
+(print)((hex)((abs)((ord)("B"))))
+
+(print)((sum)(range(11)))
+
+(print)((sum)((reversed)(range(11))))
+
+@_coconut_tco
+def evens(sequence):
+    return _coconut_tail_call(filter, lambda x: x % 2 == 0, sequence)
+
+(print)((sum)((evens)([1, 2, 3, 4, 5, 6, 30])))
