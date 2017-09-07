7f9770c1426af8792ca76f45df3b3018b422f04c
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-07 10:48:09 +0200

    Pyglet example #89

---
+++ pyglet/89_select_driver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pyglet
+
+pyglet.options['audio'] = ('pulse', 'openal', 'silent')
+
+source = pyglet.media.load('login.wav', streaming=False)
+
+source.play()
+pyglet.app.run()
