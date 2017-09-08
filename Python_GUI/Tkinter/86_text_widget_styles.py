bd1f336a19f7169862738af7bc39f58b63e020ae
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-08 10:50:44 +0200

    Tkinter example #86

---
+++ Python_GUI/Tkinter/86_text_widget_styles.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+import sys
+
+
+WIDTH = 400
+HEIGHT = 400
+GRID_SIZE = 100
+
+
+def exit():
+    sys.exit(0)
+
+
+root = tkinter.Tk()
+
+text = tkinter.Text(root,
+                    font="Helvetica 20",
+                    foreground="#0000c0",
+                    background="#c0ffc0",
+                    selectforeground="white",
+                    selectbackground="red",
+                    insertwidth=4,
+                    insertbackground="red",
+                    insertborderwidth=1,
+                    wrap=tkinter.WORD,
+                    width=40, height=16)
+
+text.insert(tkinter.END, "Test widgetu\n'text'")
+
+button = tkinter.Button(root, text="Close window", command=exit)
+
+text.pack()
+button.pack()
+
+root.mainloop()
