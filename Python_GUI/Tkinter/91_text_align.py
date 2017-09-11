6a35e4e763459c19440292a7790a0c397f889b76
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-11 10:37:07 +0200

    Tkinter example #91

---
+++ Python_GUI/Tkinter/91_text_align.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import tkinter
+from tkinter import ttk
+import sys
+
+
+WIDTH = 400
+HEIGHT = 400
+GRID_SIZE = 100
+
+
+def exit():
+    sys.exit(0)
+
+
+root = tkinter.Tk()
+
+text = tkinter.Text(root,
+                    font="Helvetica 20",
+                    wrap=tkinter.WORD,
+                    tabs="7c right 11c left",
+                    width=40, height=16)
+
+text.tag_configure("nadpis", foreground="red", underline=True)
+text.tag_configure("suma", foreground="blue")
+
+text.insert(tkinter.END, "Mesic\tObrat\n", "nadpis")
+text.insert(tkinter.END, "leden\t100\n")
+text.insert(tkinter.END, "unor\t200\n")
+text.insert(tkinter.END, "brezen\t0\n")
+text.insert(tkinter.END, "duben\t1000\n")
+text.insert(tkinter.END, "kveten\t100\n")
+text.insert(tkinter.END, "cerven\t200\n")
+text.insert(tkinter.END, "cervenec\t0\tdovolene\n")
+text.insert(tkinter.END, "srpen\t1000\n")
+text.insert(tkinter.END, "zari\t100\n")
+text.insert(tkinter.END, "rijen\t200\n")
+text.insert(tkinter.END, "listopad\t0\n")
+text.insert(tkinter.END, "prosinec\t1100\tvanoce\n\n")
+text.insert(tkinter.END, "suma\t3900\n", "suma")
+
+button = tkinter.Button(root, text="Close window", command=exit)
+
+text.pack()
+button.pack()
+
+root.mainloop()
