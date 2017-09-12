20123810e36827775f79a82dff4d18836bd03d14
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-12 08:37:34 +0200

    Coconut: example #11

---
+++ coconut/11_unicode_chars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __coconut_hash__ = 0x3d5464d
+
+# Compiled with Coconut version 1.3.0 [Dead Parrot]
+
+# Coconut Header: -------------------------------------------------------------
+
+
+# Compiled Coconut: -----------------------------------------------------------
+
+result = 60 * 7 / 10
+print(result)
+
+if result >= 40 and result <= 50:
+    print("very close")
+
+print(1 << 10)
+print(1 ^ 255)
+
+(print)((abs)(-42))
+
+(print)((hex)((abs)((ord)("B"))))
+
+(print)((sum)(range(11)))
+
+(print)((sum)((reversed)(range(11))))
+
+(print)((_coconut_forward_compose(ord, abs, hex))("B"))
+
+(print)((_coconut_forward_compose(reversed, sum))(range(11)))
