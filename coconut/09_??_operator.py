b63aa02d5f9b28f35c6efb3f8d77ea2dcfa75a25
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-12 08:37:10 +0200

    Coconut: example #09

---
+++ coconut/09_??_operator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __coconut_hash__ = 0x109f876f
+
+# Compiled with Coconut version 1.3.0 [Dead Parrot]
+
+# Coconut Header: -------------------------------------------------------------
+
+# Compiled Coconut: -----------------------------------------------------------
+
+import os
+
+v1 = None
+v2 = "Some"
+
+print(v2 if v1 is None else v1)
+
+v3 = "Some"
+v4 = "Something else"
+
+print(v4 if v3 is None else v3)
+
+v5 = None
+v6 = None
+
+print(v6 if v5 is None else v5)
+
+print((lambda _coconut_none_coalesce_item: "notepad" if _coconut_none_coalesce_item is None else _coconut_none_coalesce_item)(os.getenv('EDITOR')))
+print((lambda _coconut_none_coalesce_item: "notepad" if _coconut_none_coalesce_item is None else _coconut_none_coalesce_item)(os.getenv('XXEDITOR')))
