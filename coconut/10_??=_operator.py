cd5e35a0ea28ef8a4b6de964e8722fdeb550104d
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-13 08:36:51 +0200

    Coconut example #10

---
+++ coconut/10_??=_operator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __coconut_hash__ = 0xbe97918d
+
+# Compiled with Coconut version 1.3.0 [Dead Parrot]
+
+# Coconut Header: -------------------------------------------------------------
+
+
+# Compiled Coconut: -----------------------------------------------------------
+
+slovnik = {"prvni": 1, "druhy": 2, "treti": 3, "posledni": None}
+
+print(slovnik)
+
+slovnik["prvni"] = 1000 if slovnik["prvni"] is None else slovnik["prvni"]
+
+print(slovnik)
+
+slovnik["posledni"] = 1000 if slovnik["posledni"] is None else slovnik["posledni"]
+
+print(slovnik)
+
+slovnik["neexistujici"] = 10 if slovnik["neexistujici"] is None else slovnik["neexistujici"]
+
+print(slovnik)
