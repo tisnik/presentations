803744f1a32c14dbb6094905ec672f44da759faa
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-13 08:37:58 +0200

    Recursion in Python

---
+++ coconut/recursive_factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+for n in range(0, 11):
+    print("{n}! = {f}".format(n=n, f=factorial(n)))
+
+print(factorial(999))
+# print(factorial(1000))
---
+++ coconut/tail_call_factorial.py
@@ -0,0 +1,12 @@
+def factorial(n, acc=1):
+    if n < 2:
+        return acc
+    else:
+        return factorial(n-1, acc*n)
+
+
+for n in range(0, 11):
+    print("{n}! = {f}".format(n=n, f=factorial(n)))
+
+print(factorial(999))
+# print(factorial(1000))
