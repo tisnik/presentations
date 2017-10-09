7e8826e634d7c2ea5e4597820d49640548d24a70
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-09 18:28:10 +0200

    appJar example #12

---
+++ Python_GUI/appJar/12_button_padding.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onButtonPress(buttonName):
+    if buttonName == "Quit":
+        app.stop()
+    else:
+        app.infoBox("Ok, Ok", "Ok button pressed")
+
+
+app = gui()
+
+app.setSticky("news")
+app.setPadding(10, 10)
+
+app.addLabel("title", "Hello world!")
+
+app.addButton("Ok", onButtonPress)
+app.addButton("Quit", onButtonPress)
+
+app.go()
