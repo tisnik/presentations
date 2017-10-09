afcffc12b99e3658b34f63f7cb509aa87934ba62
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-09 18:28:35 +0200

    appJar example #14

---
+++ Python_GUI/appJar/14_check_box.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+CHECK_SYMBOL = "\u2713"
+MULTIPLY_SYMBOL = "\u2715"
+
+
+def state(choice):
+    return CHECK_SYMBOL if choice else MULTIPLY_SYMBOL
+
+
+def onButtonPress(buttonName):
+    if buttonName == "Quit":
+        app.stop()
+    else:
+        msg = "1st choice: {c1}\n2nd choice: {c2}".format(
+            c1=state(app.getCheckBox("1st choice")),
+            c2=state(app.getCheckBox("2nd choice")))
+        app.infoBox("Show choices:", msg)
+
+
+app = gui()
+
+app.setSticky("news")
+app.setPadding(10, 10)
+
+app.addCheckBox("1st choice", 1, 1)
+app.addCheckBox("2nd choice", 2, 1)
+app.addButton("Show choices", onButtonPress, 3, 1)
+app.addButton("Quit", onButtonPress, 3, 2)
+
+app.go()
