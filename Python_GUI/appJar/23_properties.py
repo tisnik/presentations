5f637960b20954ba597a59af9a19530809151a88
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-10 00:11:26 +0200

    appJar example #23

---
+++ Python_GUI/appJar/23_properties.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onButtonPress(buttonName):
+    if buttonName == "Quit":
+        app.stop()
+    else:
+        msg = "Your choices: {c}".format(c=app.getProperties("properties"))
+        app.infoBox("Show choices:", msg)
+
+
+app = gui()
+
+app.setSticky("news")
+app.setPadding(10, 2)
+
+app.addProperties("properties", {"Assembler": True,
+                                 "C": True,
+                                 "C++": False,
+                                 "Perl": False,
+                                 "Python": True})
+
+app.addButton("Show choice", onButtonPress)
+app.addButton("Quit", onButtonPress)
+
+app.go()
