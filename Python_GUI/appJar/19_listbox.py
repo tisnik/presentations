c85dfa5c694e065f73705cb9e04312abeaa9a291
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-09 18:29:39 +0200

    appJar example #19

---
+++ Python_GUI/appJar/19_listbox.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onButtonPress(buttonName):
+    if buttonName == "Quit":
+        app.stop()
+    else:
+        msg = "Your choice: {c}".format(c=app.getListItems("listbox"))
+        app.infoBox("Show choices:", msg)
+
+
+app = gui()
+
+app.setSticky("news")
+app.setPadding(10, 2)
+
+app.addListBox("listbox", ["Assembler", "C", "C++", "Perl", "Python"])
+
+app.addButton("Show choice", onButtonPress)
+app.addButton("Quit", onButtonPress)
+
+app.go()
