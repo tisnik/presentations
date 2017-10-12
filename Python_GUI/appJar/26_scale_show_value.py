53cb0e13d3c70d5899aa543cd3f6de9a4481beaa
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-13 00:03:12 +0200

    appJar example #26

---
+++ Python_GUI/appJar/26_scale_show_value.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onButtonPress(buttonName):
+    if buttonName == "Quit":
+        app.stop()
+    else:
+        msg = "Your choice of scale: {s}".format(s=app.getScale("scale"))
+        app.infoBox("Show scale:", msg)
+
+
+app = gui()
+
+app.setSticky("news")
+app.setPadding(10, 2)
+
+app.addLabelScale("scale", colspan=2)
+app.showScaleIntervals("scale", 20)
+app.showScaleValue("scale")
+
+app.addButton("Show scale value", onButtonPress, 1, 0)
+app.addButton("Quit", onButtonPress, 1, 1)
+
+app.go()
