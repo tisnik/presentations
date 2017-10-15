32bfa13aa27f67d13e98545ccc1bd27a8ca3fb24
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-16 00:08:44 +0200

    appJar example #38

---
+++ Python_GUI/appJar/38_set_date_picker.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onButtonPress(buttonName):
+    if buttonName == "Quit":
+        app.stop()
+
+
+def showDate(btn):
+    msg = "Selected date: {d}".format(d=app.getDatePicker("datePicker"))
+    app.infoBox("Show scale:", msg)
+
+
+app = gui()
+
+app.setSticky("news")
+app.setPadding(10, 2)
+
+app.addDatePicker("datePicker")
+app.setDatePickerRange("datePicker", 2000, 2018)
+
+# toto volani musi byt umisteno za predchozi prikaz!
+app.setDatePicker("datePicker")
+
+app.addButton("Show selected date", showDate, 2, 0)
+app.addButton("Quit", onButtonPress, 2, 1)
+
+app.go()
