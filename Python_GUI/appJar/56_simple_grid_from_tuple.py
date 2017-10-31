2c2c60289a9dfa1429f59c14921b40cd35590a74
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-01 00:05:00 +0100

    appJar example #56

---
+++ Python_GUI/appJar/56_simple_grid_from_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+app = gui()
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addGrid("grid",
+            ((' 1 ', ' 2 ', ' 3 '),
+             (' 4 ', ' 5 ', ' 6 '),
+             (' 7 ', ' 8 ', ' 9 '),
+             (' * ', ' 0 ', ' # ')))
+
+app.go()
