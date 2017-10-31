8cfcfd44d1281cfeef417151ee66e65018e46f82
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-01 00:05:23 +0100

    appJar example #57

---
+++ Python_GUI/appJar/57_simple_grid_from_range.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+app = gui()
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addGrid("grid",
+            (range(1, 4),
+             range(4, 7),
+             range(7, 10),
+             (' * ', ' 0 ', ' # ')))
+
+app.go()
