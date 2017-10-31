ca0cd06ccda1945150ae0aef549461ca36a93d7f
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-01 00:06:28 +0100

    appJar example #66

---
+++ Python_GUI/appJar/66_tree_colors.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from appJar import gui
+import re
+
+
+from appJar import gui
+
+app = gui()
+
+treeContent = """
+<html>
+    <head>
+        <title>Titulek</title>
+    </head>
+    <body>
+        <div>První odstavec</div>
+        <div>Druhý odstavec</div>
+        <div>Třetí odstavec</div>
+    </body>
+</html>
+"""
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+app = gui()
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addTree("tree", treeContent)
+app.setTreeEditable("tree", False)
+app.setTreeColours("tree", "black", "#aaffaa", "red", "yellow")
+
+app.go()
