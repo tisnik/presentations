7032118cf7f219390b4c81bc3e73fdfc531d0652
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-01 00:05:50 +0100

    appJar example #60

---
+++ Python_GUI/appJar/60_selected_cells.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from appJar import gui
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+def onButtonPress(buttonName):
+    cells = app.getGridSelectedCells("grid")
+    print(cells)
+
+
+def createTable(n):
+    return [[i*j for i in range(1, n+1)] for j in range(1, n+1)]
+
+
+app = gui()
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addGrid("grid", createTable(10))
+
+app.addButton("Show selected cells", onButtonPress)
+
+app.go()
