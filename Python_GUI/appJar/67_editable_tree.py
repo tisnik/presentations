d3376754ee09cc79f2b66be9148434d33f03f22a
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-01 00:06:34 +0100

    appJar example #67

---
+++ Python_GUI/appJar/67_editable_tree.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from appJar import gui
+import re
+
+
+from appJar import gui
+
+app = gui()
+
+treeContent = """
+<html>
+    <head>
+        <title>Titulek</title>
+    </head>
+    <body>
+        <h1>Kapitola</h1>
+        <div>První odstavec</div>
+        <div>Druhý odstavec</div>
+        <div>Třetí odstavec</div>
+    </body>
+</html>
+"""
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+app = gui()
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addTree("tree", treeContent)
+app.setTreeEditable("tree", True)
+
+app.go()
