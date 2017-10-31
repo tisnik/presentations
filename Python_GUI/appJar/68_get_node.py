f585b5c5e7c7f1274a7fd10b96d8f6faddaf0351
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-01 00:06:39 +0100

    appJar example #68

---
+++ Python_GUI/appJar/68_get_node.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from appJar import gui
+import re
+
+
+from appJar import gui
+
+app = gui()
+
+treeContent = """
+<html>
+    <head>
+        <title>Titulek</title>
+    </head>
+    <body>
+        <h1>Kapitola</h1>
+        <div>První odstavec</div>
+        <div>Druhý odstavec</div>
+        <div>Třetí odstavec</div>
+    </body>
+</html>
+"""
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+def onButtonPress(buttonName):
+    item = app.getTreeSelected("tree")
+    if item is None:
+        app.warningBox("Warning", "No item (node) selected")
+    else:
+        app.infoBox("Selected item", item)
+
+
+app = gui()
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addTree("tree", treeContent)
+app.setTreeEditable("tree", True)
+
+app.addButton("Show selected item", onButtonPress)
+
+app.go()
