d847cfa2eaa0c5807c321d461a40180ade91933e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-06 00:08:41 +0100

    appJar example #73

---
+++ Python_GUI/appJar/73_piechart.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from appJar import gui
+import tkinter
+
+
+app = gui()
+
+
+def onMenuItemSelect(menuItem):
+    if menuItem == "Quit":
+        app.stop()
+
+
+data = {
+   "ODS": 25,
+   "CSSD": 15,
+   "STAN": 6,
+   "KSČM": 15,
+   "Piráti": 22,
+   "TOP 09": 7,
+   "ANO":  78,
+   "KDU-ČSL": 10,
+   "SPD": 22}
+
+app.setSticky("news")
+
+fileMenu = ["Quit"]
+app.addMenuList("File", fileMenu, onMenuItemSelect)
+
+app.addPieChart("piechart", data)
+
+app.go()
