12e607f3495329fc040da9c11ab599cc85da04fa
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-27 19:06:49 +0100

    Added comments etc.

--- Python_GUI/PySide/11_quit_button.py
+++ Python_GUI/PySide/11_quit_button.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8
 
 import sys
 
+# import "jádra" frameworku Qt i modulu pro GUI
 from PySide import QtCore
 from PySide import QtGui
 
 
+# nový widget bude odvozen od obecného widgetu
 class MainWindow(QtGui.QWidget):
+
     def __init__(self):
+        # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
         self.prepareGUI()
 
     def prepareGUI(self):
         self.resize(320, 240)
         self.setWindowTitle("Quit Button")
 
+        # tlačítko
         button = QtGui.QPushButton("Quit", self)
         button.resize(button.sizeHint())
+
+        # navázání akce na signál
         button.clicked.connect(QtCore.QCoreApplication.instance().quit)
 
     def run(self, app):
+        # zobrazení okna na obrazovce
         self.show()
-        app.exec_()
+        # vstup do smyčky událostí (event loop)
+        sys.exit(app.exec_())
 
 
 def main():
