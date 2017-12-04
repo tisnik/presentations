f476c792e06a8860db2ce39272afbbd3f9a07365
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-04 10:03:16 +0100

    Added comments

--- Python_GUI/PySide/14_event_and_signals.py
+++ Python_GUI/PySide/14_event_and_signals.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8
 
 import sys
 
+# import "jádra" frameworku Qt i modulu pro GUI
 from PySide import QtCore
 from PySide import QtGui
 
 
+# callback funkce
+@QtCore.Slot()
 def closeApplication():
     print("Closing...")
     sys.exit(0)
 
 
+# nový widget bude odvozen od obecného widgetu
 class MainWindow(QtGui.QWidget):
+
     def __init__(self):
+        # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
         self.prepareGUI()
 
     def prepareGUI(self):
         self.resize(320, 240)
         self.setWindowTitle("Quit Button")
 
+        # tlačítko
         button = QtGui.QPushButton("Quit", self)
         button.resize(button.sizeHint())
         button.setToolTip("Immediately quit this application")
+
+        # navázání akce na signál
         button.clicked.connect(closeApplication)
 
     def run(self, app):
+        # zobrazení okna na obrazovce
         self.show()
+        # vstup do smyčky událostí (event loop)
         app.exec_()
 
 
