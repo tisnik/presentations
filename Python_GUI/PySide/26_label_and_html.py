1c73d3dbcc65dcbc35af65bc129416c56bb4ef40
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-11 10:05:45 +0100

    PySide example #26

---
+++ Python_GUI/PySide/26_label_and_html.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindow(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("QLabel widget")
+
+        # textové návěští
+        # pozor na nutnost použití prefixu "u" v Pythonu 2.x
+        text = u"<font color='black'>černá</font><br />" \
+               u"<font color='blue'>modrá</font><br />" \
+               u"<font color='red'>čevená</font><br />" \
+               u"<font color='magenta'>fialová</font><br />" \
+               u"<font color='green'>zelená</font><br />" \
+               u"<font color='cyan'>azurová</font><br />" \
+               u"<font color='yellow'>žlutá</font><br />" \
+               u"<font color='white'>bílá</font><br />"
+        testLabel = QtGui.QLabel(text)
+
+        # tlačítko pro ukončení aplikace
+        quitButton = QtGui.QPushButton("Quit")
+
+        # vytvoření správce geometrie
+        layout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        layout.addWidget(testLabel)
+        layout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(layout)
+
+        # navázání akce na stisk tlačítka pro ukončení aplikace
+        quitButton.clicked.connect(self.quit)
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+    def quit(self):
+        print("Closing...")
+        self.close()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
