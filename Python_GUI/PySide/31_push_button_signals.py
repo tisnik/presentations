a5c0bafc22716edf7d58a67e4947c3f0b8f22062
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-11 10:06:36 +0100

    PySide example #31

---
+++ Python_GUI/PySide/31_push_button_signals.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+def buttonEvent(eventName):
+    print("Event " + eventName)
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindow(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("QButton signals")
+
+        # testovací tlačítko
+        testButton = QtGui.QPushButton("Press me")
+
+        # tlačítko pro ukončení aplikace
+        quitButton = QtGui.QPushButton("Quit")
+
+        # vytvoření správce geometrie
+        layout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        layout.addWidget(testButton)
+        layout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(layout)
+
+        # navázání akce na signál
+        testButton.clicked.connect(lambda: buttonEvent('clicked'))
+        testButton.pressed.connect(lambda: buttonEvent('pressed'))
+        testButton.released.connect(lambda: buttonEvent('released'))
+
+        # navázání akce na stisk tlačítka pro ukončení aplikace
+        quitButton.clicked.connect(self.quit)
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+    def quit(self):
+        print("Closing...")
+        self.close()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
