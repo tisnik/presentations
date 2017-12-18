f2b909266e8678fc0939c7bbe2722bf84400293b
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-18 10:44:23 +0100

    PySide example #44 - status bar on main window

---
+++ Python_GUI/PySide/44_status_bar.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindowContent(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindowContent, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # tlačítko 1
+        self.counterButton = QtGui.QPushButton("Counter", self)
+        self.counterButton.resize(self.counterButton.sizeHint())
+
+        # tlačítko 2
+        quitButton = QtGui.QPushButton("Quit", self)
+        quitButton.resize(quitButton.sizeHint())
+
+        # vytvoření správce geometrie
+        layout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        layout.addWidget(self.counterButton)
+        layout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(layout)
+
+        # navázání akce na signál
+        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+        self._counter = 0
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("QMainWindow")
+
+        self.statusBar().showMessage("QMainWindow")
+
+        content = MainWindowContent()
+        self.setCentralWidget(content)
+
+        # jedna z variant naprogramování reakce na stisk tlačítka
+        content.counterButton.clicked.connect(self.counterClicked)
+
+    def counterClicked(self):
+        self._counter += 1
+        self.statusBar().showMessage(str(self._counter))
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
