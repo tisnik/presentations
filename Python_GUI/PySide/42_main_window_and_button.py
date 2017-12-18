60703b39fd15f8d7fd395e8dde50a45c2c7e81ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-18 10:43:30 +0100

    PySide example #41 - QMainWindow class + button on it

---
+++ Python_GUI/PySide/42_main_window_and_button.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("QMainWindow")
+
+        # tlačítko pro ukončení aplikace
+        quitButton = QtGui.QPushButton("Quit")
+
+        # vložení komponenty do okna
+        self.setCentralWidget(quitButton)
+
+        # navázání akce na stisk tlačítka pro ukončení aplikace
+        quitButton.clicked.connect(self.quit)
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+    def quit(self):
+        print("Closing...")
+        self.close()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
