8ddd8e1aaa8ccf8ab9281df29962de9d61b20a5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-22 10:32:49 +0100

    PySide example #49 - toolbar in the main window

---
+++ Python_GUI/PySide/49_toolbar.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle('QMainWindow + statusBar + toolBar')
+
+        # tlačítko Quit
+        quitAction = QtGui.QAction(QtGui.QIcon('icons/application-exit.png'),
+                                   '&Quit', self)
+        quitAction.triggered.connect(self.close)
+        quitAction.setStatusTip('Quit the application')
+        quitAction.setShortcut('Ctrl+Q')
+                                            
+        # tlačítko About
+        aboutAction = QtGui.QAction(QtGui.QIcon('icons/dialog-information.png'),
+                                    '&About', self)
+        aboutAction.triggered.connect(self.aboutDialog)
+        aboutAction.setStatusTip('About this application')
+        aboutAction.setShortcut('F1')
+
+        # nástrojový pruh
+        self.toolbar = self.addToolBar('title')
+
+        # přidání tlačítek na nástrojový pruh
+        self.toolbar.addAction(quitAction)
+        self.toolbar.addAction(aboutAction)
+
+        # zobrazení hlavního okna
+        self.show()
+
+    def aboutDialog(self):
+        msgBox = QtGui.QMessageBox()
+        msgBox.setText('About:\n...\n...\n...')
+        msgBox.setIcon(QtGui.QMessageBox.Information)
+        msgBox.exec_()
+        pass
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
