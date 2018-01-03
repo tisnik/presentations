c98589d5fff58ffb137ed1118233bd2f94903bf9
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-04 00:15:00 +0100

    Updated comments

--- Python_GUI/PySide/03_show_in_event_loop.py
+++ Python_GUI/PySide/03_show_in_event_loop.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8
 
 #
-#  (C) Copyright 2017  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -16,12 +16,15 @@
 # tento import je zapotřebí kvůli nutnosti zpracování parametrů
 # předávaných přes příkazový řádek
 import sys
+
+# používáme jen pro import funkce sleep
 import time
 
 # prozatím budeme využívat jen modul QtGui
 from PySide.QtGui import *
 
 
+# Uspani hlavniho vlakna aplikace na zadany pocet sekund
 def sleep(seconds):
     print('sleeping')
 
@@ -44,8 +47,14 @@ print('before window.show()')
 window.show()
 
 print('after window.show()')
+
+# pockame 5 sekund pred zobrazenim okna v hlavni smycce
 sleep(5)
 
 
 # vstup do smyčky událostí (event loop)
 sys.exit(app.exec_())
+
+# do tohoto místa se program teoreticky dostane až PO uzavření
+# hlavního okna, ve skutečnosti však skončí už na sys.exit()
+print("Finishing")
