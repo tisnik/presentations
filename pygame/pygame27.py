86f558ddf5fca0df3eed2149b6e4d2d9c4428a21
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-16 21:33:46 +0100

    Fixed pylint warnings

--- pygame/pygame27.py
+++ pygame/pygame27.py
@@ -46,11 +46,26 @@ image_height = image_surface.get_height()
 scale_ratio = 3
 
 # Vytvoření zvětšených obrázků
-horizontally_scaled_image        = pygame.transform.scale(image_surface, (int(image_width*scale_ratio), image_height))
-horizontally_smooth_scaled_image = pygame.transform.smoothscale(image_surface, (int(image_width*scale_ratio), image_height))
-vertically_scaled_image          = pygame.transform.scale(image_surface, (image_width, int(image_height*scale_ratio)))
-vertically_smooth_scaled_image   = pygame.transform.smoothscale(image_surface, (image_width, int(image_height*scale_ratio)))
-smooth_scaled_image              = pygame.transform.smoothscale(image_surface, (int(image_width*scale_ratio), int(image_height*scale_ratio)))
+horizontally_scaled_image = \
+    pygame.transform.scale(image_surface,
+                           (int(image_width*scale_ratio), image_height))
+
+horizontally_smooth_scaled_image = \
+    pygame.transform.smoothscale(image_surface,
+                                 (int(image_width*scale_ratio), image_height))
+
+vertically_scaled_image = \
+    pygame.transform.scale(image_surface,
+                           (image_width, int(image_height*scale_ratio)))
+
+vertically_smooth_scaled_image = \
+    pygame.transform.smoothscale(image_surface,
+                                 (image_width, int(image_height*scale_ratio)))
+
+smooth_scaled_image = \
+    pygame.transform.smoothscale(image_surface,
+                                 (int(image_width*scale_ratio),
+                                  int(image_height*scale_ratio)))
 
 # Přímé vykreslení původního obrázku
 display.blit(image_surface, (10, 10))
@@ -65,13 +80,14 @@ display.blit(smooth_scaled_image, (170, 170))
 # Načtení fontu (zadává se soubor se jménem fontu a velikost
 font = pygame.font.Font("fonts/FreeSans.ttf", 40)
 
-WHITE   = (255, 255, 255)
-RED     = (255,   0,   0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 # Vytvoření obrázku s vykresleným textem
 # - první parametr obsahuje řetězec, který se má vykreslit
 # - druhý parametr řídí použití antialiasingu
 # - třetí parametr volí barvu fontu
-font_surface1 = font.render(pygame.transform.get_smoothscale_backend(), True, WHITE, RED)
+font_surface1 = font.render(pygame.transform.get_smoothscale_backend(),
+                            True, WHITE, RED)
 
 # Vykreslení obrázku s nápisem do bufferu
 display.blit(font_surface1, (15, 100))
