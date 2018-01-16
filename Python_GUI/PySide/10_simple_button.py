12fbc23c963f224f80698949241344732bd41a72
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-16 21:11:44 +0100

    Updated comments

--- Python_GUI/PySide/10_simple_button.py
+++ Python_GUI/PySide/10_simple_button.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
+#
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+# tento import je zapotřebí kvůli nutnosti zpracování parametrů
+# předávaných přes příkazový řádek
 import sys
 
 # prozatím budeme využívat jen modul QtGui
