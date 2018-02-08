eb19ad8c6079ba7ef91b81cc1170d1f233af898b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-08 10:55:25 +0100

    Added comments

--- Python_GUI/PySide/18_hbox_layout.py
+++ Python_GUI/PySide/18_hbox_layout.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
+#
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+# tento import je zapotřebí kvůli nutnosti zpracování parametrů
+# předávaných přes příkazový řádek
 import sys
 
 # import "jádra" frameworku Qt i modulu pro GUI
@@ -8,7 +22,7 @@ from PySide import QtCore
 from PySide import QtGui
 
 
-# nový widget bude odvozen od obecného widgetu
+# nový widget bude odvozen od obecného widgetu QWidget
 class MainWindow(QtGui.QWidget):
 
     def __init__(self):
