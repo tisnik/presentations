cb2d01a350340ca60fa6bc2c4f29cace0c4bdd55
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-12 08:39:36 +0100

    PySide example #105 - QTreeWidget

---
+++ Python_GUI/PySide/105_qtree_widget.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindowContent(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindowContent, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # vytvoření widgetů vkládaných do okna
+        self.tree = self.prepareTree()
+        quitButton = self.prepareQuitButton()
+
+        # vytvoření správce geometrie
+        layout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        layout.addWidget(self.tree)
+        layout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(layout)
+
+    def prepareQuitButton(self):
+        # tlačítko
+        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton.resize(quitButton.sizeHint())
+
+        # navázání akce na signál
+        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        return quitButton
+
+    def prepareTree(self):
+        # vytvoření stromu
+        tree = QtGui.QTreeWidget(self)
+        tree.setHeaderLabel("strom")
+        tree.setColumnCount(1)
+        items = []
+        for i in range(1, 11):
+            item = QtGui.QTreeWidgetItem(None, ["prvek #{i}".format(i=i)])
+            items.append(item)
+        tree.insertTopLevelItems(0, items)
+
+        return tree
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        self.resize(450, 450)
+        self.setWindowTitle("QTreeWidget")
+
+        # vložení komponenty do okna
+        self.setCentralWidget(MainWindowContent())
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
