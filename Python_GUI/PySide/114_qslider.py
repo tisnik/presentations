25652eab460f123ff7ece3f87ddacc18d5ccf2ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-19 09:18:31 +0100

    PySide example #114

---
+++ Python_GUI/PySide/114_qslider.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindowContent(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindowContent, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # vytvoření widgetů vkládaných do okna
+        self.label = QtGui.QLabel("")
+        slider = self.prepareSlider()
+        quitButton = self.prepareQuitButton()
+
+        # vytvoření správce geometrie
+        layout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        layout.addWidget(self.label)
+        layout.addWidget(slider)
+        layout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(layout)
+
+    def prepareQuitButton(self):
+        # tlačítko
+        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton.resize(quitButton.sizeHint())
+
+        # navázání akce na signál
+        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        return quitButton
+
+    def prepareSlider(self):
+        # vytvoření slideru
+        slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        slider.valueChanged.connect(self.onSliderValueChanged)
+
+        return slider
+
+    def onSliderValueChanged(self, value):
+        text = "Slider value: {v}".format(v=value)
+        self.label.setText(text)
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        self.resize(450, 300)
+        self.setWindowTitle("QSlider")
+
+        # vložení komponenty do okna
+        self.setCentralWidget(MainWindowContent())
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
