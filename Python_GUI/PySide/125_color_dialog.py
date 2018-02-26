4e2a478df8726a751a2fa12dc16611e6a5dd3b3b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-26 18:18:03 +0100

    PySide example #125

---
+++ Python_GUI/PySide/125_color_dialog.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindowContent(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindowContent, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        quitButton = self.prepareQuitButton()
+        colorDialogButton = self.prepareColorDialogButton()
+
+        # vytvoření správce geometrie
+        topLayout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        topLayout.addWidget(colorDialogButton)
+        topLayout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(topLayout)
+
+    def prepareColorDialogButton(self):
+        # tlačítko
+        colorDialogButton = QtGui.QPushButton('Select color', self)
+        colorDialogButton.resize(colorDialogButton.sizeHint())
+
+        # navázání akce na signál
+        colorDialogButton.clicked.connect(self.showColorDialog)
+        return colorDialogButton
+
+    def prepareQuitButton(self):
+        # tlačítko
+        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton.resize(quitButton.sizeHint())
+
+        # navázání akce na signál
+        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        return quitButton
+
+    def showColorDialog(self):
+        colorDialog = QtGui.QColorDialog()
+        colorDialog.setCurrentColor(QtGui.QColor("#aabbcc"))
+        result = colorDialog.exec_()
+
+        selected = colorDialog.selectedColor()
+        message = "Selected color: {r} {g} {b}\nClicked on: {c}".format(
+            r=selected.red(),
+            g=selected.green(),
+            b=selected.blue(),
+            c="Ok" if result == 1 else "Cancel")
+
+        self.showMessageBox(message)
+
+    def showMessageBox(self, text):
+        msgBox = QtGui.QMessageBox()
+        msgBox.setText(text)
+        msgBox.setIcon(QtGui.QMessageBox.Information)
+        msgBox.exec_()
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("QColorDialog")
+
+        # vložení komponenty do okna
+        self.setCentralWidget(MainWindowContent())
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
