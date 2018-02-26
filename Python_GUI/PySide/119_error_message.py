853c45dfd404ee012cd1c58cfc47ee814c6c4546
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-26 18:16:21 +0100

    PySide example #119

---
+++ Python_GUI/PySide/119_error_message.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindowContent(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindowContent, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        quitButton = self.prepareQuitButton()
+        errorMessageButton = self.prepareErrorMessageButton()
+
+        # vytvoření správce geometrie
+        topLayout = QtGui.QVBoxLayout()
+
+        # umístění widgetů do okna
+        topLayout.addWidget(errorMessageButton)
+        topLayout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(topLayout)
+
+    def prepareErrorMessageButton(self):
+        # tlačítko
+        errorMessageButton = QtGui.QPushButton('Error Message', self)
+        errorMessageButton.resize(errorMessageButton.sizeHint())
+
+        # navázání akce na signál
+        errorMessageButton.clicked.connect(self.showErrorMessage)
+        return errorMessageButton
+
+    def prepareQuitButton(self):
+        # tlačítko
+        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton.resize(quitButton.sizeHint())
+
+        # navázání akce na signál
+        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        return quitButton
+
+    def showErrorMessage(self):
+        msgBox = QtGui.QErrorMessage()
+        msgBox.showMessage(u'Chybová zpráva')
+        msgBox.exec_()
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("QErrorMessage")
+
+        # vložení komponenty do okna
+        self.setCentralWidget(MainWindowContent())
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
