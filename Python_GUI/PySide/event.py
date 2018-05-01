a6bc4f3b3d935abdcd94ef8a0e23534910ee406d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-01 09:48:17 +0200

    Event processing in PySide

---
+++ Python_GUI/PySide/event.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import sys
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+
+# nový widget bude odvozen od obecného widgetu
+class MainWindowContent(QtGui.QWidget):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindowContent, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # tlačítka, na které je navázán handler
+        quitButton = self.prepareQuitButton()
+
+        # vytvoření správců geometrie
+        topLayout = QtGui.QHBoxLayout()
+
+        # vložení widgetů do okna
+        topLayout.addWidget(quitButton)
+
+        # nastavení správce geometrie a vložení všech komponent do okna
+        self.setLayout(topLayout)
+
+    def event(self, ev):
+        if (ev.type() == QtCore.QEvent.Gesture):
+            print(ev)
+        return QtGui.QWidget.event(ev)
+
+    def prepareQuitButton(self):
+        # tlačítko s popisem
+        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton.resize(quitButton.sizeHint())
+
+        # navázání akce na signál
+        quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        return quitButton
+
+
+# nový widget bude odvozen od obecného hlavního okna
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        # zavoláme konstruktor předka
+        super(MainWindow, self).__init__()
+
+        # konfigurace GUI + přidání widgetu do okna
+        self.prepareGUI()
+
+    def prepareGUI(self):
+        # velikost není potřeba specifikovat
+        # self.resize(320, 240)
+        self.setWindowTitle("Custom Stylesheets")
+
+        # vložení komponenty do okna
+        self.setCentralWidget(MainWindowContent())
+
+    def run(self, app):
+        # zobrazení okna na obrazovce
+        self.show()
+        # vstup do smyčky událostí (event loop)
+        app.exec_()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    MainWindow().run(app)
+
+
+if __name__ == '__main__':
+    main()
