295e24095e9661986b699832873846666d587693
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-08 10:17:53 +0200

    PySide example #154 - setting SVG title and description

---
+++ Python_GUI/PySide/154_svg_title_and_description.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from PySide import QtCore, QtGui, QtSvg
+
+# vytvoření instance třídy QSvgGenerator
+generator = QtSvg.QSvgGenerator()
+
+# určení typu výstupu a nastavení
+# jména výsledného souboru
+generator.setFileName("test3.svg")
+
+# specifikace rozměrů SVG obrázku
+generator.setSize(QtCore.QSize(320, 240))
+
+# viditelný výřez
+generator.setViewBox(QtCore.QRect(0, 0, 320, 240))
+
+# nastavení titulku SVG obrázku
+generator.setTitle("SVG: test 3")
+
+# popis SVG obrázku
+generator.setDescription("third SVG example")
+
+# inicializace instance třídy QPainter
+painter = QtGui.QPainter()
+
+# začátek kreslení
+painter.begin(generator)
+
+# ihned poté ukončíme kreslení
+painter.end()
