0a61e1f3e0b394f54784cfc91ccc46eb5f7c6ade
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-16 08:51:43 +0200

    PySide example #163: loading UI form 3

---
+++ Python_GUI/PySide/163_load_ui_form_3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+# třída pro načtení formuláře
+from PySide import QtUiTools
+
+import sys
+
+
+def load_ui_form(filename):
+    ui_loader = QtUiTools.QUiLoader()
+    return ui_loader.load(filename)
+    return form
+
+
+def main():
+    QtGui.QApplication.setStyle("plastique")
+    app = QtGui.QApplication(sys.argv)
+    mainWindow = load_ui_form("form.ui")
+    print(mainWindow)
+
+    mainWindow.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
