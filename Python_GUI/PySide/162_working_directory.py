cdb08fdae2233ab0242e96c0010543b356775e72
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-16 08:51:14 +0200

    PySide example #162: changing working directory

---
+++ Python_GUI/PySide/162_working_directory.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+# import "jádra" frameworku Qt i modulu pro GUI
+from PySide import QtCore
+from PySide import QtGui
+
+# třída pro načtení formuláře
+from PySide import QtUiTools
+
+import sys
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ui_loader = QtUiTools.QUiLoader()
+    working_directory = ui_loader.workingDirectory()
+    print(working_directory.path())
+    print(working_directory.absolutePath())
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
