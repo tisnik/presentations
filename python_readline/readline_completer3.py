d82e50d68fc804edb456b18da684fde735c84de2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-05 08:39:08 +0200

    3rd version of readline-based completer

---
+++ python_readline/readline_completer3.py
@@ -0,0 +1,39 @@
+import readline
+
+
+WORDS = ("quit", "exit", "eval", "help")
+
+
+def completer(text, state):
+    # print(text, state)
+    matches = []
+    n = len(text)
+    for word in WORDS:
+        if text == word[:n]:
+            matches.append(word)
+    if len(matches) >= state:
+        return matches[state]
+    else:
+        return None
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+readline.set_completer(completer)
+readline.parse_and_bind("tab: complete")
+
+while True:
+    cmd = input("Command: ")
+    if cmd in {"quit", "Quit", "exit", "Exit"}:
+        break
+    elif cmd in {"help", "Help", "?"}:
+        show_help()
+    elif cmd == "eval":
+        print("42")
