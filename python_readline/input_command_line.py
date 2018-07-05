645bd62c56ab96f3c42842df9e71d991c528afae
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-05 08:38:03 +0200

    Usage of input() as command line

---
+++ python_readline/input_command_line.py
@@ -0,0 +1,17 @@
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+while True:
+    cmd = input("Command: ")
+    if cmd in {"q", "quit", "Quit", "exit", "Exit"}:
+        break
+    elif cmd in {"help", "Help", "?"}:
+        show_help()
+    elif cmd == "eval":
+        print("42")
