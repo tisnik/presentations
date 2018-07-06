d9f6283938b79f2d11d7ff8b0d62b65f029d5a28
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-06 09:42:17 +0200

    Prompt session in Emacs mode

---
+++ prompt_toolkit/prompt/prompt8_Emacs_mode.py
@@ -0,0 +1,25 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit import enums
+from prompt_toolkit.completion import WordCompleter
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+c = WordCompleter(["quit", "exit", "help", "eval"], ignore_case=True)
+s = PromptSession(completer=c, editing_mode=enums.EditingMode.EMACS)
+
+while True:
+    cmd = s.prompt("Command: ")
+    if cmd in {"quit", "Quit", "exit", "Exit"}:
+        break
+    elif cmd in {"help", "Help", "?"}:
+        show_help()
+    elif cmd == "eval":
+        print("42")
