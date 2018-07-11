4a2b511f2791c7c3b2e92e7c9f3ff1f9d95b2490
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-11 08:52:32 +0200

    External editor

---
+++ prompt_toolkit/prompt/prompt14_external_editor.py
@@ -0,0 +1,46 @@
+from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit import PromptSession
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+class CommandValidator(Validator):
+
+    def validate(self, document):
+        user_input = document.text
+
+        if user_input and not user_input.isalpha():
+            index = 0
+
+            for index, char in enumerate(user_input):
+                if not char.isalpha():
+                    break
+
+            msg = "Wrong character '{c}' on index {i}".format(c=char, i=index)
+            raise ValidationError(message=msg, cursor_position=index)
+
+
+s = PromptSession()
+
+while True:
+    try:
+        cmd = s.prompt("Command: ", validator=CommandValidator(),
+                       validate_while_typing=True,
+                       enable_open_in_editor=True)
+        if cmd in {"q", "quit", "Quit", "exit", "Exit"}:
+            break
+        elif cmd in {"help", "Help", "?"}:
+            show_help()
+        elif cmd == "eval":
+            print("42")
+    except KeyboardInterrupt:
+        continue
+    except EOFError:
+        break
