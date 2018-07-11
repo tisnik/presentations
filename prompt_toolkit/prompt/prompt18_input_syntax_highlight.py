037f13d37e04c5cae8563384a8a4c7465222bf2c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-11 08:53:28 +0200

    Syntax highligting

---
+++ prompt_toolkit/prompt/prompt18_input_syntax_highlight.py
@@ -0,0 +1,44 @@
+from pygments.lexers import PythonLexer
+
+from prompt_toolkit.styles import Style
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit import PromptSession
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+new_tui_style = Style.from_dict({
+    'rprompt': 'bg:#ff0066 #ffffff',
+    'bottom-toolbar': 'bg:#ffffff #333333 reverse',
+    'prompt': 'bg:#ansiyellow #000000',
+    })
+
+
+s = PromptSession()
+
+while True:
+    try:
+        cmd = s.prompt("Command: ",
+                       validate_while_typing=True,
+                       enable_open_in_editor=True,
+                       bottom_toolbar="Available commands: quit, exit, help, eval",
+                       rprompt="Don't panic!",
+                       style=new_tui_style,
+                       lexer=PygmentsLexer(PythonLexer))
+        if cmd in {"q", "quit", "Quit", "exit", "Exit"}:
+            break
+        elif cmd in {"help", "Help", "?"}:
+            show_help()
+        elif cmd == "eval":
+            print("42")
+    except KeyboardInterrupt:
+        continue
+    except EOFError:
+        break
