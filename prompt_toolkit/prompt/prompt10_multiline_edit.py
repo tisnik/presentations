a9d4c5b59a654fdb86aab9bbd3e0f63e062fbd1b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-11 08:51:27 +0200

    Multiline editor using prompt()

---
+++ prompt_toolkit/prompt/prompt10_multiline_edit.py
@@ -0,0 +1,23 @@
+from prompt_toolkit import PromptSession
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+s = PromptSession()
+
+while True:
+    cmd = s.prompt("Command: ", multiline=True)
+    print("Entered text: {}".format(cmd))
+    if cmd in {"q", "quit", "Quit", "exit", "Exit"}:
+        break
+    elif cmd in {"help", "Help", "?"}:
+        show_help()
+    elif cmd == "eval":
+        print("42")
