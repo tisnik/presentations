b6b3e68d9b4c9eb82eab30effe7b1de6f4d440d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-11 08:51:48 +0200

    Ctrl+C and Ctrl+D handling

---
+++ prompt_toolkit/prompt/prompt11_ctrl_c_ctrl_d.py
@@ -0,0 +1,27 @@
+from prompt_toolkit import PromptSession
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+s = PromptSession()
+
+while True:
+    try:
+        cmd = s.prompt("Command: ")
+        if cmd in {"q", "quit", "Quit", "exit", "Exit"}:
+            break
+        elif cmd in {"help", "Help", "?"}:
+            show_help()
+        elif cmd == "eval":
+            print("42")
+    except KeyboardInterrupt:
+        continue
+    except EOFError:
+        break
