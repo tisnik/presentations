a4a9d45e32056ea7522d1e9c6d8d77f951566f62
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-16 08:50:35 +0200

    Button dialog

---
+++ prompt_toolkit/dialogs/dialog5_button_dialog.py
@@ -0,0 +1,13 @@
+from prompt_toolkit.shortcuts import message_dialog, button_dialog
+
+response = button_dialog(
+    title='Tento program provedl neplatnou operaci',
+    text='Not ready reading drive A',
+    buttons=[
+        ('Abort', 'abort'),
+        ('Retry', 'retry'),
+        ('Fail', 'fail')])
+
+message_dialog(
+    title='Zadali jste volbu',
+    text=response)
