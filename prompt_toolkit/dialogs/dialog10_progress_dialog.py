f704d6f36793317a49d8b47c15650df8c4216dbe
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-16 08:52:32 +0200

    Progress bar dialog

---
+++ prompt_toolkit/dialogs/dialog10_progress_dialog.py
@@ -0,0 +1,16 @@
+from time import sleep
+from prompt_toolkit.shortcuts import message_dialog, progress_dialog
+
+
+def simple_callback(set_percentage_function, log_text_function):
+    for counter in range(0, 101, 5):
+        log_text_function("Pocitam: {counter}\n".format(counter=counter))
+        set_percentage_function(counter)
+        sleep(0.5)
+    sleep(2)
+
+
+response = progress_dialog(
+    title='Výpočet',
+    text='Probíhá výpočet, prosím čekejte',
+    run_callback=simple_callback)
