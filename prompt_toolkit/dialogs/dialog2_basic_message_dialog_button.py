947ae691ae03a355c9dc2b849b3b8d521e770e00
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-16 08:49:12 +0200

    Basic message dialog with custom buttons

---
+++ prompt_toolkit/dialogs/dialog2_basic_message_dialog_button.py
@@ -0,0 +1,6 @@
+from prompt_toolkit.shortcuts import message_dialog
+
+message_dialog(
+    title='Software failure',
+    text='Guru Meditation #12345678.ABCDEFFF\nPress ENTER to continue.',
+    ok_text='[Enter]')
