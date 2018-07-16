bf310d529f52a0f947e1b8113b171c590897b4dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-16 08:52:03 +0200

    Input dialog in password mode

---
+++ prompt_toolkit/dialogs/dialog8_password_mode.py
@@ -0,0 +1,15 @@
+from prompt_toolkit.shortcuts import message_dialog, input_dialog
+
+response = input_dialog(
+    title='Zadání hesla',
+    text='Heslo:',
+    password=True)
+
+if response is not None:
+    message_dialog(
+        title='Zadání hesla',
+        text='zadali jste: {password}'.format(password=response if response else 'nic :)'))
+else:
+    message_dialog(
+        title='Zadání hesla',
+        text='Heslo nebylo zadáno')
