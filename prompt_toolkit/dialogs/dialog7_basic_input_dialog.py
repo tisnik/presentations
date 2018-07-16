97ad53f3c79c51631e7e851d48586f7f441c4a13
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-16 08:51:32 +0200

    Basic input dialog

---
+++ prompt_toolkit/dialogs/dialog7_basic_input_dialog.py
@@ -0,0 +1,14 @@
+from prompt_toolkit.shortcuts import message_dialog, input_dialog
+
+response = input_dialog(
+    title='Zadání uživatelského jména',
+    text='Uživatelské jméno:')
+
+if response is not None:
+    message_dialog(
+        title='Zadání uživatelského jména',
+        text='zadali jste: {name}'.format(name=response if response else 'nic :)'))
+else:
+    message_dialog(
+        title='Zadání uživatelského jména',
+        text='Jméno nebylo zadáno')
