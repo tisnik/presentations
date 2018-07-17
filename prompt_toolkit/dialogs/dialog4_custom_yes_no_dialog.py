60eabe9aae6bf8bc027b157c621982a4d63d1dc7
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-17 07:51:49 +0200

    Fixed typo

--- prompt_toolkit/dialogs/dialog4_custom_yes_no_dialog.py
+++ prompt_toolkit/dialogs/dialog4_custom_yes_no_dialog.py
@@ -2,7 +2,7 @@ from prompt_toolkit.shortcuts import message_dialog, yes_no_dialog
 
 response = yes_no_dialog(
     title='Tento program provedl neplatnou operaci',
-    text='Nevíme, co se stalo, známe jen kód chybÿ́:\n#12345678.ABCDEFFF\n\nProvést restart?',
+    text='Nevíme, co se stalo, známe jen kód chyby:\n#12345678.ABCDEFFF\n\nProvést restart?',
     yes_text='Ano',
     no_text='Ne')
 
