e480489469d50618096ee1553c780e3785f31d85
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-18 20:42:54 +0200

    Full LaTeX output (the whole document)

---
+++ pygments/pygments04_basic_usage_latex_full_output.py
@@ -0,0 +1,10 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import LatexFormatter
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+"""
+
+print(highlight(code, PythonLexer(), LatexFormatter(full=True)))
