95e80a62062d052599fd4543ae5bd6c66c3d9e84
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-18 20:40:46 +0200

    Basic usage of Pygments - full page HTML export

---
+++ pygments/pygments02_basic_usage_full_html_output.py
@@ -0,0 +1,10 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+"""
+
+print(highlight(code, PythonLexer(), HtmlFormatter(full=True)))
