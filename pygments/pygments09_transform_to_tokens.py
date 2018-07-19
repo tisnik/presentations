3cc5d7e6a2d1d3a7685b8d30d39c77c6fb13e15c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:00:51 +0200

    Transform source code to stream of tokens

---
+++ pygments/pygments09_transform_to_tokens.py
@@ -0,0 +1,17 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import RawTokenFormatter
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+"""
+
+tokens = highlight(code, PythonLexer(), RawTokenFormatter())
+
+tokens = tokens.decode()
+
+for token in tokens.split("\n"):
+    foobar = token.split("\t")
+    if len(foobar) == 2:
+        print("{token:30}    {value}".format(token=foobar[0], value=foobar[1]))
