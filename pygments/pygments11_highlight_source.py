8653a7fad822136956d2aa11724a6374d3c8b09e
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:01:35 +0200

    Script to highlight source code

---
+++ pygments/pygments11_highlight_source.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from sys import argv, exit
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import TerminalFormatter
+
+if len(argv) <= 1:
+    print("Usage: pygments11_highlight_source.py FILENAME")
+    exit(1)
+
+with open(argv[1], 'r') as fin:
+    code = fin.read()
+    print(highlight(code, PythonLexer(), TerminalFormatter()))
