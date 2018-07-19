b16f9b0af1c90fd586de3b47a15b2eb0c97d523f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:07:03 +0200

    Custom (very simple) lexer

---
+++ pygments/pygments18_custom_lexer.py
@@ -0,0 +1,42 @@
+from pygments import highlight
+from pygments.lexer import RegexLexer
+from pygments.token import *
+from pygments.formatters import TerminalFormatter
+from pygments.filters import NameHighlightFilter
+
+
+class FooLangLexer(RegexLexer):
+    name = 'foolang'
+    aliases = ['foolang']
+    filenames = ['*.foolang']
+
+    tokens = {
+        'root': [
+            (r'\ *print', Name.Function),
+            (r'for', Keyword),
+            (r'while', Keyword),
+            (r'goto', Generic.Error),
+            (r'begin', Keyword),
+            (r'end', Keyword),
+            (r'.+', Generic.Normal),
+        ]
+    }
+
+
+code = """
+for i in range(1, 11)
+begin
+    print("Hello world!")
+end
+
+while i < 10
+begin
+    inc i
+    print(i)
+end
+
+goto 10
+"""
+
+
+print(highlight(code, FooLangLexer(), TerminalFormatter()))
