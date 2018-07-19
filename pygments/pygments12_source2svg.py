38c154b3343a026b11f017996bd51c0120c8ebbf
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:02:01 +0200

    Script to transform source code into SVG + examples

---
+++ pygments/pygments12_source2svg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from sys import argv, exit
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import SvgFormatter
+
+if len(argv) <= 1:
+    print("Usage: pygments12_source2svg.py FILENAME > DRAWING.svg")
+    exit(1)
+
+with open(argv[1], 'r') as fin:
+    code = fin.read()
+    print(highlight(code, PythonLexer(), SvgFormatter()))
