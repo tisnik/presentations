a326e0cd7b525acc41deb3fb394867539e8c3071
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:01:14 +0200

    Transform source code with bad syntax to stream of tokens

---
+++ pygments/pygments10_transform_to_tokens_bad_syntax.py
@@ -0,0 +1,18 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import RawTokenFormatter
+
+code = """
+range(1, "FDA") for while with i
+except for for i else
+    print("Hello world!")
+"""
+
+tokens = highlight(code, PythonLexer(), RawTokenFormatter())
+
+tokens = tokens.decode()
+
+for token in tokens.split("\n"):
+    foobar = token.split("\t")
+    if len(foobar) == 2:
+        print("{token:30}    {value}".format(token=foobar[0], value=foobar[1]))
