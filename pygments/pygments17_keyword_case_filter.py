04816d8750fe28461c46840fdd728c7b78ced452
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:06:35 +0200

    Usage of KeywordCase filter

---
+++ pygments/pygments17_keyword_case_filter.py
@@ -0,0 +1,59 @@
+from pygments import highlight
+from pygments.lexers.pascal import DelphiLexer
+from pygments.formatters import TerminalFormatter
+from pygments.filters import KeywordCaseFilter
+
+code = """
+procedure change(s:string);
+begin
+  s[10]:='a';
+end;
+
+const
+  c:packed array [0..1000] of char = 'staticky retezec';
+var
+  s1:string[10];
+  s2:string;
+  i:integer;
+begin
+  s1:='hello';
+  writeln('"', s1, '"');
+  s1:=s1+' world';
+  writeln('"', s1, '"');
+  for i:=0 to 1000 do begin
+      s2:=s2+'*';
+  end;
+  change(s2);
+  writeln(s2);
+end.
+"""
+
+
+print(highlight(code, DelphiLexer(), TerminalFormatter()))
+
+input()
+
+print("-----------------------")
+
+lexer = DelphiLexer()
+lexer.add_filter(KeywordCaseFilter(case='lower'))
+
+print(highlight(code, lexer, TerminalFormatter()))
+
+input()
+
+print("-----------------------")
+
+lexer = DelphiLexer()
+lexer.add_filter(KeywordCaseFilter(case='upper'))
+
+print(highlight(code, lexer, TerminalFormatter()))
+
+input()
+
+print("-----------------------")
+
+lexer = DelphiLexer()
+lexer.add_filter(KeywordCaseFilter(case='capitalize'))
+
+print(highlight(code, lexer, TerminalFormatter()))
