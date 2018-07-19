593b8c7e4784559affd2cc61d9e26084598037fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:02:39 +0200

    Styles for ANSI terminal formatter

---
+++ pygments/pygments14_ansi_terminal_formatter_style.py
@@ -0,0 +1,28 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import Terminal256Formatter
+from pygments.style import Style
+from pygments.token import Keyword, Name, Comment, String, Error, \
+     Number, Operator, Generic
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+"""
+
+print(highlight(code, PythonLexer(), Terminal256Formatter()))
+
+print("-----------------------")
+
+
+class NewStyle(Style):
+    default_style = ""
+    styles = {
+        Comment:                'italic #ansidarkgray',
+        Keyword:                'underline #ansired',
+        Name.Builtin:           'bold #ansiyellow',
+        String:                 '#ansilightgray'
+    }
+
+
+print(highlight(code, PythonLexer(), Terminal256Formatter(style=NewStyle)))
