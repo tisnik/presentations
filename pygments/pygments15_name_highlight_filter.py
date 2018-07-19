f8989205cd88543d3905cd4752be3d699535b761
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-19 08:03:16 +0200

    Usage of NameHighlightFilter

---
+++ pygments/pygments15_name_highlight_filter.py
@@ -0,0 +1,30 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import TerminalFormatter
+from pygments.filters import NameHighlightFilter
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+if x and y:
+    print("yes")
+if x or y:
+    print("dunno")
+if x xor y:
+    print("different")
+goto 10
+"""
+
+
+print(highlight(code, PythonLexer(), TerminalFormatter()))
+
+print("-----------------------")
+
+lexer = PythonLexer()
+
+# pridani filtru
+lexer.add_filter(NameHighlightFilter(
+    names=['xor', 'goto']
+))
+
+print(highlight(code, lexer, TerminalFormatter()))
