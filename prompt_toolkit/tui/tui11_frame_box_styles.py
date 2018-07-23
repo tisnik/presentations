3d02b63d46501f8f9046d21ff03276902053c543
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-23 09:19:34 +0200

    Settings styles for frame and box

---
+++ prompt_toolkit/tui/tui11_frame_box_styles.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from prompt_toolkit import Application, HTML
+from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout import Layout, HSplit, VSplit
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.widgets import *
+
+
+# naformátovaná zpráva
+message = HTML("<ansibrightred>Hello</ansibrightred> " +
+               "<ansibrightyellow>world!</ansibrightyellow>")
+
+# widget
+widget = Label(message)
+
+# struktura obrazovky
+root = Box(Frame(widget, title="About:", style="bg:#ansiblue #ansiwhite"),
+           padding=2)
+
+# správce rozvržení
+layout = Layout(container=root)
+
+# napojení na klávesové zkratky
+key_bindings = KeyBindings()
+
+
+@key_bindings.add('escape')
+def on_escape_press(event):
+    """Callback funkce volaná při stisku klávesy Esc."""
+    print("\n\n[escape]\n\n")
+    event.app.exit()
+
+
+def main():
+    # vytvoření aplikace s textovým uživatelským rozhraním
+    application = Application(layout=layout,
+                              key_bindings=key_bindings,
+                              full_screen=True)
+
+    # spuštění aplikace
+    application.run()
+
+
+if __name__ == '__main__':
+    main()
