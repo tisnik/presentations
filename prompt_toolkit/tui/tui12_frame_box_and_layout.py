bf0acdc7c011b8b47d785713345ddfccb71b5b96
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-23 09:19:48 +0200

    Frame and Box layout

---
+++ prompt_toolkit/tui/tui12_frame_box_and_layout.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from prompt_toolkit import Application, HTML
+from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout import Layout, HSplit, VSplit
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.widgets import *
+
+
+# naformátované zprávy
+message1 = HTML("<ansibrightred>Hello</ansibrightred>")
+message2 = HTML("<ansibrightyellow>world!</ansibrightyellow>")
+message3 = HTML("<ansibrightyellow>(Esc to quit)</ansibrightyellow>")
+
+# widgety
+widget1 = Label(message1)
+widget2 = Label(message2)
+widget3 = Label(message3)
+
+# styl rámce
+style = "bg:#ansiblue #ansiwhite"
+
+# správce rozvržení
+vsplit = HSplit([
+    VSplit([
+        Frame(widget1, style=style),
+        Frame(widget2, style=style)]),
+    Frame(widget3, style=style)])
+layout = Layout(vsplit)
+
+# napojení na klávesové zkratky
+key_bindings = KeyBindings()
+
+
+@key_bindings.add('escape')
+def on_escape_press(event):
+    """Callback funkce volaná při stisku klávesy Esc."""
+    print("\n\n[escape]\n\n")
+    event.app.exit()
+
+
+def main():
+    # vytvoření aplikace s textovým uživatelským rozhraním
+    application = Application(layout=layout,
+                              key_bindings=key_bindings,
+                              full_screen=True)
+
+    # spuštění aplikace
+    application.run()
+
+
+if __name__ == '__main__':
+    main()
