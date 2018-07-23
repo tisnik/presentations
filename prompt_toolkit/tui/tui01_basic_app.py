66ef8554a7ceff48a6e755166cd9745e21764220
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-23 09:16:54 +0200

    A basic TUI application

---
+++ prompt_toolkit/tui/tui01_basic_app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from prompt_toolkit import Application
+
+
+# vytvoření aplikace s textovým uživatelským rozhraním
+application = Application()
+
+# spuštění aplikace
+application.run()
