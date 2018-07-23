6f028c9b2b0f54c7bae34cdee6d81e05914330e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-23 09:19:22 +0200

    Frame and Box control items

---
+++ prompt_toolkit/tui/tui10_frame_box.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from prompt_toolkit import Application, HTML
+from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout import Layout, HSplit, VSplit
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.widgets import *
+
+
+# naformátovaná zpráva
+message = HTML("<ansired>Hello</ansired> <ansiblue>world!</ansiblue>")
+
+# widget
+widget = Label(message)
+
+# struktura obrazovky
+root = Box(Frame(widget))
+
+# správce rozvržení
+layout = Layout(container=root)
+
+# napojení na klávesové zkratky
+key_bindings = KeyBindings()
+
+
+@key_bindings.add('escape')
+def on_escape_press(event):
+    """Callback funkce volaná při stisku klávesy Esc."""
+    print("\n\n[escape]\n\n")
+    event.app.exit()
+
+
+def main():
+    # vytvoření aplikace s textovým uživatelským rozhraním
+    application = Application(layout=layout,
+                              key_bindings=key_bindings,
+                              full_screen=True)
+
+    # spuštění aplikace
+    application.run()
+
+
+if __name__ == '__main__':
+    main()
