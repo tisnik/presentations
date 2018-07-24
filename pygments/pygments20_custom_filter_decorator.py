84a8458280591d9a2604190467509e257be00fc3
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-24 09:18:43 +0200

    Filter created via decorator

---
+++ pygments/pygments20_custom_filter_decorator.py
@@ -0,0 +1,50 @@
+import re
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import TerminalFormatter
+from pygments.filter import simplefilter
+from pygments.token import Name
+from pygments.util import get_bool_opt
+
+
+@simplefilter
+def to_snake_case(self, lexer, stream, options):
+    for ttype, value in stream:
+        if ttype is Name.Function or ttype is Name:
+            results = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', value)
+            value = re.sub('([a-z0-9])([A-Z])', r'\1_\2', value).lower()
+        yield ttype, value
+
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+
+if x and y:
+    print("yes")
+
+if x or y:
+    print("dunno")
+
+globalVariable = 42
+
+def helloWorld():
+    print("Hello world!")
+
+helloWorld()
+
+print(globalVariable)
+"""
+
+
+print(highlight(code, PythonLexer(), TerminalFormatter()))
+
+print("-----------------------")
+
+lexer = PythonLexer()
+
+# pridani filtru
+lexer.add_filter(to_snake_case())
+
+print(highlight(code, lexer, TerminalFormatter()))
