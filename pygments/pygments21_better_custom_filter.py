70f38f0a1d8e613d6c8ccda9ca64065a9d17b0f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-24 09:19:28 +0200

    Small fixes in custom filter

---
+++ pygments/pygments21_better_custom_filter.py
@@ -0,0 +1,54 @@
+import re
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import TerminalFormatter
+from pygments.filter import simplefilter
+from pygments.token import Name
+from pygments.util import get_bool_opt
+
+
+def name_to_snake_case(name):
+    results = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', results).lower()
+
+
+@simplefilter
+def to_snake_case(self, lexer, stream, options):
+    for ttype, value in stream:
+        if ttype is Name.Function or ttype is Name:
+            value = name_to_snake_case(value)
+        yield ttype, value
+
+
+code = """
+for i in range(1, 11):
+    print("Hello world!")
+
+if x and y:
+    print("yes")
+
+if x or y:
+    print("dunno")
+
+globalVariable = 42
+
+def helloWorld():
+    print("Hello world!")
+
+helloWorld()
+
+print(globalVariable)
+"""
+
+
+print(highlight(code, PythonLexer(), TerminalFormatter()))
+
+print("-----------------------")
+
+lexer = PythonLexer()
+
+# pridani filtru
+lexer.add_filter(to_snake_case())
+
+print(highlight(code, lexer, TerminalFormatter()))
