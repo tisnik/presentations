60817f01e48290947ef5df4b545ac94ffd88d05f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-25 09:43:06 +0200

    How to use 'bygroups'

---
+++ pygments/pygments25_bygroups.py
@@ -0,0 +1,52 @@
+import re
+from pygments import highlight
+from pygments.lexer import *
+from pygments.token import *
+from pygments.style import Style
+from pygments.formatters import Terminal256Formatter
+from pygments.filters import NameHighlightFilter
+
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+
+# viz http://pygments.org/docs/lexerdevelopment/
+class IniFileLexer(RegexLexer):
+    name = 'INI'
+    aliases = ['ini', 'cfg']
+    filenames = ['*.ini', '*.cfg']
+
+    tokens = {
+        'root': [
+            (r'\s+', Text),
+            (r';.*?$', Comment),
+            (r'\[.*?\]$', Keyword),
+            (r'(.*?)(\s*)(=)(\s*)(.*?)$',
+             bygroups(Name.Attribute, Text, Operator, Text, String))
+        ]
+    }
+
+
+code = """
+; komentar
+
+[sekce]
+x=10
+y=20
+
+[dalsi-sekce]
+foo=bar
+"""
+
+
+class NewStyle(Style):
+    default_style = ""
+    styles = {
+        Comment:        '#888',
+        Text:           '#ansired',
+        Keyword:        '#88f',
+        Name.Attribute: 'nobold #ansiyellow',
+    }
+
+
+print(highlight(code, IniFileLexer(), Terminal256Formatter(style=NewStyle)))
