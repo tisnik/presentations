82718f8a5b49f4f677a5555703492d78c9af6be5
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-30 08:53:38 +0200

    Basic usage of checkox

---
+++ prompt_toolkit/tui/tui18_basic_checkbox.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from prompt_toolkit import Application
+from prompt_toolkit.layout import Layout
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.application.current import get_app
+from prompt_toolkit.widgets import Checkbox
+
+
+def exit_clicked():
+    get_app().exit()
+
+
+checkbox = Checkbox('Checkbox')
+
+layout = Layout(checkbox)
+
+# napojení na klávesové zkratky
+key_bindings = KeyBindings()
+
+
+@key_bindings.add('escape')
+def on_escape_press(event):
+    """Callback funkce volaná při stisku klávesy Esc."""
+    event.app.exit()
+
+
+def main():
+    # vytvoření aplikace s textovým uživatelským rozhraním
+    application = Application(layout=layout,
+                              key_bindings=key_bindings,
+                              full_screen=True)
+
+    # spuštění aplikace
+    application.run()
+
+
+if __name__ == '__main__':
+    main()
