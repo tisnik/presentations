ea8927117645265d4a3c28fb9e02220a7592488d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-07-31 09:08:01 +0200

    Normal usage of text area

---
+++ prompt_toolkit/tui/tui27_text_area.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from prompt_toolkit import Application
+from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout.controls import FormattedTextControl
+from prompt_toolkit.layout import Layout, HSplit, VSplit
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
+from prompt_toolkit.application.current import get_app
+from prompt_toolkit.widgets import *
+
+
+# napojení na klávesové zkratky
+key_bindings = KeyBindings()
+key_bindings.add('s-tab')(focus_previous)
+key_bindings.add('tab')(focus_next)
+
+
+@key_bindings.add('escape')
+def on_exit_selected(event=None):
+    """Callback funkce volaná při stisku klávesy Esc."""
+    get_app().exit()
+
+
+text_area = TextArea(focusable=True)
+
+file_menu = MenuItem("File", children=[MenuItem("Exit", handler=on_exit_selected)])
+menu = MenuContainer(text_area, menu_items=[file_menu])
+
+layout = Layout(menu, focused_element=text_area)
+
+
+def main():
+    # vytvoření aplikace s textovým uživatelským rozhraním
+    application = Application(layout=layout,
+                              key_bindings=key_bindings,
+                              full_screen=True)
+
+    # spuštění aplikace
+    application.run()
+
+
+if __name__ == '__main__':
+    main()
