a4a8cbb5db3f8bf3ac0dda0db9d444f1d5648daa
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-17 08:57:27 +0200

    Jedi example #2: attributes of completions object

---
+++ jedi/jedi02_completions_attributes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+anybody=True
+answer="42"
+an'''
+
+script = jedi.Script(src, 4, len('an'), '')
+
+completions = script.completions()
+
+for completion in completions:
+    print(completion.complete)
+
+print()
+
+for completion in completions:
+    print(completion.name)
