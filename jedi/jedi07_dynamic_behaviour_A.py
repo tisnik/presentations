b5ca4d4455e825f1c68dbb9d1560ed98f7978a4f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-17 08:59:20 +0200

    Jedi example #7: dynamic behaviour of Python language

---
+++ jedi/jedi07_dynamic_behaviour_A.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+if random.random() < 0.5:
+    def x():
+        return 1
+else:
+    def x():
+        return 2
+
+print(x())
+'''
+
+
+def print_definitions(source, line, column, module):
+    print("-"*40)
+
+    script = jedi.Script(source, line, column, module)
+
+    goto_definitions = script.goto_definitions()
+
+    if not goto_definitions:
+        print("not found")
+        return
+
+    for definition in goto_definitions:
+        print("{type} {name} in {module}.py:{line}".format(type=definition.type,
+                                                           name=definition.full_name,
+                                                           module=definition.module_name,
+                                                           line=definition.line))
+
+
+script = print_definitions(src, 9, 7, 'example.py')
