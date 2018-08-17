f6f2fb04efefdcebc22b099aa393e00ed18de927
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-17 08:57:02 +0200

    Jedi example #1: simple completions

---
+++ jedi/jedi01_simple_completions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+anybody=True
+answer="42"
+an'''
+
+script = jedi.Script(src, 4, len('an'), '')
+print(script.completions())
