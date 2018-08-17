0829f295366dbc1cd87116db21fc6671fa0128b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-17 08:58:20 +0200

    Jedi example #4: goto definitions

---
+++ jedi/jedi04_goto_definitions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+def x():
+    return 42
+
+print(x())
+print(y())
+'''
+
+script = jedi.Script(src, 5, 7, 'example.py')
+
+goto_definitions = script.goto_definitions()
+print(goto_definitions)
+
+print("-"*40)
+
+script = jedi.Script(src, 6, 7, 'example.py')
+
+goto_definitions = script.goto_definitions()
+print(goto_definitions)
