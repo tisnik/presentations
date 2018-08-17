c394b42721c0f5ca6086e7dba57ee8c72880654b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-17 08:58:59 +0200

    Jedi example #6: function redeclarations and goto definitions

---
+++ jedi/jedi06_function_redeclaration.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+def x():
+    return 1
+
+def x():
+    return 2
+
+def x():
+    return 3
+
+print(x())
+'''
+
+
+def print_definitions(source, line, column, module):
+    print("-"*40)
+
+    script = jedi.Script(source, line, column, module)
+
+    goto_definitions = script.goto_definitions()
+
+    if not goto_definitions:
+        print("not found")
+        return
+
+    for definition in goto_definitions:
+        print("{type} {name} in {module}.py:{line}".format(type=definition.type,
+                                                           name=definition.full_name,
+                                                           module=definition.module_name,
+                                                           line=definition.line))
+
+
+script = print_definitions(src, 11, 7, 'example.py')
