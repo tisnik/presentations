ead3d82e7207884e62ac5e2de9703f2ef1e16817
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-17 08:58:01 +0200

    Jedi example #3: completion objects docstrings

---
+++ jedi/jedi03_completions_docstrings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+def anagrams(word):
+    """Very primitive anagram generator."""
+    if len(word) < 2:
+        return word
+    else:
+        tmp = []
+        for i, letter in enumerate(word):
+            for j in anagrams(word[:i]+word[i+1:]):
+                tmp.append(j+letter)
+    return tmp
+
+anybody=True
+answer="42"
+an'''
+
+lines = src.count('\n')
+script = jedi.Script(src, lines+1, len('an'), '')
+
+completions = script.completions()
+
+for completion in completions:
+    print(completion.name)
+    print("-"*40)
+    print(completion.docstring())
+    print("\n"*3)
