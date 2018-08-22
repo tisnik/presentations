9b63af70a4091769d0de56caf5e65cbcc3a70c28
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-22 09:01:43 +0200

    Example #14: method signatures

---
+++ jedi/jedi14_method_signatures.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+class C1:
+    def foo(self):
+        return 0
+
+    def bar(self, x):
+        return 1
+
+    def baz(self, x,y):
+        return 2
+
+obj = C1()
+obj.foo()
+obj.bar(1)
+obj.baz(1,2)
+obj.baz(obj.foo(), obj.bar(1))'''
+
+
+def print_call_signatures(script):
+    call_signatures = script.call_signatures()
+
+    for call_signature in call_signatures:
+        print(call_signatures.__str__(), call_signature.index, call_signature.bracket_start)
+
+    print()
+
+
+lines = src.count('\n')
+
+script = jedi.Script(src, lines-2, len('obj.foo('), 'test.py')
+print_call_signatures(script)
+
+script = jedi.Script(src, lines-1, len('obj.bar(1'), 'test.py')
+print_call_signatures(script)
+
+script = jedi.Script(src, lines, len('obj.baz(1,2'), 'test.py')
+print_call_signatures(script)
+
+script = jedi.Script(src, lines+1, len('obj.baz(obj.foo(), obj.bar(1'), 'test.py')
+print_call_signatures(script)
