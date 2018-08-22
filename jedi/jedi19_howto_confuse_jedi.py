77a70cdc7aa252c568ad7b9bc945de56041a9ee1
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-22 09:03:20 +0200

    Example #19: how to confuse Jedi

---
+++ jedi/jedi19_howto_confuse_jedi.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import jedi
+
+src = '''
+class C1:
+    def foo(self):
+        """Function foo defined in class C1."""
+        return 1
+
+class C2:
+    def foo(self):
+        """Function foo defined in class C2."""
+        return 2
+
+class C3:
+    def foo(self):
+        """Function foo defined in class C3."""
+        return 2
+
+if True:
+    obj = C1()
+elif True:
+    obj = C2()
+else:
+    obj = C3()
+obj.fo'''
+
+
+def print_definitions(definitions):
+    if not definitions:
+        print("not found")
+        return
+
+    for definition in definitions:
+        print("{type} {name} in {module}.py:{line}".format(type=definition.type,
+                                                           name=definition.full_name,
+                                                           module=definition.module_name,
+                                                           line=definition.line))
+
+
+lines = src.count('\n')
+script = jedi.Script(src, lines+1, len('obj.fo'), 'test.py')
+
+completions = script.completions()
+
+for completion in completions:
+    print(completion.name)
+    print("-"*40)
+    definitions = completion.follow_definition()
+    print_definitions(definitions)
+    print(completion.docstring())
+    print("\n"*3)
