d43684625c471459e02b735660c5844cf4caa037
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-28 08:20:54 +0200

    Child cipher #1

---
+++ sifry/s1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_roman(n) {
+    while (n>0) {
+        if (n>=10) {
+            putchar('X');
+            n-=10;
+        }
+        else if (n==9) {
+            printf("IX");
+            n-=9;
+        }
+        else if (n>=5) {
+            putchar('V');
+            n-=5;
+        }
+        else if (n==4) {
+            printf("IV");
+            n-=4;
+        }
+        else {
+            putchar('I');
+            n--;
+        }
+    }
+}
+
+char *s="ABCDEFGHJKMNOPQRSTUWYZ";
+char *t="chteli byste zjistit jak to cele funguje ze";
+
+void main(void) {
+    int i;
+    char *c;
+    for (c=t;*c;c++) {
+        print_roman(*c-'a'+1);
+        int x=rand()%strlen(s);
+        putchar(s[x]);
+        /*putchar(' ');*/
+    }
+}
