aa3e09059e4ee8dddae66b0d1a0f9cfd3b734485
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-05 09:56:21 +0100

    Added comments

--- pyglet/02_empty_window_script.py
+++ pyglet/02_empty_window_script.py
@@ -5,8 +5,10 @@
 
 import pyglet
 
+# vytvoření okna
 window = pyglet.window.Window(width=640,
                               height=480,
                               caption="Pyglet library")
 
+# spuštění smyčky pro zpracování událostí
 pyglet.app.run()
