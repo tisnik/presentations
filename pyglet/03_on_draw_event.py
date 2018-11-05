cf3e268fe45214ec6b93ab9f57048bd270c0468b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-05 10:12:20 +0100

    Added comments

--- pyglet/03_on_draw_event.py
+++ pyglet/03_on_draw_event.py
@@ -5,7 +5,7 @@
 
 import pyglet
 
-
+# vytvoření okna
 window = pyglet.window.Window(width=640,
                               height=480,
                               caption="Pyglet library")
@@ -16,4 +16,5 @@ def on_draw():
     window.clear()
 
 
+# spuštění smyčky pro zpracování událostí
 pyglet.app.run()
