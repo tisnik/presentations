a907bc33e7c78ab240da8620bf072f7b941462ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-30 11:37:18 +0100

    Improved comments

--- pyglet/04_label.py
+++ pyglet/04_label.py
@@ -3,7 +3,7 @@
 
 # Demonstrační příklady využívající knihovnu Pyglet
 
-# všechny třídy a funkce jsou obsaženy v jediném modulu pyglet
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
 import pyglet
 
 # vytvoření okna
@@ -11,6 +11,7 @@ window = pyglet.window.Window(width=640,
                               height=480,
                               caption="Pyglet library")
 
+# textové návěští, které se má zobrazit v oknu
 label = pyglet.text.Label("Pyglet library",
                           font_name="Terminus",
                           font_size=36,
@@ -20,7 +21,7 @@ label = pyglet.text.Label("Pyglet library",
 
 @window.event
 def on_draw():
-    """Obsluha události."""
+    """Obsluha události - překreslení obsahu okna."""
     window.clear()
     label.draw()
 
