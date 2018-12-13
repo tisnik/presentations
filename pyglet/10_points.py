96ed94e776123d6a527ad016185eda242cc87432
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-13 09:19:26 +0100

    Added comments

--- pyglet/10_points.py
+++ pyglet/10_points.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8
 
+# Demonstrační příklady využívající knihovnu Pyglet
+
+# všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
 import pyglet
+
+# druhý import s funkcemi převzatými z OpenGL
 from pyglet.gl import *
 
+# vytvoření okna
 window = pyglet.window.Window(width=450,
                               height=350,
                               caption="Pyglet+OpenGL")
@@ -10,36 +17,37 @@ window = pyglet.window.Window(width=450,
 
 @window.event
 def on_draw():
+    """Obsluha události - překreslení obsahu okna."""
     glClear(GL_COLOR_BUFFER_BIT)
     glLoadIdentity()
 
     glDisable(GL_POINT_SMOOTH)                  # zakazani antialiasingu bodu
     glPointSize(1.0)                            # velikost vykreslovanych bodu je jeden pixel
-    glBegin(GL_POINTS) 
+    glBegin(GL_POINTS)
     for i in xrange(0, 10):                     # vykresleni prvni rady bodu ruzne barvy
         step = i/10.0
         glColor3f(step, 0.5, 1.0-step)          # zmena barvy uvnitr prikazovych "zavorek" glBegin()/glEnd()
-        glVertex2f(50.0+300.0*step, 50.0) 
-    glEnd() 
+        glVertex2f(50.0+300.0*step, 50.0)
+    glEnd()
 
     glDisable(GL_POINT_SMOOTH)                  # zakazani antialiasingu bodu
     for i in range(0, 10):                      # vykresleni druhe rady bodu ruzne velikosti a barvy
         step = i/10.0
         glColor3f(step, 0.5, 1.0-step)          # zmena barvy vne prikazovych "zavorek" glBegin()/glEnd()
         glPointSize(step*20.0+1.0)              # zmena velikosti vykreslovanych bodu
-        glBegin(GL_POINTS) 
-        glVertex2f(50.0+300.0*step, 100.0) 
-        glEnd() 
+        glBegin(GL_POINTS)
+        glVertex2f(50.0+300.0*step, 100.0)
+        glEnd()
 
     glEnable(GL_POINT_SMOOTH)                   # povoleni antialiasingu bodu
     for i in range(0, 10):                      # vykresleni treti rady bodu ruzne velikosti a barvy
         step = i/10.0
         glColor3f(step, 0.5, 1.0-step)          # zmena barvy vne prikazovych "zavorek" glBegin()/glEnd()
         glPointSize(step*20.0+1.0)              # zmena velikosti vykreslovanych bodu
-        glBegin(GL_POINTS) 
-        glVertex2f(50.0+300.0*step, 150.0) 
-        glEnd() 
+        glBegin(GL_POINTS)
+        glVertex2f(50.0+300.0*step, 150.0)
+        glEnd()
 
 
+# spuštění smyčky pro zpracování událostí
 pyglet.app.run()
-
