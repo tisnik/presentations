52959935b7f91b9a8045fcb676b447082f248907
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-01 12:04:22 +0100

    wxPython example #3: custom class derived from Frame

---
+++ Python_GUI/wxPython/03_custom_frame.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from wx import App, Frame, ID_ANY
+
+
+class MainFrame(Frame):
+    def __init__(self, parent, id, title):
+        # zavolání konstruktoru předka (kompatibilní s Pythonem 2.x)
+        super(MainFrame, self).__init__(parent, id, title, size=(320, 240))
+        # zobrazení hlavního okna aplikace
+        self.Show(True)
+
+
+# vytvoření instance objektu představujícího běžící aplikaci
+app = App()
+
+# vytvoření hlavního okna se specifikací jeho vlastností a titulku
+# okno se automaticky zobrazí
+frame = MainFrame(None, ID_ANY, "wxPython!")
+
+# vstup do smyčky pro čtení a zpracování událostí (event loop)
+app.MainLoop()
