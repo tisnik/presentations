2e3bd46b21e41a4438c464f1427dd589b4d9b08c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-01 12:03:49 +0100

    wxPython example #1: empty window (frame)

---
+++ Python_GUI/wxPython/01_empty_window.py
@@ -0,0 +1,15 @@
+# vim: set fileencoding=utf-8
+
+from wx import App, Frame, ID_ANY
+
+# vytvoření instance objektu představujícího běžící aplikaci
+app = App()
+
+# vytvoření hlavního okna se specifikací jeho vlastností a titulku
+frame = Frame(None, ID_ANY, "wxPython!")
+
+# zobrazení hlavního okna aplikace
+frame.Show(True)
+
+# vstup do smyčky pro čtení a zpracování událostí (event loop)
+app.MainLoop()
