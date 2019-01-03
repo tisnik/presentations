b9e89e889297c83ab5b7262a0a8e850f1569e181
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-03 09:51:56 +0100

    Example 6: on_press event

---
+++ Python_GUI/wxPython/06_on_press.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from wx import App, Frame, Panel, Button, ID_ANY, EVT_BUTTON
+
+
+def onButtonPress(event):
+    print("Pressed...")
+
+
+class MainFrame(Frame):
+    def __init__(self, parent, id, title):
+        # zavolání konstruktoru předka (kompatibilní s Pythonem 2.x)
+        super(MainFrame, self).__init__(parent, id, title, size=(320, 240))
+
+        # vytvoření panelu
+        panel = Panel(self, ID_ANY)
+
+        # vytvoření tlačítka s jeho vložením do panelu
+        x = y = 20
+        button = Button(panel, ID_ANY, 'Press me', (x, y))
+        button.Bind(EVT_BUTTON, onButtonPress)
+
+        # zobrazení hlavního okna aplikace
+        self.Show(True)
+
+
+# vytvoření instance objektu představujícího běžící aplikaci
+app = App()
+
+# vytvoření hlavního okna se specifikací jeho vlastností a titulku
+# okno se automaticky zobrazí
+frame = MainFrame(None, ID_ANY, "wxPython!")
+
+# vstup do smyčky pro čtení a zpracování událostí (event loop)
+app.MainLoop()
