31fcd40719e8528958002166e03ab66baf92d060
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-07 09:21:08 +0100

    Updated comments

--- pyglet/01_empty_window.py
+++ pyglet/01_empty_window.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8
 
+# ---------------------------------------------------------------------
+#
 # Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 1: vytvoření a zobrazení prázdného okna
+#
+# ---------------------------------------------------------------------
+
 
 # všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
 import pyglet
