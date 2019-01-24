ac986cc4e3039da2ad2faec9ec74d7b68722dd18
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-24 09:27:31 +0100

    Added comments

--- pyglet/09_primitives.py
+++ pyglet/09_primitives.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
+# ---------------------------------------------------------------------
+#
 # Demonstrační příklady využívající knihovnu Pyglet
+#
+# Příklad číslo 9: ukázka použití základních funkcí OpenGL
+#                  vykreslení primitiv podporovaných knihovnou OpenGL
+#
+# ---------------------------------------------------------------------
 
 # všechny třídy a funkce jsou obsaženy v jediném modulu nazvaném pyglet
 import pyglet
