67b32dba084704588d5c6ca28d8a41cdf7c33850
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-26 14:57:35 +0200

    Example #39: pointer to item of structure

---
+++ linuxdays2019/golang-workshop/sources/39-pointer-to-struct-item.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type User struct {
+	id      uint32
+	name    string
+	surname string
+}
+
+func main() {
+	var u User
+	u.id = 1
+	u.name = "Linus"
+	u.surname = "Torvalds"
+
+	fmt.Println(u)
+	fmt.Println("------------------")
+
+	var p_n *string
+	var p_s *string
+	p_n = &u.name
+	p_s = &u.surname
+
+	fmt.Println(p_n)
+	fmt.Println(p_s)
+	fmt.Println(*p_n)
+	fmt.Println(*p_s)
+	fmt.Println("------------------")
+
+	(*p_n) = "Rob"
+	(*p_s) = "Pike"
+	fmt.Println(*p_n)
+	fmt.Println(*p_s)
+}
