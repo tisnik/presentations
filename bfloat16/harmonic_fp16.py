acedfda3953819fb1951fe00abbf06637bec9172
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-10 08:46:40 +0100

    Harmonic serie using fp16

---
+++ bfloat16/harmonic_fp16.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+h1 = np.float16(0)
+h2 = np.float16(0)
+
+n = 1
+
+while True:
+    h2 = h1 + np.float16(1)/np.float16(n)
+    print(h1, h2, h2-h1, n)
+    if h1 == h2:
+        break
+    h1 = h2
+    n += 1
