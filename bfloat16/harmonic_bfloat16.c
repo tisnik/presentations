81739f43e48a661dd46af5aa83710b35e49f2fc9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-10 08:46:00 +0100

    Harmonic serie using bfloat16

---
+++ bfloat16/harmonic_bfloat16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+typedef union {
+    float f;
+    uint32_t i;
+} bfloat16mock;
+
+int main(void) {
+    long n = 1;
+    bfloat16mock h1;
+    bfloat16mock h2;
+
+    h1.f = 0.0;
+    h2.f = 0.0;
+
+    while (1) {
+        h2.f = h1.f + 1.0 / (float)n;
+        h2.i &= 0xffff8000;
+        printf("%f %f %10.8lf %ld\n", h1.f, h2.f, h2.f-h1.f, n);
+        if (h1.f == h2.f) break;
+        h1 = h2;
+        n++;
+    }
+    printf("%f %f %ld\n", h1.f, h2.f, n);
+
+    return 0;
+}
