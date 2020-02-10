eeabe8511f564e24d24ca3d2a249696c7bdd4a1d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-10 08:45:46 +0100

    Harmonic serie using float

---
+++ bfloat16/harmonic_float.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    long n = 1;
+    float h1 = 0.0, h2 = 0.0;
+
+    while (1) {
+        h2 = h1 + 1.0 / (float)n;
+        if (n%1000 == 0) {
+            printf("%f %f %10.8f %ld\n", h1, h2, h2-h1, n);
+        }
+        if (h1 == h2) break;
+        h1 = h2;
+        n++;
+    }
+    printf("%f %f %ld\n", h1, h2, n);
+
+    return 0;
+}
