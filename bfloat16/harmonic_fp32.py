939f64eea50caeb4b4f7ac5d18d798ebdb5cce7c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-02-10 08:46:36 +0100

    Harmonic serie using fp32

---
+++ bfloat16/harmonic_fp32.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+h1 = np.float32(0)
+h2 = np.float32(0)
+
+n = 1
+
+while True:
+    h2 = h1 + np.float32(1)/np.float32(n)
+    if n%1000 == 0:
+        print(h1, h2, h2-h1, n)
+    if h1 == h2:
+        break
+    h1 = h2
+    n += 1
+
+print(h1, h2, n)
