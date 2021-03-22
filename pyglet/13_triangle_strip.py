a92b945b111b7b264c5ca7cc2860568288215a63
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-22 09:15:02 +0100

    Fixed issue #58

--- pyglet/13_triangle_strip.py
+++ pyglet/13_triangle_strip.py
@@ -21,7 +21,7 @@ def draw_triangle_strip(x, y):
     glColor3f(0.0, 1.0, 0.0) 
     glVertex2i(x+100, y) 
     glColor3f(1.0, 1.0, 0.0)                 # kazdy vertex bude vykresleny jinou barvou
-    glVertex2i(x+150, y+80) 
+    glVertex2i(x+150, y+80)
     glColor3f(0.0, 1.0, 1.0)
     glVertex2i(x+200, y)
     glColor3f(1.0, 0.0, 1.0)
