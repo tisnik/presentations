88143ee5ee513f8a969370983ca176d42d6d8c5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-25 08:31:27 +0100

    Fixed issue #178

--- pyglet/22_z_buffer.py
+++ pyglet/22_z_buffer.py
@@ -143,7 +143,7 @@ def on_draw():
               0.0, 1.0,  0.0)                 # poloha "stropu" ve scene
 
     glRotatef(r1, 1.0, 0.0, 0.0)              # rotace objektu
-    glRotatef(r2, 0.0, 1.0, 0.0) 
+    glRotatef(r2, 0.0, 1.0, 0.0)
 
     draw_walls()
     draw_roof()
