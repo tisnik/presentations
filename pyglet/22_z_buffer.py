824e18efbe653504c199d49945f869c3933ec71d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-25 08:34:47 +0100

    Fixed issue #158

--- pyglet/22_z_buffer.py
+++ pyglet/22_z_buffer.py
@@ -61,7 +61,7 @@ def draw_walls():
     glVertex3f(-5.0,  5.0, -5.0)
 
     glColor3f(1.0, 1.0, 0.0)                  # zluta barva steny
-    glVertex3f( 5.0, -5.0, -5.0)
+    glVertex3f(5.0, -5.0, -5.0)
     glVertex3f(5.0, -5.0,  5.0)
     glVertex3f(5.0,  5.0,  5.0)
     glVertex3f(5.0,  5.0, -5.0)
