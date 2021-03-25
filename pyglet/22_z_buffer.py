113fbbcbe5f55a3adc7d32b67ca56a206180b032
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-25 08:33:53 +0100

    Fixed issue #165

--- pyglet/22_z_buffer.py
+++ pyglet/22_z_buffer.py
@@ -75,7 +75,7 @@ def draw_roof():
     glVertex3f(-5.0,  5.0, -5.0)
     glVertex3f( 5.0,  5.0, -5.0)
     glVertex3f( 0.0, 11.0,  0.0)
- 
+
     glColor3f(1.0, 0.0, 1.0)
     glVertex3f(5.0,  5.0, -5.0)
     glVertex3f(5.0,  5.0,  5.0)
