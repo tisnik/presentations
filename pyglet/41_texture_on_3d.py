fe3c09a7db1b40a806d0a749fa67c02ebf371af9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-23 11:29:17 +0200

    Fixed issue #226

--- pyglet/41_texture_on_3d.py
+++ pyglet/41_texture_on_3d.py
@@ -181,7 +181,7 @@ def on_draw():
               0.0, 1.0,  0.0)                 # poloha "stropu" ve scene
 
     glRotatef(r1, 1.0, 0.0, 0.0)              # rotace objektu
-    glRotatef(r2, 0.0, 1.0, 0.0) 
+    glRotatef(r2, 0.0, 1.0, 0.0)
 
     if texturesEnabled:
         glEnable(GL_TEXTURE_2D)
