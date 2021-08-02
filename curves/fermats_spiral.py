63e87b6882ee75948c4a31069f6d6c4f74351d3b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-02 08:38:54 +0200

    Fermat's spiral

---
+++ curves/fermats_spiral.py
@@ -0,0 +1,27 @@
+"""Fermatova spirála."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# úhel v polárním grafu
+theta = np.arange(0.00, 20.0, 0.05)
+
+a = 1.0
+
+# funkce: vzdálenost od středu
+radius = a*np.sqrt(theta)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+
+# titulek grafu
+fig.suptitle('Fermatova spirála', fontsize=15)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(theta, radius, 'g-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("fermat_spiral.png")
+
+# zobrazení grafu
+plt.show()
