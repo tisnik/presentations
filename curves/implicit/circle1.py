562f1ba572e2b82f4b3092899f6a9bf3c2b04456
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-06 07:52:18 +0200

    Implicit curve: circle

---
+++ curves/implicit/circle1.py
@@ -0,0 +1,43 @@
+"""Kružnice zadaná implicitní funkcí."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-2, 2, 10)
+y = np.linspace(-2, 2, 10)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# implicitní funkce kružnice
+r = 1
+z = x**2 + y ** 2 - r ** 2
+
+# hodnota, která se má zvýraznit na isoploše
+levels = [0]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Kružnice', fontsize=15)
+
+# vykreslení implicitní funkce
+ax.contour(x, y, z, levels)
+
+# zobrazit mřížku
+ax.grid(True)
+
+# zachovat poměr stran
+ax.axis('scaled')
+
+# popisek os
+plt.xlabel('Osa x')
+plt.ylabel('Osa y')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("circle1.png")
+
+# zobrazení grafu
+plt.show()
