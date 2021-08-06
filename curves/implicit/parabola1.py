d52e6c8b17b094e37ebb4459af86b709a0392912
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-06 07:52:18 +0200

    Implicit curve: parabola

---
+++ curves/implicit/parabola1.py
@@ -0,0 +1,44 @@
+"""Parabola zadaná implicitní funkcí."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-4, 4, 50)
+y = np.linspace(0, 8, 50)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# implicitní funkce paraboly
+f = 0.5
+p = 2*f
+z = x**2 - p*p*y
+
+# hodnota, která se má zvýraznit na isoploše
+levels = [0]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Parabola', fontsize=15)
+
+# vykreslení implicitní funkce
+ax.contour(x, y, z, levels)
+
+# zobrazit mřížku
+ax.grid(True)
+
+# zachovat poměr stran
+ax.axis('scaled')
+
+# popisek os
+plt.xlabel('Osa x')
+plt.ylabel('Osa y')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("parabola1.png")
+
+# zobrazení grafu
+plt.show()
