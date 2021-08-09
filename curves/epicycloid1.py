1df38350db3879cb0419fe3f022832b0f94c6ff7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-09 08:13:08 +0200

    Epicycloid

---
+++ curves/epicycloid1.py
@@ -0,0 +1,34 @@
+"""Parametrická křivka: epicykloida."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty parametru t
+t = np.arange(0, 10*np.pi, 0.01)
+
+# parametry
+a = 8
+b = 5
+
+# výpočet bodů ležících na křivce
+x = (a+b)*np.cos(t)-b*np.cos((a/b+1)*t)
+y = (a+b)*np.sin(t)-b*np.sin((a/b+1)*t)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Epycykloida', fontsize=15)
+
+# určení rozsahů na obou souřadných osách
+ax.set_xlim(-32, 32)
+ax.set_ylim(-24, 24)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(x, y, 'g-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("epycyclodid1.png")
+
+# zobrazení grafu
+plt.show()
