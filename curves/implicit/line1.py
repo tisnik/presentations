1b376a0316fc7b09f8a2271a8a51353594bb8401
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-10 08:35:15 +0200

    Implicit curve: line

---
+++ curves/implicit/line1.py
@@ -0,0 +1,45 @@
+"""Úsečka zadaná implicitní funkcí."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-2, 2, 10)
+y = np.linspace(-2, 2, 10)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# implicitní funkce úsečky
+A = 1
+B = 1
+C = 0
+z = A*x + B*y + C
+
+# hodnota, která se má zvýraznit na isoploše
+levels = [0]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Úsečka', fontsize=15)
+
+# vykreslení implicitní funkce
+ax.contour(x, y, z, levels)
+
+# zobrazit mřížku
+ax.grid(True)
+
+# zachovat poměr stran
+ax.axis('scaled')
+
+# popisek os
+plt.xlabel('Osa x')
+plt.ylabel('Osa y')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("line1.png")
+
+# zobrazení grafu
+plt.show()
