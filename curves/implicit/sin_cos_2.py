3dc9d847c081da03314e9b94c04ff400bbbfafce
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-10 08:35:29 +0200

    Implicit curve: sin_cos

---
+++ curves/implicit/sin_cos_2.py
@@ -0,0 +1,42 @@
+"""Implicitně zadaná křivka."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# příprava vektorů pro konstrukci mřížky
+x = np.linspace(-5, 5, 150)
+y = np.linspace(-5, 5, 150)
+
+# konstrukce mřížky
+x, y = np.meshgrid(x, y)
+
+# implicitní funkce
+z = np.sin(x**2) - np.cos(y**2)
+
+# hodnoty, které se mají zvýraznit na isoploše
+levels = [-0.5, 0, 0.5]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Implicit curve', fontsize=15)
+
+# vykreslení implicitní funkce
+ax.contour(x, y, z, levels)
+
+# zobrazit mřížku
+ax.grid(True)
+
+# zachovat poměr stran
+ax.axis('scaled')
+
+# popisek os
+plt.xlabel('Osa x')
+plt.ylabel('Osa y')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("implicit3.png")
+
+# zobrazení grafu
+plt.show()
