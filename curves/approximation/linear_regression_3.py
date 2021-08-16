91635c405fb86f1051e375f4b05ada29f1fbf145
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-16 08:56:50 +0200

    Linear regression

---
+++ curves/approximation/linear_regression_3.py
@@ -0,0 +1,40 @@
+"""Vykreslení sady (pseudo)náhodných dat + lineární regrese."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty na x-ové ose
+x = np.arange(0, 10)
+
+# generátor pseudonáhodných dat
+rng = np.random.default_rng(seed=42)
+
+# hodnoty na y-ové ose
+y = x + 20*rng.random((len(x))) - 10
+
+# výpočet lineární regrese
+coefficients = np.polyfit(x, y, 1)
+
+# koeficienty úsečky
+print(coefficients)
+
+# konstrukce lineární funkce
+poly1d_fn = np.poly1d(coefficients)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Lineární regrese', fontsize=15)
+
+# vrcholy na křivce
+ax.plot(x, y, 'go')
+
+# vykreslení interpolační křivky
+plt.plot(poly1d_fn(np.arange(0, len(x))), 'r-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("linear_regression_3.png")
+
+# zobrazení grafu
+plt.show()
