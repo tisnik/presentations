1d67212d15d1a037f8fab11f528348329c699115
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-16 08:56:51 +0200

    Bezier quadratic curve

---
+++ curves/parametric/Bezier_kvadric.py
@@ -0,0 +1,45 @@
+"""Parametrická křivka: Bézierova kvadrika."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty parametru t
+t = np.arange(0, 1.05, 0.05)
+
+# řídicí body Bézierovy kubiky
+xc = (1, 2, 3)
+yc = (1, 2.9, 1)
+
+# Bernsteinovy polynomy
+B = [(1-t)**2,
+     2*t*(1-t),
+     t**2]
+
+# výpočet bodů ležících na Bézierově kvadrice
+x = 0
+y = 0
+for i in range(0, 3):
+    x += xc[i]*B[i]
+    y += yc[i]*B[i]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Bézierova kvadrika', fontsize=15)
+
+# určení rozsahů na obou souřadných osách
+ax.set_xlim(0, 4)
+ax.set_ylim(0, 3)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(x, y, 'g-')
+
+# řídicí body Bézierovy kvadriky
+ax.plot(xc, yc, 'ro')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("bezier_quadric.png")
+
+# zobrazení grafu
+plt.show()
