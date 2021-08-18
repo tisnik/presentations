631bd8122750e59887319eee5411c764ed0a7701
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-18 11:03:20 +0200

    Fixed issue #119

--- pyglet/20_fills.py
+++ pyglet/20_fills.py
@@ -75,7 +75,7 @@ def on_draw():
     glColor3f(1.0, 0.0, 1.0)
     glVertex3f(-5.0,  5.0, -5.0)
     glVertex3f(-5.0,  5.0,  5.0)
-    glVertex3f( 5.0,  5.0,  5.0)
+    glVertex3f(5.0,  5.0,  5.0)
     glVertex3f(5.0,  5.0, -5.0)
 
     glColor3f(1.0, 1.0, 0.0)
