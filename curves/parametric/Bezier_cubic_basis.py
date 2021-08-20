ca7633b1c156b9e16886cca7936142705ae9cd5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-20 13:42:35 +0200

    Basis polynoms for Bezier cubic

---
+++ curves/parametric/Bezier_cubic_basis.py
@@ -0,0 +1,38 @@
+"""Bázové polynomy Bézierovy kubiky."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# hodnoty parametru t
+t = np.arange(0, 1.05, 0.02)
+
+# Bernsteinovy polynomy
+B = [1 * (1-t)**3,
+     3 * t * (1-t)**2,
+     3 * t**2 * (1-t),
+     1 * t**3]
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Bázové polynomy', fontsize=15)
+
+# určení rozsahů na obou souřadných osách
+ax.set_xlim(0, 1)
+ax.set_ylim(0, 1)
+
+# bázové polynomy
+ax.plot(t, B[0], 'r-', label='b0,3')
+ax.plot(t, B[1], 'g-', label='b1,3')
+ax.plot(t, B[2], 'b-', label='b2,3')
+ax.plot(t, B[3], 'k-', label='b3,3')
+
+# zobrazení legendy
+ax.legend()
+
+# uložení grafu do rastrového obrázku
+plt.savefig("bezier_cubic_basis.png")
+
+# zobrazení grafu
+plt.show()
