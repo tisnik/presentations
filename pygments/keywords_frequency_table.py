0b829e675d4762fae92c7f8ebefb85a2bdc4fe02
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-20 11:02:12 +0200

    Fixed issue #6

--- pygments/keywords_frequency_table.py
+++ pygments/keywords_frequency_table.py
@@ -30,7 +30,9 @@ def keywords_for_file(filename):
     keywords = []
     for token in tokens:
         t_type = token[0]
-        if t_type is Token.Keyword or t_type in Token.Keyword.Namespace or t_type in Token.Keyword.Declaration:
+        if t_type is Token.Keyword or \
+                t_type in Token.Keyword.Namespace or \
+                t_type in Token.Keyword.Declaration:
             keywords.append(token[1])
     return keywords
 
