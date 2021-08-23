d78cb40a138a4bc7fb2f190eeb6a27cc738f95b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-23 09:31:55 +0200

    Sum of basic polynoms for linear interpolation

---
+++ curves/parametric/linear_basis_sum.py
@@ -0,0 +1,25 @@
+"""Bázové polynomy lineární interpolace."""
+
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+
+# hodnoty parametru t
+t = np.arange(0, 1.01, 0.10)
+
+# Bernsteinovy polynomy pro lineární interpolaci
+B = [1 * (1-t),
+     1 * t]
+
+# vytvoření datového rámce pro uložení hodnot Bernsteinových polynomů
+df = pd.DataFrame(index=t, columns=["b0,1", "b1,1", "sum"])
+
+# inicializace jednotlivých sloupců datového rámce
+df["b0,1"] = B[0]
+df["b1,1"] = B[1]
+
+# součet hodnot Bernsteinových polynomů
+df["sum"] = B[0]+B[1]
+
+# vytištění obsahu datového rámce
+print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
