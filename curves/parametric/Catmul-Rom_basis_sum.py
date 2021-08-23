190a72cccfcf1b956c939300f105d05d64fd6882
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-23 09:31:55 +0200

    Sum of basic polynoms for Catmul-Rom spline

---
+++ curves/parametric/Catmul-Rom_basis_sum.py
@@ -0,0 +1,32 @@
+"""Bázové polynomy Catmul-Romovy spline."""
+
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+
+# hodnoty parametru t
+t = np.arange(0, 1.01, 0.10)
+
+# koeficient Catmul-Romovy spline
+tau = 0.5
+
+# bázové polynomy
+Q = [-tau*t + 2*tau*t**2 - tau*t**3,
+     1+(tau-3)*t**2+(2-tau)*t**3,
+     tau*t + (3-2*tau)*t**2 + (tau-2)*t**3,
+     -tau*t**2+tau*t**3]
+
+# vytvoření datového rámce pro uložení hodnot polynomů
+df = pd.DataFrame(index=t, columns=["q0", "q1", "q2", "q3", "sum"])
+
+# inicializace jednotlivých sloupců datového rámce
+df["q0"] = Q[0]
+df["q1"] = Q[1]
+df["q2"] = Q[2]
+df["q3"] = Q[3]
+
+# součet hodnot polynomů
+df["sum"] = Q[0]+Q[1]+Q[2]+Q[3]
+
+# vytištění obsahu datového rámce
+print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
