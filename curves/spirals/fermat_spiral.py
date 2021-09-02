f78f30e989c3118933a5326f3fdff1fba7dfc352
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-02 07:49:08 +0200

    Fermat spiral

---
+++ curves/spirals/fermat_spiral.py
@@ -0,0 +1,31 @@
+"""Fermatova spirála."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# počet bodů na spirále
+points = 150
+
+# úhel v polárním grafu
+theta = np.linspace(0, 4*np.pi, points)
+
+# funkce: vzdálenost od středu
+radius = np.sqrt(theta)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+
+# titulek grafu
+fig.suptitle('Fermatova spirála', fontsize=15)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(theta + (radius<0)*np.pi, np.abs(radius), 'r-')
+
+# druhá polovina křivky
+ax.plot(np.pi + theta + (radius<0)*np.pi, np.abs(radius), 'b-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("fermat_spiral.png")
+
+# zobrazení grafu
+plt.show()
