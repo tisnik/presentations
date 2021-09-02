bbd78788c933c747a588e78b118941d974b24f04
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-02 07:49:08 +0200

    Atomic spiral, second attempt

---
+++ curves/spirals/atomic_spiral_2.py
@@ -0,0 +1,31 @@
+"""Atomická spirála."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# počet bodů na spirále
+points = 150
+
+# úhel v polárním grafu
+theta = np.linspace(-6*np.pi, 6*np.pi, points)
+
+# koeficient spirály
+a = 1
+
+# funkce: vzdálenost od středu
+radius = theta / (theta - a)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+
+# titulek grafu
+fig.suptitle('Atomická spirála', fontsize=15)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(theta + (radius<0)*np.pi, np.abs(radius), 'g-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("atomic_spiral.png")
+
+# zobrazení grafu
+plt.show()
