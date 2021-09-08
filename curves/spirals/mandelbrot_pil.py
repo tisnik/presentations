6aab93513c271def11f05923ca6aacf70301c995
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-08 07:45:29 +0200

    Mandelbrot rendered based on PIL/Pillow

---
+++ curves/spirals/mandelbrot_pil.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""Vykreslení klasické Mandelbrotovy množiny."""
+
+from PIL import Image
+import palette_greens
+
+# textura by měla být čtvercová a její šířka i výška by měla být
+# mocninou čísla 2
+IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
+
+
+def mandelbrot(cx, cy, maxiter):
+    """Výpočet, kolik iterací je nutné pro opuštění jednotkového kruhu."""
+    c = complex(cx, cy)
+    z = 0
+    for i in range(0, maxiter):
+        if abs(z) > 2:
+            return i
+        z = z * z + c
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
+    """Přepočet celého fraktálu."""
+    width, height = image.size       # rozměry obrázku
+    stepx = (xmax - xmin)/width
+    stepy = (ymax - ymin)/height
+
+    y1 = ymin
+    for y in range(0, height):
+        x1 = xmin
+        for x in range(0, width):
+            i = mandelbrot(x1, y1, maxiter)
+            i = 3*i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+
+
+image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+recalc_fractal(image, palette_greens.palette, -2.0, -1.5, 1.0, 1.5, 1000)
+image.save("mandelbrot.png")
