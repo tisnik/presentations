dd3684bb8ac4834fb0828ed16968a7e2fdfeddc5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-08 07:45:21 +0200

    Mandelbrot rendered based on Matplotlib

---
+++ curves/spirals/mandelbrot_matplotlib.py
@@ -0,0 +1,57 @@
+"""Vykreslení klasické Mandelbrotovy množiny."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import palette_greens
+
+
+# velikost bitmapy s nakresleným fraktálem
+WIDTH = 256
+HEIGHT = 256
+
+
+def mandelbrot(cx, cy, maxiter):
+    """Výpočet, kolik iterací je nutné pro opuštění jednotkového kruhu."""
+    c = complex(cx, cy)
+    z = 0
+    for i in range(0, maxiter):
+        if abs(z) > 2:
+            return i
+        z = z * z + c
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
+    """Přepočet celého fraktálu."""
+    stepx = (xmax - xmin)/WIDTH
+    stepy = (ymax - ymin)/HEIGHT
+
+    y1 = ymin
+    for y in range(0, HEIGHT):
+        x1 = xmin
+        for x in range(0, WIDTH):
+            i = mandelbrot(x1, y1, maxiter)
+            i = 3*i % 256
+            for index in range(0, 3):
+                image[y][x][index] = palette[i][index]
+                image[y][x][index] = palette[i][index]
+                image[y][x][index] = palette[i][index]
+            x1 += stepx
+        y1 += stepy
+
+
+# vytvoření prázdné bitmapy
+image = np.zeros(shape=(HEIGHT, WIDTH, 3), dtype=np.uint8)
+
+recalc_fractal(image, palette_greens.palette, -2.0, -1.5, 1.0, 1.5, 1000)
+
+# vykreslení bitmapy do grafu
+plt.figure(1, figsize=(8, 6), dpi=100)
+plt.imshow(image)
+
+# uložení grafu do rastrového obrázku
+plt.savefig("test.png")
+
+# zobrazení grafu s bitmapou
+plt.show()
