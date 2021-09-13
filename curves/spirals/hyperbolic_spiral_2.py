4a2bb4324436f08c4140c9e1d07e6737ea16d2eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-13 07:29:30 +0200

    Hyperbolic spiral with both branches

---
+++ curves/spirals/hyperbolic_spiral_2.py
@@ -0,0 +1,30 @@
+"""Hyperbolická spirála."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# úhel v polárním grafu
+theta = np.arange(1.00, 50.0, 0.05)
+
+a = 1.0
+
+# funkce: inverzní vzdálenost od středu
+radius = a/theta
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+
+# titulek grafu
+fig.suptitle('Hyperbolická spirála - obě ramena', fontsize=15)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(theta, radius, 'r-')
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(np.pi - theta, radius, 'b-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("hyperbolic_spiral_2.png")
+
+# zobrazení grafu
+plt.show()
