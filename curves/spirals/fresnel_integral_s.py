bf9ddcd3ef18f1319c7da4a2c70ca8055af12853
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-20 08:11:20 +0200

    Fresnel ingegral S(x)

---
+++ curves/spirals/fresnel_integral_s.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+# počet hodnot na x-ové ose
+points = 300
+
+# integrační krok
+step = 0.001
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 5, points)
+
+
+# výpočet Fresnelova integrálu S(x)
+def fresnel_s(x):
+    result = 0
+    for t in np.arange(0, x, step):
+        result += math.sin(t**2)
+    return result*step
+
+
+# vektorizace předchozí funkce
+fresnel_s_v = np.vectorize(fresnel_s)
+
+# aplikace vektorizované funkce na všechny x-ové hodnoty
+y = fresnel_s_v(x)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Fresnelův integrál S(x)', fontsize=15)
+
+# vrcholy na křivce pospojované úsečkami
+ax.plot(x, y, 'g-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("fresnel_s.png")
+
+# zobrazení grafu
+plt.show()
