d414d15d8adbf440f6f95bed9fb1909d9cb8f23f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-20 08:11:50 +0200

    Fresnel spiral (clothoid)

---
+++ curves/spirals/fresnel_spiral.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+# počet hodnot na x-ové ose
+points = 300
+
+# integrační krok
+step = 0.001
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 5, points)
+
+
+# výpočet Fresnelova integrálu S(x)
+def fresnel_s(x):
+    result = 0
+    for t in np.arange(0, x, step):
+        result += math.sin(t**2)
+    return result*step
+
+
+# výpočet Fresnelova integrálu C(x)
+def fresnel_c(x):
+    result = 0
+    for t in np.arange(0, x, step):
+        result += math.cos(t**2)
+    return result*step
+
+
+# vektorizace předchozích funkcí
+fresnel_s_v = np.vectorize(fresnel_s)
+fresnel_c_v = np.vectorize(fresnel_c)
+
+# aplikace vektorizovaných funkcí na všechny x-ové hodnoty
+y_s = fresnel_s_v(x)
+y_c = fresnel_c_v(x)
+
+# rozměry grafu při uložení: 640x480 pixelů
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
+
+# titulek grafu
+fig.suptitle('Fresnelova spirála', fontsize=15)
+
+# určení rozsahů na obou souřadných osách
+ax.set_xlim(0, 1.2)
+ax.set_ylim(0, 0.9)
+
+# vrcholy na spirále
+ax.plot(y_c, y_s, 'g-')
+
+# uložení grafu do rastrového obrázku
+plt.savefig("fresnel_spiral.png")
+
+# zobrazení grafu
+plt.show()
