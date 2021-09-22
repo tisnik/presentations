4d5a8fb55e05c70fcf2f301d2c161c2df3f97763
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-22 07:52:49 +0200

    Added link to literate programming style variant of this source file

--- curves/spirals/involute2.py
+++ curves/spirals/involute2.py
@@ -1,5 +1,17 @@
 """Evolventa."""
 
+#
+# Použito v článku:
+#
+# Křivky v přírodě i v počítačové grafice – svět spirál (dokončení)
+# https://www.root.cz/clanky/krivky-v-prirode-i-v-pocitacove-grafice-svet-spiral-dokonceni/
+#
+# Součást seriálu:
+#
+# Křivky nejen v počítačové grafice
+# https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
+#
+
 import numpy as np
 import matplotlib.pyplot as plt
 
