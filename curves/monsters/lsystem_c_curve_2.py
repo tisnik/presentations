51ad9abbd9f089ccd2c5d4117f095ad1e932b570
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-27 10:29:01 +0200

    C-curve, example #2

---
+++ curves/monsters/lsystem_c_curve_2.py
@@ -0,0 +1,79 @@
+"""Jednoduchý L-systém: C-křivka."""
+
+from turtle import *
+
+# krok želvy pro příkazy "F" a "B"
+step = 5
+
+# úhel otočení želvy pro příkazy "+" a "-"
+angle = 45
+
+# startovní symbol
+axiom = "F"
+
+# přepisovací pravidla
+rewrite_rules = {}
+rewrite_rules["F"] = "+F--F+"
+
+# počet aplikací přepisovacích pravidel
+iterations = 11
+
+# počáteční pozice želvy
+start_x = -100
+start_y = 0
+
+
+def apply_rule(rules, c):
+    """Aplikace přepisovacího pravidla."""
+    output = ""
+    for rule, result in rules.items():
+        if c == rule:
+            return result
+        else:
+            output = c
+    return output
+
+
+def produce_control_string(axiom, rewrite_rules, n):
+    """Výpočet řídicího řetězce."""
+    s = axiom
+    for i in range(n):
+        tmp = ""
+        for c in s:
+            tmp += apply_rule(rewrite_rules, c)
+        s = tmp
+    return s
+
+
+def draw_l_system(control_string, step, angle, start_x, start_y):
+    """Vykreslení L-systému na obrazovku s využitím želví grafiky."""
+    # styl vykreslování
+    color("red")
+    pensize(2)
+    penup()
+    setpos(start_x, start_y)
+    pendown()
+
+    # projít všemi příkazy z řídicího řetězce
+    for command in control_string:
+        if command == "F":
+            forward(step)
+        elif command == "B":
+            backward(step)
+        elif command == "+":
+            left(angle)
+        elif command == "-":
+            right(angle)
+
+    # dokončení vykreslení a čekání na zavření okna
+    done()
+
+
+# výpočet řídicího řetězce
+control_string = produce_control_string(axiom, rewrite_rules, iterations)
+print(control_string)
+
+# vykreslení L-systému
+speed(0)
+hideturtle()
+draw_l_system(control_string, step, angle, start_x, start_y)
