a6036519ed9e04e4389d44d7f25e69e7253d80bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-01 07:34:21 +0100

    Added link to source in literate format

--- curves/monsters/lsystem_snowflake_1.py
+++ curves/monsters/lsystem_snowflake_1.py
@@ -11,6 +11,9 @@
 # Křivky nejen v počítačové grafice
 # https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
 #
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/monsters/lsystem_snowflake_1.html
+#
 
 from turtle import *
 
