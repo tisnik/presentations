a1b05d8a90ab308778adc0e237bc1a7099a229da
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-03 07:57:11 +0100

    Added link to source in literate format

--- curves/parametric/Catmul-Rom_basis_sum.py
+++ curves/parametric/Catmul-Rom_basis_sum.py
@@ -11,6 +11,9 @@
 # Křivky nejen v počítačové grafice
 # https://www.root.cz/serialy/krivky-nejen-v-pocitacove-grafice/
 #
+# Zdrojový kód tohoto skriptu ve stylu literate programming
+# https://tisnik.github.io/presentations/appendix/lit_sources/parametric/Catmul-Rom_basis_sum.html
+#
 
 import numpy as np
 import pandas as pd
