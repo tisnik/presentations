8adaa84b5e644aede0553a71cc269ff41ae766f5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-26 08:07:26 +0100

    Storing primitive value: true

---
+++ docs/coffee-talks/messagepack/msgpack_nil.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ugorji/go/codec"
+)
+
+const filename = "/tmp/nil.bin"
+
+func main() {
+	// vytvořit soubor s binárními daty
+	fout, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fout.Close()
+
+	log.Print("Output file created")
+
+	// handler
+	var handler codec.MsgpackHandle
+
+	// objekt realizující zakódování dat
+	encoder := codec.NewEncoder(fout, &handler)
+
+	log.Print("Encoder created")
+
+	// zakódování dat
+	err = encoder.Encode(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Done")
+}
