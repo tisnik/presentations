7357feecfff3f2720347220f62271025a714c689
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-26 08:07:26 +0100

    Serializing map of any values

---
+++ docs/coffee-talks/messagepack/msgpack_map2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ugorji/go/codec"
+)
+
+const filename = "/tmp/map2.bin"
+
+func main() {
+	// vytvořit soubor s binárními daty
+	fout, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fout.Close()
+
+	log.Print("Output file created")
+
+	// handler
+	var handler codec.MsgpackHandle
+
+	// objekt realizující zakódování dat
+	encoder := codec.NewEncoder(fout, &handler)
+
+	log.Print("Encoder created")
+
+	var m map[string]interface{} = make(map[string]interface{})
+	m["foo"] = 1
+	m["bar"] = 2
+	m["baz"] = 1000000
+	m["wee"] = "test"
+	m["array"] = []int{1, 2, 3}
+	m["map"] = map[string]string{
+		"one": "jedna",
+		"two": "dve",
+	}
+
+	// zakódování dat
+	err = encoder.Encode(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Done")
+}
