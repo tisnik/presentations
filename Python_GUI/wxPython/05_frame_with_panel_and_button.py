ed0d7ee6b67b8a6943baedd39f7127f844df6342
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-11 09:25:37 +0100

    Reformatted

--- Python_GUI/wxPython/05_frame_with_panel_and_button.py
+++ Python_GUI/wxPython/05_frame_with_panel_and_button.py
@@ -14,7 +14,7 @@ class MainFrame(Frame):
 
         # vytvoření tlačítka s jeho vložením do panelu
         x = y = 20
-        button = Button(panel, ID_ANY, 'Press me', (x, y))
+        button = Button(panel, ID_ANY, "Press me", (x, y))
 
         # zobrazení hlavního okna aplikace
         self.Show(True)
