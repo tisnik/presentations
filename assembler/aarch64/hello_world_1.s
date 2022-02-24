98455f18d8f3ac6c06998e05c5bc0da862648867
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-24 10:10:50 +0100

    A classic Hello world example

---
+++ assembler/aarch64/hello_world_1.s
@@ -0,0 +1,44 @@
+# asmsyntax=as
+
+# Jednoducha aplikace typu "Hello world!" naprogramovana
+# v assembleru GNU as.
+#
+# Autor: Pavel Tisnovsky
+
+
+
+# Linux kernel system call table
+sys_exit=93
+sys_write=64
+
+# List of syscalls for AArch64:
+# https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/unistd.h
+
+
+
+#-----------------------------------------------------------------------------
+.section .data
+
+hello_lbl:
+        .string "Hello World!\n"
+
+#-----------------------------------------------------------------------------
+.section .bss
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start          // tento symbol ma byt dostupny i linkeru
+
+_start:
+        mov  x8, #sys_write     // cislo sycallu pro funkci "write"
+        mov  x0, #1             // standardni vystup
+        ldr  x1, =hello_lbl     // adresa retezce, ktery se ma vytisknout
+        mov  x2, #13            // pocet znaku, ktere se maji vytisknout
+        svc  0                  // volani Linuxoveho kernelu
+
+        mov  x8, #sys_exit      // cislo sycallu pro funkci "exit"
+        mov  x0, #0             // exit code = 0
+        svc  0                  // volani Linuxoveho kernelu
+
