184408a5fd3862b02a526c7cc763ab5f6aef6048
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-24 09:12:12 +0100

    Arithmetic operations

---
+++ assembler/aarch64/aritmetic1.s
@@ -0,0 +1,61 @@
+# asmsyntax=as
+
+# Zakladni aritmeticke instrukce
+# v assembleru GNU as.
+#
+# Autor: Pavel Tisnovsky
+
+
+
+# Linux kernel system call table
+sys_exit=93
+sys_write=64
+
+# List of syscalls for AArch64:
+# https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/unistd.h
+
+
+
+#-----------------------------------------------------------------------------
+.section .data
+
+result_lbl:
+        .string "x=?\ny=?\n"
+
+#-----------------------------------------------------------------------------
+.section .bss
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start          // tento symbol ma byt dostupny i linkeru
+
+_start:
+        ldr  x3, =result_lbl    // adresa retezce, ktery se ma vytisknout
+
+        mov  x0, #1             // prvni operand
+        mov  x1, #2             // druhy operand
+        add  x0, x0, x1         // operace souctu
+
+        mov  x2, #'0'           // prvni cifra v ASCII kodu
+        add  x0, x0, x2         // převod na ASCII
+        strb w0, [x3, 2]        // zapis znaku s vyuzitim offsetu
+
+        mov  x0, #1000          // prvni operand
+        mov  x1, #995           // druhy operand
+        sub  x0, x0, x1         // operace souctu
+
+        mov  x2, #'0'           // prvni cifra v ASCII kodu
+        add  x0, x0, x2         // převod na ASCII
+        strb w0, [x3, 6]        // zapis znaku s vyuzitim offsetu
+
+        mov  x8, #sys_write     // cislo sycallu pro funkci "write"
+        mov  x0, #1             // standardni vystup
+        ldr  x1, =result_lbl    // adresa retezce, ktery se ma vytisknout
+        mov  x2, #8             // pocet znaku, ktere se maji vytisknout
+        svc  0                  // volani Linuxoveho kernelu
+
+        mov  x8, #sys_exit      // cislo sycallu pro funkci "exit"
+        mov  x0, #0             // exit code = 0
+        svc  0                  // volani Linuxoveho kernelu
