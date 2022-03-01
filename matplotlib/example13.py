6e90a5e60b5b69e7d4c78d5ffc42e7a9324122da
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:15 +0100

    Reformatted by black

--- matplotlib/example13.py
+++ matplotlib/example13.py
@@ -8,17 +8,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # hodnoty na x-ové ose
-x = np.linspace(0.2, 2*np.pi, 100)
+x = np.linspace(0.2, 2 * np.pi, 100)
 
 # hodnoty na y-ové ose
-y = np.sin(5*x)/x
-y2 = 1/x
+y = np.sin(5 * x) / x
+y2 = 1 / x
 y3 = -y2
 
 # vykreslit průběh funkce
-plt.plot(x, y2, color='red',  label='obalka sinc', drawstyle='default')
-plt.plot(x, y3, color='red',  label='obalka sinc', drawstyle='default')
-plt.plot(x, y,  color='blue', label='sinc(x)',     drawstyle='steps')
+plt.plot(x, y2, color="red", label="obalka sinc", drawstyle="default")
+plt.plot(x, y3, color="red", label="obalka sinc", drawstyle="default")
+plt.plot(x, y, color="blue", label="sinc(x)", drawstyle="steps")
 
 # povolení zobrazení mřížky
 plt.grid(True)
