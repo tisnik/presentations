3c96c749d7c8a513bd326b6bc64a0bec94863ce2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:27:36 +0100

    Hello world, 3rd version

---
+++ assembler/aarch64/hello_world_3.s
@@ -0,0 +1,53 @@
+# asmsyntax=as
+
+# Jednoducha aplikace typu "Hello world!" naprogramovana
+# v assembleru GNU as.
+#
+# Autor: Pavel Tisnovsky
+
+
+
+# Linux kernel system call table
+sys_exit=93
+sys_write=64
+
+# List of syscalls for AArch64:
+# https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/unistd.h
+
+
+# Deklarace makra pro ukonceni aplikace
+.macro exit
+        mov  x8, #sys_exit      // cislo sycallu pro funkci "exit"
+        mov  x0, #0             // exit code = 0
+        svc  0                  // volani Linuxoveho kernelu
+.endm
+
+
+# Deklarace makra pro vytisteni zpravy na standardni vystup
+.macro writeMessage message,messageLength
+        mov  x8, #sys_write       // cislo sycallu pro funkci "write"
+        mov  x0, #1               // standardni vystup
+        ldr  x1, =\message        // adresa retezce, ktery se ma vytisknout
+        mov  x2, #\messageLength  // pocet znaku, ktere se maji vytisknout
+        svc  0                    // volani Linuxoveho kernelu
+.endm
+
+
+#-----------------------------------------------------------------------------
+.section .data
+
+hello_lbl:
+        .string "Hello World!\n"
+
+#-----------------------------------------------------------------------------
+.section .bss
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start          // tento symbol ma byt dostupny i linkeru
+
+_start:
+        writeMessage hello_lbl, 13
+        exit
