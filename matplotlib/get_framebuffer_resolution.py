a559c9ed363301cc19dc63750d8f851688a218ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/get_framebuffer_resolution.py
+++ matplotlib/get_framebuffer_resolution.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 
+
 def get_framebuffer_resolution(framebuffer_device):
     fbset_output = subprocess.check_output(["fbset", "-s", "-fb", framebuffer_device])
 
@@ -11,5 +12,5 @@ def get_framebuffer_resolution(framebuffer_device):
             parsed = re.match(r"geometry (\d+) (\d+)", line)
             return (parsed.group(1), parsed.group(2))
 
-print(get_framebuffer_resolution("/dev/fb0"))
 
+print(get_framebuffer_resolution("/dev/fb0"))
