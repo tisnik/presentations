16adf3e3c24f44b52fd99ea92decf8a77cb243fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/example24.py
+++ matplotlib/example24.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection="3d")
 
 delta = 0.1
 
@@ -20,18 +20,17 @@ x = np.arange(-10.0, 10.0, delta)
 # průběh nezávislé proměnné y
 y = np.arange(-10.0, 10.0, delta)
 
-# vytvoření dvou polí se souřadnicemi [x,y] 
+# vytvoření dvou polí se souřadnicemi [x,y]
 X, Y = np.meshgrid(x, y)
 
 # vzdálenost od bodu [0,0]
-R = np.sqrt(X*X+Y*Y)
+R = np.sqrt(X * X + Y * Y)
 
 # výpočet funkce, kterou použijeme při vykreslování grafu
-Z = np.sin(R)/R
+Z = np.sin(R) / R
 
 # zobrazení 3D grafu
 ax.plot_wireframe(X, Y, Z, rstride=7, cstride=7)
 
 # zobrazení grafu
 plt.show()
-
