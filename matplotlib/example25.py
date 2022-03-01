867383b990b1c52d3ead2fabd3a3133315b89bb3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/example25.py
+++ matplotlib/example25.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 delta = 0.1
 
@@ -21,19 +21,19 @@ x = np.arange(-10.0, 10.0, delta)
 # průběh nezávislé proměnné y
 y = np.arange(-10.0, 10.0, delta)
 
-# vytvoření dvou polí se souřadnicemi [x,y] 
+# vytvoření dvou polí se souřadnicemi [x,y]
 X, Y = np.meshgrid(x, y)
 
 # vzdálenost od bodu [0,0]
-R = np.sqrt(X*X+Y*Y)
+R = np.sqrt(X * X + Y * Y)
 
 # výpočet funkce, kterou použijeme při vykreslování grafu
-Z = np.sin(R)/R
+Z = np.sin(R) / R
 
 # zobrazení 3D grafu formou plochy
-ax.plot_surface(X, Y, Z, rstride=2, cstride=2, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+ax.plot_surface(
+    X, Y, Z, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth=0, antialiased=False
+)
 
 # zobrazení grafu
 plt.show()
-
