5039ab19e7b3f5a1ddd8fa01798fd8f725e465c2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/pygame_matplotlib_4.py
+++ matplotlib/pygame_matplotlib_4.py
@@ -2,6 +2,7 @@
 # vim: set fileencodings=utf-8
 
 import matplotlib
+
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,8 +27,8 @@ FRAMEBUFFER_DEVICE = "/dev/fb0"
 
 # Vytvoreni grafu
 def create_graph(width, height, dpi):
-    fig = plt.figure(figsize=(1.0*width/dpi, 1.0*height/dpi), dpi=dpi)
-    x = np.linspace(0, 2*np.pi, 100)
+    fig = plt.figure(figsize=(1.0 * width / dpi, 1.0 * height / dpi), dpi=dpi)
+    x = np.linspace(0, 2 * np.pi, 100)
     y = np.sin(x)
     plt.plot(x, y)
     plt.xlabel("x")
@@ -76,8 +77,7 @@ def wait_for_key():
 
 # precteni aktualne nastaveneho rozliseni framebufferu
 def get_framebuffer_resolution(framebuffer_device):
-    fbset_output = subprocess.check_output(["fbset", "-s",
-                                            "-fb", framebuffer_device])
+    fbset_output = subprocess.check_output(["fbset", "-s", "-fb", framebuffer_device])
 
     for line in fbset_output.split("\n"):
         line = line.strip()
