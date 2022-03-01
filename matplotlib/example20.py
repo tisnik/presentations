f1391241b12b2ecbffd3be0451995573528a8e70
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/example20.py
+++ matplotlib/example20.py
@@ -12,7 +12,7 @@ vals1 = [10, 15, 20, 12, 14, 8]
 delta1 = [1, 2, 3, 4, 5, 0]
 
 # druhé pole hodnot a pole odchylek
-vals2 = [19, 18,  6, 11,  6, 14]
+vals2 = [19, 18, 6, 11, 6, 14]
 delta2 = [4, 2, 3, 2, 2, 4]
 
 # počet prvků
@@ -25,13 +25,28 @@ indexes = np.arange(N)
 width = 0.30
 
 # sloupcový graf se dvěma skupinami sloupců
-plt.bar(indexes, vals1, width, color='gray', edgecolor='black', label='CPU#1',
-        yerr=delta1, error_kw=dict(elinewidth=2, ecolor='red'))
+plt.bar(
+    indexes,
+    vals1,
+    width,
+    color="gray",
+    edgecolor="black",
+    label="CPU#1",
+    yerr=delta1,
+    error_kw=dict(elinewidth=2, ecolor="red"),
+)
 
 # posunuté sloupce
-plt.bar(indexes+width, vals2, width, color='red', edgecolor='black',
-        label='CPU#2',
-        yerr=delta2, error_kw=dict(elinewidth=2, ecolor='black'))
+plt.bar(
+    indexes + width,
+    vals2,
+    width,
+    color="red",
+    edgecolor="black",
+    label="CPU#2",
+    yerr=delta2,
+    error_kw=dict(elinewidth=2, ecolor="black"),
+)
 
 # povolení zobrazení mřížky
 plt.grid(True)
