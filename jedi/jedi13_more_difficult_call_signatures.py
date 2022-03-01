f3fe81083871ddd9b7f756b0eb6d17a0cb89218c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:38:41 +0100

    Reformatted by black

--- jedi/jedi13_more_difficult_call_signatures.py
+++ jedi/jedi13_more_difficult_call_signatures.py
@@ -3,7 +3,7 @@
 
 import jedi
 
-src = '''
+src = """
 def foo():
     return 0
 
@@ -22,37 +22,41 @@ print   (10, 20, 30, 40, 50)
 print(
 
 42)
-'''
+"""
 
 
 def print_call_signatures(script):
     call_signatures = script.call_signatures()
 
     for call_signature in call_signatures:
-        print(call_signatures.__str__(), call_signature.index, call_signature.bracket_start)
+        print(
+            call_signatures.__str__(),
+            call_signature.index,
+            call_signature.bracket_start,
+        )
 
     print()
 
 
-lines = src.count('\n')
+lines = src.count("\n")
 
-script = jedi.Script(src, 11, len('foo('), 'test.py')
+script = jedi.Script(src, 11, len("foo("), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, 12, len('bar(1'), 'test.py')
+script = jedi.Script(src, 12, len("bar(1"), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, 13, len('baz(1,2'), 'test.py')
+script = jedi.Script(src, 13, len("baz(1,2"), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, 14, len('baz(foo(), bar(1'), 'test.py')
+script = jedi.Script(src, 14, len("baz(foo(), bar(1"), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, 15, len('print(10, 20, 30, 40, '), 'test.py')
+script = jedi.Script(src, 15, len("print(10, 20, 30, 40, "), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, 16, len('print   (10, 20, 30, 40, '), 'test.py')
+script = jedi.Script(src, 16, len("print   (10, 20, 30, 40, "), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, lines, 1, 'test.py')
+script = jedi.Script(src, lines, 1, "test.py")
 print_call_signatures(script)
