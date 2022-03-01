5f7d8faa22d3515c31af5005cc41f594b63b2c5b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:38:41 +0100

    Reformatted by black

--- jedi/jedi14_method_signatures.py
+++ jedi/jedi14_method_signatures.py
@@ -3,7 +3,7 @@
 
 import jedi
 
-src = '''
+src = """
 class C1:
     def foo(self):
         return 0
@@ -18,28 +18,32 @@ obj = C1()
 obj.foo()
 obj.bar(1)
 obj.baz(1,2)
-obj.baz(obj.foo(), obj.bar(1))'''
+obj.baz(obj.foo(), obj.bar(1))"""
 
 
 def print_call_signatures(script):
     call_signatures = script.call_signatures()
 
     for call_signature in call_signatures:
-        print(call_signatures.__str__(), call_signature.index, call_signature.bracket_start)
+        print(
+            call_signatures.__str__(),
+            call_signature.index,
+            call_signature.bracket_start,
+        )
 
     print()
 
 
-lines = src.count('\n')
+lines = src.count("\n")
 
-script = jedi.Script(src, lines-2, len('obj.foo('), 'test.py')
+script = jedi.Script(src, lines - 2, len("obj.foo("), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, lines-1, len('obj.bar(1'), 'test.py')
+script = jedi.Script(src, lines - 1, len("obj.bar(1"), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, lines, len('obj.baz(1,2'), 'test.py')
+script = jedi.Script(src, lines, len("obj.baz(1,2"), "test.py")
 print_call_signatures(script)
 
-script = jedi.Script(src, lines+1, len('obj.baz(obj.foo(), obj.bar(1'), 'test.py')
+script = jedi.Script(src, lines + 1, len("obj.baz(obj.foo(), obj.bar(1"), "test.py")
 print_call_signatures(script)
