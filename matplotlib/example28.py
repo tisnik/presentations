cb4eea3c03e3c7d1226768d2e3fae3d21742a1f8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/example28.py
+++ matplotlib/example28.py
@@ -10,18 +10,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # nezávislá proměnná
-t = np.arange(0, 8*np.pi, 0.1)
+t = np.arange(0, 8 * np.pi, 0.1)
 
 # vzdálenost od osy spirály
-r = 10.0/(t+4)
+r = 10.0 / (t + 4)
 
 # výpočet souřadnic [x,y,z]) pro každé t
-x = r*np.cos(t)
-y = r*np.sin(t)
+x = r * np.cos(t)
+y = r * np.sin(t)
 z = t
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # vykreslení grafu
 ax.plot(x, y, z)
