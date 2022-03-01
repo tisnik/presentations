a96d20cfaa2676758363b687a8dd37df66b2cbf1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:40:23 +0100

    Reformatted by black

--- rust/libs/test5/test_python3.py
+++ rust/libs/test5/test_python3.py
@@ -7,6 +7,6 @@ libtest5 = ctypes.CDLL("target/debug/libtest5.so")
 
 libtest5.print_string.argtypes = (ctypes.c_char_p,)
 
-libtest5.print_string("Hello world!".encode('utf-8'))
-libtest5.print_string("Příliš žluťoučký kůň".encode('utf-8'))
-libtest5.print_string("Ну, погоди!".encode('utf-8'))
+libtest5.print_string("Hello world!".encode("utf-8"))
+libtest5.print_string("Příliš žluťoučký kůň".encode("utf-8"))
+libtest5.print_string("Ну, погоди!".encode("utf-8"))
