5c69a4e7f4c92823ea3b1a31cbb1191fe49f8af2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/example27.py
+++ matplotlib/example27.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 delta = 0.1
 
@@ -27,34 +27,35 @@ y = np.arange(-10.0, 10.0, delta)
 X, Y = np.meshgrid(x, y)
 
 # vzdálenost od bodu [0,0]
-R = np.sqrt(X*X+Y*Y)
+R = np.sqrt(X * X + Y * Y)
 
 # výpočet funkce, kterou použijeme při vykreslování grafu
-Z = np.sin(R)/R
+Z = np.sin(R) / R
 
 # zobrazení 3D grafu formou plochy
-surface = ax.plot_surface(X, Y, Z, rstride=2, cstride=2, cmap=cm.coolwarm,
-                          linewidth=0, antialiased=False)
+surface = ax.plot_surface(
+    X, Y, Z, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth=0, antialiased=False
+)
 
 # kontutra: průmět na rovinu x-y
-cset = ax.contour(X, Y, Z, zdir='z', offset=-5, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir="z", offset=-5, cmap=cm.coolwarm)
 
 # kontutra: průmět na rovinu y-z
-cset = ax.contour(X, Y, Z, zdir='x', offset=-15, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir="x", offset=-15, cmap=cm.coolwarm)
 
 # kontutra: průmět na rovinu x-z
-cset = ax.contour(X, Y, Z, zdir='y', offset=15, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir="y", offset=15, cmap=cm.coolwarm)
 
 # rozměry grafu ve směru osy x
-ax.set_xlabel('X')
+ax.set_xlabel("X")
 ax.set_xlim(-15, 15)
 
 # rozměry grafu ve směru osy y
-ax.set_ylabel('Y')
+ax.set_ylabel("Y")
 ax.set_ylim(-15, 15)
 
 # rozměry grafu ve směru osy z
-ax.set_zlabel('Z')
+ax.set_zlabel("Z")
 ax.set_zlim(-5, 5)
 
 # zobrazení grafu
