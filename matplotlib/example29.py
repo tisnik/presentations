315e49d38d2d5809b46fab7446e27bdb4266960f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:16 +0100

    Reformatted by black

--- matplotlib/example29.py
+++ matplotlib/example29.py
@@ -12,9 +12,9 @@ import numpy as np
 
 # funkce pro výpočet dalšího bodu Lorenzova atraktoru
 def lorenz(x, y, z, s=10, r=28, b=2.667):
-    x_dot = s*(y - x)
-    y_dot = r*x - y - x*z
-    z_dot = x*y - b*z
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
     return x_dot, y_dot, z_dot
 
 
@@ -30,17 +30,17 @@ y = np.zeros((n,))
 z = np.zeros((n,))
 
 # počáteční hodnoty
-x[0], y[0], z[0] = (0., 1., 1.05)
+x[0], y[0], z[0] = (0.0, 1.0, 1.05)
 
 # vlastní výpočet atraktoru
-for i in range(n-1):
+for i in range(n - 1):
     x_dot, y_dot, z_dot = lorenz(x[i], y[i], z[i])
-    x[i+1] = x[i] + x_dot * dt
-    y[i+1] = y[i] + y_dot * dt
-    z[i+1] = z[i] + z_dot * dt
+    x[i + 1] = x[i] + x_dot * dt
+    y[i + 1] = y[i] + y_dot * dt
+    z[i + 1] = z[i] + z_dot * dt
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # vykreslení grafu
 ax.plot(x, y, z)
