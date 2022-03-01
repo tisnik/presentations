8455e16d1cf479af1bc98845a8483f92e508f474
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:36:15 +0100

    Reformatted by black

--- matplotlib/example08.py
+++ matplotlib/example08.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # hodnoty na x-ové ose
-x = np.linspace(0, 2*np.pi, 100)
+x = np.linspace(0, 2 * np.pi, 100)
 
 # hodnoty na y-ové ose: první funkce
 y1 = np.sin(x)
@@ -37,16 +37,20 @@ plt.xlabel("x")
 plt.ylabel("sin(x) a cos(x)")
 
 # vložit první popisek do grafu
-plt.annotate("maximální hodnota sin(x)",
-             xy=(np.pi/2, 1.0),
-             xytext=(1, 1.3),
-             arrowprops=dict(arrowstyle="->"))
+plt.annotate(
+    "maximální hodnota sin(x)",
+    xy=(np.pi / 2, 1.0),
+    xytext=(1, 1.3),
+    arrowprops=dict(arrowstyle="->"),
+)
 
 # vložit druhý popisek do grafu
-plt.annotate("minimální hodnota cos(x)",
-             xy=(np.pi, -1.0),
-             xytext=(2, -1.3),
-             arrowprops=dict(arrowstyle="->"))
+plt.annotate(
+    "minimální hodnota cos(x)",
+    xy=(np.pi, -1.0),
+    xytext=(2, -1.3),
+    arrowprops=dict(arrowstyle="->"),
+)
 
 # zobrazení grafu
 plt.show()
