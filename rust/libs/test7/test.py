2ed52b2d0ca62cdc908176f7c4f80c4bd1a07d74
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-01 07:40:28 +0100

    Reformatted by black

--- rust/libs/test7/test.py
+++ rust/libs/test7/test.py
@@ -8,12 +8,13 @@ libtest7 = ctypes.CDLL("target/debug/libtest7.so")
 libtest7.sum.argtypes = (ctypes.POINTER(ctypes.c_uint32), ctypes.c_size_t)
 libtest7.sum.restype = ctypes.c_int32
 
+
 def sum(numbers):
     buf_type = ctypes.c_uint32 * len(numbers)
     buf = buf_type(*numbers)
     return libtest7.sum(buf, len(numbers))
 
-print(sum([1,2,3,4]))
+
+print(sum([1, 2, 3, 4]))
 
 print(sum(range(11)))
-
