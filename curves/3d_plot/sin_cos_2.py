002c5cc37545dbaf3fb58141dc78cdc78e8fc546
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-02 10:57:03 +0100

    Reformatted by black

--- curves/3d_plot/sin_cos_2.py
+++ curves/3d_plot/sin_cos_2.py
@@ -27,31 +27,32 @@ y = np.linspace(-5, 5, 150)
 x, y = np.meshgrid(x, y)
 
 # implicitní funkce
-z = np.sin(x**2) - np.cos(y**2)
+z = np.sin(x ** 2) - np.cos(y ** 2)
 
 # inicializace grafu
 fig = plt.figure()
 
 # nastavení 3D projekce
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # zobrazení 3D grafu formou plochy
-surface = ax.plot_surface(x, y, z, rstride=2, cstride=2, cmap=cm.coolwarm,
-                          linewidth=0, antialiased=False)
+surface = ax.plot_surface(
+    x, y, z, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth=0, antialiased=False
+)
 
 # titulek grafu
-fig.suptitle('Sin/cos', fontsize=15)
+fig.suptitle("Sin/cos", fontsize=15)
 
 # rozměry grafu ve směru osy x
-ax.set_xlabel('X')
+ax.set_xlabel("X")
 ax.set_xlim(-4, 4)
 
 # rozměry grafu ve směru osy y
-ax.set_ylabel('Y')
+ax.set_ylabel("Y")
 ax.set_ylim(-4, 4)
 
 # rozměry grafu ve směru osy z
-ax.set_zlabel('Z')
+ax.set_zlabel("Z")
 ax.set_zlim(-5, 15)
 
 # uložení grafu do rastrového obrázku
