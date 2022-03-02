01810625aa4ea5a29bdb68db97bd428bb37c5c3d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-02 10:57:03 +0100

    Reformatted by black

--- curves/3d_plot/parabola1A.py
+++ curves/3d_plot/parabola1A.py
@@ -28,32 +28,33 @@ x, y = np.meshgrid(x, y)
 
 # implicitní funkce paraboly
 f = 0.5
-p = 2*f
-z = x**2 - p*p*y
+p = 2 * f
+z = x ** 2 - p * p * y
 
 # inicializace grafu
 fig = plt.figure()
 
 # nastavení 3D projekce
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # zobrazení 3D grafu formou plochy
-surface = ax.plot_surface(x, y, z, rstride=2, cstride=2, cmap=cm.coolwarm,
-                          linewidth=0, antialiased=False)
+surface = ax.plot_surface(
+    x, y, z, rstride=2, cstride=2, cmap=cm.coolwarm, linewidth=0, antialiased=False
+)
 
 # titulek grafu
-fig.suptitle('Parabola', fontsize=15)
+fig.suptitle("Parabola", fontsize=15)
 
 # rozměry grafu ve směru osy x
-ax.set_xlabel('X')
+ax.set_xlabel("X")
 ax.set_xlim(-4, 4)
 
 # rozměry grafu ve směru osy y
-ax.set_ylabel('Y')
+ax.set_ylabel("Y")
 ax.set_ylim(0, 8)
 
 # rozměry grafu ve směru osy z
-ax.set_zlabel('Z')
+ax.set_zlabel("Z")
 ax.set_zlim(0, 10)
 
 # uložení grafu do rastrového obrázku
