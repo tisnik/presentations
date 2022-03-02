997d79ba33be570dfcfeb61390858d7aafa9e3fd
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-02 10:54:29 +0100

    Reformatted

--- Python_GUI/PyQt/hello_world.py
+++ Python_GUI/PyQt/hello_world.py
@@ -3,19 +3,19 @@ import sys
 # zajisteni importu noveho rozhrani
 import sip
 
-sip.setapi('QDate', 2)
-sip.setapi('QDateTime', 2)
-sip.setapi('QString', 2)
-sip.setapi('QTextStream', 2)
-sip.setapi('QTime', 2)
-sip.setapi('QUrl', 2)
-sip.setapi('QVariant', 2)
+sip.setapi("QDate", 2)
+sip.setapi("QDateTime", 2)
+sip.setapi("QString", 2)
+sip.setapi("QTextStream", 2)
+sip.setapi("QTime", 2)
+sip.setapi("QUrl", 2)
+sip.setapi("QVariant", 2)
 
 from PyQt4.Qt import *
 
 qt_application = QApplication(sys.argv)
 
-label = QLabel('Hello world!')
+label = QLabel("Hello world!")
 
 label.show()
 
