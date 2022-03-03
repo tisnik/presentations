7c4faa63d996e083297b48bb5545c00d436961fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-03 08:21:06 +0100

    Reformatted by black

--- Python_GUI/PySide/128_message_box_pixmap.py
+++ Python_GUI/PySide/128_message_box_pixmap.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -34,7 +33,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareMessageBoxButton(self):
         # tlačítko
-        messageBoxButton = QtGui.QPushButton('Message Box', self)
+        messageBoxButton = QtGui.QPushButton("Message Box", self)
         messageBoxButton.resize(messageBoxButton.sizeHint())
 
         # navázání akce na signál
@@ -43,7 +42,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareQuitButton(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -59,7 +58,7 @@ class MainWindowContent(QtGui.QWidget):
 
         # nastavení zprávy a ikony, která se má zobrazit vedle zprávy
         msgBox.setStandardButtons(buttons)
-        msgBox.setText(u'')
+        msgBox.setText(u"")
 
         # načtení pixmapy a její nastavení jako ikony pro dialog
         pixmap = QtGui.QPixmap("pixmaps/pysidelogo.png")
@@ -71,7 +70,6 @@ class MainWindowContent(QtGui.QWidget):
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -99,5 +97,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
