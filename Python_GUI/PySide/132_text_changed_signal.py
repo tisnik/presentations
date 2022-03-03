f24fb80ef87e10d643a8781383b91a9faca65b13
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-03 08:21:06 +0100

    Reformatted by black

--- Python_GUI/PySide/132_text_changed_signal.py
+++ Python_GUI/PySide/132_text_changed_signal.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -22,7 +21,8 @@ class MainWindowContent(QtGui.QWidget):
         quitButton = self.prepareQuitButton()
 
         textInputDialogButton = self.prepareButton(
-            "Text Input", self.textInputDialogHandler)
+            "Text Input", self.textInputDialogHandler
+        )
 
         self.enteredTextLabel = QtGui.QLabel("")
 
@@ -62,7 +62,8 @@ class MainWindowContent(QtGui.QWidget):
         # zpracování a zobrazení výsledků
         text = dialog.textValue()
         message = "Entered text: '{t}'\nClicked on: {c}".format(
-            t=text, c="Ok" if result == 1 else "Cancel")
+            t=text, c="Ok" if result == 1 else "Cancel"
+        )
 
         # zobrazení dialogu s informací o vstupu od uživatele
         self.showMessageBox(message)
@@ -78,7 +79,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareQuitButton(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -99,7 +100,6 @@ class MainWindowContent(QtGui.QWidget):
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -127,5 +127,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
