07e1f068953d96fe4858fa989f2213b4670faeb4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-03 08:21:06 +0100

    Reformatted by black

--- Python_GUI/PySide/118_message_box.py
+++ Python_GUI/PySide/118_message_box.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -34,7 +33,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareMessageBoxButton(self):
         # tlačítko
-        messageBoxButton = QtGui.QPushButton('Message Box', self)
+        messageBoxButton = QtGui.QPushButton("Message Box", self)
         messageBoxButton.resize(messageBoxButton.sizeHint())
 
         # navázání akce na signál
@@ -43,7 +42,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareQuitButton(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -52,7 +51,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def showMessageBox(self):
         msgBox = QtGui.QMessageBox()
-        msgBox.setText(u'Zpráva')
+        msgBox.setText(u"Zpráva")
         msgBox.setIcon(QtGui.QMessageBox.Information)
         # msgBox.setIcon(QtGui.QMessageBox.Question)
         # msgBox.setIcon(QtGui.QMessageBox.Warning)
@@ -62,7 +61,6 @@ class MainWindowContent(QtGui.QWidget):
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -90,5 +88,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
