557581940e82c4948298021543d6170f69a0b976
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/40_custom_shortcuts.py
+++ Python_GUI/PySide/40_custom_shortcuts.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindow(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -120,5 +119,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
