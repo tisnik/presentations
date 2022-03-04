ebe4f8c10d8678e9f9a4685ad7f5c4df2fe47b4d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/52_insert_items.py
+++ Python_GUI/PySide/52_insert_items.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -20,7 +19,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareGUI(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -28,10 +27,7 @@ class MainWindowContent(QtGui.QWidget):
 
         # seznam prvků
         listWidget = QtGui.QListWidget(self)
-        items = [u'jedna',
-                 u'dva',
-                 u'tři',
-                 u'čtyři']
+        items = [u"jedna", u"dva", u"tři", u"čtyři"]
         listWidget.insertItems(listWidget.currentRow(), items)
 
         # vytvoření správce geometrie
@@ -47,7 +43,6 @@ class MainWindowContent(QtGui.QWidget):
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -75,5 +70,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
