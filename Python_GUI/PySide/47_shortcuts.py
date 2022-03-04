a2b0f056ff0a0796f1d37092da115fc26bc8b58b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/47_shortcuts.py
+++ Python_GUI/PySide/47_shortcuts.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -21,32 +20,32 @@ class MainWindow(QtGui.QMainWindow):
     def prepareGUI(self):
         # velikost není potřeba specifikovat
         # self.resize(320, 240)
-        self.setWindowTitle('QMainWindow + statusBar + mainMenu')
+        self.setWindowTitle("QMainWindow + statusBar + mainMenu")
 
         # stavový řádek
-        self.statusBar().showMessage('QMainWindow')
+        self.statusBar().showMessage("QMainWindow")
 
         # hlavní menu
         menubar = self.menuBar()
 
         # příkaz File/Quit
-        fileQuitItem = QtGui.QAction('&Quit', self)
+        fileQuitItem = QtGui.QAction("&Quit", self)
         fileQuitItem.triggered.connect(self.close)
-        fileQuitItem.setStatusTip('Quit the application')
-        fileQuitItem.setShortcut('Ctrl+Q')
+        fileQuitItem.setStatusTip("Quit the application")
+        fileQuitItem.setShortcut("Ctrl+Q")
 
         # položka File v hlavním menu
-        fileMenu = menubar.addMenu('&File')
+        fileMenu = menubar.addMenu("&File")
         fileMenu.addAction(fileQuitItem)
 
         # příkaz Help/About
-        helpAboutItem = QtGui.QAction('&About', self)
+        helpAboutItem = QtGui.QAction("&About", self)
         helpAboutItem.triggered.connect(self.aboutDialog)
-        helpAboutItem.setStatusTip('About this application')
-        helpAboutItem.setShortcut('F1')
+        helpAboutItem.setStatusTip("About this application")
+        helpAboutItem.setShortcut("F1")
 
         # položka Help v hlavním menu
-        helpMenu = menubar.addMenu('&Help')
+        helpMenu = menubar.addMenu("&Help")
         helpMenu.addAction(helpAboutItem)
 
         # zobrazení hlavního okna
@@ -54,7 +53,7 @@ class MainWindow(QtGui.QMainWindow):
 
     def aboutDialog(self):
         msgBox = QtGui.QMessageBox()
-        msgBox.setText('About:\n...\n...\n...')
+        msgBox.setText("About:\n...\n...\n...")
         msgBox.setIcon(QtGui.QMessageBox.Information)
         msgBox.exec_()
 
@@ -70,5 +69,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
