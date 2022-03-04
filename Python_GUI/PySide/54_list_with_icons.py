b8bb353a068afd72c745feb2d171f3ad01878d09
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/54_list_with_icons.py
+++ Python_GUI/PySide/54_list_with_icons.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self, parentWidget):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -21,7 +20,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareGUI(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -29,11 +28,11 @@ class MainWindowContent(QtGui.QWidget):
 
         # seznam prvků
         listWidget = QtGui.QListWidget(self)
-        QtGui.QListWidgetItem(QtGui.QIcon('icons/battery-low.png'), u'0%', listWidget)
-        QtGui.QListWidgetItem(QtGui.QIcon('icons/battery-040.png'), u'40%', listWidget)
-        QtGui.QListWidgetItem(QtGui.QIcon('icons/battery-060.png'), u'60%', listWidget)
-        QtGui.QListWidgetItem(QtGui.QIcon('icons/battery-080.png'), u'80%', listWidget)
-        QtGui.QListWidgetItem(QtGui.QIcon('icons/battery-100.png'), u'100%', listWidget)
+        QtGui.QListWidgetItem(QtGui.QIcon("icons/battery-low.png"), u"0%", listWidget)
+        QtGui.QListWidgetItem(QtGui.QIcon("icons/battery-040.png"), u"40%", listWidget)
+        QtGui.QListWidgetItem(QtGui.QIcon("icons/battery-060.png"), u"60%", listWidget)
+        QtGui.QListWidgetItem(QtGui.QIcon("icons/battery-080.png"), u"80%", listWidget)
+        QtGui.QListWidgetItem(QtGui.QIcon("icons/battery-100.png"), u"100%", listWidget)
 
         # události vyvolávané manipulací se seznamem
         listWidget.itemActivated.connect(self.onItemActivated)
@@ -63,7 +62,6 @@ class MainWindowContent(QtGui.QWidget):
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -77,7 +75,7 @@ class MainWindow(QtGui.QMainWindow):
         self.setWindowTitle("QMainWindow + QListWidget")
 
         # vytvoření stavového řádku
-        self.statusBar().showMessage('QMainWindow')
+        self.statusBar().showMessage("QMainWindow")
 
         # vložení komponenty do okna
         self.setCentralWidget(MainWindowContent(self))
@@ -94,5 +92,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
