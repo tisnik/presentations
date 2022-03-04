f4086ab30c480c2efde41eb716d7cda52d11adc4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/25_label.py
+++ Python_GUI/PySide/25_label.py
@@ -24,7 +24,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindow(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -73,5 +72,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
