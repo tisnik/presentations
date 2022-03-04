79eb3e71c87aca926679884d5d4d4ebf14e6192e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/58_qimage_setpixel_overflow.py
+++ Python_GUI/PySide/58_qimage_setpixel_overflow.py
@@ -25,16 +25,16 @@ class MainWindow(QtGui.QMainWindow):
 
     def prepareImage(self):
         # vytvoření instance třídy QImage
-        self.image = QtGui.QImage(MainWindow.IMAGE_WIDTH,
-                                  MainWindow.IMAGE_HEIGHT,
-                                  QtGui.QImage.Format_RGB32)
+        self.image = QtGui.QImage(
+            MainWindow.IMAGE_WIDTH, MainWindow.IMAGE_HEIGHT, QtGui.QImage.Format_RGB32
+        )
 
         # vyplnění celého obrázku barvovým přechodem
         for y in range(MainWindow.IMAGE_HEIGHT):
             for x in range(MainWindow.IMAGE_WIDTH):
                 # zde dochází k přetečení hodnoty barvových složek
                 # Red, Green, Blue
-                self.image.setPixel(x, y, QtGui.qRgb(x, x*10, y))
+                self.image.setPixel(x, y, QtGui.qRgb(x, x * 10, y))
 
         # vytvoření instance třídy QPixmap z objektu QImage
         self.pixmap = QtGui.QPixmap.fromImage(self.image)
@@ -42,17 +42,18 @@ class MainWindow(QtGui.QMainWindow):
     def prepareGUI(self):
         # velikost okna nezadávejte ručně - špatně se počítá kvůli toolbaru
         # self.resize(256, 300)
-        self.setWindowTitle('QImage demo #3')
+        self.setWindowTitle("QImage demo #3")
 
         # tlačítko Quit
-        quitAction = QtGui.QAction(QtGui.QIcon('icons/application-exit.png'),
-                                   '&Quit', self)
+        quitAction = QtGui.QAction(
+            QtGui.QIcon("icons/application-exit.png"), "&Quit", self
+        )
         quitAction.triggered.connect(self.close)
-        quitAction.setStatusTip('Quit the application')
-        quitAction.setShortcut('Ctrl+Q')
+        quitAction.setStatusTip("Quit the application")
+        quitAction.setShortcut("Ctrl+Q")
 
         # nástrojový pruh
-        self.toolbar = self.addToolBar('title')
+        self.toolbar = self.addToolBar("title")
         self.toolbar.setMovable(False)
 
         # přidání tlačítka na nástrojový pruh
@@ -84,5 +85,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
