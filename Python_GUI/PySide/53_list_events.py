6ca122295e0171c5deda101e39f90863cbd84a60
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-04 07:56:26 +0100

    Reformatted by black

--- Python_GUI/PySide/53_list_events.py
+++ Python_GUI/PySide/53_list_events.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self, parentWidget):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -21,7 +20,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareGUI(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -29,10 +28,7 @@ class MainWindowContent(QtGui.QWidget):
 
         # seznam prvků
         listWidget = QtGui.QListWidget(self)
-        items = [u'jedna',
-                 u'dva',
-                 u'tři',
-                 u'čtyři']
+        items = [u"jedna", u"dva", u"tři", u"čtyři"]
         listWidget.insertItems(listWidget.currentRow(), items)
 
         # události vyvolávané manipulací se seznamem
@@ -68,7 +64,6 @@ class MainWindowContent(QtGui.QWidget):
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -82,7 +77,7 @@ class MainWindow(QtGui.QMainWindow):
         self.setWindowTitle("QMainWindow + QListWidget")
 
         # vytvoření stavového řádku
-        self.statusBar().showMessage('QMainWindow')
+        self.statusBar().showMessage("QMainWindow")
 
         # vložení komponenty do okna
         self.setCentralWidget(MainWindowContent(self))
@@ -99,5 +94,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
