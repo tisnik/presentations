ae8b45a30df57f16d46d5a5aa06f94253fe117ed
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-14 08:15:13 +0100

    Proper czech comments, 1st version

--- assembler/aarch64/move4.s
+++ assembler/aarch64/move4.s
@@ -2,7 +2,7 @@
 
 # Presun bloku dat.
 #
-# Autor: Pavel Tisnovsky
+# Autor: Pavel Tišnovský
 
 
 
@@ -13,26 +13,26 @@ sys_write=64
 # List of syscalls for AArch64:
 # https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/unistd.h
 
-# pocet bajtu
+# počet bajtu
 rep_count  = 13
 
 
 # Deklarace makra pro ukonceni aplikace
 .macro exit
-        mov  x8, #sys_exit      // cislo sycallu pro funkci "exit"
+        mov  x8, #sys_exit      // číslo sycallu pro funkci "exit"
         mov  x0, #0             // exit code = 0
-        svc  0                  // volani Linuxoveho kernelu
+        svc  0                  // volání Linuxového kernelu
 .endm
 
 
 
 # Deklarace makra pro vytisteni zpravy na standardni vystup
 .macro writeMessage message,messageLength
-        mov  x8, #sys_write       // cislo sycallu pro funkci "write"
+        mov  x8, #sys_write       // číslo sycallu pro funkci "write"
         mov  x0, #1               // standardni vystup
-        ldr  x1, =\message        // adresa retezce, ktery se ma vytisknout
-        mov  x2, #\messageLength  // pocet znaku, ktere se maji vytisknout
-        svc  0                    // volani Linuxoveho kernelu
+        ldr  x1, =\message        // adresa řetězce, ktery se ma vytisknout
+        mov  x2, #\messageLength  // počet znaku, ktere se maji vytisknout
+        svc  0                    // volání Linuxového kernelu
 .endm
 
 
@@ -40,7 +40,7 @@ rep_count  = 13
 .macro moveBlock from,to,length
         ldr   x1, =\from        // adresa bloku pro cteni
         ldr   x2, =\to          // adresa bloku pro zapis
-        mov   x4, #\length      // pocet bajtu
+        mov   x4, #\length      // počet bajtu
 loop\@:
         ldrb  w3, [x1], 1       // cteni bajtu
         strb  w3, [x2], 1       // zapis bajtu
@@ -66,7 +66,7 @@ buffer:
 
 #-----------------------------------------------------------------------------
 .section .text
-        .global _start          // tento symbol ma byt dostupny i linkeru
+        .global _start          // tento symbol má být dostupný i linkeru
 
 _start:
         writeMessage buffer, rep_count
