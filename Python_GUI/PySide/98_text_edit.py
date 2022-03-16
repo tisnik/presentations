4bb347b318f7645bb5cfcfa652d2bdb5c2daca95
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-16 10:18:52 +0100

    Reformatted by black

--- Python_GUI/PySide/98_text_edit.py
+++ Python_GUI/PySide/98_text_edit.py
@@ -10,7 +10,6 @@ from PySide import QtGui
 
 # nový widget bude odvozen od obecného widgetu
 class MainWindowContent(QtGui.QWidget):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindowContent, self).__init__()
@@ -42,7 +41,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareShowTextButton(self):
         # druhé tlačítko
-        showTextButton = QtGui.QPushButton('Show text', self)
+        showTextButton = QtGui.QPushButton("Show text", self)
         showTextButton.resize(showTextButton.sizeHint())
 
         # navázání akce na signál
@@ -51,7 +50,7 @@ class MainWindowContent(QtGui.QWidget):
 
     def prepareQuitButton(self):
         # tlačítko
-        quitButton = QtGui.QPushButton('Quit', self)
+        quitButton = QtGui.QPushButton("Quit", self)
         quitButton.resize(quitButton.sizeHint())
 
         # navázání akce na signál
@@ -67,14 +66,13 @@ class MainWindowContent(QtGui.QWidget):
     def showTextDialog(self):
         msgBox = QtGui.QMessageBox()
         text = self.textEdit.toPlainText()
-        msgBox.setText(u'Text:\n----------------\n{t}\n----------------'.format(t=text))
+        msgBox.setText(u"Text:\n----------------\n{t}\n----------------".format(t=text))
         msgBox.setIcon(QtGui.QMessageBox.Information)
         msgBox.exec_()
 
 
 # nový widget bude odvozen od obecného hlavního okna
 class MainWindow(QtGui.QMainWindow):
-
     def __init__(self):
         # zavoláme konstruktor předka
         super(MainWindow, self).__init__()
@@ -102,5 +100,5 @@ def main():
     MainWindow().run(app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
