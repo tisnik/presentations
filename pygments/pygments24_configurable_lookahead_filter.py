9328fd4df077d9074d3889c2177fe2df5112f79d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-17 10:34:48 +0100

    Reformatted

--- pygments/pygments24_configurable_lookahead_filter.py
+++ pygments/pygments24_configurable_lookahead_filter.py
@@ -11,25 +11,23 @@ import itertools
 
 
 def name_to_snake_case(name):
-    results = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    results = re.sub('([a-z0-9])([A-Z])', r'\1_\2', results)
+    results = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    results = re.sub("([a-z0-9])([A-Z])", r"\1_\2", results)
     return results.lower()
 
 
 def is_function_call_token(current_type, next_type, next_value):
-    return current_type is Name \
-           and next_type is Punctuation \
-           and next_value == "("
+    return current_type is Name and next_type is Punctuation and next_value == "("
 
 
 @simplefilter
 def to_snake_case(self, lexer, stream, options):
-    convert_all_names = get_bool_opt(options, 'convert_all_names', default=False)
+    convert_all_names = get_bool_opt(options, "convert_all_names", default=False)
     if convert_all_names:
         for ttype, value in stream:
             if ttype is Name.Function or ttype is Name:
-                results = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', value)
-                results = re.sub('([a-z0-9])([A-Z])', r'\1_\2', results)
+                results = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", value)
+                results = re.sub("([a-z0-9])([A-Z])", r"\1_\2", results)
                 value = results.lower()
             yield ttype, value
     else:
@@ -42,8 +40,9 @@ def to_snake_case(self, lexer, stream, options):
             if next_token:
                 next_type = next_token[0]
                 next_value = next_token[1]
-                if current_type is Name.Function \
-                   or is_function_call_token(current_type, next_type, next_value):
+                if current_type is Name.Function or is_function_call_token(
+                    current_type, next_type, next_value
+                ):
                     current_value = name_to_snake_case(current_value)
             yield current_type, current_value
 
