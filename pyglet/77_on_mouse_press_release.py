edf4d8dfc5275ef7303392d8362eca6f7a86002b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:21:00 +0100

    Reformatted

--- pyglet/77_on_mouse_press_release.py
+++ pyglet/77_on_mouse_press_release.py
@@ -8,23 +8,17 @@ RED = (255, 128, 128, 255)
 
 
 def create_window(width, height):
-    return pyglet.window.Window(width=width,
-                                height=height,
-                                caption="Pyglet library")
+    return pyglet.window.Window(width=width, height=height, caption="Pyglet library")
 
 
 def create_gray_label(text, x, y, anchor_x, anchor_y):
-    return pyglet.text.Label(text,
-                             font_size=18,
-                             x=x,
-                             y=y,
-                             anchor_x=anchor_x,
-                             anchor_y=anchor_y,
-                             color=GRAY)
+    return pyglet.text.Label(
+        text, font_size=18, x=x, y=y, anchor_x=anchor_x, anchor_y=anchor_y, color=GRAY
+    )
 
 
 window = create_window(640, 480)
-label = create_gray_label('Mouse:', 10, 10, 'left', 'bottom')
+label = create_gray_label("Mouse:", 10, 10, "left", "bottom")
 
 
 @window.event
@@ -33,15 +27,16 @@ def on_draw():
     label.draw()
 
 
-button_names = {pyglet.window.mouse.LEFT: "left",
-                pyglet.window.mouse.RIGHT: "right",
-                pyglet.window.mouse.MIDDLE: "middle"}
+button_names = {
+    pyglet.window.mouse.LEFT: "left",
+    pyglet.window.mouse.RIGHT: "right",
+    pyglet.window.mouse.MIDDLE: "middle",
+}
 
 
 def on_mouse_action(x, y, button, action):
     button_name = button_names.get(button, "unknown")
-    text = format("Mouse %s %s button at [%d, %d]" %
-                  (action, button_name, x, y))
+    text = format("Mouse %s %s button at [%d, %d]" % (action, button_name, x, y))
     label.text = text
     print(text)
     on_draw()
