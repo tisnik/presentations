c721458e8834208d309d41a61fb6f783cc32b1ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:21:00 +0100

    Reformatted

--- pyglet/40_texture_smooth.py
+++ pyglet/40_texture_smooth.py
@@ -3,21 +3,18 @@
 import pyglet
 from pyglet.gl import *
 
-window = pyglet.window.Window(width=500,
-                              height=500,
-                              caption="Pyglet library")
+window = pyglet.window.Window(width=500, height=500, caption="Pyglet library")
 
 image_stream = open("gnome-globe.png", "rb")
-image = pyglet.image.load('gnome-globe.png', file=image_stream)
+image = pyglet.image.load("gnome-globe.png", file=image_stream)
 texture = image.get_texture()
 
 
-
 def init():
-    glClearColor(0.0, 0.0, 0.3, 0.0)          # barva pozadi obrazku
-    glPolygonMode(GL_FRONT, GL_FILL)          # nastaveni rezimu vykresleni modelu
+    glClearColor(0.0, 0.0, 0.3, 0.0)  # barva pozadi obrazku
+    glPolygonMode(GL_FRONT, GL_FILL)  # nastaveni rezimu vykresleni modelu
     glPolygonMode(GL_BACK, GL_FILL)
-    glDisable(GL_CULL_FACE)                   # zadne hrany ani steny se nebudou odstranovat
+    glDisable(GL_CULL_FACE)  # zadne hrany ani steny se nebudou odstranovat
 
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
@@ -25,41 +22,39 @@ def init():
     glBindTexture(GL_TEXTURE_2D, texture.id)
 
 
-
 @window.event
 def on_resize(width, height):
     init()
-    glViewport(0, 0, width, height)           # viditelna oblast pres cele okno
-
+    glViewport(0, 0, width, height)  # viditelna oblast pres cele okno
 
 
 def draw_quad(x, y):
-    glBegin(GL_QUADS) 
+    glBegin(GL_QUADS)
 
     glTexCoord2f(0.4, 0.4)
-    glVertex2i(x, y) 
+    glVertex2i(x, y)
 
     glTexCoord2f(0.6, 0.4)
-    glVertex2i(x+400, y) 
+    glVertex2i(x + 400, y)
 
     glTexCoord2f(0.6, 0.6)
-    glVertex2i(x+400, y+400) 
+    glVertex2i(x + 400, y + 400)
 
     glTexCoord2f(0.4, 0.6)
-    glVertex2i(x, y+400) 
+    glVertex2i(x, y + 400)
 
-    glEnd() 
+    glEnd()
 
 
 @window.event
 def on_draw():
-    glClear(GL_COLOR_BUFFER_BIT)              # vymazani vsech bitovych rovin barvoveho bufferu
+    glClear(GL_COLOR_BUFFER_BIT)  # vymazani vsech bitovych rovin barvoveho bufferu
     glLoadIdentity()
-    glClearColor(0.0, 0.0, 0.0, 0.0)            # nastaveni mazaci barvy na cernou
-    glClear(GL_COLOR_BUFFER_BIT)                # vymazani bitovych rovin barvoveho bufferu
+    glClearColor(0.0, 0.0, 0.0, 0.0)  # nastaveni mazaci barvy na cernou
+    glClear(GL_COLOR_BUFFER_BIT)  # vymazani bitovych rovin barvoveho bufferu
 
     glEnable(GL_TEXTURE_2D)
-    draw_quad(50, 50) 
+    draw_quad(50, 50)
+
 
 pyglet.app.run()
-
