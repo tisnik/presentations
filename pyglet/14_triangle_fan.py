e9fa2a0f4e252d8ef644133981584c08b5c629ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:20:59 +0100

    Reformatted

--- pyglet/14_triangle_fan.py
+++ pyglet/14_triangle_fan.py
@@ -7,43 +7,41 @@ import pyglet
 from pyglet.gl import *
 
 # vytvoření okna
-window = pyglet.window.Window(width=450,
-                              height=350,
-                              caption="Pyglet+OpenGL")
+window = pyglet.window.Window(width=450, height=350, caption="Pyglet+OpenGL")
 
 
 def draw_triangle_fan(x, y):
-    glBegin(GL_TRIANGLE_FAN) 
-    glColor3f(1.0, 0.0, 0.0)                 # kazdy vertex bude vykresleny jinou barvou
-    glVertex2i(x+175, y) 
-    glColor3f(0.0, 0.0, 1.0) 
-    glVertex2i(x+50, y+80) 
-    glColor3f(0.0, 1.0, 0.0) 
-    glVertex2i(x+100, y+80) 
-    glColor3f(1.0, 1.0, 0.0)                 # kazdy vertex bude vykresleny jinou barvou
-    glVertex2i(x+150, y+80) 
-    glColor3f(0.0, 1.0, 1.0) 
-    glVertex2i(x+200, y+80) 
-    glColor3f(1.0, 0.0, 1.0) 
-    glVertex2i(x+250, y+80) 
-    glColor3f(1.0, 1.0, 1.0) 
-    glVertex2i(x+300, y+80) 
-    glEnd() 
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(1.0, 0.0, 0.0)  # kazdy vertex bude vykresleny jinou barvou
+    glVertex2i(x + 175, y)
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex2i(x + 50, y + 80)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex2i(x + 100, y + 80)
+    glColor3f(1.0, 1.0, 0.0)  # kazdy vertex bude vykresleny jinou barvou
+    glVertex2i(x + 150, y + 80)
+    glColor3f(0.0, 1.0, 1.0)
+    glVertex2i(x + 200, y + 80)
+    glColor3f(1.0, 0.0, 1.0)
+    glVertex2i(x + 250, y + 80)
+    glColor3f(1.0, 1.0, 1.0)
+    glVertex2i(x + 300, y + 80)
+    glEnd()
+
 
 @window.event
 def on_draw():
     glClear(GL_COLOR_BUFFER_BIT)
     glLoadIdentity()
 
-    glClearColor(0.0, 0.0, 0.0, 0.0)            # nastaveni mazaci barvy na cernou
-    glClear(GL_COLOR_BUFFER_BIT)                # vymazani bitovych rovin barvoveho bufferu
+    glClearColor(0.0, 0.0, 0.0, 0.0)  # nastaveni mazaci barvy na cernou
+    glClear(GL_COLOR_BUFFER_BIT)  # vymazani bitovych rovin barvoveho bufferu
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-    draw_triangle_fan(50, 50) 
+    draw_triangle_fan(50, 50)
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-    draw_triangle_fan(50, 200) 
+    draw_triangle_fan(50, 200)
 
 
 pyglet.app.run()
-
