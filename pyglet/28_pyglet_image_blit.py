baed436d8219b9afea118ce3ad5b60db2bfd9e9c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:20:59 +0100

    Reformatted

--- pyglet/28_pyglet_image_blit.py
+++ pyglet/28_pyglet_image_blit.py
@@ -2,20 +2,20 @@
 
 import pyglet
 
-window = pyglet.window.Window(width=640,
-                              height=480,
-                              caption="Pyglet library")
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
 
 image_stream = open("gnome-globe.png", "rb")
-image = pyglet.image.load('gnome-globe.png', file=image_stream)
+image = pyglet.image.load("gnome-globe.png", file=image_stream)
 
 print("Loaded image with size %d x %d pixels" % (image.width, image.height))
 
-label = pyglet.text.Label("Pyglet library",
-                          font_name="Terminus",
-                          font_size=36,
-                          x=window.width//2,
-                          y=window.height//2)
+label = pyglet.text.Label(
+    "Pyglet library",
+    font_name="Terminus",
+    font_size=36,
+    x=window.width // 2,
+    y=window.height // 2,
+)
 
 
 @window.event
