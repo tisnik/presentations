78af5f09a5642cd01bcb10578106752a1e13f903
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:20:59 +0100

    Reformatted

--- pyglet/08_triangle.py
+++ pyglet/08_triangle.py
@@ -17,9 +17,7 @@ import pyglet
 from pyglet.gl import *
 
 # vytvoření okna
-window = pyglet.window.Window(width=640,
-                              height=480,
-                              caption="Pyglet+OpenGL")
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet+OpenGL")
 
 
 @window.event
@@ -29,7 +27,7 @@ def on_draw():
     glLoadIdentity()
     glBegin(GL_TRIANGLES)
     glColor3f(1, 0, 0)
-    glVertex2f(window.width/2, 0)
+    glVertex2f(window.width / 2, 0)
     glColor3f(0, 1, 0)
     glVertex2f(0, window.height)
     glColor3f(0, 0, 1)
