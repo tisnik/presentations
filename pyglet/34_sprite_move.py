aedf76ba9a68c645b69bb94eaee7c1ce22b4c9b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:20:59 +0100

    Reformatted

--- pyglet/34_sprite_move.py
+++ pyglet/34_sprite_move.py
@@ -2,21 +2,21 @@
 
 import pyglet
 
-window = pyglet.window.Window(width=640,
-                              height=480,
-                              caption="Pyglet library")
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
 
 image_stream = open("gnome-globe.png", "rb")
-image = pyglet.image.load('gnome-globe.png', file=image_stream)
+image = pyglet.image.load("gnome-globe.png", file=image_stream)
 
 sprite = pyglet.sprite.Sprite(image)
 sprite.dx = 1
 sprite.dy = 1
 
+
 @window.event
 def on_draw():
     window.clear()
     sprite.draw()
+
 
 def update(dt):
     sprite.x += sprite.dx * dt * 200
@@ -34,6 +34,6 @@ def update(dt):
         sprite.y = window.height - sprite.height
         sprite.dy = -sprite.dy
 
-pyglet.clock.schedule_interval(update, 1/60.)
-pyglet.app.run()
 
+pyglet.clock.schedule_interval(update, 1 / 60.0)
+pyglet.app.run()
