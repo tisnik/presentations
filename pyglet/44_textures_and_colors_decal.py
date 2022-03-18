bbd69f3a4b336038af7364e8982e7974098065a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:21:00 +0100

    Reformatted

--- pyglet/44_textures_and_colors_decal.py
+++ pyglet/44_textures_and_colors_decal.py
@@ -3,20 +3,18 @@
 import pyglet
 from pyglet.gl import *
 
-window = pyglet.window.Window(width=450,
-                              height=450,
-                              caption="Pyglet library")
+window = pyglet.window.Window(width=450, height=450, caption="Pyglet library")
 
 image_stream = open("gnome-globe.png", "rb")
-image = pyglet.image.load('gnome-globe.png', file=image_stream)
+image = pyglet.image.load("gnome-globe.png", file=image_stream)
 texture = image.get_texture()
 
 
 def init():
-    glClearColor(0.0, 0.0, 0.3, 0.0)       # barva pozadi obrazku
-    glPolygonMode(GL_FRONT, GL_FILL)       # nastaveni rezimu vykresleni modelu
+    glClearColor(0.0, 0.0, 0.3, 0.0)  # barva pozadi obrazku
+    glPolygonMode(GL_FRONT, GL_FILL)  # nastaveni rezimu vykresleni modelu
     glPolygonMode(GL_BACK, GL_FILL)
-    glDisable(GL_CULL_FACE)                # zadne hrany ani steny se nebudou odstranovat
+    glDisable(GL_CULL_FACE)  # zadne hrany ani steny se nebudou odstranovat
 
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
@@ -29,7 +27,7 @@ def init():
 @window.event
 def on_resize(width, height):
     init()
-    glViewport(0, 0, width, height)        # viditelna oblast pres cele okno
+    glViewport(0, 0, width, height)  # viditelna oblast pres cele okno
 
 
 def draw_quad(x, y):
@@ -39,23 +37,23 @@ def draw_quad(x, y):
     glVertex2i(x, y)
 
     glTexCoord2f(1.0, 0.0)
-    glVertex2i(x+150, y)
+    glVertex2i(x + 150, y)
 
     glTexCoord2f(1.0, 1.0)
-    glVertex2i(x+150, y+150)
+    glVertex2i(x + 150, y + 150)
 
     glTexCoord2f(0.0, 1.0)
-    glVertex2i(x, y+150)
+    glVertex2i(x, y + 150)
 
     glEnd()
 
 
 @window.event
 def on_draw():
-    glClear(GL_COLOR_BUFFER_BIT)           # vymazani vsech bitovych rovin barvoveho bufferu
+    glClear(GL_COLOR_BUFFER_BIT)  # vymazani vsech bitovych rovin barvoveho bufferu
     glLoadIdentity()
-    glClearColor(0.0, 0.0, 0.0, 0.0)       # nastaveni mazaci barvy na cernou
-    glClear(GL_COLOR_BUFFER_BIT)           # vymazani bitovych rovin barvoveho bufferu
+    glClearColor(0.0, 0.0, 0.0, 0.0)  # nastaveni mazaci barvy na cernou
+    glClear(GL_COLOR_BUFFER_BIT)  # vymazani bitovych rovin barvoveho bufferu
 
     glEnable(GL_TEXTURE_2D)
 
