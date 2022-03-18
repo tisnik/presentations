5d425d3fe0443da157bf902a0153776c66622374
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-18 15:20:59 +0100

    Reformatted

--- pyglet/04_label.py
+++ pyglet/04_label.py
@@ -14,16 +14,16 @@
 import pyglet
 
 # vytvoření okna
-window = pyglet.window.Window(width=640,
-                              height=480,
-                              caption="Pyglet library")
+window = pyglet.window.Window(width=640, height=480, caption="Pyglet library")
 
 # textové návěští, které se má zobrazit v oknu
-label = pyglet.text.Label("Pyglet library",
-                          font_name="Terminus",
-                          font_size=36,
-                          x=window.width//2,
-                          y=window.height//2)
+label = pyglet.text.Label(
+    "Pyglet library",
+    font_name="Terminus",
+    font_size=36,
+    x=window.width // 2,
+    y=window.height // 2,
+)
 
 
 @window.event
