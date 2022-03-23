e924337e4d3fb80e760b15265642d9740ca19e62
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-23 08:31:15 +0100

    Load zero into s1 register

---
+++ assembler/aarch64/fmov4.s
@@ -0,0 +1,38 @@
+# asmsyntax=as
+
+# Načtení FP konstanty do registru s1
+# v assembleru GNU AS pro architekturu AArch64.
+#
+# Autor: Pavel Tišnovský
+
+
+
+# Linux kernel system call table
+sys_exit=93
+
+# List of syscalls for AArch64:
+# https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/unistd.h
+
+
+
+#-----------------------------------------------------------------------------
+.section .data
+
+
+
+#-----------------------------------------------------------------------------
+.section .bss
+
+
+
+#-----------------------------------------------------------------------------
+.section .text
+        .global _start          // tento symbol má být dostupný i z linkeru
+
+_start:
+        fmov s1, wzr            // načtení konstanty do registru
+
+        mov  x8, #sys_exit      // číslo sycallu pro funkci "exit"
+        mov  x0, #0             // exit code = 0
+        svc  0                  // volání Linuxového kernelu
+
