b4f4a0ed4e34117ad8107aceaa0cc9eda8a3880c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-28 08:20:45 +0200

    Example #9: compare for various types (no overloading)

---
+++ docs/coffee-talks/go_generics/09_comparable_variable_types.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func compareInts(x int, y int) bool {
+	return x < y
+}
+
+func compareFloats(x float64, y float64) bool {
+	return x < y
+}
+
+func compareStrings(x string, y string) bool {
+	return x < y
+}
+
+func main() {
+	fmt.Println(compareInts(1, 2))
+	fmt.Println(compareFloats(1.5, 2.6))
+	fmt.Println(compareStrings("foo", "bar"))
+}
