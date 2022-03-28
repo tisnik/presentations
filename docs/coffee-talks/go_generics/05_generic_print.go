a308e3e9723412f9e92d5f49b807245603b4a8cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-28 08:20:45 +0200

    Example #5: generic print function

---
+++ docs/coffee-talks/go_generics/05_generic_print.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func printValue[T any](value T) {
+	fmt.Println(value)
+}
+
+func main() {
+	printValue("www.root.cz")
+	printValue('*')
+	printValue(42)
+	printValue(3.14)
+	printValue(1 + 2i)
+	printValue([]int{1, 2, 3})
+}
