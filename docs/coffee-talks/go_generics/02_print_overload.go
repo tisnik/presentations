af5e3bea3a16f52c4c06c7ec77b5cf5e009c95d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-28 08:20:45 +0200

    Example #2: function overloading not supported

---
+++ docs/coffee-talks/go_generics/02_print_overload.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func printValue(value string) {
+	fmt.Println(value)
+}
+
+func printValue(int string) {
+	fmt.Println(value)
+}
+
+func main() {
+	printValue("www.root.cz")
+}
