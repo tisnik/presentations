f772d794bca8aea2cfb16d5b6b81d1098af4f39e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-28 08:20:45 +0200

    Example #6: type parameter(s)

---
+++ docs/coffee-talks/go_generics/06_type_parameter.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func printValue[T any](value T) {
+	fmt.Println(value)
+}
+
+func main() {
+	printValue[string]("www.root.cz")
+	printValue[rune]('*')
+	printValue[int](42)
+	printValue[float32](3.14)
+	printValue[complex64](1 + 2i)
+	printValue[[]int]([]int{1, 2, 3})
+}
