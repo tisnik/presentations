da61aba0b2cd00384341685f51a6a7862d586233
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-28 08:20:45 +0200

    Example #12: type parameters + return value

---
+++ docs/coffee-talks/go_generics/12_add_type_parameters.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type numeric interface {
+	int | float64 | complex128
+}
+
+func add[T numeric](x T, y T) T {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(1, 2))
+	fmt.Println(add(1.5, 2.6))
+	fmt.Println(add(1i, 2+4i))
+}
