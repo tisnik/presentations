ab687c451ad9e28bae9afbb33a40f06d2807314a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-03-28 08:20:45 +0200

    Example #4: using interface{}

---
+++ docs/coffee-talks/go_generics/04_print_interface.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func printValue(value interface{}) {
+	fmt.Println(value)
+}
+
+func main() {
+	printValue("www.root.cz")
+	printValue('*')
+	printValue(42)
+	printValue(3.14)
+	printValue(1 + 2i)
+	printValue([]int{1, 2, 3})
+}
