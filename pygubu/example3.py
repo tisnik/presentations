c046cb57ed91687d15233e44c3528f7702bba742
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-12 10:07:28 +0200

    Reformatted by Black

--- pygubu/example3.py
+++ pygubu/example3.py
@@ -16,16 +16,16 @@ class Example3App(pygubu.TkApplication):
         self.builder = builder = pygubu.Builder()
 
         # step #2: Load an ui file
-        builder.add_from_file('example3.ui')
+        builder.add_from_file("example3.ui")
 
         # step #2B: Specify path to images and other resources
         builder.add_resource_path(".")
 
         # step #3: Create the mainwindow
-        self.mainwindow = builder.get_object('MainWindow', self.master)
+        self.mainwindow = builder.get_object("MainWindow", self.master)
 
         # step #4: Set main menu
-        self.mainmenu = menu = builder.get_object('MainMenu', self.master)
+        self.mainmenu = menu = builder.get_object("MainMenu", self.master)
         self.set_menu(menu)
 
         # step $5: Configure callbacks
@@ -33,16 +33,16 @@ class Example3App(pygubu.TkApplication):
 
     def on_button_clicked(self):
         """Define handler for Quit button."""
-        tk.messagebox.showinfo('Message', 'You clicked on Quit button')
+        tk.messagebox.showinfo("Message", "You clicked on Quit button")
         root.destroy()
 
     def on_command_quit_selected(self):
         """Define handler for Quit command."""
-        tk.messagebox.showinfo('Message', 'You selected Quit command')
+        tk.messagebox.showinfo("Message", "You selected Quit command")
         root.destroy()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # needed to have a menu
     root = tk.Tk()
 
