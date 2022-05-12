9e1e99548f8462387d73dc1ebcb32c7ad4c8c75a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-12 10:07:28 +0200

    Reformatted by Black

--- pygubu/example9A.py
+++ pygubu/example9A.py
@@ -17,18 +17,18 @@ class Example9App(pygubu.TkApplication):
         self.builder = builder = pygubu.Builder()
 
         # step #2: Load an ui file
-        builder.add_from_file('example9.ui')
+        builder.add_from_file("example9.ui")
 
         # step #2B: Specify path to images and other resources
         builder.add_resource_path(".")
 
         # step #3: Create the mainwindow
-        self.mainwindow = builder.get_object('MainWindow', self.master)
+        self.mainwindow = builder.get_object("MainWindow", self.master)
 
         # step #4: Configure callbacks
         builder.connect_callbacks(self)
 
-        root.bind('<Control-q>', lambda event: self.on_quit_button_click())
+        root.bind("<Control-q>", lambda event: self.on_quit_button_click())
 
     def on_quit_button_click(self):
         root.destroy()
@@ -36,16 +36,19 @@ class Example9App(pygubu.TkApplication):
     def on_button_display_selections_click(self):
         vars = self.builder.tkvariables
 
-        message = \
-            "Checkbutton A: {}\n" \
-            "Checkbutton B: {}\n" \
-            "Checkbutton C: {}\n".format(vars["checkbutton_a"].get(),
-                                         vars["checkbutton_b"].get(),
-                                         vars["checkbutton_c"].get())
+        message = (
+            "Checkbutton A: {}\n"
+            "Checkbutton B: {}\n"
+            "Checkbutton C: {}\n".format(
+                vars["checkbutton_a"].get(),
+                vars["checkbutton_b"].get(),
+                vars["checkbutton_c"].get(),
+            )
+        )
         messagebox.askokcancel("Selections", message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # needed to have a menu
     root = tk.Tk()
 
