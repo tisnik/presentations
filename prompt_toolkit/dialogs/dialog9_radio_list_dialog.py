3935a71707c6cf8e26122f97dd624a52b902a400
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: dialogs/dialog9_radio_list_dialog.py

--- prompt_toolkit/dialogs/dialog9_radio_list_dialog.py
+++ prompt_toolkit/dialogs/dialog9_radio_list_dialog.py
@@ -1,19 +1,17 @@
 from prompt_toolkit.shortcuts import message_dialog, radiolist_dialog
 
 response = radiolist_dialog(
-    title='Zadání příkazu',
-    text='Zadejte příkaz (quit, exit, help, eval):',
-    values=[
-        ('quit', 'Quit'),
-        ('exit', 'Exit'),
-        ('help', 'Help'),
-        ('eval', 'Eval')])
+    title="Zadání příkazu",
+    text="Zadejte příkaz (quit, exit, help, eval):",
+    values=[("quit", "Quit"), ("exit", "Exit"), ("help", "Help"), ("eval", "Eval")],
+)
 
 if response is not None:
     message_dialog(
-        title='Zadání příkazu',
-        text='zadali jste: {command}'.format(command=response if response else 'nic :)'))
+        title="Zadání příkazu",
+        text="zadali jste: {command}".format(
+            command=response if response else "nic :)"
+        ),
+    )
 else:
-    message_dialog(
-        title='Zadání uživatelského jména',
-        text='Příkaz nebyl zadán')
+    message_dialog(title="Zadání uživatelského jména", text="Příkaz nebyl zadán")
