97efb2bda1d9d4e587163d94d6f4f1427733c737
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: dialogs/dialog2_basic_message_dialog_button.py

--- prompt_toolkit/dialogs/dialog2_basic_message_dialog_button.py
+++ prompt_toolkit/dialogs/dialog2_basic_message_dialog_button.py
@@ -1,6 +1,7 @@
 from prompt_toolkit.shortcuts import message_dialog
 
 message_dialog(
-    title='Software failure',
-    text='Guru Meditation #12345678.ABCDEFFF\nPress ENTER to continue.',
-    ok_text='[Enter]')
+    title="Software failure",
+    text="Guru Meditation #12345678.ABCDEFFF\nPress ENTER to continue.",
+    ok_text="[Enter]",
+)
