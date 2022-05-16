ba384c7991e08b5a1431a3142d354e49290b192f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: dialogs/dialog3_yes_no_dialog.py

--- prompt_toolkit/dialogs/dialog3_yes_no_dialog.py
+++ prompt_toolkit/dialogs/dialog3_yes_no_dialog.py
@@ -1,9 +1,7 @@
 from prompt_toolkit.shortcuts import message_dialog, yes_no_dialog
 
 response = yes_no_dialog(
-    title='Software failure',
-    text='Guru Meditation #12345678.ABCDEFFF\nRestart system?')
+    title="Software failure", text="Guru Meditation #12345678.ABCDEFFF\nRestart system?"
+)
 
-message_dialog(
-    title='Your choice',
-    text='Yes' if response else 'No')
+message_dialog(title="Your choice", text="Yes" if response else "No")
