84489d98ba8bf1b930265447148fca02b35a59d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:07 +0200

    New results for benchmarking in EDN format: tui/tui17_buttons_styles.py

--- prompt_toolkit/tui/tui17_buttons_styles.py
+++ prompt_toolkit/tui/tui17_buttons_styles.py
@@ -11,56 +11,57 @@ from prompt_toolkit.styles import Style
 
 
 def button1_clicked():
-    text_area.text += 'Button 1 clicked\n'
+    text_area.text += "Button 1 clicked\n"
 
 
 def button2_clicked():
-    text_area.text += 'Button 2 clicked\n'
+    text_area.text += "Button 2 clicked\n"
 
 
 def button3_clicked():
-    text_area.text += 'Button 3 clicked\n'
+    text_area.text += "Button 3 clicked\n"
 
 
 def exit_clicked():
     get_app().exit()
 
 
-button1 = Button('Button 1', handler=button1_clicked)
-button2 = Button('Button 2', handler=button2_clicked)
-button3 = Button('Button 3', handler=button3_clicked)
-button4 = Button('Exit', handler=exit_clicked)
+button1 = Button("Button 1", handler=button1_clicked)
+button2 = Button("Button 2", handler=button2_clicked)
+button3 = Button("Button 3", handler=button3_clicked)
+button4 = Button("Exit", handler=exit_clicked)
 
-buttons = HSplit([button1,
-                  button2,
-                  button3,
-                  button4])
+buttons = HSplit([button1, button2, button3, button4])
 
 text_area = TextArea(focusable=False)
 
 # správce rozvržení
-root = VSplit([Box(Frame(buttons, style="class:button_panel"),
-                   padding=2),
-               Box(Frame(text_area, title="Events", style="class:edit_panel"),
-                   padding=2)])
+root = VSplit(
+    [
+        Box(Frame(buttons, style="class:button_panel"), padding=2),
+        Box(Frame(text_area, title="Events", style="class:edit_panel"), padding=2),
+    ]
+)
 
 layout = Layout(root)
 
 # napojení na klávesové zkratky
 key_bindings = KeyBindings()
-key_bindings.add('up')(focus_previous)
-key_bindings.add('down')(focus_next)
+key_bindings.add("up")(focus_previous)
+key_bindings.add("down")(focus_next)
 
-style = Style([
-    ('button_panel', 'bg:#ansiblue #ansiwhite'),
-    ('edit_panel',   'bg:#ansigreen #000000'),
-    ('button',       '#ansibrightyellow'),
-    ('button focused', 'bg:#ff0000'),
-    ('text-area focused', 'bg:#ff0000'),
-])
+style = Style(
+    [
+        ("button_panel", "bg:#ansiblue #ansiwhite"),
+        ("edit_panel", "bg:#ansigreen #000000"),
+        ("button", "#ansibrightyellow"),
+        ("button focused", "bg:#ff0000"),
+        ("text-area focused", "bg:#ff0000"),
+    ]
+)
 
 
-@key_bindings.add('escape')
+@key_bindings.add("escape")
 def on_escape_press(event):
     """Callback funkce volaná při stisku klávesy Esc."""
     print("\n\n[escape]\n\n")
@@ -69,14 +70,13 @@ def on_escape_press(event):
 
 def main():
     # vytvoření aplikace s textovým uživatelským rozhraním
-    application = Application(layout=layout,
-                              key_bindings=key_bindings,
-                              full_screen=True,
-                              style=style)
+    application = Application(
+        layout=layout, key_bindings=key_bindings, full_screen=True, style=style
+    )
 
     # spuštění aplikace
     application.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
