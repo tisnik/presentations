3ace94a33a2b3e994947db91cc45cb9fb85fc56d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: dialogs/dialog5_button_dialog.py

--- prompt_toolkit/dialogs/dialog5_button_dialog.py
+++ prompt_toolkit/dialogs/dialog5_button_dialog.py
@@ -1,13 +1,9 @@
 from prompt_toolkit.shortcuts import message_dialog, button_dialog
 
 response = button_dialog(
-    title='Tento program provedl neplatnou operaci',
-    text='Not ready reading drive A',
-    buttons=[
-        ('Abort', 'abort'),
-        ('Retry', 'retry'),
-        ('Fail', 'fail')])
+    title="Tento program provedl neplatnou operaci",
+    text="Not ready reading drive A",
+    buttons=[("Abort", "abort"), ("Retry", "retry"), ("Fail", "fail")],
+)
 
-message_dialog(
-    title='Zadali jste volbu',
-    text=response)
+message_dialog(title="Zadali jste volbu", text=response)
