5028bb96c35c67bf6ba567e934b713878f137099
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: dialogs/dialog7_basic_input_dialog.py

--- prompt_toolkit/dialogs/dialog7_basic_input_dialog.py
+++ prompt_toolkit/dialogs/dialog7_basic_input_dialog.py
@@ -1,14 +1,11 @@
 from prompt_toolkit.shortcuts import message_dialog, input_dialog
 
-response = input_dialog(
-    title='Zadání uživatelského jména',
-    text='Uživatelské jméno:')
+response = input_dialog(title="Zadání uživatelského jména", text="Uživatelské jméno:")
 
 if response is not None:
     message_dialog(
-        title='Zadání uživatelského jména',
-        text='zadali jste: {name}'.format(name=response if response else 'nic :)'))
+        title="Zadání uživatelského jména",
+        text="zadali jste: {name}".format(name=response if response else "nic :)"),
+    )
 else:
-    message_dialog(
-        title='Zadání uživatelského jména',
-        text='Jméno nebylo zadáno')
+    message_dialog(title="Zadání uživatelského jména", text="Jméno nebylo zadáno")
