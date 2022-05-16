5f5fcb35a5bac5052b371ab786a7bc4e80bdd110
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: print/print9_use_pygments.py

--- prompt_toolkit/print/print9_use_pygments.py
+++ prompt_toolkit/print/print9_use_pygments.py
@@ -3,18 +3,18 @@ from prompt_toolkit import print_formatted_text
 from prompt_toolkit.formatted_text import PygmentsTokens
 
 text = [
-    (Token.Keyword, 'print'),
-    (Token.Punctuation, '('),
+    (Token.Keyword, "print"),
+    (Token.Punctuation, "("),
     (Token.Literal.String.Double, '"'),
-    (Token.Literal.String, 'hello'),
+    (Token.Literal.String, "hello"),
     (Token.Literal.String.Double, '"'),
-    (Token.Punctuation, ','),
-    (Token.Text.Whitespace, ' '),
+    (Token.Punctuation, ","),
+    (Token.Text.Whitespace, " "),
     (Token.Literal.String.Single, '"'),
-    (Token.Literal.String, 'world'),
+    (Token.Literal.String, "world"),
     (Token.Literal.String.Single, '"'),
-    (Token.Punctuation, ')'),
-    (Token.Text, '\n'),
+    (Token.Punctuation, ")"),
+    (Token.Text, "\n"),
 ]
 
 print_formatted_text(PygmentsTokens(text))
