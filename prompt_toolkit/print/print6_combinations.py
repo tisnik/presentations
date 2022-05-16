12cbe213baa472fe1acd79b7d461b053eb02cf09
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: print/print6_combinations.py

--- prompt_toolkit/print/print6_combinations.py
+++ prompt_toolkit/print/print6_combinations.py
@@ -4,7 +4,8 @@ from prompt_toolkit.output.vt100 import FG_ANSI_COLORS, BG_ANSI_COLORS
 for bg_color in sorted(BG_ANSI_COLORS):
     for fg_color in sorted(FG_ANSI_COLORS):
         message = "<p fg='{fg_color}' bg='{bg_color}'>XX <u>XX</u> <i>XX</i> <b>XX</b>   </p>".format(
-            fg_color=fg_color, bg_color=bg_color)
+            fg_color=fg_color, bg_color=bg_color
+        )
         print_formatted_text(HTML(message), end="")
 
     print()
