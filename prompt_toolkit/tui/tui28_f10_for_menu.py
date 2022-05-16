0abf2ac20bd2bd6dd163d303e1a4d250ed13369d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:07 +0200

    New results for benchmarking in EDN format: tui/tui28_f10_for_menu.py

--- prompt_toolkit/tui/tui28_f10_for_menu.py
+++ prompt_toolkit/tui/tui28_f10_for_menu.py
@@ -13,17 +13,17 @@ from prompt_toolkit.widgets import *
 
 # napojení na klávesové zkratky
 key_bindings = KeyBindings()
-key_bindings.add('s-tab')(focus_previous)
-key_bindings.add('tab')(focus_next)
+key_bindings.add("s-tab")(focus_previous)
+key_bindings.add("tab")(focus_next)
 
 
-@key_bindings.add('escape')
+@key_bindings.add("escape")
 def on_exit_selected(event=None):
     """Callback funkce volaná při stisku klávesy Esc."""
     get_app().exit()
 
 
-@key_bindings.add('f10')
+@key_bindings.add("f10")
 def on_f10_pressed(event=None):
     """Callback funkce volaná při stisku klávesy F10."""
     get_app().layout.focus(menu.window)
@@ -39,13 +39,13 @@ layout = Layout(menu, focused_element=text_area)
 
 def main():
     # vytvoření aplikace s textovým uživatelským rozhraním
-    application = Application(layout=layout,
-                              key_bindings=key_bindings,
-                              full_screen=True)
+    application = Application(
+        layout=layout, key_bindings=key_bindings, full_screen=True
+    )
 
     # spuštění aplikace
     application.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
