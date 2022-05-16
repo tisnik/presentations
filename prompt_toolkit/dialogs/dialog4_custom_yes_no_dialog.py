fc208292a073df77a93ef9bcce335ffd69293d19
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: dialogs/dialog4_custom_yes_no_dialog.py

--- prompt_toolkit/dialogs/dialog4_custom_yes_no_dialog.py
+++ prompt_toolkit/dialogs/dialog4_custom_yes_no_dialog.py
@@ -1,11 +1,10 @@
 from prompt_toolkit.shortcuts import message_dialog, yes_no_dialog
 
 response = yes_no_dialog(
-    title='Tento program provedl neplatnou operaci',
-    text='Nevíme, co se stalo, známe jen kód chyby:\n#12345678.ABCDEFFF\n\nProvést restart?',
-    yes_text='Ano',
-    no_text='Ne')
+    title="Tento program provedl neplatnou operaci",
+    text="Nevíme, co se stalo, známe jen kód chyby:\n#12345678.ABCDEFFF\n\nProvést restart?",
+    yes_text="Ano",
+    no_text="Ne",
+)
 
-message_dialog(
-    title='Zadali jste volbu',
-    text='Ano' if response else 'Ne')
+message_dialog(title="Zadali jste volbu", text="Ano" if response else "Ne")
