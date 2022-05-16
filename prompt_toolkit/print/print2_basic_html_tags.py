07237fd689871b1765710a034b76f7454f080303
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:06 +0200

    New results for benchmarking in EDN format: print/print2_basic_html_tags.py

--- prompt_toolkit/print/print2_basic_html_tags.py
+++ prompt_toolkit/print/print2_basic_html_tags.py
@@ -1,8 +1,10 @@
 from prompt_toolkit import print_formatted_text, HTML
 
-print_formatted_text(HTML('zpráva obsahující <b>tučný text</b>'))
-print_formatted_text(HTML('zpráva s <i>textem tištěným kurzivou</i>'))
-print_formatted_text(HTML('text obsahující <u>tato podtržená slova</u>'))
-print_formatted_text(HTML('test kombinace <b><i>tučné kurzivy</i></b>'))
-print_formatted_text(HTML('test kombinace <b><u>tučného podtrženého textu</u></b>'))
-print_formatted_text(HTML('test kombinace <i><u>podtrženého textu psaného kurzivou</u></i>'))
+print_formatted_text(HTML("zpráva obsahující <b>tučný text</b>"))
+print_formatted_text(HTML("zpráva s <i>textem tištěným kurzivou</i>"))
+print_formatted_text(HTML("text obsahující <u>tato podtržená slova</u>"))
+print_formatted_text(HTML("test kombinace <b><i>tučné kurzivy</i></b>"))
+print_formatted_text(HTML("test kombinace <b><u>tučného podtrženého textu</u></b>"))
+print_formatted_text(
+    HTML("test kombinace <i><u>podtrženého textu psaného kurzivou</u></i>")
+)
