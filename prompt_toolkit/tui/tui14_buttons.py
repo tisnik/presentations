93850b4375b548035c7157256cc99161e27d13fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-16 07:46:07 +0200

    New results for benchmarking in EDN format: tui/tui14_buttons.py

--- prompt_toolkit/tui/tui14_buttons.py
+++ prompt_toolkit/tui/tui14_buttons.py
@@ -9,36 +9,37 @@ from prompt_toolkit.widgets import *
 
 
 def button1_clicked():
-    text_area.text += 'Button 1 clicked\n'
+    text_area.text += "Button 1 clicked\n"
 
 
 def button2_clicked():
-    text_area.text += 'Button 2 clicked\n'
+    text_area.text += "Button 2 clicked\n"
 
 
 def button3_clicked():
-    text_area.text += 'Button 3 clicked\n'
+    text_area.text += "Button 3 clicked\n"
 
 
 def exit_clicked():
     get_app().exit()
 
 
-button1 = Button('Button 1', handler=button1_clicked)
-button2 = Button('Button 2', handler=button2_clicked)
-button3 = Button('Button 3', handler=button3_clicked)
-button4 = Button('Exit', handler=exit_clicked)
+button1 = Button("Button 1", handler=button1_clicked)
+button2 = Button("Button 2", handler=button2_clicked)
+button3 = Button("Button 3", handler=button3_clicked)
+button4 = Button("Exit", handler=exit_clicked)
 
-buttons = HSplit([button1,
-                  button2,
-                  button3,
-                  button4])
+buttons = HSplit([button1, button2, button3, button4])
 
 text_area = TextArea()
 
 # správce rozvržení
-root = VSplit([Box(Frame(buttons, style="bg:#ansiblue #ansiwhite"), padding=2),
-               Box(Frame(text_area, title="Events"), padding=2)])
+root = VSplit(
+    [
+        Box(Frame(buttons, style="bg:#ansiblue #ansiwhite"), padding=2),
+        Box(Frame(text_area, title="Events"), padding=2),
+    ]
+)
 
 layout = Layout(root)
 
@@ -46,7 +47,7 @@ layout = Layout(root)
 key_bindings = KeyBindings()
 
 
-@key_bindings.add('escape')
+@key_bindings.add("escape")
 def on_escape_press(event):
     """Callback funkce volaná při stisku klávesy Esc."""
     print("\n\n[escape]\n\n")
@@ -55,13 +56,13 @@ def on_escape_press(event):
 
 def main():
     # vytvoření aplikace s textovým uživatelským rozhraním
-    application = Application(layout=layout,
-                              key_bindings=key_bindings,
-                              full_screen=True)
+    application = Application(
+        layout=layout, key_bindings=key_bindings, full_screen=True
+    )
 
     # spuštění aplikace
     application.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
