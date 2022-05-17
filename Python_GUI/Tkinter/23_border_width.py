b978bdf8db2aef8231fa7c6cd3b1ada9e31f06b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:35:59 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/23_border_width.py
+++ Python_GUI/Tkinter/23_border_width.py
@@ -26,15 +26,16 @@ def exit():
 root = tkinter.Tk()
 
 style = ttk.Style()
-style.configure('Red.TButton', background='#ff8080')
+style.configure("Red.TButton", background="#ff8080")
 
 buttonStyles = ("sunken", "solid", "flat", "groove", "raised", "ridge")
 
-buttons = (Button(root, text=buttonStyle, relief=buttonStyle, borderwidth=2)
-           for buttonStyle in buttonStyles)
+buttons = (
+    Button(root, text=buttonStyle, relief=buttonStyle, borderwidth=2)
+    for buttonStyle in buttonStyles
+)
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 for i, button in enumerate(buttons):
     button.grid(column=1, row=i, sticky="we", padx=6, pady=6)
