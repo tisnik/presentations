293d15d991da76a318325024b077940f5f17c490
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:35:59 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/15_button_styles.py
+++ Python_GUI/Tkinter/15_button_styles.py
@@ -19,17 +19,19 @@ import sys
 
 root = tkinter.Tk()
 
-button1 = tkinter.Button(root, background='yellow', text="1st btn",
-                         command=lambda: sys.exit(0))
-button2 = tkinter.Button(root, background='#ff8080', text="Second button",
-                         command=lambda: sys.exit(0))
-button3 = tkinter.Button(root, text="Third button",
-                         command=lambda: sys.exit(0))
-button4 = tkinter.Button(root, text="This is fourth button, the last one",
-                         command=lambda: sys.exit(0))
+button1 = tkinter.Button(
+    root, background="yellow", text="1st btn", command=lambda: sys.exit(0)
+)
+button2 = tkinter.Button(
+    root, background="#ff8080", text="Second button", command=lambda: sys.exit(0)
+)
+button3 = tkinter.Button(root, text="Third button", command=lambda: sys.exit(0))
+button4 = tkinter.Button(
+    root, text="This is fourth button, the last one", command=lambda: sys.exit(0)
+)
 
-button3.configure(background='#8080ff')
-button4['background'] = '#80ff80'
+button3.configure(background="#8080ff")
+button4["background"] = "#80ff80"
 
 button1.grid(column=1, row=1, sticky="we")
 button2.grid(column=2, row=1, sticky="we")
