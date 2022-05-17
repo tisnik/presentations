b5dc43b02d06e2a2ec845827cef7740e32098552
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:35:59 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/21_button_styles_2.py
+++ Python_GUI/Tkinter/21_button_styles_2.py
@@ -26,15 +26,16 @@ def exit():
 root = tkinter.Tk()
 
 style = ttk.Style()
-style.configure('Red.TButton', background='#ff8080')
+style.configure("Red.TButton", background="#ff8080")
 
 buttonStyles = ("sunken", "solid", "flat", "groove", "raised", "ridge")
 
-buttons = (tkinter.Button(root, text=buttonStyle, relief=buttonStyle)
-           for buttonStyle in buttonStyles)
+buttons = (
+    tkinter.Button(root, text=buttonStyle, relief=buttonStyle)
+    for buttonStyle in buttonStyles
+)
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 for i, button in enumerate(buttons):
     button.grid(column=1, row=i, sticky="we")
