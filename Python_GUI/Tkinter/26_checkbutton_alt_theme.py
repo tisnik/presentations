9b8de692496e8ec79dc31821712407143f9758fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:36:00 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/26_checkbutton_alt_theme.py
+++ Python_GUI/Tkinter/26_checkbutton_alt_theme.py
@@ -22,13 +22,13 @@ root = tkinter.Tk()
 
 style = ttk.Style()
 style.theme_use("alt")
-style.configure('Red.TButton', background='#ff8080')
+style.configure("Red.TButton", background="#ff8080")
 
-checkbutton = ttk.Checkbutton(root, text="Delete Internet?",
-                              command=lambda: print("changed"))
+checkbutton = ttk.Checkbutton(
+    root, text="Delete Internet?", command=lambda: print("changed")
+)
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 checkbutton.grid(column=1, row=1, sticky="we", padx=6, pady=6)
 quitButton.grid(column=1, row=2, sticky="we", padx=6, pady=6)
