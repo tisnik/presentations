3e06dbc42146ecd2c2e7e6d8a9d580d0b28f42a7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:36:00 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/55_checkbuttons_in_menu.py
+++ Python_GUI/Tkinter/55_checkbuttons_in_menu.py
@@ -34,7 +34,8 @@ image_names = [
     "edit-copy",
     "edit-paste",
     "edit-delete",
-    "edit-select-all"]
+    "edit-select-all",
+]
 
 images = {}
 for image_name in image_names:
@@ -42,44 +43,56 @@ for image_name in image_names:
 
 filemenu = tkinter.Menu(menubar, tearoff=0)
 
-filemenu.add_command(label="Open", underline=0, image=images["document-open"],
-                     compound="left")
+filemenu.add_command(
+    label="Open", underline=0, image=images["document-open"], compound="left"
+)
 
-filemenu.add_command(label="Save", underline=0, image=images["document-save"],
-                     compound="left")
+filemenu.add_command(
+    label="Save", underline=0, image=images["document-save"], compound="left"
+)
 
 filemenu.add_separator()
 
-filemenu.add_command(label="Exit", underline=1,
-                     image=images["application-exit"], compound="left",
-                     command=root.quit)
+filemenu.add_command(
+    label="Exit",
+    underline=1,
+    image=images["application-exit"],
+    compound="left",
+    command=root.quit,
+)
 
 menubar.add_cascade(label="File", menu=filemenu, underline=0)
 
 
 editmenu = tkinter.Menu(menubar, tearoff=0)
 
-editmenu.add_command(label="Undo", underline=0, image=images["edit-undo"],
-                     compound="left")
+editmenu.add_command(
+    label="Undo", underline=0, image=images["edit-undo"], compound="left"
+)
 
 editmenu.add_separator()
 
-editmenu.add_command(label="Cut", underline=2, image=images["edit-cut"],
-                     compound="left")
+editmenu.add_command(
+    label="Cut", underline=2, image=images["edit-cut"], compound="left"
+)
 
-editmenu.add_command(label="Copy", underline=0, image=images["edit-copy"],
-                     compound="left")
+editmenu.add_command(
+    label="Copy", underline=0, image=images["edit-copy"], compound="left"
+)
 
-editmenu.add_command(label="Paste", underline=0, image=images["edit-paste"],
-                     compound="left")
+editmenu.add_command(
+    label="Paste", underline=0, image=images["edit-paste"], compound="left"
+)
 
-editmenu.add_command(label="Delete", underline=2, image=images["edit-delete"],
-                     compound="left")
+editmenu.add_command(
+    label="Delete", underline=2, image=images["edit-delete"], compound="left"
+)
 
 editmenu.add_separator()
 
-editmenu.add_command(label="Select All", underline=7,
-                     image=images["edit-select-all"], compound="left")
+editmenu.add_command(
+    label="Select All", underline=7, image=images["edit-select-all"], compound="left"
+)
 
 menubar.add_cascade(label="Edit", menu=editmenu, underline=0)
 
@@ -89,16 +102,17 @@ show_all = tkinter.BooleanVar()
 show_all.set(True)
 
 optionmenu = tkinter.Menu(menubar, tearoff=0)
-optionmenu.add_checkbutton(label="Word wrap", onvalue=True, offvalue=False,
-                           variable=word_wrap)
-optionmenu.add_checkbutton(label="Show all", onvalue=True, offvalue=False,
-                           variable=show_all)
+optionmenu.add_checkbutton(
+    label="Word wrap", onvalue=True, offvalue=False, variable=word_wrap
+)
+optionmenu.add_checkbutton(
+    label="Show all", onvalue=True, offvalue=False, variable=show_all
+)
 
 menubar.add_cascade(label="Options", menu=optionmenu, underline=0)
 
 
-colors = ("white", "yellow", "orange", "red", "magenta",
-          "blue", "cyan", "green")
+colors = ("white", "yellow", "orange", "red", "magenta", "blue", "cyan", "green")
 colormenu = tkinter.Menu(menubar, tearoff=0)
 
 for color in colors:
