aaf83f0fcd2e6c407c3b776a372cad0f98a6d195
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:35:59 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/19_theme_settings_and_selection.py
+++ Python_GUI/Tkinter/19_theme_settings_and_selection.py
@@ -27,21 +27,16 @@ root = tkinter.Tk()
 
 style = ttk.Style()
 
-for style_name in ('clam', 'alt', 'default', 'classic'):
+for style_name in ("clam", "alt", "default", "classic"):
     style.theme_use(style_name)
-    style.configure('Red.TButton', background='#ff8080')
+    style.configure("Red.TButton", background="#ff8080")
 
-button1 = ttk.Button(root, text="clam",
-                     command=lambda: style.theme_use("clam"))
-button2 = ttk.Button(root, text="alt",
-                     command=lambda: style.theme_use("alt"))
-button3 = ttk.Button(root, text="default",
-                     command=lambda: style.theme_use("default"))
-button4 = ttk.Button(root, text="classic",
-                     command=lambda: style.theme_use("classic"))
+button1 = ttk.Button(root, text="clam", command=lambda: style.theme_use("clam"))
+button2 = ttk.Button(root, text="alt", command=lambda: style.theme_use("alt"))
+button3 = ttk.Button(root, text="default", command=lambda: style.theme_use("default"))
+button4 = ttk.Button(root, text="classic", command=lambda: style.theme_use("classic"))
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 button1.grid(column=1, row=1, sticky="we")
 button2.grid(column=2, row=1, sticky="we")
@@ -50,9 +45,9 @@ button4.grid(column=2, row=2, sticky="we")
 
 quitButton.grid(column=2, row=5, sticky="we")
 
-label = tkinter.Label(root, text='Hello world')
+label = tkinter.Label(root, text="Hello world")
 entry = tkinter.Entry(root)
-checkbutton = tkinter.Checkbutton(text='Do you like Tkinter?')
+checkbutton = tkinter.Checkbutton(text="Do you like Tkinter?")
 
 checkbutton.grid(column=1, row=3, columnspan=2, sticky="w")
 label.grid(column=1, row=4)
