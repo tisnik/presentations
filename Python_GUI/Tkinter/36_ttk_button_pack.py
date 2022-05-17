28f63e04256789a1d16c1fe31023a649a6ee7c5a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:36:00 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/36_ttk_button_pack.py
+++ Python_GUI/Tkinter/36_ttk_button_pack.py
@@ -22,22 +22,20 @@ root = tkinter.Tk()
 
 style = ttk.Style()
 style.theme_use("alt")
-style.configure('Red.TButton', background='#ff8080')
+style.configure("Red.TButton", background="#ff8080")
 
 radio_var = tkinter.StringVar()
 radio_var.set("Python")
 
 langs = ("Assembler", "Basic", "Brainfuck", "C", "Python")
 
-radio_buttons = (ttk.Radiobutton(root, text=lang, value=lang,
-                                 variable=radio_var)
-                 for lang in langs)
+radio_buttons = (
+    ttk.Radiobutton(root, text=lang, value=lang, variable=radio_var) for lang in langs
+)
 
-showButton = ttk.Button(root, text="Show var",
-                        command=lambda: print(radio_var.get()))
+showButton = ttk.Button(root, text="Show var", command=lambda: print(radio_var.get()))
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 for radio_button in radio_buttons:
     radio_button.pack(fill="x")
