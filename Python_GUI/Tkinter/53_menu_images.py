4f467b57f848424aff2da6477a42b1486e965439
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:36:00 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/53_menu_images.py
+++ Python_GUI/Tkinter/53_menu_images.py
@@ -37,49 +37,43 @@ select_all_image = tkinter.PhotoImage(file="icons/edit-select-all.gif")
 
 filemenu = tkinter.Menu(menubar, tearoff=0)
 
-filemenu.add_command(label="Open", underline=0, image=open_image,
-                     compound="left")
+filemenu.add_command(label="Open", underline=0, image=open_image, compound="left")
 
-filemenu.add_command(label="Save", underline=0, image=save_image,
-                     compound="left")
+filemenu.add_command(label="Save", underline=0, image=save_image, compound="left")
 
 filemenu.add_separator()
 
-filemenu.add_command(label="Exit", underline=1, image=exit_image,
-                     compound="left", command=root.quit)
+filemenu.add_command(
+    label="Exit", underline=1, image=exit_image, compound="left", command=root.quit
+)
 
 menubar.add_cascade(label="File", menu=filemenu, underline=0)
 
 
 editmenu = tkinter.Menu(menubar, tearoff=0)
 
-editmenu.add_command(label="Undo", underline=0, image=undo_image,
-                     compound="left")
+editmenu.add_command(label="Undo", underline=0, image=undo_image, compound="left")
 
 editmenu.add_separator()
 
-editmenu.add_command(label="Cut", underline=2, image=cut_image,
-                     compound="left")
+editmenu.add_command(label="Cut", underline=2, image=cut_image, compound="left")
 
-editmenu.add_command(label="Copy", underline=0, image=copy_image,
-                     compound="left")
+editmenu.add_command(label="Copy", underline=0, image=copy_image, compound="left")
 
-editmenu.add_command(label="Paste", underline=0, image=paste_image,
-                     compound="left")
+editmenu.add_command(label="Paste", underline=0, image=paste_image, compound="left")
 
-editmenu.add_command(label="Delete", underline=2, image=delete_image,
-                     compound="left")
+editmenu.add_command(label="Delete", underline=2, image=delete_image, compound="left")
 
 editmenu.add_separator()
 
-editmenu.add_command(label="Select All", underline=7, image=select_all_image,
-                     compound="left")
+editmenu.add_command(
+    label="Select All", underline=7, image=select_all_image, compound="left"
+)
 
 menubar.add_cascade(label="Edit", menu=editmenu, underline=0)
 
 
-colors = ("white", "yellow", "orange", "red", "magenta",
-          "blue", "cyan", "green")
+colors = ("white", "yellow", "orange", "red", "magenta", "blue", "cyan", "green")
 colormenu = tkinter.Menu(menubar, tearoff=0)
 
 for color in colors:
