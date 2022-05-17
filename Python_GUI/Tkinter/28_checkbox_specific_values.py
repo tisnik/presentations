68c4f777850d185775a51938a80943dbc8b693bc
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:36:00 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/28_checkbox_specific_values.py
+++ Python_GUI/Tkinter/28_checkbox_specific_values.py
@@ -22,18 +22,20 @@ root = tkinter.Tk()
 
 style = ttk.Style()
 style.theme_use("alt")
-style.configure('Red.TButton', background='#ff8080')
+style.configure("Red.TButton", background="#ff8080")
 
 delete_internet = tkinter.StringVar()
 
-checkbutton = ttk.Checkbutton(root, text="Delete Internet?",
-                              variable=delete_internet,
-                              onvalue="yes",
-                              offvalue="no",
-                              command=lambda: print(delete_internet.get()))
+checkbutton = ttk.Checkbutton(
+    root,
+    text="Delete Internet?",
+    variable=delete_internet,
+    onvalue="yes",
+    offvalue="no",
+    command=lambda: print(delete_internet.get()),
+)
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 checkbutton.grid(column=1, row=1, sticky="we", padx=6, pady=6)
 quitButton.grid(column=1, row=2, sticky="we", padx=6, pady=6)
