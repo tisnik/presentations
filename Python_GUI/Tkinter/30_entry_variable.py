1ae6f2114fc08e0c2ef94dd347898811252a4499
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-17 07:36:00 +0200

    Reformatted by Black

--- Python_GUI/Tkinter/30_entry_variable.py
+++ Python_GUI/Tkinter/30_entry_variable.py
@@ -22,18 +22,16 @@ root = tkinter.Tk()
 
 style = ttk.Style()
 style.theme_use("alt")
-style.configure('Red.TButton', background='#ff8080')
+style.configure("Red.TButton", background="#ff8080")
 
 value = tkinter.StringVar()
 
 entry = ttk.Entry(root, textvariable=value)
 entry.insert(0, "xyzzy")
 
-showButton = ttk.Button(root, text="Show var",
-                        command=lambda: print(value.get()))
+showButton = ttk.Button(root, text="Show var", command=lambda: print(value.get()))
 
-quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
-                        command=exit)
+quitButton = ttk.Button(root, text="Exit", style="Red.TButton", command=exit)
 
 entry.grid(column=1, row=1, sticky="we", padx=6, pady=6)
 showButton.grid(column=1, row=2, sticky="we", padx=6, pady=6)
