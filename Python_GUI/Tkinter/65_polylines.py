df70858a30ad2f03daaf5fc3846af70d7e4bdc62
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:24:58 +0200

    Reformatted 65_polylines.py by Black

--- Python_GUI/Tkinter/65_polylines.py
+++ Python_GUI/Tkinter/65_polylines.py
@@ -22,9 +22,6 @@ root = tkinter.Tk()
 canvas = tkinter.Canvas(root, width=300, height=300)
 canvas.pack()
 
-canvas.create_line(0, 150,
-                   80, 20,
-                   220, 280,
-                   300, 150, dash=10)
+canvas.create_line(0, 150, 80, 20, 220, 280, 300, 150, dash=10)
 
 root.mainloop()
