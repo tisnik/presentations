7f8e33490aa3f4772ee479b5808ec69ee7ae27be
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:24:58 +0200

    Reformatted 86_text_widget_styles.py by Black

--- Python_GUI/Tkinter/86_text_widget_styles.py
+++ Python_GUI/Tkinter/86_text_widget_styles.py
@@ -24,17 +24,20 @@ def exit():
 
 root = tkinter.Tk()
 
-text = tkinter.Text(root,
-                    font="Helvetica 20",
-                    foreground="#0000c0",
-                    background="#c0ffc0",
-                    selectforeground="white",
-                    selectbackground="red",
-                    insertwidth=4,
-                    insertbackground="red",
-                    insertborderwidth=1,
-                    wrap=tkinter.WORD,
-                    width=40, height=16)
+text = tkinter.Text(
+    root,
+    font="Helvetica 20",
+    foreground="#0000c0",
+    background="#c0ffc0",
+    selectforeground="white",
+    selectbackground="red",
+    insertwidth=4,
+    insertbackground="red",
+    insertborderwidth=1,
+    wrap=tkinter.WORD,
+    width=40,
+    height=16,
+)
 
 text.insert(tkinter.END, "Test widgetu\n'text'")
 
