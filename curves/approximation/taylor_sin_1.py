c75c9c7eceffdaf16564764a7ca2e90c65c0da2e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:28:01 +0200

    Reformatted taylor_sin_1.py by Black

--- curves/approximation/taylor_sin_1.py
+++ curves/approximation/taylor_sin_1.py
@@ -24,24 +24,24 @@ def taylor_series(x, order):
     a = x
     sum = a
     for i in range(1, order):
-        a *= -1 * x**2 / ((2 * i) * (2 * i + 1))
+        a *= -1 * x ** 2 / ((2 * i) * (2 * i + 1))
         sum += a
     return sum
- 
- 
+
+
 # průběh nezávislé proměnné x
 # (hodnoty na x-ové ose)
 x = np.linspace(-20, 20, 500)
- 
+
 # funkce kterou aproximujeme Taylorovou řadou
 y = np.sin(x)
- 
+
 # vykreslení původní funkce
-plt.plot(x, y, label='sin(x)')
+plt.plot(x, y, label="sin(x)")
 
 # příprava pro vlastní výpočet
 ys = np.vectorize(taylor_series)
- 
+
 # aproximace Taylorovou řadou
 N = 1
 
@@ -49,14 +49,14 @@ N = 1
 approx = ys(x, N)
 
 # vykreslení aproximace funkce
-plt.plot(x, approx, label='order {o}'.format(o=N))
- 
+plt.plot(x, approx, label="order {o}".format(o=N))
+
 # limity na ose y
 plt.ylim([-3, 3])
- 
+
 # legenda grafu
 plt.legend()
- 
+
 # uložení grafu do rastrového obrázku
 plt.savefig("taylor_sin_1.png")
 
