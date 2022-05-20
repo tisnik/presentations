9206aa1364289d3989213696c39dc7d795138f16
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:24:58 +0200

    Reformatted 75_closed_entities_style.py by Black

--- Python_GUI/Tkinter/75_closed_entities_style.py
+++ Python_GUI/Tkinter/75_closed_entities_style.py
@@ -28,8 +28,7 @@ def exit():
 
 
 def basic_canvas(root, width, height, grid_size):
-    canvas = tkinter.Canvas(root, width=width, height=height,
-                            background='#ccffcc')
+    canvas = tkinter.Canvas(root, width=width, height=height, background="#ccffcc")
     canvas.pack()
 
     draw_grid(canvas, width, height, grid_size)
@@ -47,48 +46,43 @@ root = tkinter.Tk()
 
 canvas = basic_canvas(root, WIDTH, HEIGHT, GRID_SIZE)
 
-canvas.create_rectangle(10, 30, 90, 70, fill='#ff8080', width=2,
-                        activefill='white')
+canvas.create_rectangle(10, 30, 90, 70, fill="#ff8080", width=2, activefill="white")
 
-canvas.create_rectangle(110, 30, 190, 70, fill='#ff8080', width=2,
-                        dash=(5, 5), activedash=1)
+canvas.create_rectangle(
+    110, 30, 190, 70, fill="#ff8080", width=2, dash=(5, 5), activedash=1
+)
 
-canvas.create_rectangle(30, 110, 70, 190, fill='#ff8080',
-                        activeoutline='yellow')
+canvas.create_rectangle(30, 110, 70, 190, fill="#ff8080", activeoutline="yellow")
 
-canvas.create_rectangle(20, 220, 80, 280, fill='#ff8080',
-                        activeoutline='yellow', activewidth='5')
+canvas.create_rectangle(
+    20, 220, 80, 280, fill="#ff8080", activeoutline="yellow", activewidth="5"
+)
 
-canvas.create_oval(130, 110, 170, 190, fill='#8080ff', width=2,
-                   activedash=(10, 10))
+canvas.create_oval(130, 110, 170, 190, fill="#8080ff", width=2, activedash=(10, 10))
 
-canvas.create_oval(120, 220, 180, 280, fill=None, activefill='#8080ff')
+canvas.create_oval(120, 220, 180, 280, fill=None, activefill="#8080ff")
 
-canvas.create_rectangle(210, 30, 290, 70, fill=None, width=2,
-                        activefill='white')
+canvas.create_rectangle(210, 30, 290, 70, fill=None, width=2, activefill="white")
 
-canvas.create_rectangle(310, 30, 390, 70, fill=None, width=2, dash=(5, 5),
-                        activedash=1)
+canvas.create_rectangle(310, 30, 390, 70, fill=None, width=2, dash=(5, 5), activedash=1)
 
-canvas.create_rectangle(230, 110, 270, 190, fill=None, activeoutline='yellow',
-                        width=5)
+canvas.create_rectangle(230, 110, 270, 190, fill=None, activeoutline="yellow", width=5)
 
-canvas.create_rectangle(220, 220, 280, 280, fill=None, activeoutline='yellow',
-                        activewidth='5')
+canvas.create_rectangle(
+    220, 220, 280, 280, fill=None, activeoutline="yellow", activewidth="5"
+)
 
 canvas.create_oval(330, 110, 370, 190, fill=None, width=2, activedash=(10, 10))
 
-canvas.create_oval(320, 220, 380, 280, fill=None, activefill='#8080ff',
-                   width=5)
+canvas.create_oval(320, 220, 380, 280, fill=None, activefill="#8080ff", width=5)
 
-canvas.create_line(10, 330, 90, 370, fill='#80ff80', width=2,
-                   activefill='white')
+canvas.create_line(10, 330, 90, 370, fill="#80ff80", width=2, activefill="white")
 
-canvas.create_line(110, 330, 190, 370, fill='#80ff80', width=20,
-                   activefill='white')
+canvas.create_line(110, 330, 190, 370, fill="#80ff80", width=20, activefill="white")
 
-canvas.create_line(210, 330, 290, 370, fill='#80ff80', width=20,
-                   activefill='white', dash=10)
+canvas.create_line(
+    210, 330, 290, 370, fill="#80ff80", width=20, activefill="white", dash=10
+)
 
 
 root.mainloop()
