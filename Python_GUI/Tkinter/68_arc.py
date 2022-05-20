21d8a13851b6efa359ac2dc53b60591d76beb455
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:24:58 +0200

    Reformatted 68_arc.py by Black

--- Python_GUI/Tkinter/68_arc.py
+++ Python_GUI/Tkinter/68_arc.py
@@ -28,8 +28,7 @@ def exit():
 
 
 def basic_canvas(root, width, height, grid_size):
-    canvas = tkinter.Canvas(root, width=width, height=height,
-                            background='#ccffcc')
+    canvas = tkinter.Canvas(root, width=width, height=height, background="#ccffcc")
     canvas.pack()
 
     draw_grid(canvas, width, height, grid_size)
@@ -47,28 +46,57 @@ root = tkinter.Tk()
 
 canvas = basic_canvas(root, WIDTH, HEIGHT, GRID_SIZE)
 
-canvas.create_arc(0, 0, 100, 100, outline='red', style=tkinter.ARC,
-                  width=2)
+canvas.create_arc(0, 0, 100, 100, outline="red", style=tkinter.ARC, width=2)
 
-canvas.create_arc(100, 100, 200, 200, outline='blue', start=45,
-                  style=tkinter.ARC, width=2)
+canvas.create_arc(
+    100, 100, 200, 200, outline="blue", start=45, style=tkinter.ARC, width=2
+)
 
-canvas.create_arc(200, 0, 300, 100, outline='brown', extent=180,
-                  style=tkinter.ARC, width=2)
+canvas.create_arc(
+    200, 0, 300, 100, outline="brown", extent=180, style=tkinter.ARC, width=2
+)
 
-canvas.create_arc(300, 100, 400, 200, outline='green', start=45, extent=270,
-                  style=tkinter.ARC, width=2)
+canvas.create_arc(
+    300,
+    100,
+    400,
+    200,
+    outline="green",
+    start=45,
+    extent=270,
+    style=tkinter.ARC,
+    width=2,
+)
 
-canvas.create_arc(0, 200, 100, 300, outline='red', style=tkinter.ARC, dash=8,
-                  width=2)
+canvas.create_arc(0, 200, 100, 300, outline="red", style=tkinter.ARC, dash=8, width=2)
 
-canvas.create_arc(100, 300, 200, 400, outline='blue', start=45,
-                  style=tkinter.ARC, dash=8, width=10)
+canvas.create_arc(
+    100, 300, 200, 400, outline="blue", start=45, style=tkinter.ARC, dash=8, width=10
+)
 
-canvas.create_arc(200, 200, 300, 300, outline='green', start=45, extent=270,
-                  style=tkinter.ARC, dash=3, width=50)
+canvas.create_arc(
+    200,
+    200,
+    300,
+    300,
+    outline="green",
+    start=45,
+    extent=270,
+    style=tkinter.ARC,
+    dash=3,
+    width=50,
+)
 
-canvas.create_arc(290, 290, 390, 390, outline='brown', extent=270,
-                  style=tkinter.ARC, dash=80, width=20)
+canvas.create_arc(
+    290,
+    290,
+    390,
+    390,
+    outline="brown",
+    extent=270,
+    style=tkinter.ARC,
+    dash=80,
+    width=20,
+)
 
 root.mainloop()
