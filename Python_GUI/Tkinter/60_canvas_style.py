321f80a299f7aec62d229304b27b3ebe79f8a866
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:24:58 +0200

    Reformatted 60_canvas_style.py by Black

--- Python_GUI/Tkinter/60_canvas_style.py
+++ Python_GUI/Tkinter/60_canvas_style.py
@@ -26,7 +26,6 @@ canvas.create_oval(10, 10, 100, 100, fill="red", outline="blue", width=3)
 canvas.create_line(0, 0, 255, 255, width=5)
 canvas.create_line(0, 255, 255, 0, dash=123)
 
-canvas.create_text(150, 120, text="Hello world!", fill="white",
-                   font="Helvetica 20")
+canvas.create_text(150, 120, text="Hello world!", fill="white", font="Helvetica 20")
 
 root.mainloop()
