0869d91fa5697ff87abf23b54a6e60fa02fc85db
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:28:01 +0200

    Reformatted random_data_poly.py by Black

--- curves/approximation/random_data_poly.py
+++ curves/approximation/random_data_poly.py
@@ -25,16 +25,16 @@ x = np.arange(0, 50)
 rng = np.random.default_rng(seed=42)
 
 # hodnoty na y-ové ose
-y = np.power(x, 2) + 10*rng.random((len(x))) - 5
+y = np.power(x, 2) + 10 * rng.random((len(x))) - 5
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Pseudonáhodná data', fontsize=15)
+fig.suptitle("Pseudonáhodná data", fontsize=15)
 
 # vrcholy na křivce
-ax.plot(x, y, 'go')
+ax.plot(x, y, "go")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("random_data_poly.png")
