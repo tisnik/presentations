b2dc23ccc2da2374df3397a48bf4f2d19e5c3499
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-20 08:24:58 +0200

    Reformatted 66_splines.py by Black

--- Python_GUI/Tkinter/66_splines.py
+++ Python_GUI/Tkinter/66_splines.py
@@ -22,14 +22,8 @@ root = tkinter.Tk()
 canvas = tkinter.Canvas(root, width=300, height=300)
 canvas.pack()
 
-canvas.create_line(0, 150,
-                   80, 20,
-                   220, 280,
-                   300, 150, dash=10)
+canvas.create_line(0, 150, 80, 20, 220, 280, 300, 150, dash=10)
 
-canvas.create_line(0, 150,
-                   80, 20,
-                   220, 280,
-                   300, 150, smooth=True, width=2, fill="red")
+canvas.create_line(0, 150, 80, 20, 220, 280, 300, 150, smooth=True, width=2, fill="red")
 
 root.mainloop()
