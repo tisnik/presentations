d0739c6bd96a8a2e67e39fa3087dd65f0a1da462
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-06-28 09:20:59 +0200

    Use new octal literal

--- docs/coffee-talks/messagepack/msgpack_timestamp.go
+++ docs/coffee-talks/messagepack/msgpack_timestamp.go
@@ -12,7 +12,7 @@ const filename = "/tmp/timestamp.bin"
 
 func main() {
 	// vytvořit soubor s binárními daty
-	fout, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	fout, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
 	if err != nil {
 		log.Fatal(err)
 	}
