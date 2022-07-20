0a188708f59164558c330a8fbce6515a4834e43d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:14 +0200

    Reformatted by Black

--- coconut/06-pipeline_compiled_line_numbers.py
+++ coconut/06-pipeline_compiled_line_numbers.py
@@ -8,8 +8,10 @@
 
 (print)((sum)((reversed)(range(11))))  # line 7
 
+
 @_coconut_tco  # line 9
 def evens(sequence):  # line 9
     return _coconut_tail_call(filter, lambda x: x % 2 == 0, sequence)  # line 10
+
 
 (print)((sum)((evens)([1, 2, 3, 4, 5, 6, 30])))  # line 12
