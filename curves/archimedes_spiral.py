1921b15a35787023a77a52a367d730d3d4d3b06b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/archimedes_spiral.py
+++ curves/archimedes_spiral.py
@@ -19,19 +19,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # úhel v polárním grafu
-theta = np.linspace(0.01, 8*np.pi, 150)
+theta = np.linspace(0.01, 8 * np.pi, 150)
 
 # funkce: vzdálenost od středu
 radius = theta
 
 # rozměry grafu při uložení: 640x480 pixelů
-fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={"projection": "polar"})
 
 # titulek grafu
-fig.suptitle('Archimedova spirála', fontsize=15)
+fig.suptitle("Archimedova spirála", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(theta, radius, 'g-')
+ax.plot(theta, radius, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("archimedes_spiral.png")
