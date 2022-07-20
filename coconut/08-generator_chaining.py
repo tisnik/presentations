c0616c85b1a679bdbc4d9041a627f41e6baeafb3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:14 +0200

    Reformatted by Black

--- coconut/08-generator_chaining.py
+++ coconut/08-generator_chaining.py
@@ -8,23 +8,39 @@
 
 # Compiled Coconut: -----------------------------------------------------------
 
+
 def generator1():
     values = ["a1", "b1", "c1", "d1"]
     for value in values:
         yield value
+
 
 def generator2():
     values = ["a2", "b2", "c2", "d2"]
     for value in values:
         yield value
 
-for v in _coconut.itertools.chain.from_iterable((f() for f in (lambda: generator1(), lambda: generator2()))):
+
+for v in _coconut.itertools.chain.from_iterable(
+    (f() for f in (lambda: generator1(), lambda: generator2()))
+):
     print(v)
+
 
 def generator3(suffix):
     values = ["a", "b", "c", "d", "e"]
     for value in values:
         yield "{v}{s}".format(v=value, s=suffix)
 
-for v in _coconut.itertools.chain.from_iterable((f() for f in (lambda: generator3("1"), lambda: generator3("2"), lambda: generator3("3")))):
+
+for v in _coconut.itertools.chain.from_iterable(
+    (
+        f()
+        for f in (
+            lambda: generator3("1"),
+            lambda: generator3("2"),
+            lambda: generator3("3"),
+        )
+    )
+):
     print(v)
