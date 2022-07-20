1f719c28dfb630844f61d9103343d1bc61fdd038
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/hyperbolic_spiral.py
+++ curves/hyperbolic_spiral.py
@@ -24,16 +24,16 @@ theta = np.arange(1.00, 50.0, 0.05)
 a = 1.0
 
 # funkce: inverzní vzdálenost od středu
-radius = a/theta
+radius = a / theta
 
 # rozměry grafu při uložení: 640x480 pixelů
-fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={"projection": "polar"})
 
 # titulek grafu
-fig.suptitle('Hyperbolická spirála', fontsize=15)
+fig.suptitle("Hyperbolická spirála", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(theta, radius, 'g-')
+ax.plot(theta, radius, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("hyperbolic_spiral.png")
