4e78f2478edb9eadf6ad82743fb063276b50f25d
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/circle_polar.py
+++ curves/circle_polar.py
@@ -19,7 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # úhel v polárním grafu
-theta = np.arange(0.00, 2*np.pi, 0.05)
+theta = np.arange(0.00, 2 * np.pi, 0.05)
 
 a = 1.0
 
@@ -27,13 +27,13 @@ a = 1.0
 radius = np.repeat(a, len(theta))
 
 # rozměry grafu při uložení: 640x480 pixelů
-fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={"projection": "polar"})
 
 # titulek grafu
-fig.suptitle('Kružnice', fontsize=15)
+fig.suptitle("Kružnice", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(theta, radius, 'g-')
+ax.plot(theta, radius, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("circle_polar.png")
