eadf8ab349750fb33a5a37967a6f1a8928bd09cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:14 +0200

    Reformatted by Black

--- OpenHouse2020/HowToShootYourself/slides/slide34.py
+++ OpenHouse2020/HowToShootYourself/slides/slide34.py
@@ -1,19 +1,23 @@
 def createCounter():
     counter = 0
+
     def next():
         nonlocal counter
         counter += 1
         return counter
+
     return next
- 
+
+
 def main():
     counter1 = createCounter()
     counter2 = createCounter()
-    for i in range(1,11):
+    for i in range(1, 11):
         result1 = counter1()
         result2 = counter2()
         print("Iteration #%d" % i)
         print("    Counter1: %d" % result1)
         print("    Counter2: %d" % result2)
+
 
 main()
