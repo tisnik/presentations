f80876db2dd51f4a5596f5bc841b6709a6dbfa3c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/epicycloid1.py
+++ curves/epicycloid1.py
@@ -19,28 +19,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # hodnoty parametru t
-t = np.arange(0, 10*np.pi, 0.01)
+t = np.arange(0, 10 * np.pi, 0.01)
 
 # parametry
 a = 8
 b = 5
 
 # výpočet bodů ležících na křivce
-x = (a+b)*np.cos(t)-b*np.cos((a/b+1)*t)
-y = (a+b)*np.sin(t)-b*np.sin((a/b+1)*t)
+x = (a + b) * np.cos(t) - b * np.cos((a / b + 1) * t)
+y = (a + b) * np.sin(t) - b * np.sin((a / b + 1) * t)
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Epycykloida', fontsize=15)
+fig.suptitle("Epycykloida", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(-32, 32)
 ax.set_ylim(-24, 24)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("epycyclodid1.png")
