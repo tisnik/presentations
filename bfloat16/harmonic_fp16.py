e222f5cbaa96065ba45b5ce4d1eb2f2b66986041
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:14 +0200

    Reformatted by Black

--- bfloat16/harmonic_fp16.py
+++ bfloat16/harmonic_fp16.py
@@ -6,8 +6,8 @@ h2 = np.float16(0)
 n = 1
 
 while True:
-    h2 = h1 + np.float16(1)/np.float16(n)
-    print(h1, h2, h2-h1, n)
+    h2 = h1 + np.float16(1) / np.float16(n)
+    print(h1, h2, h2 - h1, n)
     if h1 == h2:
         break
     h1 = h2
