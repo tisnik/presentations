d1cd80f8d5d7ffa4310844f27683bfd1f64934f7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/catenary.py
+++ curves/catenary.py
@@ -25,19 +25,19 @@ x = np.arange(-4, 4, 0.04)
 a = 1.0
 
 # výpočet bodů ležících na řetězovce
-y = a*np.cosh(x/a)
+y = a * np.cosh(x / a)
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Řetězovka', fontsize=15)
+fig.suptitle("Řetězovka", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # vrcholy na křivce (každý pátý)
-ax.plot(x[::5], y[::5], 'ro')
+ax.plot(x[::5], y[::5], "ro")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("cateanry.png")
