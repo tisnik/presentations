439b5330874048d346a3525455199ed8bd789c38
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/cycloid.py
+++ curves/cycloid.py
@@ -19,26 +19,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # hodnoty parametru t
-t = np.arange(0, 3*np.pi, 0.1)
+t = np.arange(0, 3 * np.pi, 0.1)
 
 # poloměr kružnice
 a = 1.0
 
 # výpočet bodů ležících na cykloidě
-x = a*(t-np.sin(t))
-y = a*(1-np.cos(t))
+x = a * (t - np.sin(t))
+y = a * (1 - np.cos(t))
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Cykloida', fontsize=15)
+fig.suptitle("Cykloida", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # vrcholy na křivce (každý pátý)
-ax.plot(x[::5], y[::5], 'ro')
+ax.plot(x[::5], y[::5], "ro")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("cycloid.png")
