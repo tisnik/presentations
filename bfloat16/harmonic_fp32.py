a8953068cad65f260bcab9cc8a57296a54b1f601
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:14 +0200

    Reformatted by Black

--- bfloat16/harmonic_fp32.py
+++ bfloat16/harmonic_fp32.py
@@ -6,9 +6,9 @@ h2 = np.float32(0)
 n = 1
 
 while True:
-    h2 = h1 + np.float32(1)/np.float32(n)
+    h2 = h1 + np.float32(1) / np.float32(n)
     if n % 1000 == 0:
-        print(h1, h2, h2-h1, n)
+        print(h1, h2, h2 - h1, n)
     if h1 == h2:
         break
     h1 = h2
