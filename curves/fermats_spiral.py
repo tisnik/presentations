c490e69d82190111ad298642b81ad9d368bd55e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-20 08:26:15 +0200

    Reformatted by Black

--- curves/fermats_spiral.py
+++ curves/fermats_spiral.py
@@ -24,16 +24,16 @@ theta = np.arange(0.00, 20.0, 0.05)
 a = 1.0
 
 # funkce: vzdálenost od středu
-radius = a*np.sqrt(theta)
+radius = a * np.sqrt(theta)
 
 # rozměry grafu při uložení: 640x480 pixelů
-fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={"projection": "polar"})
 
 # titulek grafu
-fig.suptitle('Fermatova spirála', fontsize=15)
+fig.suptitle("Fermatova spirála", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(theta, radius, 'g-')
+ax.plot(theta, radius, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("fermat_spiral.png")
