c9c34cccd5cf8b43399b24363c3636312fcff989
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/hypotrochoid1.py
+++ curves/hypotrochoid1.py
@@ -19,7 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # hodnoty parametru t
-t = np.arange(0, 6*np.pi, 0.01)
+t = np.arange(0, 6 * np.pi, 0.01)
 
 # parametry
 R = 5
@@ -27,21 +27,21 @@ r = 3
 d = 5
 
 # výpočet bodů ležících na křivce
-x = (R-r)*np.cos(t)+d*np.cos(t * (R-r)/r)
-y = (R-r)*np.sin(t)-d*np.sin(t * (R-r)/r)
+x = (R - r) * np.cos(t) + d * np.cos(t * (R - r) / r)
+y = (R - r) * np.sin(t) - d * np.sin(t * (R - r) / r)
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Hypotrochoida', fontsize=15)
+fig.suptitle("Hypotrochoida", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(-12, 12)
 ax.set_ylim(-9, 9)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("hypotrochoid.png")
