0037e984fbf4e7c74d2ee1d0cc81b4acc835c789
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Bezier_quadric_basis.py
+++ curves/parametric/Bezier_quadric_basis.py
@@ -22,24 +22,22 @@ import matplotlib.pyplot as plt
 t = np.linspace(0, 1, 50)
 
 # Bernsteinovy polynomy pro Bézierovu kvadriku
-B = [1 * (1-t)**2,
-     2 * t * (1-t),
-     1 * t**2]
+B = [1 * (1 - t) ** 2, 2 * t * (1 - t), 1 * t ** 2]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bázové polynomy', fontsize=15)
+fig.suptitle("Bázové polynomy", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 
 # bázové polynomy
-ax.plot(t, B[0], 'r-', label='b0,2')
-ax.plot(t, B[1], 'g-', label='b1,2')
-ax.plot(t, B[2], 'b-', label='b2,2')
+ax.plot(t, B[0], "r-", label="b0,2")
+ax.plot(t, B[1], "g-", label="b1,2")
+ax.plot(t, B[2], "b-", label="b2,2")
 
 # zobrazení legendy
 ax.legend()
