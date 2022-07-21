0b2c4ae74a2c5ae180bfd471a8280251445a1fdc
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/interpolation/lagrange_poly_2.py
+++ curves/interpolation/lagrange_poly_2.py
@@ -28,9 +28,9 @@ x = [0, 1, 2]
 # hodnoty na y-ové ose
 y = [1, 3, 2]
 
-P1_coeff = [1,-1.5,.5]
-P2_coeff = [0, 2,-1]
-P3_coeff = [0,-.5,.5]
+P1_coeff = [1, -1.5, 0.5]
+P2_coeff = [0, 2, -1]
+P3_coeff = [0, -0.5, 0.5]
 
 # získání funkcí pro výpočet křivky na základě báze
 P1 = poly.Polynomial(P1_coeff)
@@ -40,18 +40,18 @@ P3 = poly.Polynomial(P3_coeff)
 x_new = np.arange(-1.0, 3.1, 0.1)
 
 # interpolační křivka
-L = P1 + 3*P2 + 2*P3
+L = P1 + 3 * P2 + 2 * P3
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig = plt.figure(figsize=(6.4, 4.8))
 
 # vykreslení interpolační křivky i vstupních bodů
-plt.plot(x_new, L(x_new), 'b', x, y, 'ro')
+plt.plot(x_new, L(x_new), "b", x, y, "ro")
 
 # popisky grafu
-plt.title('Lagrange Polynomial')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.title("Lagrange Polynomial")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.grid()
 plt.legend()
 
