c9aad1075eb42ac34b1768529b97e5d817c8925e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Bezier_quartic_basis.py
+++ curves/parametric/Bezier_quartic_basis.py
@@ -22,28 +22,30 @@ import matplotlib.pyplot as plt
 t = np.linspace(0, 1, 50)
 
 # Bernsteinovy polynomy pro Bézierovu kvartiku
-B = [1 * (1-t)**4,
-     4 * t * (1-t)**3,
-     6 * t**2 * (1-t)**2,
-     4 * t**3 * (1-t),
-     1 * t**4]
+B = [
+    1 * (1 - t) ** 4,
+    4 * t * (1 - t) ** 3,
+    6 * t ** 2 * (1 - t) ** 2,
+    4 * t ** 3 * (1 - t),
+    1 * t ** 4,
+]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bázové polynomy', fontsize=15)
+fig.suptitle("Bázové polynomy", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 
 # bázové polynomy
-ax.plot(t, B[0], 'r-', label='b0,4')
-ax.plot(t, B[1], 'g-', label='b1,4')
-ax.plot(t, B[2], 'b-', label='b2,4')
-ax.plot(t, B[3], 'k-', label='b3,4')
-ax.plot(t, B[4], 'm-', label='b4,4')
+ax.plot(t, B[0], "r-", label="b0,4")
+ax.plot(t, B[1], "g-", label="b1,4")
+ax.plot(t, B[2], "b-", label="b2,4")
+ax.plot(t, B[3], "k-", label="b3,4")
+ax.plot(t, B[4], "m-", label="b4,4")
 
 # zobrazení legendy
 ax.legend()
