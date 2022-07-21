431bed3159fbd27a34a679c6e103f51dcfcb8c53
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:55 +0200

    Reformatted by Black

--- curves/parametric/linear_basis_sum.py
+++ curves/parametric/linear_basis_sum.py
@@ -23,8 +23,7 @@ from tabulate import tabulate
 t = np.linspace(0, 1, 11)
 
 # Bernsteinovy polynomy pro lineární interpolaci
-B = [1 * (1-t),
-     1 * t]
+B = [1 * (1 - t), 1 * t]
 
 # vytvoření datového rámce pro uložení hodnot Bernsteinových polynomů
 df = pd.DataFrame(index=t, columns=["b0,1", "b1,1", "sum"])
@@ -34,7 +33,7 @@ df["b0,1"] = B[0]
 df["b1,1"] = B[1]
 
 # součet hodnot Bernsteinových polynomů
-df["sum"] = B[0]+B[1]
+df["sum"] = B[0] + B[1]
 
 # vytištění obsahu datového rámce
-print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+print(tabulate(df, headers="keys", tablefmt="psql"))
