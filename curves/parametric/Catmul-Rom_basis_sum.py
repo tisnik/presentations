c06c0577e4dd09f1877409bef807a976866815eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Catmul-Rom_basis_sum.py
+++ curves/parametric/Catmul-Rom_basis_sum.py
@@ -26,10 +26,12 @@ t = np.linspace(0, 1, 11)
 tau = 0.5
 
 # bázové polynomy
-Q = [-tau*t + 2*tau*t**2 - tau*t**3,
-     1+(tau-3)*t**2+(2-tau)*t**3,
-     tau*t + (3-2*tau)*t**2 + (tau-2)*t**3,
-     -tau*t**2+tau*t**3]
+Q = [
+    -tau * t + 2 * tau * t ** 2 - tau * t ** 3,
+    1 + (tau - 3) * t ** 2 + (2 - tau) * t ** 3,
+    tau * t + (3 - 2 * tau) * t ** 2 + (tau - 2) * t ** 3,
+    -tau * t ** 2 + tau * t ** 3,
+]
 
 # vytvoření datového rámce pro uložení hodnot polynomů
 df = pd.DataFrame(index=t, columns=["q0", "q1", "q2", "q3", "sum"])
@@ -41,7 +43,7 @@ df["q2"] = Q[2]
 df["q3"] = Q[3]
 
 # součet hodnot polynomů
-df["sum"] = Q[0]+Q[1]+Q[2]+Q[3]
+df["sum"] = Q[0] + Q[1] + Q[2] + Q[3]
 
 # vytištění obsahu datového rámce
-print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+print(tabulate(df, headers="keys", tablefmt="psql"))
