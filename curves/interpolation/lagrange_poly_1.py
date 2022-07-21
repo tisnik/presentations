1c662118be2a1bbf9b324ee982a2c3872638b835
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/interpolation/lagrange_poly_1.py
+++ curves/interpolation/lagrange_poly_1.py
@@ -26,9 +26,9 @@ import matplotlib.pyplot as plt
 x = [0, 1, 2]
 
 # vstupní koeficienty
-P1_coeff = [1,-1.5,.5]
-P2_coeff = [0, 2,-1]
-P3_coeff = [0,-.5,.5]
+P1_coeff = [1, -1.5, 0.5]
+P2_coeff = [0, 2, -1]
+P3_coeff = [0, -0.5, 0.5]
 
 # získání funkcí pro výpočet křivky na základě báze
 P1 = poly.Polynomial(P1_coeff)
@@ -41,18 +41,18 @@ x_new = np.arange(-1.0, 3.1, 0.1)
 fig = plt.figure(figsize=(6.4, 4.8))
 
 # vykreslení jednotlivých bází
-plt.plot(x_new, P1(x_new), 'b', label = 'P1')
-plt.plot(x_new, P2(x_new), 'r', label = 'P2')
-plt.plot(x_new, P3(x_new), 'g', label = 'P3')
+plt.plot(x_new, P1(x_new), "b", label="P1")
+plt.plot(x_new, P2(x_new), "r", label="P2")
+plt.plot(x_new, P3(x_new), "g", label="P3")
 
 # zobrazení pomocných bodů v mřížce
 
-plt.plot(x, np.ones(len(x)), 'ko', x, np.zeros(len(x)), 'ko')
+plt.plot(x, np.ones(len(x)), "ko", x, np.zeros(len(x)), "ko")
 
 # popisky grafu
-plt.title('Lagrange Basis Polynomials')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.title("Lagrange Basis Polynomials")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.grid()
 plt.legend()
 
