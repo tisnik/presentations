eae62b7f87ec3943582a9738ab7dc31d1c1aa268
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:55 +0200

    Reformatted by Black

--- curves/parametric/linear_basis.py
+++ curves/parametric/linear_basis.py
@@ -22,22 +22,21 @@ import matplotlib.pyplot as plt
 t = np.linspace(0, 1, 50)
 
 # Bernsteinovy polynomy pro lineární interpolaci
-B = [1 * (1-t),
-     1 * t]
+B = [1 * (1 - t), 1 * t]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bázové polynomy', fontsize=15)
+fig.suptitle("Bázové polynomy", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 
 # bázové polynomy
-ax.plot(t, B[0], 'r-', label='b0,1')
-ax.plot(t, B[1], 'g-', label='b1,1')
+ax.plot(t, B[0], "r-", label="b0,1")
+ax.plot(t, B[1], "g-", label="b1,1")
 
 # zobrazení legendy
 ax.legend()
