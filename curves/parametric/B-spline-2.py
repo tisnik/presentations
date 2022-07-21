c8f39192ed71f1c702df527d9f852747f2d02f35
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/B-spline-2.py
+++ curves/parametric/B-spline-2.py
@@ -26,10 +26,12 @@ xc = (1, 1, 2, 2.5, 3, 2)
 yc = (1, 2, 2, 0.1, 2.9, 2.9)
 
 # Coonsovy polynomy
-C = [(1-t)**3,
-     3*t**3 - 6*t**2 + 4,
-     -3*t**3 + 3*t**2 + 3*t + 1,
-     t**3]
+C = [
+    (1 - t) ** 3,
+    3 * t ** 3 - 6 * t ** 2 + 4,
+    -3 * t ** 3 + 3 * t ** 2 + 3 * t + 1,
+    t ** 3,
+]
 
 
 def draw_coons_arc(xc, yc, ax, style):
@@ -37,8 +39,8 @@ def draw_coons_arc(xc, yc, ax, style):
     x = 0
     y = 0
     for i in range(0, 4):
-        x += xc[i]*C[i]
-        y += yc[i]*C[i]
+        x += xc[i] * C[i]
+        y += yc[i] * C[i]
 
     # konečná úprava sumy
     x /= 6
@@ -52,19 +54,18 @@ def draw_coons_arc(xc, yc, ax, style):
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('B-spline', fontsize=15)
+fig.suptitle("B-spline", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 4)
 ax.set_ylim(0, 3)
 
 # řídicí body B-spline
-ax.plot(xc, yc, 'k--', alpha=0.5)
-ax.plot(xc, yc, 'ro')
+ax.plot(xc, yc, "k--", alpha=0.5)
+ax.plot(xc, yc, "ro")
 
 # oblouk s násobnými body
-draw_coons_arc((xc[0], xc[0], xc[1], xc[2]),
-               (yc[0], yc[0], yc[1], yc[2]), ax, "k-")
+draw_coons_arc((xc[0], xc[0], xc[1], xc[2]), (yc[0], yc[0], yc[1], yc[2]), ax, "k-")
 
 # první oblouk
 draw_coons_arc(xc[0:4], yc[0:4], ax, "r-")
@@ -76,8 +77,7 @@ draw_coons_arc(xc[1:5], yc[1:5], ax, "b-")
 draw_coons_arc(xc[2:6], yc[2:6], ax, "g-")
 
 # oblouk s násobnými body
-draw_coons_arc((xc[3], xc[4], xc[5], xc[5]),
-               (yc[3], yc[4], yc[5], yc[5]), ax, "k-")
+draw_coons_arc((xc[3], xc[4], xc[5], xc[5]), (yc[3], yc[4], yc[5], yc[5]), ax, "k-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("B-spline_2.png")
