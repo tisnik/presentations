10fd2519bb54f82ca6f7d730b5a3b9337b8268c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Bezier_basis.py
+++ curves/parametric/Bezier_basis.py
@@ -22,26 +22,23 @@ import matplotlib.pyplot as plt
 t = np.arange(0, 1.05, 0.02)
 
 # Bernsteinovy polynomy
-B = [(1-t)**3,
-     3*t*(1-t)**2,
-     3*t**2*(1-t),
-     t**3]
+B = [(1 - t) ** 3, 3 * t * (1 - t) ** 2, 3 * t ** 2 * (1 - t), t ** 3]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bázové polynomy', fontsize=15)
+fig.suptitle("Bázové polynomy", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
 
 # bázové polynomy
-ax.plot(t, B[0], 'r-')
-ax.plot(t, B[1], 'g-')
-ax.plot(t, B[2], 'b-')
-ax.plot(t, B[3], 'k-')
+ax.plot(t, B[0], "r-")
+ax.plot(t, B[1], "g-")
+ax.plot(t, B[2], "b-")
+ax.plot(t, B[3], "k-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("bezier_basis.png")
