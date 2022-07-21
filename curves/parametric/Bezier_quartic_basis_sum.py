0e465026fb77c86ef404c7487fa4d5731b4d6394
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Bezier_quartic_basis_sum.py
+++ curves/parametric/Bezier_quartic_basis_sum.py
@@ -23,11 +23,13 @@ from tabulate import tabulate
 t = np.linspace(0, 1, 11)
 
 # Bernsteinovy polynomy pro Bézierovu kvartiku
-B = [1 * (1-t)**4,
-     4 * t * (1-t)**3,
-     6 * t**2 * (1-t)**2,
-     4 * t**3 * (1-t),
-     1 * t**4]
+B = [
+    1 * (1 - t) ** 4,
+    4 * t * (1 - t) ** 3,
+    6 * t ** 2 * (1 - t) ** 2,
+    4 * t ** 3 * (1 - t),
+    1 * t ** 4,
+]
 
 # vytvoření datového rámce pro uložení hodnot Bernsteinových polynomů
 df = pd.DataFrame(index=t, columns=["b0,4", "b1,4", "b2,4", "b3,4", "b4,4", "sum"])
@@ -40,7 +42,7 @@ df["b3,4"] = B[3]
 df["b4,4"] = B[4]
 
 # součet hodnot Bernsteinových polynomů
-df["sum"] = B[0]+B[1]+B[2]+B[3]+B[4]
+df["sum"] = B[0] + B[1] + B[2] + B[3] + B[4]
 
 # vytištění obsahu datového rámce
-print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+print(tabulate(df, headers="keys", tablefmt="psql"))
