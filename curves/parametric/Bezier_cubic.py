27e6e94660774fe310a91d5fa9abdc231090e5b7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Bezier_cubic.py
+++ curves/parametric/Bezier_cubic.py
@@ -26,33 +26,30 @@ xc = (1, 1, 3, 3)
 yc = (1, 2.9, 0.1, 2)
 
 # Bernsteinovy polynomy
-B = [(1-t)**3,
-     3*t*(1-t)**2,
-     3*t**2*(1-t),
-     t**3]
+B = [(1 - t) ** 3, 3 * t * (1 - t) ** 2, 3 * t ** 2 * (1 - t), t ** 3]
 
 # výpočet bodů ležících na Bézierově kubice
 x = 0
 y = 0
 for i in range(0, 4):
-    x += xc[i]*B[i]
-    y += yc[i]*B[i]
+    x += xc[i] * B[i]
+    y += yc[i] * B[i]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bézierova kubika', fontsize=15)
+fig.suptitle("Bézierova kubika", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 4)
 ax.set_ylim(0, 3)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # řídicí body Bézierovy kubiky
-ax.plot(xc, yc, 'ro')
+ax.plot(xc, yc, "ro")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("bezier_cubic.png")
