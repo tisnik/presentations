f21a3da701a42eae50647987c60e22acd5a87946
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Catmul-Rom_cubic.py
+++ curves/parametric/Catmul-Rom_cubic.py
@@ -29,33 +29,35 @@ yc = (1, 2.9, 0.1, 2)
 tau = 0.5
 
 # bázové polynomy
-Q = [-tau*t + 2*tau*t**2 - tau*t**3,
-     1+(tau-3)*t**2+(2-tau)*t**3,
-     tau*t + (3-2*tau)*t**2 + (tau-2)*t**3,
-     -tau*t**2+tau*t**3]
+Q = [
+    -tau * t + 2 * tau * t ** 2 - tau * t ** 3,
+    1 + (tau - 3) * t ** 2 + (2 - tau) * t ** 3,
+    tau * t + (3 - 2 * tau) * t ** 2 + (tau - 2) * t ** 3,
+    -tau * t ** 2 + tau * t ** 3,
+]
 
 # výpočet bodů ležících na spline
 x = 0
 y = 0
 for i in range(0, 4):
-    x += xc[i]*Q[i]
-    y += yc[i]*Q[i]
+    x += xc[i] * Q[i]
+    y += yc[i] * Q[i]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Catmul-Romova spline', fontsize=15)
+fig.suptitle("Catmul-Romova spline", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 4)
 ax.set_ylim(0, 3)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # řídicí body kubiky
-ax.plot(xc, yc, 'ro')
+ax.plot(xc, yc, "ro")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("catmul-rom_cubic.png")
