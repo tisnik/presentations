54915cb9be9f8ad648e11e22eb5971c357026709
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Bezier_quintic_basis_sum.py
+++ curves/parametric/Bezier_quintic_basis_sum.py
@@ -23,15 +23,19 @@ from tabulate import tabulate
 t = np.linspace(0, 1, 11)
 
 # Bernsteinovy polynomy pro Bézierovu křivku stupně 5
-B = [1 * (1-t)**5,
-     5 * t * (1-t)**4,
-     10 * t**2 * (1-t)**3,
-     10 * t**3 * (1-t)**2,
-     5 * t**4 * (1-t),
-     1 * t**5]
+B = [
+    1 * (1 - t) ** 5,
+    5 * t * (1 - t) ** 4,
+    10 * t ** 2 * (1 - t) ** 3,
+    10 * t ** 3 * (1 - t) ** 2,
+    5 * t ** 4 * (1 - t),
+    1 * t ** 5,
+]
 
 # vytvoření datového rámce pro uložení hodnot Bernsteinových polynomů
-df = pd.DataFrame(index=t, columns=["b0,5", "b1,5", "b2,5", "b3,5", "b4,5", "b5,5", "sum"])
+df = pd.DataFrame(
+    index=t, columns=["b0,5", "b1,5", "b2,5", "b3,5", "b4,5", "b5,5", "sum"]
+)
 
 # inicializace jednotlivých sloupců datového rámce
 df["b0,5"] = B[0]
@@ -42,7 +46,7 @@ df["b4,5"] = B[4]
 df["b5,5"] = B[5]
 
 # součet hodnot Bernsteinových polynomů
-df["sum"] = B[0]+B[1]+B[2]+B[3]+B[4]+B[5]
+df["sum"] = B[0] + B[1] + B[2] + B[3] + B[4] + B[5]
 
 # vytištění obsahu datového rámce
-print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+print(tabulate(df, headers="keys", tablefmt="psql"))
