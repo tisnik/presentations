cc7bc4b7037472724b0a81ce4507142028cfa2de
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Hermite_basis.py
+++ curves/parametric/Hermite_basis.py
@@ -22,26 +22,28 @@ import matplotlib.pyplot as plt
 t = np.linspace(0, 1, 50)
 
 # bázové polynomy
-H = [2*t**3 - 3*t**2+1,
-     t**3 - 2*t**2 + t,
-     -2*t**3 + 3*t**2,
-     t**3 - t**2]
+H = [
+    2 * t ** 3 - 3 * t ** 2 + 1,
+    t ** 3 - 2 * t ** 2 + t,
+    -2 * t ** 3 + 3 * t ** 2,
+    t ** 3 - t ** 2,
+]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bázové polynomy', fontsize=15)
+fig.suptitle("Bázové polynomy", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 1)
 ax.set_ylim(-0.25, 1)
 
 # bázové polynomy
-ax.plot(t, H[0], 'r-')
-ax.plot(t, H[1], 'g-')
-ax.plot(t, H[2], 'b-')
-ax.plot(t, H[3], 'k-')
+ax.plot(t, H[0], "r-")
+ax.plot(t, H[1], "g-")
+ax.plot(t, H[2], "b-")
+ax.plot(t, H[3], "k-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("hermite_basis.png")
