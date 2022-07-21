0fe2fdd5ca1544337117549ba8daf9aeb1106020
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Catmul-Rom_spline_D.py
+++ curves/parametric/Catmul-Rom_spline_D.py
@@ -29,18 +29,20 @@ yc = [1, 2, 1, 2, 1, 2, 1]
 tau = 0.5
 
 # bázové polynomy
-Q = [-tau*t + 2*tau*t**2 - tau*t**3,
-     1+(tau-3)*t**2+(2-tau)*t**3,
-     tau*t + (3-2*tau)*t**2 + (tau-2)*t**3,
-     -tau*t**2+tau*t**3]
+Q = [
+    -tau * t + 2 * tau * t ** 2 - tau * t ** 3,
+    1 + (tau - 3) * t ** 2 + (2 - tau) * t ** 3,
+    tau * t + (3 - 2 * tau) * t ** 2 + (tau - 2) * t ** 3,
+    -tau * t ** 2 + tau * t ** 3,
+]
 
 
 def draw_catmul_rom_arc(xc, yc, ax, style):
     x = 0
     y = 0
     for i in range(0, 4):
-        x += xc[i]*Q[i]
-        y += yc[i]*Q[i]
+        x += xc[i] * Q[i]
+        y += yc[i] * Q[i]
 
     # vrcholy na křivce pospojované úsečkami
     ax.plot(x, y, style)
@@ -51,40 +53,50 @@ def draw_catmul_rom_spline(filename, xc, yc, draw_first_arcs, draw_second_arcs):
     fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
     # titulek grafu
-    fig.suptitle('Catmul-Rom spline', fontsize=15)
+    fig.suptitle("Catmul-Rom spline", fontsize=15)
 
     # určení rozsahů na obou souřadných osách
     ax.set_xlim(0.5, 7.5)
     ax.set_ylim(0.5, 2.5)
 
     # řídicí body Catmul-Romovy spline
-    ax.plot(xc, yc, 'k--', alpha=0.5)
-    ax.plot(xc, yc, 'ro')
+    ax.plot(xc, yc, "k--", alpha=0.5)
+    ax.plot(xc, yc, "ro")
 
     # první dva oblouky s násobnými body
     if draw_first_arcs:
-        draw_catmul_rom_arc((xc[0], xc[0], xc[0], xc[1]),
-                            (yc[0], yc[0], yc[0], yc[1]), ax, "g-")
+        draw_catmul_rom_arc(
+            (xc[0], xc[0], xc[0], xc[1]), (yc[0], yc[0], yc[0], yc[1]), ax, "g-"
+        )
 
     if draw_second_arcs:
-        draw_catmul_rom_arc((xc[0], xc[0], xc[1], xc[2]),
-                            (yc[0], yc[0], yc[1], yc[2]), ax, "y-")
+        draw_catmul_rom_arc(
+            (xc[0], xc[0], xc[1], xc[2]), (yc[0], yc[0], yc[1], yc[2]), ax, "y-"
+        )
 
     last = len(xc)
 
     # další oblouky
-    for start in range(0, last-3):
+    for start in range(0, last - 3):
         style = "r-" if start % 2 == 0 else "b-"
-        draw_catmul_rom_arc(xc[start:start+4], yc[start:start+4], ax, style)
+        draw_catmul_rom_arc(xc[start : start + 4], yc[start : start + 4], ax, style)
 
     # poslední dva oblouky s násobnými body
     if draw_second_arcs:
-        draw_catmul_rom_arc((xc[last-3], xc[last-2], xc[last-1], xc[last-1]),
-                            (yc[last-3], yc[last-2], yc[last-1], yc[last-1]), ax, "y-")
+        draw_catmul_rom_arc(
+            (xc[last - 3], xc[last - 2], xc[last - 1], xc[last - 1]),
+            (yc[last - 3], yc[last - 2], yc[last - 1], yc[last - 1]),
+            ax,
+            "y-",
+        )
 
     if draw_first_arcs:
-        draw_catmul_rom_arc((xc[last-2], xc[last-1], xc[last-1], xc[last-1]),
-                            (yc[last-2], yc[last-1], yc[last-1], yc[last-1]), ax, "g-")
+        draw_catmul_rom_arc(
+            (xc[last - 2], xc[last - 1], xc[last - 1], xc[last - 1]),
+            (yc[last - 2], yc[last - 1], yc[last - 1], yc[last - 1]),
+            ax,
+            "g-",
+        )
 
     # uložení grafu do rastrového obrázku
     plt.savefig(filename)
