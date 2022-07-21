7455b4312c8bcdb5ead04f6743696fb5bd1cabf4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Coons.py
+++ curves/parametric/Coons.py
@@ -26,17 +26,19 @@ xc = (1, 1, 3, 3)
 yc = (1, 2, 2, 0.5)
 
 # Coonsovy polynomy
-C = [(1-t)**3,
-     3*t**3 - 6*t**2 + 4,
-     -3*t**3 + 3*t**2 + 3*t + 1,
-     t**3]
+C = [
+    (1 - t) ** 3,
+    3 * t ** 3 - 6 * t ** 2 + 4,
+    -3 * t ** 3 + 3 * t ** 2 + 3 * t + 1,
+    t ** 3,
+]
 
 # výpočet bodů ležících na Coonsově kubice
 x = 0
 y = 0
 for i in range(0, 4):
-    x += xc[i]*C[i]
-    y += yc[i]*C[i]
+    x += xc[i] * C[i]
+    y += yc[i] * C[i]
 
 # konečná úprava sumy
 x /= 6
@@ -46,17 +48,17 @@ y /= 6
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Coonsova kubika', fontsize=15)
+fig.suptitle("Coonsova kubika", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 4)
 ax.set_ylim(0, 3)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # řídicí body Coonsovy kubiky
-ax.plot(xc, yc, 'ro')
+ax.plot(xc, yc, "ro")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("coons.png")
