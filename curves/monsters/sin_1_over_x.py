d62f59d19470c75928959a77aba2f630a0fe48b8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/monsters/sin_1_over_x.py
+++ curves/monsters/sin_1_over_x.py
@@ -22,16 +22,16 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 2, 500)
 
 # výpočet bodů ležících na křivce vytvořené funkcí
-y = np.sin(1/x)
+y = np.sin(1 / x)
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('y=sin 1/x', fontsize=15)
+fig.suptitle("y=sin 1/x", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("sin_1_over_x.png")
