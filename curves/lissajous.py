c6ed7988adc16a1f57aaf554e155b65ef3a165af
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/lissajous.py
+++ curves/lissajous.py
@@ -19,7 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # hodnoty parametru t
-t = np.arange(0, 2*np.pi, 0.02)
+t = np.arange(0, 2 * np.pi, 0.02)
 
 # poloměry v osách
 a = 3.0
@@ -30,24 +30,24 @@ kx = 3
 ky = 2
 
 # výpočet bodů ležících na obrazci
-x = a*np.cos(kx*t)
-y = b*np.sin(ky*t)
+x = a * np.cos(kx * t)
+y = b * np.sin(ky * t)
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Lissajousův obrazec', fontsize=15)
+fig.suptitle("Lissajousův obrazec", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(-4, 4)
 ax.set_ylim(-3, 3)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # vrcholy na křivce (každý čtvrtý)
-ax.plot(x[::4], y[::4], 'ro')
+ax.plot(x[::4], y[::4], "ro")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("lissajous.png")
