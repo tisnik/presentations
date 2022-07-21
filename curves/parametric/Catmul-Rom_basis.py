5348a25eb96f94e7c75a84a9009df8f0cce55815
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parametric/Catmul-Rom_basis.py
+++ curves/parametric/Catmul-Rom_basis.py
@@ -25,26 +25,28 @@ t = np.linspace(0, 1, 50)
 tau = 0.5
 
 # bázové polynomy
-Q = [-tau*t + 2*tau*t**2 - tau*t**3,
-     1+(tau-3)*t**2+(2-tau)*t**3,
-     tau*t + (3-2*tau)*t**2 + (tau-2)*t**3,
-     -tau*t**2+tau*t**3]
+Q = [
+    -tau * t + 2 * tau * t ** 2 - tau * t ** 3,
+    1 + (tau - 3) * t ** 2 + (2 - tau) * t ** 3,
+    tau * t + (3 - 2 * tau) * t ** 2 + (tau - 2) * t ** 3,
+    -tau * t ** 2 + tau * t ** 3,
+]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Bázové polynomy', fontsize=15)
+fig.suptitle("Bázové polynomy", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 1)
 ax.set_ylim(-0.25, 1)
 
 # bázové polynomy
-ax.plot(t, Q[0], 'r-')
-ax.plot(t, Q[1], 'g-')
-ax.plot(t, Q[2], 'b-')
-ax.plot(t, Q[3], 'k-')
+ax.plot(t, Q[0], "r-")
+ax.plot(t, Q[1], "g-")
+ax.plot(t, Q[2], "b-")
+ax.plot(t, Q[3], "k-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("catmul-rom_basis.png")
