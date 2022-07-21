165f8b4ce4487dabffce00e010f1926bdbc61e22
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:54 +0200

    Reformatted by Black

--- curves/parabola_catenary_2.py
+++ curves/parabola_catenary_2.py
@@ -22,25 +22,25 @@ import matplotlib.pyplot as plt
 x = np.arange(-2, 2, 0.04)
 
 # parabola: hodnoty na y-ové ose (sklon úsečky řízen konstantou)
-y1 = 3-0.65*np.power(x, 2)
+y1 = 3 - 0.65 * np.power(x, 2)
 
 # poloměr křivosti ve vrcholu
 a = 1.0
 
 # řetězovka: výpočet bodů ležících na řetězovce
-y2 = 4-a*np.cosh(x/a)
+y2 = 4 - a * np.cosh(x / a)
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Parabola versus řetězovka #2', fontsize=15)
+fig.suptitle("Parabola versus řetězovka #2", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y1, 'g-', label="Parabola")
+ax.plot(x, y1, "g-", label="Parabola")
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y2, 'r-', label="Řetězovka")
+ax.plot(x, y2, "r-", label="Řetězovka")
 
 # zobrazení popisku křivek
 plt.legend()
