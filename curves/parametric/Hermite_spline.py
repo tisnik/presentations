20fb1cc692483f9f68dcb175b59f4b2d34967c5e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-21 10:46:55 +0200

    Reformatted by Black

--- curves/parametric/Hermite_spline.py
+++ curves/parametric/Hermite_spline.py
@@ -30,34 +30,36 @@ mx = (0, 0)
 my = (1, -1)
 
 # bázové polynomy
-H = [2*t**3 - 3*t**2+1,
-     t**3 - 2*t**2 + t,
-     -2*t**3 + 3*t**2,
-     t**3 - t**2]
+H = [
+    2 * t ** 3 - 3 * t ** 2 + 1,
+    t ** 3 - 2 * t ** 2 + t,
+    -2 * t ** 3 + 3 * t ** 2,
+    t ** 3 - t ** 2,
+]
 
 # výpočet bodů ležících na spline
-x = xc[0]*H[0] + mx[0]*H[1] + xc[1]*H[2] + mx[1]*H[3]
-y = yc[0]*H[0] + my[0]*H[1] + yc[1]*H[2] + my[1]*H[3]
+x = xc[0] * H[0] + mx[0] * H[1] + xc[1] * H[2] + mx[1] * H[3]
+y = yc[0] * H[0] + my[0] * H[1] + yc[1] * H[2] + my[1] * H[3]
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Hermitova spline', fontsize=15)
+fig.suptitle("Hermitova spline", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(0, 4)
 ax.set_ylim(-0.5, 2.5)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # řídicí body
-ax.plot(xc, yc, 'ro')
+ax.plot(xc, yc, "ro")
 
 # tangenty
-ax.plot([xc[0]+mx[0], xc[0]], [yc[0]+my[0], yc[0]], 'b-')
-ax.plot([xc[1]+mx[1], xc[1]], [yc[1]+my[1], yc[1]], 'y-')
+ax.plot([xc[0] + mx[0], xc[0]], [yc[0] + my[0], yc[0]], "b-")
+ax.plot([xc[1] + mx[1], xc[1]], [yc[1] + my[1], yc[1]], "y-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("hermite_spline.png")
