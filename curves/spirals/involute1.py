850cf37e72de88ef00b6028503d4d6358974c088
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-22 09:29:54 +0200

    Reformatted by Black

--- curves/spirals/involute1.py
+++ curves/spirals/involute1.py
@@ -16,27 +16,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # nezávislý parametr t
-t = np.linspace(0.00, 3*np.pi, 100)
+t = np.linspace(0.00, 3 * np.pi, 100)
 
 # koeficient evolventy
 r = 10
 
 # funkce evolventy
-x = r*(np.cos(t) + t*np.sin(t))
-y = r*(np.sin(t) - t*np.cos(t))
+x = r * (np.cos(t) + t * np.sin(t))
+y = r * (np.sin(t) - t * np.cos(t))
 
 # rozměry grafu při uložení: 640x480 pixelů
 fig, ax = plt.subplots(1, figsize=(6.4, 4.8))
 
 # titulek grafu
-fig.suptitle('Evolventa', fontsize=15)
+fig.suptitle("Evolventa", fontsize=15)
 
 # určení rozsahů na obou souřadných osách
 ax.set_xlim(-100, 140)
 ax.set_ylim(-80, 100)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(x, y, 'g-')
+ax.plot(x, y, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("involute1.png")
