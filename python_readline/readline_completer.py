59b08af714f7a2ac75d65413823b56385693d3dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-22 09:29:55 +0200

    Reformatted by Black

--- python_readline/readline_completer.py
+++ python_readline/readline_completer.py
@@ -16,12 +16,14 @@ def completer(text, state):
 
 
 def show_help():
-    print("""Help
+    print(
+        """Help
 --------
 quit - quit this application
 exit - exit from this application
 eval - evaluate
-""")
+"""
+    )
 
 
 readline.set_completer(completer)
