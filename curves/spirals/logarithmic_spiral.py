25d6c1dd5cf37ead69006ade00a0e5ddc8ef5743
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-22 09:29:55 +0200

    Reformatted by Black

--- curves/spirals/logarithmic_spiral.py
+++ curves/spirals/logarithmic_spiral.py
@@ -19,22 +19,22 @@ import matplotlib.pyplot as plt
 points = 150
 
 # úhel v polárním grafu
-theta = np.linspace(0.01, 8*np.pi, points)
+theta = np.linspace(0.01, 8 * np.pi, points)
 
 # koeficient spirály
 k = 0.15
 
 # funkce: vzdálenost od středu
-radius = np.exp(k*theta)
+radius = np.exp(k * theta)
 
 # rozměry grafu při uložení: 640x480 pixelů
-fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={'projection': 'polar'})
+fig, ax = plt.subplots(1, figsize=(6.4, 4.8), subplot_kw={"projection": "polar"})
 
 # titulek grafu
-fig.suptitle('Logaritmická spirála', fontsize=15)
+fig.suptitle("Logaritmická spirála", fontsize=15)
 
 # vrcholy na křivce pospojované úsečkami
-ax.plot(theta, radius, 'g-')
+ax.plot(theta, radius, "g-")
 
 # uložení grafu do rastrového obrázku
 plt.savefig("logarithmic_spiral.png")
