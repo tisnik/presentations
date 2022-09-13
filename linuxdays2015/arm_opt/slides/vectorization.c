7794fd9439d345f9bf912f049a56750ad1886672
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-13 07:48:59 +0200

    Reformatted by indent

--- linuxdays2015/arm_opt/slides/vectorization.c
+++ linuxdays2015/arm_opt/slides/vectorization.c
@@ -1,11 +1,8 @@
-void add_offset_to_vector(int * __restrict px,
-                          int * __restrict py,
-                          int n, int offset)
+void add_offset_to_vector(int *__restrict px,
+                          int *__restrict py, int n, int offset)
 {
     unsigned int i;
-    for (i=0; i < (n & ~3); i++)
-    {
+    for (i = 0; i < (n & ~3); i++) {
         px[i] = py[i] + offset;
     }
 }
-
