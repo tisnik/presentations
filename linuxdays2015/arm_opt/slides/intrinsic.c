a6cd52c232984db82c480ac7ac0a09f34fa5e350
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-13 07:49:07 +0200

    Reformatted by indent

--- linuxdays2015/arm_opt/slides/intrinsic.c
+++ linuxdays2015/arm_opt/slides/intrinsic.c
@@ -1,7 +1,6 @@
 #include <arm_neon.h>
- 
-uint32x4_t double_elements(uint32x4_t input)
-{ 
-    return(vaddq_u32(input, input)); 
-}
 
+uint32x4_t double_elements(uint32x4_t input)
+{
+    return (vaddq_u32(input, input));
+}
