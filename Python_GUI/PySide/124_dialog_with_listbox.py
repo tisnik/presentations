253df3591db429cb37d635a9e71a9525f39d3432
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-19 08:03:10 +0200

    Added copyright into the source

--- Python_GUI/PySide/124_dialog_with_listbox.py
+++ Python_GUI/PySide/124_dialog_with_listbox.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
+#
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 import sys
 
 # import "jádra" frameworku Qt i modulu pro GUI
