60b28732c6e0dd4ea888ddeeba22507fb1a5e4b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-22 12:04:17 +0200

    Reformatted by indent

--- cordic/fx.c
+++ cordic/fx.c
@@ -25,8 +25,8 @@
 typedef signed int fx;
 
 /* hlavičky použitých funkcí */
-void   fx_print(fx x);
-fx     fp2fx(double x);
+void fx_print(fx x);
+fx fp2fx(double x);
 double fx2fp(fx x);
 
 /* tabulka arkustangentu úhlů */
@@ -42,12 +42,13 @@ fx pows[MAX_ITER];
 void fx_print(fx x)
 {
     int i;
-    int val=x;                         /* pomocná proměnná pro převod do dvojkové soustavy */
+    int val = x;                /* pomocná proměnná pro převod do dvojkové soustavy */
     printf("bin: ");
-    for (i=0; i<A+B; i++) {            /* převod na řetězec bitů (do dvojkové soustavy) */
-        putchar(!!(val & (1<<(A+B-1)))+'0'); /* výpis hodnoty aktuálně nejvyššího bitu */
-        if (i==B-1) putchar('.');      /* po řádové binární čárce vypsat značku */
-        val=val<<1;                    /* posun na další (méně významný) bit */
+    for (i = 0; i < A + B; i++) {       /* převod na řetězec bitů (do dvojkové soustavy) */
+        putchar(!!(val & (1 << (A + B - 1))) + '0');    /* výpis hodnoty aktuálně nejvyššího bitu */
+        if (i == B - 1)
+            putchar('.');       /* po řádové binární čárce vypsat značku */
+        val = val << 1;         /* posun na další (méně významný) bit */
     }
 
     printf("   hex: %08x   fp: %+11.5f\n", x, fx2fp(x));
@@ -59,7 +60,7 @@ void fx_print(fx x)
  */
 fx fp2fx(double x)
 {
-    return (fx)(x*(2<<(B-1)));
+    return (fx) (x * (2 << (B - 1)));
 }
 
 /*
@@ -68,7 +69,7 @@ fx fp2fx(double x)
  */
 fx int2fx(int x)
 {
-    return (fx)(x<<B);
+    return (fx) (x << B);
 }
 
 /*
@@ -77,7 +78,7 @@ fx int2fx(int x)
  */
 double fx2fp(fx x)
 {
-    return (double)x/(2<<(B-1));
+    return (double) x / (2 << (B - 1));
 }
 
 /*
@@ -86,7 +87,7 @@ double fx2fp(fx x)
  */
 fx fx_add(fx x, fx y)
 {
-    return x+y;
+    return x + y;
 }
 
 /*
@@ -95,7 +96,7 @@ fx fx_add(fx x, fx y)
  */
 fx fx_sub(fx x, fx y)
 {
-    return x-y;
+    return x - y;
 }
 
 /*
@@ -104,7 +105,7 @@ fx fx_sub(fx x, fx y)
  */
 fx fx_mul(fx x, fx y)
 {
-    fx result=(x>>(B/2))*(y>>(B/2));
+    fx result = (x >> (B / 2)) * (y >> (B / 2));
     return result;
 }
 
@@ -114,8 +115,8 @@ fx fx_mul(fx x, fx y)
  */
 fx fx_div(fx x, fx y)
 {
-    fx result=x/(y>>(B/2));
-    return result<<(B/2);
+    fx result = x / (y >> (B / 2));
+    return result << (B / 2);
 }
 
 /*
@@ -125,10 +126,10 @@ fx fx_div(fx x, fx y)
 void fx_create_tables(void)
 {
     int i;
-    for (i=0; i<MAX_ITER; i++) {
-        double p=pow(2.0, -i);
-        atans[i]=fp2fx(atan(p));
-        pows[i]=fp2fx(p);
+    for (i = 0; i < MAX_ITER; i++) {
+        double p = pow(2.0, -i);
+        atans[i] = fp2fx(atan(p));
+        pows[i] = fp2fx(p);
     }
 }
 
@@ -138,29 +139,31 @@ fx fx_tan_cordic(fx delta)
 {
     int i;
     /* nastavení počátečních podmínek */
-    fx x0=fp2fx(1.0);
-    fx y0=fp2fx(0.0);
+    fx x0 = fp2fx(1.0);
+    fx y0 = fp2fx(0.0);
     fx xn;
-    if (delta==0) return 0;                 /* ošetření nulového úhlu */
-    for (i=0; i<MAX_ITER; i++) {            /* iterační smyčka */
-        if (delta<0) {                      /* úhel je záporný =&gt; rotace doleva */
-            xn=fx_add(x0, fx_mul(y0, pows[i]));
-            y0=fx_sub(y0, fx_mul(x0, pows[i]));
-            delta=fx_add(delta, atans[i]);
+    if (delta == 0)
+        return 0;               /* ošetření nulového úhlu */
+    for (i = 0; i < MAX_ITER; i++) {    /* iterační smyčka */
+        if (delta < 0) {        /* úhel je záporný =&gt; rotace doleva */
+            xn = fx_add(x0, fx_mul(y0, pows[i]));
+            y0 = fx_sub(y0, fx_mul(x0, pows[i]));
+            delta = fx_add(delta, atans[i]);
+        } else {                /* úhel je kladný =&gt; rotace doprava */
+            xn = fx_sub(x0, fx_mul(y0, pows[i]));
+            y0 = fx_add(y0, fx_mul(x0, pows[i]));
+            delta = fx_sub(delta, atans[i]);
         }
-        else {                              /* úhel je kladný =&gt; rotace doprava */
-            xn=fx_sub(x0, fx_mul(y0, pows[i]));
-            y0=fx_add(y0, fx_mul(x0, pows[i]));
-            delta=fx_sub(delta, atans[i]);
-        }
-        x0=xn;
+        x0 = xn;
 /*        printf("%i\t%+f\t%+f\t%+f\n", i, fx2fp(x0), fx2fp(y0), fx2fp(delta)); */
     }
-    if (x0==0)                              /* ošetření tangenty pravého úhlu */
-        if (y0<0) return 0;
-        else return 0;
+    if (x0 == 0)                /* ošetření tangenty pravého úhlu */
+        if (y0 < 0)
+            return 0;
+        else
+            return 0;
     else
-        return fx_div(y0,x0);               /* vrátit výsledek operace */
+        return fx_div(y0, x0);  /* vrátit výsledek operace */
 }
 
 /* výpočet funkce tan() pro zadaný úhel delta */
@@ -169,166 +172,164 @@ fx fx_tan_cordic_optim(fx delta)
 {
     int i;
     /* nastavení počátečních podmínek */
-    fx x0=int2fx(1);
-    fx y0=0;
+    fx x0 = int2fx(1);
+    fx y0 = 0;
     fx xn;
-    if (delta==0) return 0;                 /* ošetření nulového úhlu */
-    for (i=0; i<MAX_ITER; i++) {            /* iterační smyčka */
-        if (delta<0) {                      /* úhel je záporný =&gt; rotace doleva */
-            xn=fx_add(x0, y0>>i);           /* místo násobení bitový posuv */
-            y0=fx_sub(y0, x0>>i);
-            delta=fx_add(delta, atans[i]);
+    if (delta == 0)
+        return 0;               /* ošetření nulového úhlu */
+    for (i = 0; i < MAX_ITER; i++) {    /* iterační smyčka */
+        if (delta < 0) {        /* úhel je záporný =&gt; rotace doleva */
+            xn = fx_add(x0, y0 >> i);   /* místo násobení bitový posuv */
+            y0 = fx_sub(y0, x0 >> i);
+            delta = fx_add(delta, atans[i]);
+        } else {                /* úhel je kladný =&gt; rotace doprava */
+            xn = fx_sub(x0, y0 >> i);
+            y0 = fx_add(y0, x0 >> i);
+            delta = fx_sub(delta, atans[i]);
         }
-        else {                              /* úhel je kladný =&gt; rotace doprava */
-            xn=fx_sub(x0, y0>>i);
-            y0=fx_add(y0, x0>>i);
-            delta=fx_sub(delta, atans[i]);
-        }
-        x0=xn;
+        x0 = xn;
     }
-    if (x0==0)                              /* ošetření tangenty pravého úhlu */
-        if (y0<0) return 0;
-        else return 0;
+    if (x0 == 0)                /* ošetření tangenty pravého úhlu */
+        if (y0 < 0)
+            return 0;
+        else
+            return 0;
     else
-        return fx_div(y0,x0);               /* vrátit výsledek operace */
+        return fx_div(y0, x0);  /* vrátit výsledek operace */
 }
 
 /* výpočet funkce sin() pro zadaný úhel delta */
 fx fx_sin_cordic_optim(fx delta)
 {
     int i;
-    static fx K_fx=(fx)(K_float*(2<<(B-1)));
+    static fx K_fx = (fx) (K_float * (2 << (B - 1)));
     /* nastavení počátečních podmínek */
-    fx x0=int2fx(1);
-    fx y0=0;
+    fx x0 = int2fx(1);
+    fx y0 = 0;
     fx xn;
-    for (i=0; i<MAX_ITER; i++) {            /* iterační smyčka */
-        if (delta<0) {                      /* úhel je záporný =&gt; rotace doleva */
-            xn=fx_add(x0, y0>>i);           /* místo násobení bitový posuv */
-            y0=fx_sub(y0, x0>>i);
-            delta=fx_add(delta, atans[i]);
+    for (i = 0; i < MAX_ITER; i++) {    /* iterační smyčka */
+        if (delta < 0) {        /* úhel je záporný =&gt; rotace doleva */
+            xn = fx_add(x0, y0 >> i);   /* místo násobení bitový posuv */
+            y0 = fx_sub(y0, x0 >> i);
+            delta = fx_add(delta, atans[i]);
+        } else {                /* úhel je kladný =&gt; rotace doprava */
+            xn = fx_sub(x0, y0 >> i);
+            y0 = fx_add(y0, x0 >> i);
+            delta = fx_sub(delta, atans[i]);
         }
-        else {                              /* úhel je kladný =&gt; rotace doprava */
-            xn=fx_sub(x0, y0>>i);
-            y0=fx_add(y0, x0>>i);
-            delta=fx_sub(delta, atans[i]);
-        }
-        x0=xn;
+        x0 = xn;
     }
-    return fx_mul(y0, K_fx);                /* opravit "zesílení" výsledku */
+    return fx_mul(y0, K_fx);    /* opravit "zesílení" výsledku */
 }
 
 /* výpočet funkce cos() pro zadaný úhel delta */
 fx fx_cos_cordic_optim(fx delta)
 {
     int i;
-    static fx K_fx=(fx)(K_float*(2<<(B-1)));
+    static fx K_fx = (fx) (K_float * (2 << (B - 1)));
     /* nastavení počátečních podmínek */
-    fx x0=int2fx(1);
-    fx y0=0;
+    fx x0 = int2fx(1);
+    fx y0 = 0;
     fx xn;
-    for (i=0; i<MAX_ITER; i++) {            /* iterační smyčka */
-        if (delta<0) {                      /* úhel je záporný =&gt; rotace doleva */
-            xn=fx_add(x0, y0>>i);           /* místo násobení bitový posuv */
-            y0=fx_sub(y0, x0>>i);
-            delta=fx_add(delta, atans[i]);
+    for (i = 0; i < MAX_ITER; i++) {    /* iterační smyčka */
+        if (delta < 0) {        /* úhel je záporný =&gt; rotace doleva */
+            xn = fx_add(x0, y0 >> i);   /* místo násobení bitový posuv */
+            y0 = fx_sub(y0, x0 >> i);
+            delta = fx_add(delta, atans[i]);
+        } else {                /* úhel je kladný =&gt; rotace doprava */
+            xn = fx_sub(x0, y0 >> i);
+            y0 = fx_add(y0, x0 >> i);
+            delta = fx_sub(delta, atans[i]);
         }
-        else {                              /* úhel je kladný =&gt; rotace doprava */
-            xn=fx_sub(x0, y0>>i);
-            y0=fx_add(y0, x0>>i);
-            delta=fx_sub(delta, atans[i]);
-        }
-        x0=xn;
+        x0 = xn;
     }
-    return fx_mul(x0, K_fx);                /* opravit "zesílení" výsledku */
+    return fx_mul(x0, K_fx);    /* opravit "zesílení" výsledku */
 }
 
 /* výpočet funkce sin() pro zadaný úhel delta */
 fx fx_sin_cordic_optim_iter(fx delta, int iter)
 {
     int i;
-    static fx K_fx=(fx)(K_float*(2<<(B-1)));
+    static fx K_fx = (fx) (K_float * (2 << (B - 1)));
     /* nastavení počátečních podmínek */
-    fx x0=int2fx(1);
-    fx y0=0;
+    fx x0 = int2fx(1);
+    fx y0 = 0;
     fx xn;
-    for (i=0; i<iter; i++) {                /* iterační smyčka */
-        if (delta<0) {                      /* úhel je záporný =&gt; rotace doleva */
-            xn=fx_add(x0, y0>>i);           /* místo násobení bitový posuv */
-            y0=fx_sub(y0, x0>>i);
-            delta=fx_add(delta, atans[i]);
+    for (i = 0; i < iter; i++) {        /* iterační smyčka */
+        if (delta < 0) {        /* úhel je záporný =&gt; rotace doleva */
+            xn = fx_add(x0, y0 >> i);   /* místo násobení bitový posuv */
+            y0 = fx_sub(y0, x0 >> i);
+            delta = fx_add(delta, atans[i]);
+        } else {                /* úhel je kladný =&gt; rotace doprava */
+            xn = fx_sub(x0, y0 >> i);
+            y0 = fx_add(y0, x0 >> i);
+            delta = fx_sub(delta, atans[i]);
         }
-        else {                              /* úhel je kladný =&gt; rotace doprava */
-            xn=fx_sub(x0, y0>>i);
-            y0=fx_add(y0, x0>>i);
-            delta=fx_sub(delta, atans[i]);
-        }
-        x0=xn;
+        x0 = xn;
     }
-    return fx_mul(y0, K_fx);                /* opravit "zesílení" výsledku */
+    return fx_mul(y0, K_fx);    /* opravit "zesílení" výsledku */
 }
 
 /* výpočet funkce cos() pro zadaný úhel delta */
 fx fx_cos_cordic_optim_iter(fx delta, int iter)
 {
     int i;
-    static fx K_fx=(fx)(K_float*(2<<(B-1)));
+    static fx K_fx = (fx) (K_float * (2 << (B - 1)));
     /* nastavení počátečních podmínek */
-    fx x0=int2fx(1);
-    fx y0=0;
+    fx x0 = int2fx(1);
+    fx y0 = 0;
     fx xn;
-    for (i=0; i<iter; i++) {                /* iterační smyčka */
-        if (delta<0) {                      /* úhel je záporný =&gt; rotace doleva */
-            xn=fx_add(x0, y0>>i);           /* místo násobení bitový posuv */
-            y0=fx_sub(y0, x0>>i);
-            delta=fx_add(delta, atans[i]);
+    for (i = 0; i < iter; i++) {        /* iterační smyčka */
+        if (delta < 0) {        /* úhel je záporný =&gt; rotace doleva */
+            xn = fx_add(x0, y0 >> i);   /* místo násobení bitový posuv */
+            y0 = fx_sub(y0, x0 >> i);
+            delta = fx_add(delta, atans[i]);
+        } else {                /* úhel je kladný =&gt; rotace doprava */
+            xn = fx_sub(x0, y0 >> i);
+            y0 = fx_add(y0, x0 >> i);
+            delta = fx_sub(delta, atans[i]);
         }
-        else {                              /* úhel je kladný =&gt; rotace doprava */
-            xn=fx_sub(x0, y0>>i);
-            y0=fx_add(y0, x0>>i);
-            delta=fx_sub(delta, atans[i]);
-        }
-        x0=xn;
+        x0 = xn;
     }
-    return fx_mul(x0, K_fx);                /* opravit "zesílení" výsledku */
+    return fx_mul(x0, K_fx);    /* opravit "zesílení" výsledku */
 }
 
 int main(void)
 {
     int i;
-    fx     cosfx;
-    double delta;                           /* úhel, ze kterého se funkce počítá */
-    double value;                           /* vypočtené hodnoty */
-    double abs_err;                         /* absolutní chyby */
-    double rel_err;                         /* relativní chyby */
-    char  *zvyr1, *zvyr2;                   /* ukazatele na konstantní řetězce pro */
-                                            /* generování HTML */
+    fx cosfx;
+    double delta;               /* úhel, ze kterého se funkce počítá */
+    double value;               /* vypočtené hodnoty */
+    double abs_err;             /* absolutní chyby */
+    double rel_err;             /* relativní chyby */
+    char *zvyr1, *zvyr2;        /* ukazatele na konstantní řetězce pro */
+    /* generování HTML */
 
     fx_create_tables();
 
     puts("\n<h2>Výpočet funkce cos() optimalizovanou metodou CORDIC</h2>\n");
     puts("<table>");
-    printf("<tr><th>Úhel</th><th>cos FP</th><th>cos FX</th><th>Abs.chyba</th><th>Rel.chyba</th></tr>\n");
-    for (i=0; i<=90; i++) {                 /* výpočetní smyčka */
-        delta=deg2rad(i);                   /* převod úhlu na radiány */
-        cosfx=fx_cos_cordic_optim(fp2fx(delta));  /* aplikace algoritmu CORDIC */
-        value=fx2fp(cosfx);                 /* výpočet funkce cos */
-        abs_err=fabs(value-cos(delta));     /* výpočet absolutních chyb */
-        rel_err=cos(delta)<=1e-10 ? 0:100.0*abs_err/cos(delta);
-        if (rel_err<=1.0) {
-            zvyr1="<strong>";
-            zvyr2="</strong>";
+    printf
+        ("<tr><th>Úhel</th><th>cos FP</th><th>cos FX</th><th>Abs.chyba</th><th>Rel.chyba</th></tr>\n");
+    for (i = 0; i <= 90; i++) { /* výpočetní smyčka */
+        delta = deg2rad(i);     /* převod úhlu na radiány */
+        cosfx = fx_cos_cordic_optim(fp2fx(delta));      /* aplikace algoritmu CORDIC */
+        value = fx2fp(cosfx);   /* výpočet funkce cos */
+        abs_err = fabs(value - cos(delta));     /* výpočet absolutních chyb */
+        rel_err = cos(delta) <= 1e-10 ? 0 : 100.0 * abs_err / cos(delta);
+        if (rel_err <= 1.0) {
+            zvyr1 = "<strong>";
+            zvyr2 = "</strong>";
+        } else {
+            zvyr1 = "";
+            zvyr2 = "";
         }
-        else {
-            zvyr1="";
-            zvyr2="";
-        }
-        printf("<tr><td>%02d</td><td>%5.3f</td><td>%5.3f%%</td><td>%5.3f</td><td>%s%5.3f%%%s</td></tr>\n",
-           i, value, cos(delta), abs_err, zvyr1, rel_err, zvyr2);
+        printf
+            ("<tr><td>%02d</td><td>%5.3f</td><td>%5.3f%%</td><td>%5.3f</td><td>%s%5.3f%%%s</td></tr>\n",
+             i, value, cos(delta), abs_err, zvyr1, rel_err, zvyr2);
     }
     puts("</table>");
     return 0;
 }
 
 // finito
-
