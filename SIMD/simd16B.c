304968d844c1821acbabc37a9f8987b067f162a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-10 08:13:51 +0200

    Add operation for vector of size 65536 bytes

---
+++ SIMD/simd16B.c
@@ -0,0 +1,17 @@
+typedef float v1024f __attribute__((vector_size(65536)));
+
+void addVectors(v1024f * x, v1024f * y, v1024f * z)
+{
+    *z = *x + *y;
+}
+
+int main(void)
+{
+    v1024f x = { 1.0 };
+    v1024f y = { 1.0 };
+    v1024f z;
+
+    addVectors(&x, &y, &z);
+
+    return 0;
+}
