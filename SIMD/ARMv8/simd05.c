a78ceb1d950d9f6eb0602fa17e6044e6f8d51b93
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-17 10:03:34 +0200

    Basic vectorised arithmetic operation (integers), accessing vector items

---
+++ SIMD/ARMv8/simd05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef unsigned short int v16us __attribute__((vector_size(16)));
+
+int main(void)
+{
+    v16us x = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    v16us y = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+    v16us z = x + y;
+
+    int i;
+
+    for (i = 0; i < 8; i++) {
+        printf("%d %d\n", i, z[i]);
+    }
+
+    return 0;
+}
