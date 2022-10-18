3f6f11554d5def3b5c6d3ba3ac7c94442a3c131b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-18 13:59:00 +0200

    MMX instrinsic: 4th example

---
+++ SIMD/intrinsic_mmx_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <mmintrin.h>
+
+int main(void)
+{
+    __v8qi x = { 0, 2, 4, 6, 8, 10, 12, 14 };
+    __v8qi y = { 120, 120, 120, 120, 120, 120, 120, 120 };
+    __v8qi z;
+    int i;
+
+    z = __builtin_ia32_paddsb(x, y);
+
+    for (i = 0; i < 8; i++) {
+        printf("%d %d %d %d\n", i, x[i], y[i], z[i]);
+    }
+}
