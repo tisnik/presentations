f1673ca6d8a55f6ae7afc6e5ea21a38047e66b78
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-01 07:37:47 +0100

    Working with args in C, example #1

---
+++ C/args1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    printf("Arguments count: %d\n", argc);
+
+    for (i = 0; i < argc; i++) {
+        printf("Argument #1 is '%s'\n", argv[i]);
+    }
+
+    return 0;
+}
