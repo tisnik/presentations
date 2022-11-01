2fc7a4fa767f0db743332ca2fb64f0c3299f66b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-01 07:37:52 +0100

    Working with args in C, example #2

---
+++ C/args2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int startswith(char *prefix, char *str)
+{
+    return strncmp(prefix, str, strlen(prefix)) == 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    printf("Arguments count: %d\n", argc);
+
+    for (i = 0; i < argc; i++) {
+        char *argument;
+
+        argument = argv[i];
+        if (startswith("-name=", argument)) {
+            printf("Found name in argument '%s'\n", argument);
+        }
+    }
+
+    return 0;
+}
