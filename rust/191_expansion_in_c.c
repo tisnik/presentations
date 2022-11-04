3d29e492786ca900f61ab8cf246e6a1ab9c63b39
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-04 08:42:17 +0100

    Indented code

--- rust/191_expansion_in_c.c
+++ rust/191_expansion_in_c.c
@@ -5,7 +5,6 @@
 int main(void)
 {
     printf("%d\n", sqr(10));
-    printf("%d\n", sqr(5+5));
+    printf("%d\n", sqr(5 + 5));
     return 0;
 }
-
--- rust/192_expansion_in_c_2.c
+++ rust/192_expansion_in_c_2.c
@@ -5,12 +5,11 @@
 int main(void)
 {
     printf("%d\n", sqr(10));
-    printf("%d\n", sqr(5+5));
-    
+    printf("%d\n", sqr(5 + 5));
+
     int x = 2;
     printf("%d\n", sqr(x));
     printf("%d\n", sqr(x++));
 
     return 0;
 }
-
