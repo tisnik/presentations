971410127472cd6306cd3f81ed97e4ad7f4cbdae
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-04 09:50:12 +0100

    Indented code

--- bfloat16/harmonic_double.c
+++ bfloat16/harmonic_double.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     long n = 1;
     double h1 = 0.0, h2 = 0.0;
 
     while (1) {
-        h2 = h1 + 1.0 / (double)n;
-        if (n%10000000 == 0) {
-            printf("%lf %lf %20.18lf %ld\n", h1, h2, h2-h1, n);
+        h2 = h1 + 1.0 / (double) n;
+        if (n % 10000000 == 0) {
+            printf("%lf %lf %20.18lf %ld\n", h1, h2, h2 - h1, n);
         }
-        if (h1 == h2) break;
+        if (h1 == h2)
+            break;
         h1 = h2;
         n++;
     }
