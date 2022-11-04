702d99246a7b13c77dce0651e3267bcf50f3b1ca
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-04 09:50:16 +0100

    Indented code

--- bfloat16/harmonic_float.c
+++ bfloat16/harmonic_float.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     long n = 1;
     float h1 = 0.0, h2 = 0.0;
 
     while (1) {
-        h2 = h1 + 1.0 / (float)n;
-        if (n%1000 == 0) {
-            printf("%f %f %10.8f %ld\n", h1, h2, h2-h1, n);
+        h2 = h1 + 1.0 / (float) n;
+        if (n % 1000 == 0) {
+            printf("%f %f %10.8f %ld\n", h1, h2, h2 - h1, n);
         }
-        if (h1 == h2) break;
+        if (h1 == h2)
+            break;
         h1 = h2;
         n++;
     }
