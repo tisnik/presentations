b4094c5fd0f0a1f9ab30ed9e05732d3a839a7029
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-07 08:35:07 +0100

    Fused add+multiply

---
+++ SIMD/intrinsic_fma3_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <immintrin.h>
+
+void print_results(const char *title, __v4sf * a, __v4sf * b, __v4sf * c,
+                   __v4sf * result)
+{
+    int i;
+
+    puts(title);
+    for (i = 0; i < sizeof(*a) / sizeof(float); i++) {
+        printf("%2d  %1.0f * %1.0f + %1.0f = %1.0f\n", i, (*a)[i], (*b)[i],
+               (*c)[i], (*result)[i]);
+    }
+
+    putchar('\n');
+}
+
+int main(void)
+{
+    __v4sf a = { 1, 2, 3, 4 };
+    __v4sf b = { 2, 2, 2, 2 };
+    __v4sf c = { 1, 1, 1, 1 };;
+    __v4sf result;
+
+    result = __builtin_ia32_vfmaddps(a, b, c);
+    print_results(" #  a   b   c   result", &a, &b, &c, &result);
+}
