dcba9b361cd879966d71952caeaebb26309b9267
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-14 09:24:31 +0200

    Msgpack for bytes array

---
+++ docs/coffee-talks/messagepack/msgpack_bytes.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ugorji/go/codec"
+)
+
+const filename = "/tmp/bytes.bin"
+
+func main() {
+	// vytvořit soubor s binárními daty
+	fout, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer fout.Close()
+
+	log.Print("Output file created")
+
+	// handler
+	var handler codec.MsgpackHandle
+
+	// objekt realizující zakódování dat
+	encoder := codec.NewEncoder(fout, &handler)
+
+	log.Print("Encoder created")
+
+	const N = 1000
+	var values [N]byte
+
+	for i := 0; i < N; i++ {
+		values[i] = byte(i)
+	}
+
+	// zakódování dat
+	err = encoder.Encode(values)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Print("Done")
+}
