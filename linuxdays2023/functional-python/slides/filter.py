8f07a47d25bfe5e4da3167be0e4f891e6174ace6
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-02 08:38:42 +0200

    Filter higher order function

---
+++ linuxdays2023/functional-python/slides/filter.py
@@ -0,0 +1,5 @@
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+filtered = filter(lambda word: len(word) > 4, words)
+print(list(filtered))
