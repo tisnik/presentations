c26465bdbb9c5c1d19139fbd7a9936c4eb43497a
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-02 08:38:42 +0200

    Higher order operations in Python

---
+++ linuxdays2023/functional-python/slides/higher_order_function_2.py
@@ -0,0 +1,33 @@
+def add(x, y):
+    return x + y
+
+
+def mul(x, y):
+    return x * y
+
+
+def less_than(x, y):
+    return x < y
+
+
+def get_operator(symbol):
+    operators = {
+            "+": add,
+            "*": mul,
+            "<": less_than,
+    }
+    return operators[symbol]
+
+
+def calc(operator, x, y):
+    return operator(x, y)
+
+
+z = calc(get_operator("+"), 10, 20)
+print(z)
+
+z = calc(get_operator("*"), 10, 20)
+print(z)
+
+z = calc(get_operator("<"), 10, 20)
+print(z)
