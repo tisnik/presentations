a9b59f48a6045c6493fc3851b4c92b1017662d76
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-03 08:55:39 +0200

    List generator notation

---
+++ linuxdays2023/functional-python/slides/gen_notation_list.py
@@ -0,0 +1,4 @@
+message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+words = message.split()
+
+lengths = [len(word) for word in words]
