aa89bfa5b426b98ddbb7b1c66b79ecfea45c4428
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-03 08:55:39 +0200

    Partial method + its call

---
+++ linuxdays2023/functional-python/slides/partial_method.py
@@ -0,0 +1,12 @@
+class Foo:
+    def __init__(self):
+        self._enabled = False
+
+    def set_enabled(self, state):
+        self._enabled = state
+
+    enable = partialmethod(set_enabled, True)
+    disable = partialmethod(set_enabled, False)
+
+    def __str__(self):
+        return "Foo that is " + ("enabled" if self._enabled else "disabled")
