4335a5c8e3f0adf7fca213fa73514ffc47c54eb5
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-04 08:54:16 +0200

    Persistent vector, example #2

---
+++ linuxdays2023/functional-python/slides/persistent_vector2.py
@@ -0,0 +1,12 @@
+from pyrsistent import v
+
+vector1 = v(1, "foo", (1, 2, 3), None)
+print(vector1)
+print(type(vector1))
+
+vector2 = vector1.append("Five!")
+print(vector1)
+print(type(vector1))
+
+print(vector2)
+print(type(vector2))
