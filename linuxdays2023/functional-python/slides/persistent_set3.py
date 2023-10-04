88ef61e4891ba42fdf537c52916253508b7a7156
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-04 08:53:59 +0200

    Persistent set, example #3

---
+++ linuxdays2023/functional-python/slides/persistent_set3.py
@@ -0,0 +1,12 @@
+from pyrsistent import s
+
+set1 = s(1, 2, 3)
+set2 = s(2, 3, 4)
+
+print(set1)
+print(set2)
+
+print("sjednoceni", set1 | set2)
+print("prunik", set1 & set2)
+print("rozdil", set1 - set2)
+print("rozdil", set2 - set1)
