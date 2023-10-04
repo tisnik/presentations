b5e831b3009b14471f204ae3cbc3e14be09ce056
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-04 08:53:33 +0200

    LRU cache, 1st example

---
+++ linuxdays2023/functional-python/slides/lru_cache1.py
@@ -0,0 +1,17 @@
+from time import time
+from functools import lru_cache
+
+@lru_cache
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+max_n = 300
+
+for _ in range(10):
+    start = time()
+    result = fib(max_n)
+    end = time()
+    print(result, end - start)
