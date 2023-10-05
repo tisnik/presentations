5f95290c27e18df7e005eb8a623dd2d8bbeb3d17
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-05 07:57:21 +0200

    Decorators, example #7

---
+++ linuxdays2023/functional-python/slides/decorators_7_ignore.py
@@ -0,0 +1,11 @@
+from funcy import ignore
+
+@ignore(errors=ZeroDivisionError, default=0)
+@ignore(errors=TypeError, default=-1)
+def divide(a, b):
+    return a/b
+
+
+print(divide(1, 2))
+print(divide(1, 0))
+print(divide(None, 1))
