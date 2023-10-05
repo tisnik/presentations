cc1ce1b159c00d1919600e816f6fcf3628f2a8c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-05 07:56:57 +0200

    Decorators, example #2

---
+++ linuxdays2023/functional-python/slides/decorators_2.py
@@ -0,0 +1,24 @@
+from funcy import decorator
+
+
+@decorator
+def wrapper1(function):
+    print("-" * 40)
+    function()
+    print("-" * 40)
+
+
+@decorator
+def wrapper2(function):
+    print("=" * 40)
+    function()
+    print("=" * 40)
+
+
+@wrapper1
+@wrapper2
+def hello():
+    print("Hello!")
+
+
+hello()
