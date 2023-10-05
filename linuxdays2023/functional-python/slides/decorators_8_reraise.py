ffd24d55b13881f7785c44e5affe37a1acb529af
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-05 07:57:25 +0200

    Decorators, example #8

---
+++ linuxdays2023/functional-python/slides/decorators_8_reraise.py
@@ -0,0 +1,14 @@
+from funcy import reraise
+
+class MathError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+@reraise(errors=Exception, into=MathError("neděl nulou!"))
+def divide(a, b):
+    return a/b
+
+
+print(divide(1, 2))
+print(divide(1, 0))
