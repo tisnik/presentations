17588203a5efbec424f29a92bc8ae5e04589177c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-05 07:57:36 +0200

    Decorators, example #10

---
+++ linuxdays2023/functional-python/slides/decorators_A_retry.py
@@ -0,0 +1,15 @@
+from funcy import retry
+
+
+@retry(4, timeout=lambda delay: 2 ** delay, errors=Exception)
+def call_function_to_raise_exception():
+    print("Trying to call problematic code...")
+    raise_exception()
+
+
+def raise_exception():
+    raise Exception("foo")
+
+
+while True:
+    call_function_to_raise_exception()
