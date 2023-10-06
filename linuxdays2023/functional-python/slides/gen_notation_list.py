b8c41efa29529c1fc3c2428e3e3b68016bb88269
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-06 09:04:07 +0200

    Comment with title

--- linuxdays2023/functional-python/slides/gen_notation_list.py
+++ linuxdays2023/functional-python/slides/gen_notation_list.py
@@ -1,3 +1,7 @@
+#
+# Generátorová notace: seznamy
+#
+
 message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
 words = message.split()
 
