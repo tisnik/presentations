93d9e288d1879f5df009b55641e9816ce0881959
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-23 08:20:53 +0200

    Object attributes pattern matching

---
+++ docs/coffee-talks/pattern-matching/object1.py
@@ -0,0 +1,43 @@
+class Complex():
+
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __str__(self):
+        return f"Complex number {self.real}+i{self.imag} represented as object"
+
+
+def test_number(value):
+    match value:
+        case (0, 0):
+            print("Zero")
+        case (real, 0) if real>0:
+            print(f"Positive real number {real}")
+        case (real, 0):
+            print(f"Negative real number {real}")
+        case (0, imag) if imag<0:
+            print(f"Negative imaginary number {imag}")
+        case (0, imag):
+            print(f"Negative imaginary number {imag}")
+        case (real, imag):
+            print(f"Complex number {real}+i{imag}")
+        case Complex():
+            print(value)
+        case _:
+            raise ValueError("Not a complex number")
+
+
+test_number((0,0))
+test_number((1,0))
+test_number((-1,0))
+test_number((0,1))
+test_number((0,-1))
+test_number((1,1))
+
+test_number(Complex(0,0))
+test_number(Complex(1,0))
+test_number(Complex(-1,0))
+test_number(Complex(0,1))
+test_number(Complex(0,-1))
+test_number(Complex(1,1))
