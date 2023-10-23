57e3603f101a76da0475ad09517fa311e2193751
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-23 08:20:53 +0200

    Multiword match, example #3

---
+++ docs/coffee-talks/pattern-matching/multiword_commands_3.py
@@ -0,0 +1,19 @@
+def perform_command():
+    response = input("> ")
+
+    match response.split():
+        case ["quit"]:
+            return "Quit"
+        case ["list", "employees"]:
+            return "List employees"
+        case ["list", "departments"]:
+            return "List departments"
+        case ["list", "rooms"]:
+            return "List rooms"
+        case ["info", subject]:
+            return f"Info about subject '{subject}'"
+        case _:
+            return "Wrong command"
+
+
+print(perform_command())
