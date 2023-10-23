07d7109b8ad2aaaefad541275e76f405c74f27d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-23 08:20:53 +0200

    Pattern matching with value capturing and conditions

---
+++ docs/coffee-talks/pattern-matching/complex2.py
@@ -0,0 +1,24 @@
+def test_number(value):
+    match value:
+        case (0, 0):
+            print("Zero")
+        case (real, 0) if real>0:
+            print(f"Positive real number {real}")
+        case (real, 0):
+            print(f"Negative real number {real}")
+        case (0, imag) if imag<0:
+            print(f"Negative imaginary number {imag}")
+        case (0, imag):
+            print(f"Negative imaginary number {imag}")
+        case (real, imag):
+            print(f"Complex number {real}+i{imag}")
+        case _:
+            raise ValueError("Not a complex number")
+
+
+test_number((0,0))
+test_number((1,0))
+test_number((-1,0))
+test_number((0,1))
+test_number((0,-1))
+test_number((1,1))
