388e842007375b975e5dfd71f744084cfba70444
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-23 08:20:53 +0200

    Factorial computation with condition in one branch and or operator

---
+++ docs/coffee-talks/pattern-matching/factorial4.py
@@ -0,0 +1,25 @@
+def factorial(n):
+    match n:
+        case 0 | 1:
+            return 1
+        case x if isinstance(x, int) and x>1:
+            return x * factorial(x-1)
+        case _:
+            raise TypeError("expecting integer >= 0")
+
+
+for i in range(-1, 10):
+    try:
+        print(i, factorial(i))
+    except Exception as e:
+        print(e)
+
+try:
+    print(factorial(3.14))
+except Exception as e:
+    print(e)
+
+try:
+    print(factorial("hello"))
+except Exception as e:
+    print(e)
