11aa618fc68ebc4fd5e56489c46d1b632844aa80
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-23 08:20:53 +0200

    Pattern matching with value capturing

---
+++ docs/coffee-talks/pattern-matching/abort_retry_fail_6.py
@@ -0,0 +1,18 @@
+print("Not ready reading drive A")
+
+
+def abort_retry_fail():
+    response = input("Abort, Retry, Fail? ")
+
+    match response:
+        case "a" | "A":
+            return "Abort"
+        case "r" | "R":
+            return "Retry"
+        case "f" | "F":
+            return "Fail"
+        case _ as x:
+            return f"Wrong response {x}"
+
+
+print(abort_retry_fail())
