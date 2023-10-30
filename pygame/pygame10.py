13d4e43c8a4212fd2f3596320cbd654c8f0e4bb3
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-30 07:47:45 +0100

    Fixed imports

--- pygame/pygame10.py
+++ pygame/pygame10.py
@@ -11,7 +11,7 @@ import sys
 import os
 
 # Nutno importovat kvůli konstantám QUIT atd.
-from pygame.locals import *
+from pygame.locals import QUIT, K_ESCAPE, KEYDOWN
 
 # Velikost okna aplikace
 WIDTH = 320
