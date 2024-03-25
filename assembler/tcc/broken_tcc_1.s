17326bf7af6c87d0d8534cedfcf8c4fd055bb018
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-25 07:27:49 +0100

    Incorrect program (unknown instruction), variant for GNU Assembler

---
+++ assembler/tcc/broken_tcc_1.s
@@ -0,0 +1,22 @@
+# Linux kernel system call table
+sys_write = 1
+sys_exit  = 60
+
+
+.section .text
+        .global main               # tento symbol ma byt dostupny i linkeru
+
+main:
+	mov  $sys_write, %rax      # cislo sycallu pro funkci "sys_write" na architekture x86-64
+	mov  $1, %rdi              # handle, 1 = STDOUT
+	mov  $message_, %rsi       # adresa zpravy (spatny identifikator)
+	mov  $length, %rdx         # delka zpravy
+	syscall                    # zavolat funkci Linuxoveho kernelu
+
+        mov  $sys_exit, %rax       # cislo sycallu pro funkci "sys_exit" na architekture x86-64
+        mov  $0, %rdi              # exit code = 0
+        syscall                    # zavolat funkci Linuxoveho kernelu
+
+message:
+	.ascii "Hello, world!\n"   # zprava, ktera se ma vypsat
+	length = . - message       # vypocet delky zpravy primo v prubehu preklad
