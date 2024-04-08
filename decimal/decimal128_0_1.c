af857b80694ba8ad590a788eac5dcb2af59fd14c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-08 14:42:17 +0200

    Correct constants

--- decimal/decimal128_0_1.c
+++ decimal/decimal128_0_1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    _Decimal128 x = 0.1dd;
-    _Decimal128 y = 0.2dd;
-    _Decimal128 z = 0.3dd;
+    _Decimal128 x = 0.1dl;
+    _Decimal128 y = 0.2dl;
+    _Decimal128 z = 0.3dl;
 
     puts(x + y == z ? "rovnost" : "nerovnost");
     return 0;
