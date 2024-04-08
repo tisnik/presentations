6a05f35702d64db04e0156a6e03745090b823922
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-08 08:28:41 +0200

    Print float numbers in hexadecimal format

---
+++ decimal/hexa_float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float values[] = {0.0, 0.1, 0.2, 1.0, 2.0, 10.0, 100.0, 1000.0, 1000.1, 1.0/0.0, -0.0, -0.1, -0.2, -1.0, -1000.0, -1.0/0.0, 0.0/0.0};
+    int i;
+
+    for (i=0; i<sizeof(values)/sizeof(float); i++) {
+        printf("%9.4f   %a\n", values[i], values[i]);
+    }
+
+    return 0;
+}
