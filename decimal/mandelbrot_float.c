13b0fee178ccd0be7d174f98678abc67e0f85030
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-08 08:29:22 +0200

    Benchmark: Mandelbrot set computed using type float

---
+++ decimal/mandelbrot_float.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "palette_mandmap.h"
+
+typedef float number_type;
+
+#include "mandelbrot.h"
+
+int main(int argc, char **argv)
+{
+    if (argc < 4) {
+        puts("usage: ./mandelbrot width height maxiter");
+        return 1;
+    }
+    int width = atoi(argv[1]);
+    int height = atoi(argv[2]);
+    int maxiter = atoi(argv[3]);
+    calc_mandelbrot(width, height, maxiter, palette);
+    return 0;
+}
+
