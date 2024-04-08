5538b06038df7f2eb3a7ca709ecdf3f6f0e7a3ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-08 14:46:38 +0200

    Correct constants

--- decimal/decimal128_loop.c
+++ decimal/decimal128_loop.c
@@ -4,7 +4,7 @@
 int main(void) {
     _Decimal128 x;
 
-    for (x=0.0df; x!=1.0df; x+=0.1df) {
+    for (x=0.0dl; x!=1.0dl; x+=0.1dl) {
         printf("%f\n", (float)x);
     }
     return 0;
--- decimal/decimal64_loop.c
+++ decimal/decimal64_loop.c
@@ -4,7 +4,7 @@
 int main(void) {
     _Decimal64 x;
 
-    for (x=0.0df; x!=1.0df; x+=0.1df) {
+    for (x=0.0dd; x!=1.0dd; x+=0.1dd) {
         printf("%f\n", (float)x);
     }
     return 0;
