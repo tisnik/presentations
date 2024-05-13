d49014f8c21776bb011c47de54971fd736df7376
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-13 09:06:21 +0200

    x86-64 version written in FASM

---
+++ assembler/fasm/hello_x86_64_2.asm
@@ -0,0 +1,36 @@
+; Jednoducha aplikace typu "Hello world!" naprogramovana
+; v assembleru FASM pro Linux na x86-64.
+;
+; Autor: Pavel Tisnovsky
+
+
+
+format ELF64 executable 3
+
+sys_exit = 60
+sys_write = 1
+stdout = 1
+
+
+
+segment readable executable
+
+entry main
+
+main:
+        lea  rsi, [msg]            ; adresa retezce, ktery se ma tisknout
+        mov  rdx, length           ; delka retezce vcetne konce radku
+        mov  rdi, stdout           ; handle specialniho souboru stdout
+        mov  rax, sys_write        ; cislo syscallu pro funkci sys_write 
+        syscall                    ; volani linuxoveho kernelu
+
+        mov  rax, sys_exit         ; cislo syscallu pro funkci sys_exit
+        xor  rdi, rdi              ; navratovy kod
+        syscall                    ; volani linuxoveho kernelu
+
+
+
+segment readable writable
+
+msg  db 'Hello world!', 10
+length = $ - msg
