e872b30fcad77e1c94d03bdcbb3396673134bc01
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-13 09:04:34 +0200

    Added comments

--- assembler/fasm/hello_exe.asm
+++ assembler/fasm/hello_exe.asm
@@ -1,12 +1,24 @@
+; Jednoducha aplikace typu "Hello world!" naprogramovana
+; v assembleru FASM pro DOS, vysledkem je soubor .EXE.
+;
+;
+; pouziti DOS funkci Print String a Terminate Process
+; https://www.stanislavs.org/helppc/int_21-9.html
+; https://www.stanislavs.org/helppc/int_21-4c.html
+
+
+
 format MZ
 
         push    cs
-        pop     ds
-        mov     ah,9
-        mov     dx,message
-        int     21h
+        pop     ds           ; DS = CS
 
-        mov     ax,4C00h
-        int     21h
+        mov     ah, 9        ; kod funkce Print String
+        mov     dx, message  ; adresa zpravy ukoncene dolarem
+        int     21h          ; volani funkce DOSu
 
+        mov     ax,4C00h     ; kod funkce Terminate Process With Return Code
+        int     21h          ; volani funkce DOSu
+
+; zprava v kodovem segmentu
 message db 'Hello world!$'
