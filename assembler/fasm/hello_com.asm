cf9ece3ae441da32a988286a2b09c560ca9a8d58
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-13 09:04:24 +0200

    Added comments

--- assembler/fasm/hello_com.asm
+++ assembler/fasm/hello_com.asm
@@ -1,11 +1,23 @@
+; Jednoducha aplikace typu "Hello world!" naprogramovana
+; v assembleru FASM pro DOS, vysledkem je soubor .COM.
+;
+;
+; pouziti DOS funkce Print String
+; https://www.stanislavs.org/helppc/int_21-9.html
+
+
+
 format binary as "COM"
+
+; nutne pro COM - pocatecni adresa kodu
 org 0x100
 
-        mov     ah,9
-        mov     dx,message
-        int     21h
+        mov     ah, 9        ; kod funkce Print String
+        mov     dx, message  ; adresa zpravy ukoncene dolarem
+        int     21h          ; volani funkce DOSu
 
-        xor     ax, ax
-        retn
+        xor     ax, ax       ; navratovy kod
+        retn                 ; navrat do DOSu
 
+; zprava v kodovem segmentu
 message db 'Hello world!$'
