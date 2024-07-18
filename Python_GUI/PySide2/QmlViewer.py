cbe45b50a2150d089604130815123c3b21f9cb4a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-18 12:55:03 +0200

    Fixed import statement order, removed unused imports

--- Python_GUI/PySide2/QmlViewer.py
+++ Python_GUI/PySide2/QmlViewer.py
@@ -4,11 +4,8 @@
 import sys
 
 # import "jádra" frameworku Qt i modulu pro GUI
-from PySide2 import QtCore
-from PySide2 import QtGui
-
 # modul pro práci s QML
-from PySide2 import QtQuick
+from PySide2 import QtCore, QtGui, QtQuick
 
 
 # nový widget bude odvozen od QDeclarativeView
