7b9ae6391cc92786574803b7ae654252d749b89f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-18 12:55:03 +0200

    Fixed import statement order, removed unused imports

--- Python_GUI/PySide2/01_load_qml.py
+++ Python_GUI/PySide2/01_load_qml.py
@@ -3,11 +3,8 @@
 import sys
 
 # import "jádra" frameworku Qt i modulu pro GUI
-from PySide2 import QtCore
-from PySide2 import QtGui
-
 # modul pro práci s QML
-from PySide2 import QtQuick
+from PySide2 import QtCore, QtGui, QtQuick
 
 QML_FILE = "01.qml"
 
