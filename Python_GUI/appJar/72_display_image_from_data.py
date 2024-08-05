20bf66b97117eb13434c3c496984267a9924decb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-05 09:32:38 +0200

    Fixed import statement order, removed unused imports

--- Python_GUI/appJar/72_display_image_from_data.py
+++ Python_GUI/appJar/72_display_image_from_data.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from appJar import gui
 import tkinter
 
+from appJar import gui
 
 image_data = """
 R0lGODlhFgAWAPZJAAAAAHgCAYcHBYgZGZUJA5cRCpsYEpkkFpkrJKQLBKgYBaQQELQCArEYGKQo
