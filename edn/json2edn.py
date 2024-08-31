91b8eac55d0548284cb276cf78c61c473767ad76
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-31 08:51:19 +0200

    Fixed import statement order, removed unused imports

--- edn/json2edn.py
+++ edn/json2edn.py
@@ -2,8 +2,9 @@
 
 # Converts structured data from JSON format into EDN format.
 
-import sys
 import json
+import sys
+
 import edn_format
 
 # Check if command line argument is specified (it is mandatory).
