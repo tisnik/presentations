dc41e42293fa4b3d36bf29ef691611a1356e6522
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-06 14:31:38 +0200

    Fixed import statement order, removed unused imports

--- matplotlib/example29.py
+++ matplotlib/example29.py
@@ -5,9 +5,10 @@
 # Dvacátý devátý demonstrační příklad:
 # - Lorenzův atraktor
 
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+
+import matplotlib.pyplot as plt
 
 
 # funkce pro výpočet dalšího bodu Lorenzova atraktoru
