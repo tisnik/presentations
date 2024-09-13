f0979848cbc95946cc0b41b9d3e6d6fd53657c68
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-13 10:22:02 +0200

    Fixed import statement order, removed unused imports

--- pygame/pygame15.py
+++ pygame/pygame15.py
@@ -5,10 +5,14 @@
 # Příklad číslo 15: použití TrueType fontů
 
 
-import pygame, sys, os, math
+import math
+import os
+import sys
+
+import pygame
 
 # Nutno importovat kvůli konstantám QUIT atd.
-from pygame.locals import QUIT, K_ESCAPE, KEYDOWN
+from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 
 # Velikost okna aplikace
 WIDTH = 320
