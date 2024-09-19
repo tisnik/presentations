5c3dd8d2d0d2fbc1906abf64bc5890132b22b334
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-19 15:45:34 +0200

    Fixed import statement order, removed unused imports

--- pygments/pygments11_highlight_source.py
+++ pygments/pygments11_highlight_source.py
@@ -3,8 +3,8 @@
 from sys import argv, exit
 
 from pygments import highlight
-from pygments.lexers import PythonLexer
 from pygments.formatters import TerminalFormatter
+from pygments.lexers import PythonLexer
 
 if len(argv) <= 1:
     print("Usage: pygments11_highlight_source.py FILENAME")
