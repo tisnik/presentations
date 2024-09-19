55b4e9a7acd03592937de3b006d19ba83d9a0ce4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-19 15:45:34 +0200

    Fixed import statement order, removed unused imports

--- pygments/pygments09_transform_to_tokens.py
+++ pygments/pygments09_transform_to_tokens.py
@@ -1,6 +1,6 @@
 from pygments import highlight
-from pygments.lexers import PythonLexer
 from pygments.formatters import RawTokenFormatter
+from pygments.lexers import PythonLexer
 
 code = """
 for i in range(1, 11):
