8070e34c7804fa9c42898a903605d88ad1230efb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-19 15:45:34 +0200

    Fixed import statement order, removed unused imports

--- pygments/pygments12_source2svg.py
+++ pygments/pygments12_source2svg.py
@@ -3,8 +3,8 @@
 from sys import argv, exit
 
 from pygments import highlight
-from pygments.lexers import PythonLexer
 from pygments.formatters import SvgFormatter
+from pygments.lexers import PythonLexer
 
 if len(argv) <= 1:
     print("Usage: pygments12_source2svg.py FILENAME > DRAWING.svg")
